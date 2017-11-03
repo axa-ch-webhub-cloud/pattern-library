42c0a70b365efb44eef12e212cac4efc0b0355f7
Luca Mele <luca.mele@axa-winterthur.ch>
2017-11-03 13:22:47 +0100

    webcomponents polyfill also for dev

--- stack/tasks/copy-vendors.js
+++ stack/tasks/copy-vendors.js
@@ -1,4 +1,10 @@
+const constants = require('../constants');
 const cpx = require("cpx");
+const ENV = process.argv[2]; // second element is the first argument.
 const CWD = process.cwd();
 
-cpx.copy(`${CWD}/node_modules/webcomponents.js/webcomponents-lite.min.js`, `${CWD}/dist/app`)
+if (ENV === constants.ENV.PROD) {
+  cpx.copy(`${CWD}/node_modules/webcomponents.js/webcomponents-lite.min.js`, `${CWD}/dist/app`);
+} else {
+  cpx.copy(`${CWD}/node_modules/webcomponents.js/webcomponents-lite.min.js`, `${CWD}/.tmp/app`);
+}
