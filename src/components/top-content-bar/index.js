28df9632f0bbb6e069d1a61c6aacd94c46eeb37e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-06 14:30:45 +0100

    fixed broken node insertion

--- src/components/top-content-bar/index.js
+++ src/components/top-content-bar/index.js
@@ -1,8 +1,17 @@
 class TopContentBar extends HTMLElement {
     connectedCallback() {
-        const clone = document.importNode(, true);
+        //const clone = document.importNode(, true);
 
-        this.appendChild(clone);
+        //this.appendChild(clone);
+
+        const type = this.getAttribute('type');
+
+        this.innerHTML = `
+            <section class="c-top-content-bar c-top-content-bar--${type}">
+                <p class="c-top-content-bar__text">Ein Test text</p>
+                <button class="c-top-content-bar__button"></button>
+            </section>
+        `;
     }
 }
 
