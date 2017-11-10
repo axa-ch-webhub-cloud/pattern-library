28900f40f34f5887b08851e3be5aa9544a858c6e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-10 17:02:54 +0100

    added missing todo

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,6 +3,7 @@ import classnames from 'classnames'
 import styles from './index.scss';
 import { BaseComponentGlobal } from '../_abstract/component-types';
 
+// @Todo: replace by proper icon system as soon as it is ready
 const arrowIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="a-button__arrow" viewBox="0 0 32 32">
   <path d="M30.533 16.8c.333-.533.267-1.267-.2-1.667l-11.4-11.4c-1.2-1.2-3.133.667-1.867 1.867l9.133 9.133H2.666c-1.733 0-1.733 2.667 0 2.667h23.533l-9.133 9.133c-1.2 1.2.667 3.133 1.867 1.867l11.4-11.4.2-.2z"></path>
 </svg>`;
