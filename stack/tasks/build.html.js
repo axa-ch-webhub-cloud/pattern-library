e5a6e37b00532a15fd273d810e46add73682e425
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-14 15:35:42 +0100

    added assets to html build

--- stack/tasks/build.html.js
+++ stack/tasks/build.html.js
@@ -6,6 +6,10 @@ const CWD = process.cwd();
 
 if (ENV === constants.ENV.PROD) {
   cpx.copy(`${CWD}/src/components/**/*.html`, `${CWD}/dist/components`);
+  // @todo: check if this is the proper place, or is the file name too specific?
+  cpx.copy(`${CWD}/src/assets/**/*`, `${CWD}/dist/assets`);
 } else {
   cpx.copy(`${CWD}/src/components/**/*.html`, `${CWD}/.tmp/components`);
+  // @todo: check if this is the proper place, or is the file name too specific?
+  cpx.copy(`${CWD}/src/assets/**/*`, `${CWD}/.tmp/assets`);
 }
