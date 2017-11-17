42955b8c83fa26397407d3aaf9da972fe034fba0
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-17 11:07:04 +0100

    added outer click event to sub menu

--- src/components/m-main-navigation/js/sub-navigation.js
+++ src/components/m-main-navigation/js/sub-navigation.js
@@ -1,10 +1,12 @@
 import on from '../../../js/on';
+import outer from '../../../js/outer';
 import classList from '../../../js/class-list';
 
 class SubNavigation {
   static DEFAULTS = {
     list: '.m-main-navigation__list',
     listItem: 'm-main-navigation__list-item',
+    listLink: 'm-main-navigation__list-link',
     subNavigation: 'm-sub-navigation',
     stateClass: 'is-open'
   };
@@ -15,7 +17,9 @@ class SubNavigation {
       ...SubNavigation.DEFAULTS,
       ...options
     };
+
     this.handleClick = this.handleClick.bind(this);
+    this.handleOuterClick = this.handleOuterClick.bind(this);
 
     this.init();
   }
@@ -29,31 +33,55 @@ class SubNavigation {
   on() {
     this.off();
 
-    this.unlisten = on(this.list, 'click', this.options.listItem, this.handleClick);
+    this.unClick = on(this.list, 'click', this.options.listLink, this.handleClick);
   }
 
   off() {
-    if ('unlisten' in this) {
-      this.unlisten();
-      delete this.unlisten;
+    if ('unClick' in this) {
+      this.unClick();
+      delete this.unClick;
+    }
+
+    this.offInteractive();
+  }
+
+  onInteractive() {
+    this.offInteractive();
+
+    this.unOuterClick = outer(this.list, 'click', this.handleOuterClick);
+  }
+
+  offInteractive() {
+    if ('unOuterClick' in this) {
+      this.unOuterClick();
+      delete this.unOuterClick;
     }
   }
 
   handleClick(e, delegateTarget) {
     e.preventDefault();
 
-    const isSame = delegateTarget === this.lastOpen;
+    const listItem = delegateTarget.parentNode;
+    const isSame = listItem === this.lastOpen;
 
     this.close();
 
     if (!isSame) {
-      this.open(delegateTarget);
+      this.open(listItem);
     }
+  }
+
+  handleOuterClick(e) {
+    e.preventDefault();
+
+    this.close();
   }
 
   open(dom) {
     classList.add(dom, this.options.stateClass);
     this.lastOpen = dom;
+
+    this.onInteractive();
   }
 
   close() {
@@ -61,6 +89,8 @@ class SubNavigation {
       classList.remove(this.lastOpen, this.options.stateClass);
       delete this.lastOpen;
     }
+
+    this.offInteractive();
   }
 
   destroy() {
