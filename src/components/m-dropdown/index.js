0096322f781b0d5ab308337c2d3ff1954fb14142
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-22 18:35:30 +0100

    start prototyping dropdown component

---
+++ src/components/m-dropdown/index.js
@@ -0,0 +1,53 @@
+import classnames from 'classnames';
+import styles from './index.scss';
+import { BaseComponentGlobal } from '../_abstract/component-types';
+import DropDown from './js/drop-down';
+
+const arrowIcon = `<svg class="m-dropdown__icon">
+  <use xlink:href="#src--assets--icons--angle-bracket-right" />
+</svg>`;
+
+class Dropdown extends BaseComponentGlobal {
+  constructor() {
+    super(styles);
+
+    this.initialInnerHTML = this.innerHTML;
+
+    console.log('construct drop down');
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.dropDown = new DropDown(this, {
+      containerClass: null,
+    });
+
+    console.log('connected drop down', this.dropDown);
+  }
+
+  disconnectedCallback() {
+    this.dropDown.destroy();
+    delete this.dropDown;
+  }
+
+  _render() {
+    const { initialInnerHTML } = this;
+    const title = this.getAttribute('title');
+    const inFlow = this.hasAttribute('in-flow');
+    const tag = this.getAttribute('tag') || 'ul';
+
+    this.className = classnames('m-dropdown js-dropdown', {
+      'm-dropdown--in-flow': inFlow,
+    });
+
+    this.innerHTML = `<button role="button" class="m-dropdown__toggle js-dropdown__toggle">
+        ${title}${arrowIcon}
+      </button>
+      <${tag} class="m-dropdown__content">${initialInnerHTML}</${tag}>`;
+  }
+}
+
+window.customElements.define('axa-dropdown', Dropdown);
+
+BaseComponentGlobal.appendGlobalStyles(styles);
---
+++ src/components/m-dropdown/js/drop-down.js
@@ -0,0 +1,71 @@
+import getUiObserver from '../../../js/ui-observer';
+import { add, remove } from '../../../js/class-list';
+
+class DropDown {
+  static DEFAULTS = {
+    containerClass: '.js-dropdown',
+    toggleClass: 'js-dropdown__toggle',
+    isOpenClass: 'is-open',
+  }
+
+  constructor(rootNode, options) {
+    this.options = {
+      ...DropDown.DEFAULTS,
+      ...options,
+    };
+    this.rootNode = rootNode;
+
+    this.init();
+  }
+
+  init() {
+    this.observer = getUiObserver(this.rootNode, this.options);
+
+    this.on();
+  }
+
+  on() {
+    this.off();
+
+    this.unObserve = this.observer.register(this);
+  }
+
+  off() {
+    if (this.unObserve) {
+      this.unObserve();
+    }
+  }
+
+  enter(node) {
+    const { parentNode } = node;
+    const { lastElementChild } = parentNode;
+    const { scrollHeight } = lastElementChild;
+
+    lastElementChild.style.height = `${scrollHeight}px`;
+
+    add(parentNode, 'is-open');
+  }
+
+  leave(node) {
+    const { parentNode } = node;
+    const { lastElementChild } = parentNode;
+
+    lastElementChild.style.height = 0;
+
+    remove(parentNode, 'is-open');
+  }
+
+  destroy() {
+    this.off();
+
+    if (this.observer) {
+      this.observer.destroy();
+      delete this.observer;
+    }
+
+    delete this.rootNode;
+    delete this.options;
+  }
+}
+
+export default DropDown;
