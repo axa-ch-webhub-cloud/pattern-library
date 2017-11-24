2ac20093fc34ff1d68807fccdda9fa817320dfbd
difad <difada1@hotmail.com>
2017-11-24 11:14:58 +0100

    work on checkbox

--- src/components/a-checkbox/index.js
+++ src/components/a-checkbox/index.js
@@ -1,75 +1,126 @@
-import classnames from 'classnames';
-import { BaseComponentGlobal } from '../_abstract/component-types';
-import observeAttrChange from '../../js/observe-attr-change';
-import setTabbable from '../../js/set-tabbable';
+import { BaseComponent } from '../_abstract/component-types';
+import on from '../../js/on';
 import styles from './index.scss';
 
-const inputAttrs = ['id', 'name', 'value', 'disabled', 'checked'];
-const checkBoxIndicator = '<span class="a-checkbox__indicator"/>';
-const checkBoxBaseClass = 'a-checkbox';
+const KEYCODE = {
+  SPACE: 32,
+};
 
-class Checkbox extends BaseComponentGlobal {
+
+class Checkbox extends BaseComponent {
+  static get observedAttributes() {
+    return ['checked', 'disabled'];
+  }
+
   constructor() {
-    super(styles);
-    this.initialInnerHTML = this.innerHTML;
-    this.innerHTML = '';
+    super();
+
+    const template = document.createElement('template');
+    template.innerHTML = `<style>${styles}</style>`;
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   _render() {
-    const { initialInnerHTML } = this;
-
-    const disabled = this.getAttribute('disabled');
-    const checked = this.getAttribute('checked');
-    const id = this.getAttribute('id');
-
-
-    this.inputEl = this._addRealInput();
-
-    if (initialInnerHTML.trim() !== '') {
-      const label = document.createElement('label');
-      label.setAttribute('for', id);
-      label.setAttribute('class', `${checkBoxBaseClass}__label`);
-      label.innerHTML = initialInnerHTML;
-      this.appendChild(label);
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'checkbox');
     }
 
-    observeAttrChange(this, (attr, value) => {
-      if (value === null) {
-        this.inputEl.removeAttribute(attr);
-      } else {
-        this.inputEl.setAttribute(attr, value);
-      }
-    });
+    if (!this.hasAttribute('tabindex')) {
+      this.setAttribute('tabindex', '0');
+    }
 
-    this.className = classnames(checkBoxBaseClass, {
-      [`${checkBoxBaseClass}--disabled`]: disabled,
-      [`${checkBoxBaseClass}--checked`]: checked,
-    });
+    this._upgradeProperty('checked');
+    this._upgradeProperty('disabled');
 
-    if (!this.classList.contains(`${checkBoxBaseClass}--disabled`)) {
-      setTabbable(this, () => {
-        this.inputEl.click();
-      });
+    on(this, 'keyup', null, this._onKeyUp);
+    on(this, 'click', null, this._onClick);
+  }
+
+  _upgradeProperty(prop) {
+    if (Object.prototype.hasOwnProperty.call(this, prop)) {
+      const value = this[prop];
+      delete this[prop];
+      this[prop] = value;
     }
   }
 
-  _addRealInput() {
-    const inputEl = document.createElement('input');
-    inputEl.setAttribute('type', 'checkbox');
-    inputEl.setAttribute('tabindex', '-1');
-
-    Array.from(this.attributes).forEach((attr) => {
-      if (inputAttrs.includes(attr.name) && attr.value !== null) {
-        inputEl.setAttribute(attr.name, attr.value);
-        if (attr.name === 'id') {
-          this.setAttribute('id', `${checkBoxBaseClass}-${attr.value}`);
+  attributeChangedCallback(name, oldValue, newValue) {
+    const hasValue = newValue !== null;
+    switch (name) {
+      case 'checked':
+        this.setAttribute('aria-checked', hasValue);
+        break;
+      case 'disabled':
+        this.setAttribute('aria-disabled', hasValue);
+        if (hasValue) {
+          this.removeAttribute('tabindex');
+          this.blur();
+        } else {
+          this.setAttribute('tabindex', '0');
         }
-      }
-    });
-    this.appendChild(inputEl);
-    this.insertAdjacentHTML('beforeend', checkBoxIndicator);
+        break;
+      default:
+        break;
+    }
+  }
 
-    return inputEl;
+  _onKeyUp(event) {
+    if (event.altKey) {
+      return;
+    }
+
+    switch (event.keyCode) {
+      case KEYCODE.SPACE:
+        event.preventDefault();
+        this._toggleChecked();
+        break;
+      default:
+        break;
+    }
+  }
+
+  _onClick(event) {
+    this._toggleChecked();
+  }
+
+  _toggleChecked() {
+    if (this.disabled)
+      return;
+    this.checked = !this.checked;
+    this.dispatchEvent(new CustomEvent('change', {
+      detail: {
+        checked: this.checked,
+      },
+      bubbles: true,
+    }));
+  }
+
+  set checked(value) {
+    const isChecked = Boolean(value);
+    if (isChecked) {
+      this.setAttribute('checked', '');
+    } else {
+      this.removeAttribute('checked');
+    }
+  }
+
+  get checked() {
+    return this.hasAttribute('checked');
+  }
+
+  set disabled(value) {
+    const isDisabled = Boolean(value);
+    if (isDisabled) {
+      this.setAttribute('disabled', '');
+    } else {
+      this.removeAttribute('disabled');
+    }
+  }
+
+  get disabled() {
+    return this.hasAttribute('disabled');
   }
 }
 
