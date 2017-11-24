36fb260e68490ca74685f39bfc6e882b5906cd98
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-24 16:43:36 +0100

    added get attributes

---
+++ src/js/get-attributes.js
@@ -0,0 +1,27 @@
+function getAttributes(node) {
+  if (!node.hasAttributes) {
+    return null;
+  }
+
+  const out = {};
+  const { attributes } = node;
+  const { length } = attributes;
+
+  for (let i = 0; i < length; ++i) {
+    const attribute = attributes[i];
+    const { name } = attribute;
+    let { value } = attribute;
+
+    try {
+      value = JSON.parse(value);
+    } catch (error) {
+      console.error(`Attributes ${node.nodeName} has an error:\n${error}\n`); // eslint-disable-line
+    }
+
+    out[name] = value;
+  }
+
+  return out;
+}
+
+export default getAttributes;
