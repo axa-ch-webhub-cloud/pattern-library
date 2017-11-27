e07ed20fc63665a5a7baf5e6bf8ecaff5f38100e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-27 15:53:44 +0100

    fixed missing arg names

--- src/components/m-top-content-bar/_template.js
+++ src/components/m-top-content-bar/_template.js
@@ -1,5 +1,5 @@
 import bel from 'bel';
 
-export default function ({}, children) {
+export default function (props, children) {
   return bel`<div class="m-top-content-bar__box">${children}</div>`;
 }
