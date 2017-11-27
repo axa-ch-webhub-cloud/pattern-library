f997a0333207b2cb8805497e9220956ed744678f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-27 10:21:38 +0100

    fixed eslint

--- src/components/m-meta-navigation-mobile/_template.js
+++ src/components/m-meta-navigation-mobile/_template.js
@@ -4,17 +4,15 @@ export default ({ others, languages }) =>
   bel`<div class="m-meta-navigation-mobile__box">
     <nav class="m-meta-navigation-mobile__nav-other">
       <ul class="m-meta-navigation-mobile__list">
-        ${others && others.map(({ name, url, isActive = false }) =>
-          bel`<li class="m-meta-navigation-mobile__list-item">
+        ${others && others.map(({ name, url, isActive = false }) => bel`
+          <li class="m-meta-navigation-mobile__list-item">
             <a href="${url}" class="m-meta-navigation-mobile__list-link ${isActive && 'is-active'}">${name}</a>
-          </li>`
-        )}
+          </li>`)}
       </ul>
     </nav>
     
     <nav class="m-main-navigation-mobile__nav-lang">
-      ${languages && languages.map(({ code, url, isActive }) =>
-        bel`<a href="${url}" class="m-meta-navigation-mobile__lang-link ${isActive && 'is-active'}">${code}</a>`
-      )}
+      ${languages && languages.map(({ code, url, isActive }) => bel`
+        <a href="${url}" class="m-meta-navigation-mobile__lang-link ${isActive && 'is-active'}">${code}</a>`)}
     </nav>
   </div>`;
