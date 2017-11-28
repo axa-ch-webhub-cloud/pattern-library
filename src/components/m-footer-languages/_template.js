0dbe6c929ce3b88f6182c85b71802aeaaf1321c8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-28 17:32:16 +0100

    added markup structure

--- src/components/m-footer-languages/_template.js
+++ src/components/m-footer-languages/_template.js
@@ -1,3 +1,16 @@
-export default function (props, children) {
-  return children;
+import bel from 'bel';
+
+export default function ({ title, items }, children) {
+  return bel`<aside class="m-footer-languages__box">
+    <strong class="m-footer-languages__title">${title}</strong>
+    
+    <ul class="m-footer-languages__list">
+      ${items.map(({ name, short, url }) => bel`
+        <li class="m-footer-languages__list-item">
+          <a clas="m-footer-languages__link" href="${url}" lang="${short}">${name}</a>
+        </li>
+      `)}
+    </ul>
+  </aside>
+`;
 }
