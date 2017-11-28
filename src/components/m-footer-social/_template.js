0ec8c3f14e12f547ae7a68993a0a135eb4ae3ec1
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-28 17:23:43 +0100

    start markup structure of footer

--- src/components/m-footer-social/_template.js
+++ src/components/m-footer-social/_template.js
@@ -1,3 +1,17 @@
-export default function (props, children) {
-  return children;
+import bel from 'bel';
+
+export default function ({ title, items }, children) {
+  return bel`<aside class="m-footer-social__box">
+    <strong class="m-footer-social__title">${title}</strong>
+
+    <ul class="m-footer-social__list">
+      ${items.map(({ name, url }) => bel`
+        <li class="m-footer-social-list-item">
+          <a href="${url}" class="m-footer-social__link">
+            <axa-icon id="${name}" classes="m-footer-social__icon"></axa-icon>
+          </a>
+        </li>
+      `)}
+    </ul>
+  </aside>`;
 }
