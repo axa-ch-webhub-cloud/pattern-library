9573ea5f843fd02baffb5f39239007d648c2a5d1
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-29 09:05:11 +0100

    removed test components

--- src/components/a-container/_template.js
+++
@@ -1,6 +0,0 @@
-import bel from 'bel';
-import classnames from 'classnames';
-
-export default function (props, children) {
-  return children;
-}
--- src/components/a-wrap/_template.js
+++
@@ -1,8 +0,0 @@
-import bel from 'bel';
-import classnames from 'classnames';
-
-export default function (props, children) {
-  return bel`<div class="a-warp__box">
-      ${children}
-    </div>`;
-}
