11be7b46a179349f3054675cd4221ab0d13f2759
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-29 14:15:10 +0100

    added coloumns to footer links

--- src/components/m-footer-links/_template.js
+++ src/components/m-footer-links/_template.js
@@ -1,16 +1,22 @@
 import bel from 'bel';
+import chunk from '../../js/array-chunk';
 
-export default function ({ items }) {
+export default function ({ items, cols = 1 }) {
   return bel`<div class="m-footer-links__row">
-  ${items.map(({ title, items }) => bel`<div class="m-footer-links__block">
-    <strong class="m-footer-links__category">${title}</strong>
-    <ul class="m-footer-links__list">
-      ${items.map(({ name, url }) => bel`
-        <li class="m-footer-links__list-item">
-          <a class="m-footer-links__link" href="${url}">${name}</a>
-        </li>
-      `)}
-    </ul>
-  </div>`)}
-</div>`;
+    ${items.map(({ title, items }) => bel`<div class="m-footer-links__block">
+      <strong class="m-footer-links__category">${title}</strong>
+
+      <div class="m-footer-links__cols">
+        ${chunk(items, cols).map(col => bel`
+          <ul class="m-footer-links__list">
+            ${col.map(({ name, url }) => bel`
+              <li class="m-footer-links__list-item">
+                <a class="m-footer-links__link" href="${url}">${name}</a>
+              </li>
+            `)}
+          </ul>
+        `)}
+      </div>
+    </div>`)}
+  </div>`;
 }
