7d2b68af588760317991fc10928e40cbea5c600a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-30 11:45:28 +0100

    start improving mobile layout

--- src/components/m-footer-links/_template.js
+++ src/components/m-footer-links/_template.js
@@ -1,9 +1,12 @@
 import bel from 'bel';
+import raw from 'bel/raw';
 import chunk from '../../js/array-chunk';
+
+const arrowIcon = '<axa-icon id="angle-bracket-right" classes="m-footer-links__category__icon"></axa-icon>';
 
 export default function ({ title, items, cols = 1 }) {
   return bel`<div class="m-footer-links__block">
-    <strong class="m-footer-links__category">${title}</strong>
+    <strong class="m-footer-links__category">${title}${raw(arrowIcon)}</strong>
 
     <div class="m-footer-links__cols m-footer-links__cols-${cols}">
       ${chunk(items, cols).map(col => bel`
