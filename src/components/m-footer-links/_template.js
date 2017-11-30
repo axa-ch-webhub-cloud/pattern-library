34524cf97f86abb09a71c5929d1397537cea31ab
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-30 16:50:21 +0100

    start adding js dropdown classes

--- src/components/m-footer-links/_template.js
+++ src/components/m-footer-links/_template.js
@@ -5,8 +5,8 @@ import chunk from '../../js/array-chunk';
 const arrowIcon = '<axa-icon id="angle-bracket-right" classes="m-footer-links__category__icon"></axa-icon>';
 
 export default function ({ title, items, cols = 1 }) {
-  return bel`<div class="m-footer-links__block">
-    <strong class="m-footer-links__category">${title}${raw(arrowIcon)}</strong>
+  return bel`<div class="m-footer-links__block js-dropdown">
+    <strong class="m-footer-links__category js-dropdown__toggle">${title}${raw(arrowIcon)}</strong>
 
     <div class="m-footer-links__cols m-footer-links__cols-${cols}">
       ${chunk(items, cols).map(col => bel`
