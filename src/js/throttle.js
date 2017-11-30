792b1de69424a28344d9d16a07600bc2b62a5620
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-11-30 15:42:46 +0100

    removed good old jQuery

--- src/js/throttle.js
+++ src/js/throttle.js
@@ -28,15 +28,15 @@ import debounce from './debounce';
  * @example
  *
  * // Avoid excessively updating the position while scrolling.
- * jQuery(window).on('scroll', _.throttle(updatePosition, 100));
+ * window.addEventListener('scroll', _.throttle(updatePosition, 100));
  *
  * // Invoke `renewToken` when the click event is fired, but not more than once every 5 minutes.
  * var throttled = _.throttle(renewToken, 300000, { 'trailing': false });
  *
- * jQuery(element).on('click', throttled);
+ * window.addEventListener('click', throttled);
  *
  * // Cancel the trailing throttled invocation.
- * jQuery(window).on('popstate', throttled.cancel);
+ * window.addEventListener('popstate', throttled.cancel);
  */
 
 function throttle(func, wait, { leading = true, trailing = true } = {}) {
