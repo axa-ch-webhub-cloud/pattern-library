915519acc3ac7756ff595a1f3018ec0eae954923
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-01 12:34:52 +0100

    utilized CSS coloums module

--- src/components/m-footer-links/_template.js
+++ src/components/m-footer-links/_template.js
@@ -4,20 +4,16 @@ import chunk from '../../js/array-chunk';
 
 const arrowIcon = '<axa-icon id="angle-bracket-right" classes="m-footer-links__category__icon"></axa-icon>';
 
-export default function ({ title, items, cols = 1 }) {
+export default function ({ title, items }) {
   return bel`<div class="m-footer-links__block js-dropdown">
     <strong class="m-footer-links__category js-dropdown__toggle">${title}${raw(arrowIcon)}</strong>
 
-    <div class="m-footer-links__cols m-footer-links__cols-${cols}">
-      ${chunk(items, cols).map(col => bel`
-        <ul class="m-footer-links__list">
-          ${col.map(({ name, url }) => bel`
-            <li class="m-footer-links__list-item">
-              <a class="m-footer-links__link" href="${url}">${name}</a>
-            </li>
-          `)}
-        </ul>
+    <ul class="m-footer-links__list">
+      ${items.map(({ name, url }) => bel`
+        <li class="m-footer-links__list-item">
+          <a class="m-footer-links__link" href="${url}">${name}</a>
+        </li>
       `)}
-    </div>
+    </ul>
   </div>`;
 }
