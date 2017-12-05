92fb443cdd733362de731bc539c7008a43f6cf84
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-05 15:20:44 +0100

    start adding custom events polyfill

--- src/js/fire.js
+++ src/js/fire.js
@@ -1,14 +1,9 @@
-/* global Event, CustomEvent */
+import CustomEvent from './custom-event';
 
 function fire(node, eventName, eventObject) {
-  // @TODO: maybe polyfill is needed for Event or CustomEvent, like document.createEvent...
-  const event = !eventObject ?
-    new Event(eventName)
-    :
-    new CustomEvent(eventName, {
-      bubble: true,
-      detail: eventObject,
-    });
+  const event = new CustomEvent(eventName, {
+    detail: eventObject,
+  });
 
   // Dispatch the event.
   node.dispatchEvent(event);
