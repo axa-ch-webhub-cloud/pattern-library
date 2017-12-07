a17a5276ac34952cfb7796b8460da28560b4eebb
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-07 17:36:13 +0100

    use viewport funcs

--- src/js/element-from-page-point.js
+++ src/js/element-from-page-point.js
@@ -1,10 +1,11 @@
 import getScrollTop from './get-scroll-top';
 import getScrollLeft from './get-scroll-left';
+import { getViewportWidth, getViewportHeight } from './viewport';
 
 // Test with a point larger than the viewport. If it returns an element,
 // then that means elementFromPoint takes page coordinates.
 const elementFromPagePoint = ('pageYOffset' in window
-  && document.elementFromPoint(getScrollLeft() + window.innerHeight, getScrollTop() + window.innerHeight)) ?
+  && document.elementFromPoint(getScrollLeft() + getViewportHeight(), getScrollTop() + getViewportWidth())) ?
   document.elementFromPoint.bind(document) :
   elementFromPoint;
 
