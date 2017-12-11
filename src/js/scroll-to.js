e8d051fa9bb96c26b57bc15d098640a18858639b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-11 14:19:00 +0100

    utilized posy

--- src/js/scroll-to.js
+++ src/js/scroll-to.js
@@ -1,15 +1,9 @@
 import ownerWindow from './owner-window';
+import posY from './pos-y';
 
 export default function scrollTo(node) {
   const window = ownerWindow(node);
-  let offsetParent = node;
-  let y = 0;
-
-  if (offsetParent) {
-    do {
-      y += offsetParent.offsetTop;
-    } while (offsetParent = offsetParent.offsetParent);
-  }
+  const y = posY(node);
 
   window.scrollTo(0, y);
 }
