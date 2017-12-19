a50e8cc5dde1154b3e0fe28fc3aa43bca498687e
Luca Mele <luca.mele@axa-winterthur.ch>
2017-12-19 10:07:30 +0100

    add link to the colors

--- stack/tasks/release.js
+++ stack/tasks/release.js
@@ -3,6 +3,7 @@ const { exec } = require('child_process');
 
 process.stdin.setEncoding('utf8');
 
+// ref: https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 console.log('\x1b[40m', '\x1b[36m', // eslint-disable-line
   outdent`
 
