4286fc487fbb0f0d996fb80749384dafebdab9e7
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-20 14:13:07 +0100

    added first-left modifier

--- src/components/m-header-main/index.js
+++ src/components/m-header-main/index.js
@@ -1,9 +1,11 @@
+import classnames from 'classnames';
 import { BaseComponentGlobal } from '../_abstract/component-types';
 // import the styles used for this component
 import styles from './index.scss';
 // import the template used for this component
 import template from './_template';
 import wcdomready from '../../js/wcdomready';
+import getAttribute from '../../js/get-attribute';
 
 class AXAHeaderMain extends BaseComponentGlobal {
   constructor() {
@@ -13,7 +15,11 @@ class AXAHeaderMain extends BaseComponentGlobal {
   connectedCallback() {
     super.connectedCallback();
 
-    this.className = `${this.initialClassName} m-header-main`;
+    const firstLeft = getAttribute('first-left');
+
+    this.className = classnames(this.initialClassName, 'm-header-main', {
+      'm-header-main--first-left': firstLeft,
+    });
   }
 }
 
