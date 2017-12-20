33e3e8ce22c63572f1408a02a030faa7fa4102d3
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-20 12:20:13 +0100

    added header languages

---
+++ src/components/m-header-languages/_template.js
@@ -0,0 +1,19 @@
+import bel from 'bel';
+import raw from 'bel/raw';
+
+export default ({ items }) => [bel`
+  <button role="button" class="m-header-languages__drop-down-toggle js-dropdown__toggle">
+    ${items[0].name}
+    <axa-icon id="angle-bracket-right" classes="m-header-languages__drop-down-icon"></axa-icon>
+  </button>
+`, bel`
+  <ul class="m-header-languages__list">
+    ${items && items.map(({url, name}, index) => bel`
+      <li class="m-header-languages__list-item">
+        <a class="m-header-languages__list-link${index === 0 ? ' is-active' : ''}" href="${url}">
+          ${raw(name)}
+        </a>
+      </li>
+    `)}
+  </ul>
+`];
---
+++ src/components/m-header-languages/index.js
@@ -0,0 +1,34 @@
+import { BaseComponentGlobal } from '../_abstract/component-types';
+// import the styles used for this component
+import styles from './index.scss';
+// import the template used for this component
+import template from './_template';
+import DropDown from '../m-dropdown/js/drop-down';
+import { wcdomready } from '../../js/wcdomready';
+
+class AXAHeaderLanguages extends BaseComponentGlobal {
+  constructor() {
+    super(styles, template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.className = `${this.initialClassName} m-header-languages js-dropdown`;
+
+    this.dropDown = new DropDown(this, {
+      containerClass: null,
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.dropDown.destroy();
+    delete this.dropDown;
+  }
+}
+
+wcdomready(() => {
+  window.customElements.define('axa-header-languages', AXAHeaderLanguages);
+});
