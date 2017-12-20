dcbbbf12e5bde114cef00a4bf265e396e41dc10c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-20 16:07:20 +0100

    moar cleaning

--- src/components/m-header-mobile-languages/_template.js
+++ src/components/m-header-mobile-languages/_template.js
@@ -1,8 +1,5 @@
 import bel from 'bel';
 
-export default ({ items }) => bel`
-  <nav class="m-header-mobile-languages__nav">
-    ${items && items.map(({ code, url, isActive }) => bel`
-      <a href="${url}" class="m-header-mobile-languages__link ${isActive && 'is-active'}">${code}</a>`)}
-  </nav>
-`;
+export default ({ items }) => items && items.map(({ code, url, isActive }) => bel`
+  <a href="${url}" class="m-header-mobile-languages__link ${isActive && 'is-active'}">${code}</a>
+`);
