51aebad20f4512300a6b508b2a06813d4d04d698
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-20 16:12:56 +0100

    fixed data

--- src/components/m-header-mobile-others/_template.js
+++ src/components/m-header-mobile-others/_template.js
@@ -1,8 +1,8 @@
 import bel from 'bel';
 
-export default ({ others }) => bel`
+export default ({ items }) => bel`
   <ul class="m-header-mobile-others__list">
-    ${others && others.map(({ name, url, isActive = false }) => bel`
+    ${items && items.map(({ name, url, isActive = false }) => bel`
       <li class="m-header-mobile-others__list-item">
         <a href="${url}" class="m-header-mobile-others__link ${isActive && 'is-active'}">${name}</a>
       </li>`)}
