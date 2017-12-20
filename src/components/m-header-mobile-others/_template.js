b09ebe08910276781a759e1f5c6f424b06c212c5
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-20 16:08:26 +0100

    moar cleaning

--- src/components/m-header-mobile-others/_template.js
+++ src/components/m-header-mobile-others/_template.js
@@ -1,11 +1,10 @@
 import bel from 'bel';
 
 export default ({ others }) => bel`
-  <nav class="m-header-mobile-others__nav">
-    <ul class="m-header-mobile-others__list">
-      ${others && others.map(({ name, url, isActive = false }) => bel`
-        <li class="m-header-mobile-others__list-item">
-          <a href="${url}" class="m-header-mobile-others__link ${isActive && 'is-active'}">${name}</a>
-        </li>`)}
-    </ul>
-  </nav>`;
+  <ul class="m-header-mobile-others__list">
+    ${others && others.map(({ name, url, isActive = false }) => bel`
+      <li class="m-header-mobile-others__list-item">
+        <a href="${url}" class="m-header-mobile-others__link ${isActive && 'is-active'}">${name}</a>
+      </li>`)}
+  </ul>
+`;
