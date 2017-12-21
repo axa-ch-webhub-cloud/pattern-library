377640d0924a40caf0b4af1f9d84629a8856e592
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-21 11:18:53 +0100

    fixed missing children vars

--- src/components/m-header-meta-right/_template.js
+++ src/components/m-header-meta-right/_template.js
@@ -1,8 +1,8 @@
 import bel from 'bel';
 
-export default (props, fragment) => bel`
+export default (props, { children }) => bel`
   <ul class="m-header-meta-right__list">
-    ${Array.from(fragment.children).map(child => bel`
+    ${Array.from(children).map(child => bel`
       <li class="m-header-meta-right__list-item">${child}</li>
     `)}
   </ul>
