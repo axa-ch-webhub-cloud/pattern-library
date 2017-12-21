19ba0aeee95b3f0d4488bd9edd270b17380ac634
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-12-21 18:39:20 +0100

    added custim img support to header logo

--- src/components/m-header-logo/_template.js
+++ src/components/m-header-logo/_template.js
@@ -1,8 +1,10 @@
 import bel from 'bel';
 import raw from 'bel/raw';
 
-export default () => bel`
+export default ({ src, alt = 'AXA Logo' } = {}) => bel`
   <a href="#" class="m-header-logo__link">
-    ${raw('<axa-icon id="logo-AXA" classes="m-header-logo__icon"></axa-icon>')}
+    ${src ? bel`
+      <img class="m-header-logo__img" src="${src}" alt="${alt}" />
+    ` : raw('<axa-icon id="logo-AXA" classes="m-header-logo__icon"></axa-icon>')}
   </a>
 `;
