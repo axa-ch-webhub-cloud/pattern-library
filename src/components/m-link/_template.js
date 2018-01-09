287ce0eaf6ce57e9739045fa61868092a9f64fe2
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-01-09 18:07:42 +0100

    moved class names

--- src/components/m-link/_template.js
+++ src/components/m-link/_template.js
@@ -10,20 +10,20 @@ export default function ({
   icon,
   deco,
 }, childrenFragment) {
-  const classes = classnames('a-link', {
-    [`a-link--${color}`]: color,
-    [`a-link--${size}`]: size,
-    'a-link--motion': motion,
-    'a-link--arrow': arrow,
-    'a-link--listed': listed,
-    'a-link--icon': icon,
-    'a-link--deco': deco,
+  const classes = classnames('m-link', {
+    [`m-link--${color}`]: color,
+    [`m-link--${size}`]: size,
+    'm-link--motion': motion,
+    'm-link--arrow': arrow,
+    'm-link--listed': listed,
+    'm-link--icon': icon,
+    'm-link--deco': deco,
   });
 
   return bel`<a href="#" class="${classes}">
-      ${icon && bel`<axa-icon id="${icon}" classes="a-link__icon"></axa-icon>`}
-      ${listed && bel`<axa-icon id="arrow" classes="a-link__listed"></axa-icon>`}
+      ${icon && bel`<axa-icon id="${icon}" classes="m-link__icon"></axa-icon>`}
+      ${listed && bel`<axa-icon id="arrow" classes="m-link__listed"></axa-icon>`}
       ${childrenFragment}
-      ${arrow && bel`<axa-icon id="arrow" classes="a-link__arrow"></axa-icon>`}
+      ${arrow && bel`<axa-icon id="arrow" classes="m-link__arrow"></axa-icon>`}
     </a>`;
 }
