0cbd3efc693639254ad3ef565054363096065489
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-01-11 17:26:51 +0100

    cleaning maybe

--- src/js/maybe.js
+++ src/js/maybe.js
@@ -1,6 +1,9 @@
 export const noop = () => {};
 export const toEqual = (value, toBe) => value === toBe;
 
-const maybe = fn => comparison => (...values) => (...toBes) => (...args) => comparison(...[...values, ...toBes]) ? fn(...args) : noop();
+// eslint-disable-next-line no-confusing-arrow
+const maybe = fn => comparison => (...values) => (...toBes) => (...args) => comparison(...[...values, ...toBes]) ?
+  fn(...args) :
+  noop();
 
 export default maybe;
