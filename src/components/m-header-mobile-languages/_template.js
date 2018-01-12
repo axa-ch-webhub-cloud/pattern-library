b4f4398687cc332a45efc2cc20b529054aade257
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-01-12 13:10:02 +0100

    namespaced header mobile languages state class

--- src/components/m-header-mobile-languages/_template.js
+++ src/components/m-header-mobile-languages/_template.js
@@ -1,5 +1,5 @@
 import bel from 'bel';
 
 export default ({ items }) => items && items.map(({ code, url, isActive }) => bel`
-  <a href="${url}" class="m-header-mobile-languages__link ${isActive && 'is-active'}">${code}</a>
+  <a href="${url}" class="m-header-mobile-languages__link ${isActive && 'is-header-mobile-languages-active'}">${code}</a>
 `);
