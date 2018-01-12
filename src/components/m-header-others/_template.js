1c0aa29979e5c71389e66a1b5602f741261d8c04
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-01-12 13:18:54 +0100

    namespaced header-others

--- src/components/m-header-others/_template.js
+++ src/components/m-header-others/_template.js
@@ -5,7 +5,7 @@ export default ({ items }) => bel`
   <ul class="m-header-others__list">
     ${items && items.map(({ url, name }, index) => bel`
       <li class="m-header-others__list-item">
-        <a href="${url}" class="m-header-others__list-link${index === 0 ? ' is-active' : ''}">${raw(name)}</a>
+        <a href="${url}" class="m-header-others__list-link${index === 0 ? ' is-header-others-active' : ''}">${raw(name)}</a>
       </li>
     `)}
   </ul>
