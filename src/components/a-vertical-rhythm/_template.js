8fda3b76effaee320fab159fb20d2d506e96905b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-01-15 12:54:39 +0100

    start adding a-vertical-rhythm component

---
+++ src/components/a-vertical-rhythm/_template.js
@@ -0,0 +1,3 @@
+import bel from 'bel';
+
+export default (props, children) => children;
---
+++ src/components/a-vertical-rhythm/index.js
@@ -0,0 +1,24 @@
+import { BaseComponentGlobal } from '../_abstract/component-types';
+// import the styles used for this component
+import styles from './index.scss';
+// import the template used for this component
+import template from './_template';
+import wcdomready from '../../js/wcdomready';
+
+class AXAVerticalRhythm extends BaseComponentGlobal {
+  constructor() {
+    super(styles, template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.className = `${this.initialClassName} a-vertical-rhythm`;
+  }
+}
+
+wcdomready(() => {
+  window.customElements.define('axa-vertical-rhythm', AXAVerticalRhythm);
+});
+
+export default AXAVerticalRhythm;
