9b90fb155b86024ca590c995938e5673fec140e2
Luca Mele <luca.mele@axa-winterthur.ch>
2018-01-19 15:13:43 +0100

    BETA, demo react + WC

---
+++ src/demos/demo.react.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// components are loaded already in the body cause this demo is a the end of the body
+
+class MyEventDemoReact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('click');
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
+
+  render() {
+    return (
+      <axa-button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </axa-button>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<MyEventDemoReact />, document.querySelector('.my-event-demo-react'));
+});
---
+++ stack/tasks/bundle-demos.js
@@ -0,0 +1,48 @@
+const rollup = require('rollup');
+const multiEntry = require('rollup-plugin-multi-entry');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+const replace = require('rollup-plugin-replace');
+
+const constants = require('../constants');
+
+const ENV = process.argv[2]; // second element is the first argument
+const CWD = process.cwd();
+
+async function buildComponents() {
+  const fPath = `${CWD}/${ENV === constants.ENV.PROD ? 'dist' : '.tmp'}/app/all-demos.js`;
+  const bundle = await rollup.rollup({
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(ENV === constants.ENV.PROD ? 'production' : 'development'),
+      }),
+      multiEntry(),
+      resolve({
+        jsnext: true,
+        main: true,
+        browser: true,
+      }),
+      ENV === constants.ENV.PROD ? uglify() : () => {},
+      commonjs({
+        include: 'node_modules/**',
+        exclude: ['node_modules/@webcomponents/webcomponentsjs/**'],
+      }),
+      babel({
+        runtimeHelpers: true,
+        presets: ['react'],
+      }),
+    ],
+    input: './src/demos/*.jsx',
+  });
+  console.log(`Bundled to: ${fPath}`); // eslint-disable-line
+  // or write the bundle to disk
+  await bundle.write({
+    format: 'iife',
+    name: 'StyleGuideWebComponentReact',
+    file: fPath,
+  });
+}
+
+buildComponents();
