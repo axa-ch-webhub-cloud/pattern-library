b797c2d9d72d677223b041fd694dc4cf62499686
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-12 14:27:21 +0100

    added hyphante per item support

--- src/components/m-header-navigation/_template.js
+++ src/components/m-header-navigation/_template.js
@@ -3,9 +3,9 @@ import raw from 'bel/raw';
 
 const writeClasses = classes => classes ? ` ${classes}` : ''; // eslint-disable-line no-confusing-arrow
 
-const submenuItem = ({ url, name, items, classes, isActive, simplemenu }) => bel`
+const submenuItem = ({ url, name, items, classes, isActive, simplemenu, hyphenate = false }) => bel`
     <li class="m-header-navigation__list-item">
-      <a class="m-header-navigation__list-link ${(items || !!simplemenu) ? 'js-header-navigation__list-link' : ''} ${isActive ? 'is-header-navigation-active' : ''} ${writeClasses(classes)}" href="${url}">${raw(name)}</a>
+      <a class="m-header-navigation__list-link ${(items || !!simplemenu) ? 'js-header-navigation__list-link' : ''} ${isActive ? 'is-header-navigation-active' : ''} ${hyphenate ? 'm-header-navigation__list-link--hyphenate' : ''} ${writeClasses(classes)}" href="${url}">${raw(name)}</a>
 
       ${items ? bel`<axa-header-sub-navigation indextitle="${name}" indexurl="${url}" items='${JSON.stringify(items)}' flyout>
         </axa-header-sub-navigation>` : ''}
