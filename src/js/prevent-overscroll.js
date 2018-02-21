09d4eb2116f2a0f7fdb1040f63ddf941697c0a0c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-21 12:53:34 +0100

    start adding prevent overscroll js code

---
+++ src/js/prevent-overscroll.js
@@ -0,0 +1,68 @@
+import on from './on';
+
+function preventOverscroll(node) {
+  const offStart = on(node, 'touchstart', touchstart);
+  const offBody = on(document.body, 'touchmove', bodymove);
+  let offMove;
+  let offEnd;
+
+  return cleanUp;
+
+  function touchstart() {
+    touchend();
+    offMove = on(node, 'touchmove', touchmove);
+    offEnd = on(node, 'touchend', touchend);
+
+    const { scrollTop, scrollHeight, offsetHeight } = node;
+    const currentScroll = scrollTop + offsetHeight;
+
+    // If we're at the top or the bottom of the containers
+    // scroll, push up or down one pixel.
+    //
+    // this prevents the scroll from "passing through" to
+    // the body.
+    if (scrollTop === 0) {
+      node.scrollTop = 1;
+    } else if (currentScroll === scrollHeight) {
+      node.scrollTop = scrollTop - 1;
+    }
+  }
+
+  function touchmove(event) {
+    // if the content is actually scrollable, i.e. the content is long enough
+    // that scrolling can occur
+    if (node.offsetHeight < node.scrollHeight) {
+      event._isScroller = true;
+    }
+  }
+
+  function touchend() {
+    if (offMove) {
+      offMove();
+      offMove = null;
+    }
+
+    if (offEnd) {
+      offEnd();
+      offEnd = null;
+    }
+
+  }
+
+  function bodymove(event) {
+    // In this case, the default behavior is scrolling the body, which
+    // would result in an overflow.  Since we don't want that, we preventDefault.
+    if (!event._isScroller) {
+      event.preventDefault();
+    }
+  }
+
+  function cleanUp() {
+    offStart();
+    offBody();
+
+    touchend();
+  }
+}
+
+export default preventOverscroll;
