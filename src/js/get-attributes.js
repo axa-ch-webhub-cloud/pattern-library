60ff9621a4b01014fb3390080ff869e187a1ed97
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-23 15:10:24 +0100

    improved docs

--- src/js/get-attributes.js
+++ src/js/get-attributes.js
@@ -4,7 +4,7 @@ import getAttribute from './get-attribute';
  * Get all attributes of a node.
  *
  * @param {Element} node - The HTML element to get all attributes of.
- * @returns {Object|null} -Returns a hash of all attributes or `null`.
+ * @returns {?Object} - Returns a hash of all attributes or `null`.
  */
 function getAttributes(node) {
   if (!node.hasAttributes) {
