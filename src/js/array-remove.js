2cb9563f9baed7511628570a03a541e19d1c484e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-23 15:04:45 +0100

    fixed missing type

--- src/js/array-remove.js
+++ src/js/array-remove.js
@@ -2,8 +2,8 @@
  * Removes an item from an array.
  * **Note:** This does change the array itself.
  *
- * @param array - The array to update.
- * @param element - The item to be removed.
+ * @param {Array} array - The array to update.
+ * @param {Element} element - The item to be removed.
  */
 export default function remove(array, element) {
   const index = array.indexOf(element);
