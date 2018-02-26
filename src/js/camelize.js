9a72b9690ce5e46dba50aa6fef320f8f772c99c3
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-26 17:56:39 +0100

    oops, wrong doclet

--- src/js/camelize.js
+++ src/js/camelize.js
@@ -3,6 +3,7 @@ const reWord = /(?:^\w|[A-Z]|\b\w)/g;
 /**
  * Camelcase any given string.
  *
+ * @link https://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
  * @param {String} string - The string to be camel-cased.
  * @returns {String} - Returns camel-cased string.
  */
