0c3a2e5d44f63e50e57a326b3d8bc45b07a70546
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-28 16:27:47 +0100

    start implementing tap event

--- src/js/tap.js
+++ src/js/tap.js
@@ -11,6 +11,7 @@ const pointerup = 'touchend pointerup MSPointerUp mouseup';
 const regexPointer = /touch|pointer/;
 const TAP_THRESHOLD = 150;
 const TAP_PRECISION = 60 / 2;
+const getPointer = event => event.targetTouches ? event.targetTouches[0] : event;
 
 // eslint-disable-next-line consistent-return
 function addEventListenerProxy(eventType, ...args) {
@@ -24,7 +25,8 @@ function addEventListenerProxy(eventType, ...args) {
 
   const { ownerDocument: { documentElement } } = target;
   let wasPointer = false;
-  let pointerId;
+  let cachedPointerId;
+  const isSamePointer = event => !event.pointerId || typeof cachedPointerId === 'undefined' || event.pointerId === cachedPointerId;
   let downTimestamp;
   let cachedX;
   let cachedY;
@@ -38,16 +40,20 @@ function addEventListenerProxy(eventType, ...args) {
 
   function downHandler(event) {
     const { type, timeStamp, pointerId } = event;
+    const { pageX, pageY } = getPointer(event);
 
     // start by pointer or mouse?
     wasPointer = type.test(regexPointer);
+
+    // cache pointerId
+    cachedPointerId = pointerId;
 
     // cache down time
     downTimestamp = timeStamp || +(new Date());
 
     // cache coordinates
-    currentX = cachedX = event.pageX;
-    currentY = cachedY = event.pageY;
+    currentX = cachedX = pageX;
+    currentY = cachedY = pageY;
 
     // bind move and up events
     offMove = on(documentElement, pointermove, moveHandler);
@@ -56,15 +62,20 @@ function addEventListenerProxy(eventType, ...args) {
 
   function moveHandler(event) {
     const { type } = event;
+    const { pageX, pageY } = getPointer(event);
 
     // skip mouse events if a touch event was dispatched on start
     if (wasPointer && type.indexOf('mouse') !== -1) {
       return;
     }
 
+    if (isSamePointer(event)) {
+      return;
+    }
+
     // cache coordinates
-    currentX = event.pageX;
-    currentY = event.pageY;
+    currentX = pageX;
+    currentY = pageY;
   }
 
   function upHandler(event) {
@@ -76,6 +87,11 @@ function addEventListenerProxy(eventType, ...args) {
       return;
     }
     wasPointer = false;
+
+    if (isSamePointer(event)) {
+      return;
+    }
+    cachedPointerId = undefined;
 
     const timeDiff = (timeStamp || +(new Date())) - downTimestamp;
     const isValidThreshold = timeDiff < TAP_THRESHOLD;
