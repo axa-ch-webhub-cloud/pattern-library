7a137c3756c401cb20eb8d293def94740ba73a02
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-02-28 09:14:56 +0100

    adding passive to outer events

--- src/js/outer.js
+++ src/js/outer.js
@@ -1,3 +1,4 @@
+import hasPassive from './has-passive';
 import { freeByValue } from './free';
 
 /**
@@ -6,13 +7,16 @@ import { freeByValue } from './free';
  * @param {Node} node - The node is an Element in a document that supports events.
  * @param {String} eventName - A string representing the event type to listen for.
  * @param {Function} func - A function which receives a notification when an event of the specified type occurs.
- * @param {Boolean} [capture=true] - A Boolean indicating that events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
+ * @param {Object} options - An options object that specifies characteristics about the event listener.
+ * @param {Boolean} [options.capture=true] - A Boolean indicating that events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
+ * @param {Boolean} [options.passive=true] - A Boolean indicating that the listener will never call `preventDefault()`. If it does, the user agent should ignore it and generate a console warning.
  * @returns {off} - Returns a functions which properly removes the event listener from the target.
  */
-function outer(node, eventName, func, capture = true) {
+function outer(node, eventName, func, { capture = true, passive = true } = {}) {
   const root = node.ownerDocument.documentElement;
+  const eventOptions = hasPassive ? { capture, passive } : capture;
 
-  root.addEventListener(eventName, handler, capture);
+  root.addEventListener(eventName, handler, eventOptions);
 
   return off;
 
@@ -20,7 +24,7 @@ function outer(node, eventName, func, capture = true) {
    * Removes associated event listener of tracked target.
    */
   function off() {
-    root.removeEventListener(eventName, handler, capture);
+    root.removeEventListener(eventName, handler, eventOptions);
 
     // automatically free instances holding the off callback.
     freeByValue(this, off);
