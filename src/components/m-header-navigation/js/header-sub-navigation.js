d3b69b91ce99d496c655bb42077a7344ee2997d3
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-05 13:33:10 +0100

    added missing force repaint on move

--- src/components/m-header-navigation/js/header-sub-navigation.js
+++ src/components/m-header-navigation/js/header-sub-navigation.js
@@ -1,6 +1,5 @@
 import { add, remove } from '../../../js/class-list';
 import forceRepaint from '../../../js/force-repaint';
-import { requestAnimationFrame } from '../../../js/request-animation-frame';
 import UiEvents from '../../../js/ui-events';
 
 class HeaderSubNavigation extends UiEvents {
@@ -38,16 +37,18 @@ class HeaderSubNavigation extends UiEvents {
   enter(node) {
     add(node.parentNode, this.options.openClass);
 
-    requestAnimationFrame(() => {
-      // Edge 16 won't repaint -> force it
-      // see https://github.com/axa-ch/patterns-library/issues/304
-      forceRepaint(this.subMenu);
-    });
+    // Edge 16 won't repaint -> force it
+    // see https://github.com/axa-ch/patterns-library/issues/304
+    forceRepaint(this.subMenu);
   }
 
   move(node, lastNode) {
     remove(lastNode.parentNode, this.options.openClass);
     add(node.parentNode, this.options.openClass);
+
+    // Edge 16 won't repaint -> force it
+    // see https://github.com/axa-ch/patterns-library/issues/304
+    forceRepaint(this.subMenu);
   }
 
   leave(node) {
