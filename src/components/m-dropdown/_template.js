caaf669221148eacc5570f0b4ecdc1499f241627
Luca Mele <luca.mele@axa-winterthur.ch>
2018-03-14 13:28:06 +0100

    fix logic issue and make the template a bit nicer

--- src/components/m-dropdown/_template.js
+++ src/components/m-dropdown/_template.js
@@ -3,17 +3,17 @@ import raw from 'bel/raw';
 
 const arrowIcon = '<axa-icon id="angle-bracket-down" classes="m-dropdown__icon"></axa-icon>';
 
-const nativeSelect = ({ items, size }) => bel`<div class="m-dropdown__select-wrap ${size ? `m-dropdown__select-wrap--${size}` : ''}" tabindex="0">
-    <select class="m-dropdown__select ${size ? `m-dropdown__select--${size}` : ''}">
+const nativeSelect = ({ items, size }) => bel`<div class="m-dropdown__select-wrap ${size && `m-dropdown__select-wrap--${size}`}" tabindex="0">
+    <select class="m-dropdown__select ${size && `m-dropdown__select--${size}`}">
       ${items && items.map(({ name, url }) => bel`
         <option data-url="${url}">${name}</option>
       `)}
     </select>
-    <div class="m-dropdown__select-icon ${size ? `m-dropdown__select-icon--${size}` : ''}">${raw(arrowIcon)}</div>
+    <div class="m-dropdown__select-icon ${size && `m-dropdown__select-icon--${size}`}">${raw(arrowIcon)}</div>
   </div>`;
 
 const enhancedSelect = ({ title, items, size }) => [
-  bel`<button type="button" class="m-dropdown__toggle js-dropdown__toggle ${size ? `m-dropdown__toggle--${size}` : ''}">
+  bel`<button type="button" class="m-dropdown__toggle js-dropdown__toggle ${size && `m-dropdown__toggle--${size}`}">
     ${title}${raw(arrowIcon)}
   </button>`,
   bel`<ul class="m-dropdown__content">
