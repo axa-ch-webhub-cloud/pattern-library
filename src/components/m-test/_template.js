67b980ecf00e93f6b0797af4148b7aea801c72f8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-14 14:39:11 +0100

    fixed eslint

--- src/components/m-test/_template.js
+++ src/components/m-test/_template.js
@@ -1,5 +1,5 @@
 import bel from 'bel';
 
-export default ({ }, childrenFragment) => bel`
+export default (props, childrenFragment) => bel`
   <article class="m-test__article">${childrenFragment}</article>
 `;
