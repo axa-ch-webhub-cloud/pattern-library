7f4cb57851049a4174fff2556ff8097510e7a5dd
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-20 16:35:08 +0100

    made button incremental

--- src/components/m-button/index.js
+++ src/components/m-button/index.js
@@ -4,6 +4,8 @@ import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import wcdomready from '../../js/wcdomready';
 
 class AXAButton extends BaseComponentGlobal {
+  static get observedAttributes() { return ['arrow', 'classes', 'color', 'ghost', 'motion', 'size', 'tag', 'url']; }
+
   constructor() {
     super(styles, template);
   }
