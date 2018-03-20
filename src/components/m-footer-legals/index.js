a45698ed8d2614ccdbb1a432c3a5c4fd3ba736c8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-20 17:52:01 +0100

    updated footer legals

--- src/components/m-footer-legals/index.js
+++ src/components/m-footer-legals/index.js
@@ -5,12 +5,23 @@ import styles from './index.scss';
 import wcdomready from '../../js/wcdomready';
 
 class AXAFooterLegals extends BaseComponentGlobal {
+  static get observedAttributes() { return ['bottom']; }
+
   constructor() {
     super(styles);
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    this.render();
+  }
+
+  attributeChangedCallback() {
+    this.render();
+  }
+
+  render() {
     const bottom = getAttribute(this, 'bottom');
 
     this.className = classnames(this.initialClassName, 'm-footer-legals', {
