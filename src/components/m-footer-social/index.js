2934a4c50b40ec33d2fc48c69be51c30fb652e04
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-20 18:06:59 +0100

    upgraded footer social

--- src/components/m-footer-social/index.js
+++ src/components/m-footer-social/index.js
@@ -6,12 +6,24 @@ import template from './_template';
 import wcdomready from '../../js/wcdomready';
 
 class AXAFooterSocial extends BaseComponentGlobal {
+  static get observedAttributes() { return ['inline', 'items', 'light', 'title']; }
+
   constructor() {
     super(styles, template);
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    this.renderWCNode();
+  }
+
+  attributeChangedCallback() {
+    this.renderWCNode();
+    this.render();
+  }
+
+  renderWCNode() {
     const inline = getAttribute(this, 'inline');
     const light = getAttribute(this, 'light');
 
