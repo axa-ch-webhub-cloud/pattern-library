37b9ddaa668210eb174f3d531a388cbe04ddf758
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-26 13:51:41 +0200

    check if items is an array

--- src/components/m-header-mobile-others/_template.js
+++ src/components/m-header-mobile-others/_template.js
@@ -2,7 +2,7 @@ import html from 'nanohtml';
 
 export default ({ items }) => html`
   <ul class="m-header-mobile-others__list">
-    ${items && items.map(({ name, url, isActive = false }) => html`
+    ${Array.isArray(items) && items.map(({ name, url, isActive = false }) => html`
       <li class="m-header-mobile-others__list-item">
         <a href="${url}" class="m-header-mobile-others__link js-header-mobile-close ${isActive && 'is-header-mobile-others-active'}">${name}</a>
       </li>`)}
