b8ee7d06bcd138305b550f2095d63e93cfd9bebb
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-29 11:19:47 +0200

    quickfixed input losing focus within ce

--- src/demos/todomvc/todo-header.jsx
+++ src/demos/todomvc/todo-header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import withReact from '../../js/with-react';
 import AXAHeader from '../../components/o-header';
 import AXAHeaderMain from '../../components/m-header-main';
@@ -9,27 +9,68 @@ const AXAHeaderReact = withReactBound(AXAHeader);
 const AXAHeaderMainReact = withReactBound(AXAHeaderMain);
 const AXAHeaderLogoReact = withReactBound(AXAHeaderLogo);
 
+// @todo: super hacky way to keep the input's focus within custom elements
+class Input extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleRef = this.handleRef.bind(this);
+  }
+
+  componentDidUpdate() {
+    const { props: { editing }, input } = this;
+
+    if (input && !editing) {
+      input.autofocus = !editing;
+
+      setTimeout(() => {
+        input.focus();
+      }, 200);
+    }
+  }
+
+  handleRef(input) {
+    this.input = input;
+  }
+
+  render() {
+    const { props, handleRef } = this;
+
+    return (
+      <input
+        ref={handleRef}
+        {...props}
+      />
+    );
+  }
+}
+
 const TodoHeader = ({
   editing,
   newTodo,
   handleNewTodoKeyDown,
   handleChange,
-}) => (
-  <AXAHeaderReact>
-    <AXAHeaderMainReact firstLeft>
-      <AXAHeaderLogoReact />
-      <h1 className="m-todo-header__title">Todos</h1>
-      <input
-        ref={input => input && !editing && input.focus()}
-        className="m-todo-header__new"
-        placeholder="What needs to be done?"
-        value={newTodo}
-        onKeyDown={handleNewTodoKeyDown}
-        onChange={handleChange}
-        autoFocus={!editing}
-      />
-    </AXAHeaderMainReact>
-  </AXAHeaderReact>
-);
+}) => {
+  console.log('render todo header');
+
+  return (
+    <AXAHeaderReact>
+      <AXAHeaderMainReact firstLeft>
+        <AXAHeaderLogoReact />
+        <h1 className="m-todo-header__title">Todos</h1>
+
+        <Input
+          className="m-todo-header__new"
+          placeholder="What needs to be done?"
+          editing={editing}
+          value={newTodo}
+          onKeyDown={handleNewTodoKeyDown}
+          onChange={handleChange}
+          autoFocus={!editing}
+        />
+      </AXAHeaderMainReact>
+    </AXAHeaderReact>
+  );
+}
 
 export default TodoHeader;
