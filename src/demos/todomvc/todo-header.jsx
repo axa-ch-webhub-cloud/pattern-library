03b52312fb95e4ea688d08c934f100c39c4b3a2c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-30 08:25:47 +0200

    removed the input hack

--- src/demos/todomvc/todo-header.jsx
+++ src/demos/todomvc/todo-header.jsx
@@ -9,63 +9,6 @@ const AXAHeaderReact = withReactBound(AXAHeader);
 const AXAHeaderMainReact = withReactBound(AXAHeaderMain);
 const AXAHeaderLogoReact = withReactBound(AXAHeaderLogo);
 
-// @todo: super hacky way to keep the input's focus within custom elements
-class Input extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.onBlur = this.onBlur.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.handleRef = this.handleRef.bind(this);
-
-    this.state = {
-      reFocus: false,
-    };
-  }
-
-  onBlur() {
-    const { state: { reFocus } } = this;
-
-    if (reFocus === true) {
-      this.setState({
-        reFocus: false,
-      });
-
-      setTimeout(() => {
-        this.input.focus();
-      }, 10);
-    }
-  }
-
-  onChange(event) {
-    const { props: { onChange } } = this;
-
-    this.setState({
-      reFocus: true,
-    });
-
-    onChange(event);
-  }
-
-  handleRef(input) {
-    this.input = input;
-  }
-
-  render() {
-    const { handleRef } = this;
-    const { onChange, ...props } = this.props;
-
-    return (
-      <input
-        {...props}
-        ref={handleRef}
-        onBlur={this.onBlur}
-        onChange={this.onChange}
-      />
-    );
-  }
-}
-
 const TodoHeader = ({
   newTodo,
   handleNewTodoKeyDown,
@@ -77,7 +20,7 @@ const TodoHeader = ({
       <AXAHeaderLogoReact onAxaClick={toggleAll} />
       <h1 className="m-todo-header__title">Todos</h1>
 
-      <Input
+      <input
         className="m-todo-header__new"
         placeholder="What needs to be done?"
         value={newTodo}
