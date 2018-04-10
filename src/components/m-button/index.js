2d560bdda6afcd9a32552e343ee9ca7e9a67b2e3
dariodifazio <d>
2018-04-10 11:59:56 +0200

    remove unused

--- src/components/m-button/index.js
+++ src/components/m-button/index.js
@@ -1,10 +1,9 @@
 import styles from './index.scss';
 import template from './_template';
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
-import wcdomready from '../../js/wcdomready';
-import DefineElement from '../../js/define-element';
+import defineElement from '../../js/define-element';
 
-@DefineElement()
+@defineElement()
 class AXAButton extends BaseComponentGlobal {
   static get observedAttributes() { return ['arrow', 'classes', 'color', 'ghost', 'motion', 'size', 'tag', 'url']; }
 
