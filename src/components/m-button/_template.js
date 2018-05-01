71aaccf96b5515434cb53d674bed02de7f5aa6ec
Luca Mele <luca.mele@axa-winterthur.ch>
2018-05-01 09:43:05 +0200

    initial sizeing done

--- src/components/m-button/_template.js
+++ src/components/m-button/_template.js
@@ -5,13 +5,14 @@ import classnames from 'classnames';
 export default function ({
   tag = 'button',
   color,
-  url = '#',
+  href = '#',
   size,
   ghost,
   classes,
   motion,
   gpu,
   arrow,
+  icon = '',
 }, childrenFragment) {
   const buttonClasses = classnames('m-button', classes, {
     [`m-button--${color}`]: color,
@@ -20,19 +21,26 @@ export default function ({
     'm-button--motion': motion,
     'm-button--gpu': gpu,
     'm-button--arrow': arrow,
+    'm-button--generic-icon': icon,
   });
 
-  const arrowIcon = raw('<axa-icon icon="arrow" classes="m-button__arrow"></axa-icon>');
+  let arrowIcon;
+  let genericIcon;
+  if (arrow) {
+    arrowIcon = raw('<axa-icon icon="arrow" classes="m-button__arrow"></axa-icon>');
+  } else if (icon) {
+    genericIcon = raw(`<axa-icon icon="${icon}" classes="m-button__icon"></axa-icon>`);
+  }
 
   if (tag.toLowerCase() === 'a') {
-    return html`<a href="${url}" class="${buttonClasses}">
+    return html`<a href="${href}" class="${buttonClasses}">
       ${childrenFragment}
-      ${arrow && arrowIcon}
+      ${arrowIcon || genericIcon}
     </a>`;
   }
 
   return html`<button type="button" class="${buttonClasses}">
       ${childrenFragment}
-      ${arrow && arrowIcon}
+      ${arrowIcon || genericIcon}
     </button>`;
 }
--- src/components/m-button/index.js
+++ src/components/m-button/index.js
@@ -4,7 +4,7 @@ import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import wcdomready from '../../js/wcdomready';
 
 class AXAButton extends BaseComponentGlobal {
-  static get observedAttributes() { return ['arrow', 'classes', 'color', 'ghost', 'motion', 'size', 'tag', 'url']; }
+  static get observedAttributes() { return ['arrow', 'classes', 'color', 'ghost', 'motion', 'size', 'tag', 'href', 'icon']; }
 
   constructor() {
     super(styles, template);
