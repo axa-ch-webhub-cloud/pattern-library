167fae0124d122f135b2096e2522ff89120c6205
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-06-06 11:03:10 +0200

    improved application of hocs

--- src/js/abstract/base-component.js
+++ src/js/abstract/base-component.js
@@ -7,67 +7,67 @@ import withUpdate from './hocs/with-update';
 import withRender from './hocs/with-render';
 import withStyles from './hocs/with-styles';
 
-// eslint-disable-next-line function-paren-newline
-export default withContext(withMonkeyPatches(withUpdate(withRender(withStyles(
+/**
+ * Base class {BaseComponent}. This class checks if a template is set in the custom element
+ * and if yes appends it. It also appends custom styles to the top of the dom tree.
+ */
+class BaseComponent extends HTMLElement {
+  constructor(styles = '', template) {
+    super();
+
+    this._initialise(styles, template);
+    this._id = getId(this.nodeName);
+  }
+
   /**
-   * Base class {BaseComponent}. This class checks if a template is set in the custom element
-   * and if yes appends it. It also appends custom styles to the top of the dom tree.
+   * _initialise - description
+   *
+   * @param  {type} styles description
+   * @param  {type} template description
+   * @return {type}        description
    */
-  class BaseComponent extends HTMLElement {
-    constructor(styles = '', template) {
-      super();
+  _initialise(styles, template = null) {
+    this._styles = styles;
+    this._template = template;
+    this._hasTemplate = !!template;
+    this._hasRendered = false;
+    this._isConnected = false;
+  }
 
-      this._initialise(styles, template);
-      this._id = getId(this.nodeName);
+  /**
+   * connectedCallback - description
+   *
+   * @return {type}  description
+   */
+  connectedCallback() {
+    if (ENV !== PROD) {
+      lifecycleLogger(this.logLifecycle)(`\n^^^ connectedCallback -> ${this.nodeName}#${this._id}`);
     }
 
-    /**
-     * _initialise - description
-     *
-     * @param  {type} styles description
-     * @param  {type} template description
-     * @return {type}        description
-     */
-    _initialise(styles, template = null) {
-      this._styles = styles;
-      this._template = template;
-      this._hasTemplate = !!template;
-      this._hasRendered = false;
-      this._isConnected = false;
+    if (!this._isConnected) {
+      this._isConnected = true;
+    }
+  }
+
+  /**
+   * disconnectedCallback - description
+   *
+   * @return {type}  description
+   */
+  disconnectedCallback() {
+    if (ENV !== PROD) {
+      lifecycleLogger(this.logLifecycle)(`$$$ disconnectedCallback -> ${this.nodeName}#${this._id}\n`);
     }
 
-    /**
-     * connectedCallback - description
-     *
-     * @return {type}  description
-     */
-    connectedCallback() {
-      if (ENV !== PROD) {
-        lifecycleLogger(this.logLifecycle)(`\n^^^ connectedCallback -> ${this.nodeName}#${this._id}`);
-      }
+    this._isConnected = false;
+  }
 
-      if (!this._isConnected) {
-        this._isConnected = true;
-      }
-    }
+  static uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8); // eslint-disable-line
+      return v.toString(16);
+    });
+  }
+}
 
-    /**
-     * disconnectedCallback - description
-     *
-     * @return {type}  description
-     */
-    disconnectedCallback() {
-      if (ENV !== PROD) {
-        lifecycleLogger(this.logLifecycle)(`$$$ disconnectedCallback -> ${this.nodeName}#${this._id}\n`);
-      }
-
-      this._isConnected = false;
-    }
-
-    static uuidv4() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8); // eslint-disable-line
-        return v.toString(16);
-      });
-    }
-  })))));
+export default withContext(withMonkeyPatches(withUpdate(withRender(withStyles(BaseComponent)))));
