f8ddfe4b5a2b371bfeb1fc4bbb07498aa3cb9e9a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-06-11 11:34:13 +0200

    updated icon template

--- src/components/a-icon/_template.js
+++ src/components/a-icon/_template.js
@@ -1,16 +1,35 @@
 import html from 'nanohtml';
 
+// @todo: Icon sprite build adds directory structure to fragment identifiers (app only)
+const PATH_PREFIX_APP = '#src--assets--icons--';
+const FRAGMENT_SYMBOL = '#';
+
 export default ({
   icon,
   classes = 'a-icon',
-  pathPrefix = '#src--assets--icons--',
+  pathPrefix,
 } = {}) => {
+  const axaPLibConfig = window.__axaPLibConfig || {};
+  const { iconsPath } = axaPLibConfig;
+  let path = iconsPath || PATH_PREFIX_APP;
+
   // is intended to check on just true and false! do not rewrite as truly or falsy check
-  const path = pathPrefix === true ? '#src--assets--icons--' : pathPrefix;
-  const _pathPrefix = pathPrefix === false ? '' : path;
+  if (pathPrefix === true) {
+    path = PATH_PREFIX_APP;
+  } else if (pathPrefix === false) {
+    path = '';
+  } else if (typeof pathPrefix === 'string') {
+    path = pathPrefix;
+  }
+
+  // check for hash symbol
+  if (path.substring(-1) !== FRAGMENT_SYMBOL) {
+    path = `${path}${FRAGMENT_SYMBOL}`;
+  }
+
   return html`
     <svg class="${classes}">
-      <use xlink:href="${_pathPrefix}${icon}" href="${_pathPrefix}${icon}" />
+      <use xlink:href="${path}${icon}" href="${path}${icon}" />
     </svg>
   `;
 };
