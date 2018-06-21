1705f67074171f267a9cf01e85a7703d71bfcf1e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-06-21 15:32:29 +0200

    start adding error pages

---
+++ src/components/o-error-page/_template.js
@@ -0,0 +1,33 @@
+import classnames from 'classnames';
+import html from 'nanohtml';
+
+export default ({
+  code,
+  status,
+  title,
+  message,
+  items,
+  ctaHref,
+  ctaTitle,
+}) => {
+  const hasItems = Array.isArray(items) && items.length;
+
+  return html`
+    <article class='o-error-page__container'>
+      <h4 class="o-error-page__status">${status}</h4>
+      <h1 class="o-error-page__title">${title}</h1>
+  
+      <p class="${classnames('o-error-page__message', { 'o-error-page__message--semibold': hasItems })}">${message}</p>
+      
+      ${(hasItems && html`
+        <ul class="o-error-page__list">
+          ${items.map(item => html`<li class="o-error-page__list-item">${item}</li>`)}
+        </ul>
+      `) || null}
+      
+      ${(ctaHref && ctaTitle && html`
+        <axa-button href="${ctaHref}" tag="a" color="white" ghost motion>${ctaTitle}</axa-button>
+      `) || null}
+    </article>
+  `;
+};
