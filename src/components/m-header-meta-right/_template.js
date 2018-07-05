2a3aa6274da2da0a98546846965804252b976837
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-07-05 17:52:38 +0200

    patched children of fragment for all templates

--- src/components/m-header-meta-right/_template.js
+++ src/components/m-header-meta-right/_template.js
@@ -1,10 +1,8 @@
 import html from 'nanohtml';
 
-// IE11 does not support children on fragments
-// ref: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10060579/
-export default (props, { children, childNodes }) => html`
+export default (props, { children }) => html`
   <ul class="m-header-meta-right__list">
-    ${Array.from(children || childNodes).map(child => html`
+    ${Array.from(children).map(child => html`
       <li class="m-header-meta-right__list-item">${child}</li>
     `)}
   </ul>
