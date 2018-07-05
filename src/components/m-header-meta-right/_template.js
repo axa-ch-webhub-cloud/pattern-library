df422f5ee27db3107a77d3b23ac27e1d092af316
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-07-05 17:38:17 +0200

    fixed undefined children throws by Array.from

--- src/components/m-header-meta-right/_template.js
+++ src/components/m-header-meta-right/_template.js
@@ -2,7 +2,7 @@ import html from 'nanohtml';
 
 export default (props, { children }) => html`
   <ul class="m-header-meta-right__list">
-    ${Array.from(children).map(child => html`
+    ${children && Array.from(children).map(child => html`
       <li class="m-header-meta-right__list-item">${child}</li>
     `)}
   </ul>
