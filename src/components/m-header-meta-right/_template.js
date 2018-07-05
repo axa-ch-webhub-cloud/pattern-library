4c490fa88bdc709ef3f4bed6dee00b74589812c9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-07-05 17:44:08 +0200

    fixed ie11 header meta right template

--- src/components/m-header-meta-right/_template.js
+++ src/components/m-header-meta-right/_template.js
@@ -1,8 +1,10 @@
 import html from 'nanohtml';
 
-export default (props, { children }) => html`
+// IE11 does not support children on fragments
+// ref: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10060579/
+export default (props, { children, childNodes }) => html`
   <ul class="m-header-meta-right__list">
-    ${children && Array.from(children).map(child => html`
+    ${Array.from(children || childNodes).map(child => html`
       <li class="m-header-meta-right__list-item">${child}</li>
     `)}
   </ul>
