5ea983d58ab451b9addb9271e302b4cbd5c9b874
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-07-09 18:45:55 +0200

    removed comments

--- src/js/define-once.js
+++ src/js/define-once.js
@@ -2,9 +2,6 @@
 
 export default function defineOnce(tagName, ctor) {
   if (!customElements.get(tagName)) {
-    // TODO: Remove event when issues/515 is fixed
-    // window.addEventListener('DOMContentLoaded', () => {
     customElements.define(tagName, ctor);
-    // });
   }
 }
