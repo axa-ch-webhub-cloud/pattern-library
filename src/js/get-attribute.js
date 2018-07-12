b5c9d6d1d6ce6fc541a16d394605fc68738e94b8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-07-12 14:37:56 +0200

    clarified get-attribute

--- src/js/get-attribute.js
+++ src/js/get-attribute.js
@@ -4,8 +4,8 @@ import toProp from './to-prop';
  * Get an HTML attribute's value by it's name.
  * **Note:** The attribute's value get parsed as JSON if it's valid JSON.
  *
- * @param {Element} node - The HTML element to get an attribute from.
- * @param {String} name - The attribute's name.
+ * @param {Element|Attribute} node - The HTML element to get an attribute from.
+ * @param {String} [name] - The attribute's name.
  * @returns {*} - Returns the value of the attribute - in case of valid JSON the result of `JSON.parse` else plain text.
  */
 function getAttribute(node, name) {
@@ -13,12 +13,14 @@ function getAttribute(node, name) {
     return false;
   }
 
+  // if it's an attribute node, get it's value directly
   let { value } = node;
 
+  // and resolve it's name
   if (!name) {
     // eslint-disable-next-line
-    name = node.name;
-  } else {
+    ({ name } = node);
+  } else { // fetch the correct attribute from the Element by it's name
     value = node.getAttribute(name);
   }
 
