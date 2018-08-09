d2e764a24fe0a5a6eef17ef86b07911cc37a5b39
Ben Zahler <ben.zahler@codeflair.ch>
2018-08-09 11:19:04 +0200

    AEM-3792: fix eslint

--- src/components/o-commercial-hero-cover/_template.js
+++ src/components/o-commercial-hero-cover/_template.js
@@ -1,12 +1,12 @@
 import html from 'nanohtml';
 
 export default ({
-                  src,
-                  alt = 'Commercial Hero cover',
-                  gradient = 'white',
-                  contentAlign = 'left'
+  src,
+  alt = 'Commercial Hero cover',
+  gradient = 'white',
+  contentAlign = 'left',
 }, childrenFragment) => html`
-  <header class="o-commercial-hero-cover__header ${gradient === "white" ? "gradient-white" : "gradient-black"}">
+  <header class="o-commercial-hero-cover__header ${gradient === 'white' ? 'gradient-white' : 'gradient-black'}">
     <picture class="m-picture o-commercial-hero-cover__picture">
       <img class="m-picture__image o-commercial-hero-cover__picture__image a-focal-point a-focal-point--center"
       src="${src}" alt="${alt}" data-object-fit="cover"
@@ -14,11 +14,11 @@ export default ({
     </picture>
     <axa-container class="o-commercial-hero-cover__info">
       <axa-row>
-        <axa-col size="sm-12 lg-6" order="${contentAlign === "left" ? "0" : "1"}">
-          ${childrenFragment.querySelector(".js-commercial-hero-cover__first") || childrenFragment}
+        <axa-col size="sm-12 lg-6" order="${contentAlign === 'left' ? '0' : '1'}">
+          ${childrenFragment.querySelector('.js-commercial-hero-cover__first') || childrenFragment}
         </axa-col> 
         <axa-col size="sm-12 lg-6">
-          ${childrenFragment.querySelector(".js-commercial-hero-cover__second") || ""}
+          ${childrenFragment.querySelector('.js-commercial-hero-cover__second') || ''}
         </axa-col>
       </axa-row>
     </axa-container>
