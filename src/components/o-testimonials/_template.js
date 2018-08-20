736ec2897b653f0677f5d547e873eb22fa290ffb
Josif Nakovski <>
2018-08-20 15:15:40 +0200

    AEM-3796 - Testimonials PR - data attributes removed - now will be taken from the custom element using utils. - arrow divs are now buttons - wrapped in axa container as per design.

--- src/components/o-testimonials/_template.js
+++ src/components/o-testimonials/_template.js
@@ -3,20 +3,18 @@ import html from 'nanohtml';
 export default ({
   title,
   subtitle,
-  autoRotateEnabled = true,
-  autoRotateTime = 5000,
 }, childrenFragment) => html`
-  <article class="o-testimonials__background js-o-testimonials" 
-  data-auto-rotate-enabled="${autoRotateEnabled}" 
-  data-auto-rotate-time="${autoRotateTime}">
-    ${title && html`<h1 class="o-testimonials__title">${title}</h1>`}
-    ${subtitle && html`<p class="o-testimonials__subtitle">${subtitle}</p>`}
-    <div class="o-testimonials__navigator">
-      <div class="o-testimonials__arrow-left js-o-testimonials__control-left"></div>
-      <div class="o-testimonials__content">
-        ${childrenFragment}
-      </div>
-      <div class="o-testimonials__arrow-right js-o-testimonials__control-right"></div>
-    </div>
+  <article class="o-testimonials__background js-o-testimonials">
+    <axa-container>
+        ${title && html`<h1 class="o-testimonials__title">${title}</h1>`}
+        ${subtitle && html`<p class="o-testimonials__subtitle">${subtitle}</p>`}
+        <div class="o-testimonials__navigator">
+          <button class="o-testimonials__arrow-left js-o-testimonials__control-left"></button>
+          <div class="o-testimonials__content">
+            ${childrenFragment}
+          </div>
+          <button class="o-testimonials__arrow-right js-o-testimonials__control-right"></button>
+        </div>
+    </axa-container>
   </article>
 `;
