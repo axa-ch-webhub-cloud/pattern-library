54b98245c35aa07d7b1ea5e3c989b1fb70c20731
Ben Zahler <ben.zahler@codeflair.ch>
2018-08-22 11:03:01 +0200

    AEM-3795: icons: support both img by url and svg from child element

--- src/components/m-policy-feature-item/_template.js
+++ src/components/m-policy-feature-item/_template.js
@@ -1,8 +1,9 @@
 import html from 'nanohtml';
 
-export default ({ src, alt = 'Policy feature icon', title, description }) => html`
+export default ({ src, alt = 'Policy feature icon', title, description }, childrenFragment) => html`
   <article class="m-policy-feature-item__content">
-    <img class="m-policy-feature-item__icon" src="${src}" alt="${alt}">
+    ${src && html`<img class="m-policy-feature-item__icon" src="${src}" alt="${alt}">`}
+    ${childrenFragment.querySelector('.js-m-policy-features-icon') || ''}
     ${title && html`<h1 class="m-policy-feature-item__title">${title}</h1>`}
     ${description && html`<p class="m-policy-feature-item__description">${description}</p>`}
   </article>
