60f86506892c212485e88954b6c77c2e82aa77b3
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 16:32:31 +0200

    added proptypes to header sub navigation

--- src/components/m-header-sub-navigation/index.js
+++ src/components/m-header-sub-navigation/index.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import styles from './index.scss';
 import template from './_template';
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
@@ -6,6 +8,23 @@ import defineOnce from '../../js/define-once';
 
 class AXAHeaderSubNavigation extends BaseComponentGlobal {
   static tagName = 'axa-header-sub-navigation'
+  static propTypes = {
+    flyout: PropTypes.bool,
+    indexTitle: PropTypes.string,
+    indexUrl: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      isWide: PropTypes.bool,
+      columns: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string,
+        links: PropTypes.arrayOf(PropTypes.shape({
+          url: PropTypes.string,
+          name: PropTypes.string,
+          isActive: PropTypes.bool,
+          preventDefault: PropTypes.bool,
+        })),
+      })),
+    })),
+  }
 
   static get observedAttributes() { return ['flyout', 'index-title', 'index-url', 'items']; }
 
