291f1789abd6e4fa2636c02ebef46027fcbd17d2
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 16:56:53 +0200

    proptypes to sticky

--- src/components/o-sticky/index.js
+++ src/components/o-sticky/index.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import defineOnce from '../../js/define-once';
 import stylesStickyContainer from './scss/sticky-container.scss';
@@ -9,6 +11,9 @@ import StickyContainer from './js/sticky-container';
 
 export class AXAStickyContainer extends BaseComponentGlobal {
   static tagName = 'axa-sticky-container'
+  static propTypes = {
+    debug: PropTypes.bool,
+  }
 
   static get observedAttributes() { return ['debug']; }
 
@@ -40,6 +45,9 @@ export class AXAStickyContainer extends BaseComponentGlobal {
 
 export class AXASticky extends BaseComponentGlobal {
   static tagName = 'axa-sticky'
+  static propTypes = {
+    debug: PropTypes.bool,
+  }
 
   static get observedAttributes() { return ['debug']; }
 
