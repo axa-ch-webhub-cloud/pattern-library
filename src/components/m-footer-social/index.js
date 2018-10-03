86537f47ad00a0e1f08f9143c8d2faa4a816a46d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 15:49:30 +0200

    added proptypes to footer social

--- src/components/m-footer-social/index.js
+++ src/components/m-footer-social/index.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import defineOnce from '../../js/define-once';
 import styles from './index.scss';
@@ -6,6 +8,15 @@ import template from './_template';
 
 class AXAFooterSocial extends BaseComponentGlobal {
   static tagName = 'axa-footer-social'
+  static propTypes = {
+    inline: PropTypes.bool,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      url: PropTypes.string,
+    })),
+    light: PropTypes.bool,
+    title: PropTypes.string,
+  }
 
   static get observedAttributes() { return ['inline', 'items', 'light', 'title']; }
 
