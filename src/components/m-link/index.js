d8457306bcdb8dc84037b88c0ed97548ab791790
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 16:37:41 +0200

    added proptypes to link

--- src/components/m-link/index.js
+++ src/components/m-link/index.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import styles from './index.scss';
 import template from './_template';
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
@@ -5,6 +7,18 @@ import defineOnce from '../../js/define-once';
 
 class AXALink extends BaseComponentGlobal {
   static tagName = 'axa-link'
+  static propTypes = {
+    color: PropTypes.oneOf(['red', 'white']),
+    size: PropTypes.oneOf(['']),
+    motion: PropTypes.bool,
+    arrow: PropTypes.bool,
+    href: PropTypes.string,
+    listed: PropTypes.bool,
+    icon: PropTypes.string,
+    deco: PropTypes.bool,
+    iconsPathPrefix: PropTypes.string,
+    target: PropTypes.oneOf(['_blank', '_self']),
+  }
 
   static get observedAttributes() { return ['color', 'size', 'motion', 'arrow', 'href', 'listed', 'icon', 'deco', 'icons-path-prefix', 'target']; }
 
