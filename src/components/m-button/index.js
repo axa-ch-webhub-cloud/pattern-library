8698d0e814f19da528142b80a98f487f7a0e1a43
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 14:40:15 +0200

    added proptypes to button

--- src/components/m-button/index.js
+++ src/components/m-button/index.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import styles from './index.scss';
 import template from './_template';
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
@@ -6,6 +8,19 @@ import Button from './js/button';
 
 class AXAButton extends BaseComponentGlobal {
   static tagName = 'axa-button'
+  static propTypes = {
+    arrow: PropTypes.bool,
+    classes: PropTypes.string,
+    color: PropTypes.oneOf(['red', 'white']),
+    ghost: PropTypes.bool,
+    motion: PropTypes.bool,
+    size: PropTypes.oneOf(['sm', 'lg']),
+    tag: PropTypes.oneOf(['a', 'button']),
+    href: PropTypes.string,
+    icon: PropTypes.string,
+    target: PropTypes.oneOf('_blank', '_self'),
+    disabled: PropTypes.bool,
+  }
 
   static get observedAttributes() { return ['arrow', 'classes', 'color', 'ghost', 'motion', 'size', 'tag', 'href', 'icon', 'target', 'disabled']; }
 
