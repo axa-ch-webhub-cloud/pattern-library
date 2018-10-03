fa4596b79e268667aabcb567e5ae7f0016076a71
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-03 15:33:41 +0200

    added proptypes to footer languages

--- src/components/m-footer-languages/index.js
+++ src/components/m-footer-languages/index.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import defineOnce from '../../js/define-once';
 import styles from './index.scss';
@@ -6,6 +8,17 @@ import template from './_template';
 
 class AXAFooterLanguages extends BaseComponentGlobal {
   static tagName = 'axa-footer-languages'
+  static propTypes = {
+    inline: PropTypes.bool,
+    items: PropTypes.arrayOf(PropTypes.shap({
+      name: PropTypes.string,
+      code: PropTypes.string,
+      url: PropTypes.string,
+      isActive: PropTypes.bool,
+    })),
+    short: PropTypes.bool,
+    title: PropTypes.string,
+  }
 
   static get observedAttributes() { return ['inline', 'items', 'short', 'title']; }
 
