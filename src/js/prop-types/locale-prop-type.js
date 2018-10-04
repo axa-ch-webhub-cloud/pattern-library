25a95695715333bb846c0c5ac0127e10e24a7842
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-04 15:55:13 +0200

    fixed eslint

--- src/js/prop-types/locale-prop-type.js
+++ src/js/prop-types/locale-prop-type.js
@@ -6,7 +6,7 @@ const reLocale = /^[a-z]{2,4}(-([A-Z][a-z]{3}|[0-9]{3}))?(-([A-Z]{2}|[0-9]{3}))?
 // eslint-disable-next-line consistent-return
 function localePropType(props, propName, componentName) {
   if (!reLocale.test(props[propName])) {
-    return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`, expected a valid locale.`)
+    return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`, expected a valid locale.`);
   }
 }
 
