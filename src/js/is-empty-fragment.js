f9ed11ccad93869eb779517d039ee347dd20fe8f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-08 09:14:24 +0200

    fixed is empty fragment

--- src/js/is-empty-fragment.js
+++ src/js/is-empty-fragment.js
@@ -1,3 +1,3 @@
-const isEmptyFragment = fragment => !fragment || (typeof fragment === 'object' && fragment.length === 0);
+const isEmptyFragment = fragment => !fragment || (typeof fragment === 'object' && fragment.childNodes.length === 0);
 
 export default isEmptyFragment;
