e38a2b30e42a50c3f7bc36295d3f58b7b6caaa9b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-11 11:38:05 +0200

    fixed template and empty attributes

--- src/components/o-table/_template.js
+++ src/components/o-table/_template.js
@@ -23,48 +23,48 @@ export default ({
     <table class="o-table ${classnames({ 'o-table--all': !isEmptyFragment(childrenFragment) })} ${classes}">
       ${childrenFragment}
   
-      ${caption && html`<caption class="o-table__caption ${classnames({
+      ${(caption && html`<caption class="o-table__caption ${classnames({
         [`u-align-${captionAlign}`]: captionAlign,
         [`o-table__caption--${captionSide}`]: captionSide,
-      })}">${caption}</caption>`}
+      })}">${caption}</caption>`) || ''}
       
-      ${Array.isArray(headingsRows) && html`
+      ${(Array.isArray(headingsRows) && html`
         <thead class="o-table__head" ${headingsAttrs}>
-          ${headingsRows.map(({ cells, ...rowAttrs }) => html`
+          ${(headingsRows.map(({ cells, ...rowAttrs }) => html`
             <tr class="o-table__row o-table__row--head" ${rowAttrs}>
-              ${Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
+              ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
                 <th class="o-table__cell o-table__cell--head ${classnames({
                   [`u-align-${attrs.align}`]: attrs.align,
                   [`o-table__cell--sort o-table__cell--sort-${attrs.sort}`]: attrs.sort,
                   'o-table__cell--sort-active': attrs.sortActive,
                 })}" ${attrs}>${text}</th>
-              `)}
+              `)) || ''}
             </tr>
-          `)}
+          `)) || ''}
         </thead>
-      ` || ''}
+      `) || ''}
 
-      ${Array.isArray(footersRows) && html`
+      ${(Array.isArray(footersRows) && html`
         <tfoot class="o-table__foot" ${footersAttrs}>
-          ${footersRows.map(({ cells, ...rowAttrs }) => html`
+          ${(footersRows.map(({ cells, ...rowAttrs }) => html`
             <tr class="o-table__row o-table__row--foot" ${rowAttrs}>
-              ${Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
+              ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
                 <td class="o-table__cell o-table__cell--foot ${classnames({
                   [`u-align-${attrs.align}`]: attrs.align,
                 })}" ${attrs}>${text}</td>
-              `)}
+              `)) || ''}
             </tr>
-          `)}
+          `)) || ''}
         </tfoot>
-      ` || ''}
-  
-      ${Array.isArray(itemsRows) && html`
+      `) || ''}
+
+      ${(Array.isArray(itemsRows) && html`
         <tbody class="o-table__body" ${itemsAttrs}>
-          ${itemsRows.map(({ cells, ...rowAttrs }) => html`
+          ${(itemsRows.map(({ cells, ...rowAttrs }) => html`
             <tr class="o-table__row  ${classnames({
               'o-table__row--action': rowAttrs.action,
             })}" ${rowAttrs}>
-              ${Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
+              ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
                 <td class="o-table__cell ${classnames({
                   'o-table__cell--action': attrs.action,
                   'o-table__cell--strong': attrs.strong,
@@ -72,11 +72,11 @@ export default ({
                   [`u-align-${attrs.align}`]: attrs.align,
                   [`o-table__cell--state-${attrs.state}`]: attrs.state,
                 })}" ${attrs}>${text}</td>
-              `)}
+              `)) || ''}}
             </tr>
-          `)}
+          `)) || ''}
         </tbody>
-      ` || ''}
+      `) || ''}
     </table>
   `;
 };
