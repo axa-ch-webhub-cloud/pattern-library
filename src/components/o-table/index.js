6d5cb0b888c5f489b2e6d39cb7d00f7adfdc21b0
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 13:00:00 +0200

    utilised prop type cleanup

--- src/components/o-table/index.js
+++ src/components/o-table/index.js
@@ -4,30 +4,11 @@ import BaseComponentGlobal from '../../js/abstract/base-component-global';
 import defineOnce from '../../js/define-once';
 import alignPropType from '../../js/prop-types/align-prop-type';
 import sidePropType from '../../js/prop-types/side-prop-type';
-import valuePropType from '../../js/prop-types/value-prop-type';
+import { tableHeadPropTypes, tableBodyPropTypes, tableFootPropTypes } from '../../js/prop-types/table-prop-types';
 // import the styles used for this component
 import styles from './index.scss';
 // import the template used for this component
 import template from './_template';
-
-const cellPropType = PropTypes.oneOfType([
-  PropTypes.string,
-  PropTypes.number,
-  PropTypes.shape({
-    text: valuePropType,
-    value: PropTypes.any,
-  }),
-]);
-const rowPropType = PropTypes.oneOfType([
-  PropTypes.arrayOf(cellPropType),
-  PropTypes.shape({
-    cells: PropTypes.arrayOf(cellPropType),
-  }),
-]);
-const itemsType = PropTypes.oneOfType([
-  PropTypes.arrayOf(cellPropType),
-  PropTypes.arrayOf(rowPropType),
-]);
 
 class AXATable extends BaseComponentGlobal {
   static tagName = 'axa-table'
@@ -39,9 +20,9 @@ class AXATable extends BaseComponentGlobal {
     caption: PropTypes.string,
     captionAlign: alignPropType,
     captionSide: sidePropType,
-    items: itemsType,
-    headings: itemsType,
-    footers: itemsType,
+    items: tableBodyPropTypes,
+    headings: tableHeadPropTypes,
+    footers: tableFootPropTypes,
   }
 
   // Only use this if you need to observe attributes other than your prop-types!
