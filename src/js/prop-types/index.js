531b6e459cc4dcce8c1ea2979af82f72ebdbd916
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 12:05:48 +0200

    added table scope prop type

--- src/js/prop-types/index.js
+++ src/js/prop-types/index.js
@@ -2,5 +2,6 @@ export { default as alignPropType } from './align-prop-type';
 export { default as localePropType } from './locale-prop-type';
 export { default as sidePropType } from './side-prop-type';
 export { default as statePropType } from './state-prop-type';
+export { default as tableScopePropType } from './table-scope-prop-type';
 export { default as urlPropType } from './url-prop-type';
 export { default as valuePropType } from './value-prop-type';
