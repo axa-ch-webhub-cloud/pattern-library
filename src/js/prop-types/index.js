f04d6fde68dfa2adacb67284958125c6d78899ba
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 11:06:08 +0200

    added align prop type

--- src/js/prop-types/index.js
+++ src/js/prop-types/index.js
@@ -1,3 +1,4 @@
+export { default as alignPropType } from './align-prop-type';
 export { default as localePropType } from './locale-prop-type';
 export { default as urlPropType } from './url-prop-type';
 export { default as valuePropType } from './value-prop-type';
