b1d9ebde74d13f3563de02a3be26493468341666
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 11:22:52 +0200

    added state prop type

--- src/js/prop-types/index.js
+++ src/js/prop-types/index.js
@@ -1,5 +1,6 @@
 export { default as alignPropType } from './align-prop-type';
 export { default as localePropType } from './locale-prop-type';
 export { default as sidePropType } from './side-prop-type';
+export { default as statePropType } from './state-prop-type';
 export { default as urlPropType } from './url-prop-type';
 export { default as valuePropType } from './value-prop-type';
