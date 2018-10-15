bef1d695e91588884b750a5da2b85105a54f445e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 12:59:37 +0200

    extended table prop types

--- src/js/prop-types/table-prop-types.js
+++ src/js/prop-types/table-prop-types.js
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 
+import { cellHeadPropType, cellBodyPropType, cellFootPropType } from './cell-prop-types';
+import { rowHeadPropType, rowBodyPropType, rowFootPropType } from './row-prop-types';
+
 export const tableScopePropType = PropTypes.oneOf([
   'row',
   'col',
@@ -7,3 +10,18 @@ export const tableScopePropType = PropTypes.oneOf([
   'colgroup',
   'auto',
 ]);
+
+export const tableHeadPropTypes = PropTypes.oneOfType(
+  PropTypes.arrayOf(cellHeadPropType),
+  PropTypes.arrayOf(rowHeadPropType),
+);
+
+export const tableBodyPropTypes = PropTypes.oneOfType(
+  PropTypes.arrayOf(cellBodyPropType),
+  PropTypes.arrayOf(rowBodyPropType),
+);
+
+export const tableFootPropTypes = PropTypes.oneOfType(
+  PropTypes.arrayOf(cellFootPropType),
+  PropTypes.arrayOf(rowFootPropType),
+);
