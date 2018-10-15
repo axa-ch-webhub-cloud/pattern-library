c1046af3ffbffea46c89e781e0be4b6b558a1879
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-15 12:58:48 +0200

    fixed wrong import

--- src/js/prop-types/index.js
+++ src/js/prop-types/index.js
@@ -5,6 +5,6 @@ export { default as localePropType } from './locale-prop-type';
 export { default as sidePropType } from './side-prop-type';
 export { default as sortPropType } from './sort-prop-type';
 export { default as statePropType } from './state-prop-type';
-export * from './table-prop-type';
+export * from './table-prop-types';
 export { default as urlPropType } from './url-prop-type';
 export { default as valuePropType } from './value-prop-type';
