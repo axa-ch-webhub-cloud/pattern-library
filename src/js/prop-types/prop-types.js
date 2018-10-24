9621c5a555d59cd42ab0ff04716ce8974a780f01
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-24 10:37:10 +0200

    fixed misisng is-required shim

--- src/js/prop-types/prop-types.js
+++ src/js/prop-types/prop-types.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
+// @todo remove as soon as https://github.com/facebook/prop-types/issues/231 is resolved
 function getShim(propType) {
   function shim(...args) {
     return propType(...args);
   }
 
+  // make sure to also shim `isRequired`, etc.
   Object.keys(propType).forEach((key) => {
-    shim[key] = propType[key];
+    shim[key] = getShim(propType[key]);
   });
 
   return shim;
