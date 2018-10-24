80966b32860634851b08b2da06bbeed140c37e98
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-10-24 11:04:21 +0200

    should max call stack errors

--- src/js/prop-types/prop-types.js
+++ src/js/prop-types/prop-types.js
@@ -1,26 +1,21 @@
 import PropTypes from 'prop-types';
 
 // @todo remove as soon as https://github.com/facebook/prop-types/issues/231 is resolved
-function getShim(propType) {
+function getShim(propType, deeper = true) {
   function shim(...args) {
     return propType(...args);
   }
 
-  // guard cyclic refs causing max call stack errors
-  shim.__isShimmed = true;
-
-  // make sure to also shim `isRequired`, etc.
-  Object.keys(propType).reduce(shimKeys, propType);
+  if (deeper) {
+    // make sure to also shim `isRequired`, etc.
+    Object.keys(propType).reduce(shimKeys, propType);
+  }
 
   return shim;
 }
 
 function shimKeys(propTypes, key) {
-  const propType = propTypes[key];
-
-  if (!propType.__isShimmed) {
-    propTypes[key] = getShim(propType);
-  }
+  propTypes[key] = getShim(propTypes[key], false);
 
   return propTypes;
 }
