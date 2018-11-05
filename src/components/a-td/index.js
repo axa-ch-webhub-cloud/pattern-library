07e23c47d55b1f912ac6628778aeea6cf1904ef9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-05 14:04:09 +0100

    should implement axa-td

--- src/components/a-td/index.js
+++ src/components/a-td/index.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import { withAllHocs, withBase } from '../../js/abstract/hocs';
 import defineOnce from '../../js/define-once';
+import alignPropType from '../../js/prop-types/align-prop-type';
+import floatPropType from '../../js/prop-types/float-prop-type';
+import statePropType from '../../js/prop-types/state-prop-type';
 // import the styles used for this component
 import styles from './index.scss';
 // import the template used for this component
@@ -17,20 +21,31 @@ class AXATd extends TableCellBase {
   // this will also derived your needed observed attributes automatically for you
   static propTypes = {
     classes: PropTypes.string,
+    action: PropTypes.bool,
+    align: alignPropType,
+    bold: PropTypes.bool,
+    dense: PropTypes.bool,
+    float: floatPropType,
+    state: statePropType,
+    strong: PropTypes.bool,
   }
 
   init() {
     super.init({ styles });
-
-    this.logLifecycle = true;
   }
 
   willRenderCallback() {
-    this.className = 'a-td';
-  }
+    const { classes, action, align, bold, dense, float, state, strong } = this._props;
 
-  didRenderCallback() {
-    this.innerHTML = 'Rendered Built in Table Cell';
+    this.className = classnames('a-td', classes, {
+      'a-td--action': action,
+      'a-td--strong': strong,
+      'a-td--bold': bold,
+      'a-td--dense': dense,
+      [`u-align-${align}`]: align,
+      [`a-td--${float}`]: float,
+      [`a-td--state-${state}`]: state,
+    });
   }
 }
 
