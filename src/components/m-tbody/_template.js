4dad9c9e7d70cf30c4ea4548b3d9b07d45720ca8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-06 13:26:02 +0100

    start adding mtbody

--- src/components/m-tbody/_template.js
+++ src/components/m-tbody/_template.js
@@ -1,5 +1,13 @@
 import html from 'nanohtml';
 
-export default ({ classes }) => html`
-  <article class=${classes}>Ready to start</article>
-`;
+import expandData from '../o-table/js/expand-data';
+
+export default ({ items }, fragmentChildren) => Array.isArray(items) && items.length ?
+  expandData(items).rows.map(({ cells, ...rowAttrs }) => html`
+    <tr is="axa-tr" ${rowAttrs}>
+      ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
+        <td is="axa-td" ${attrs}>${text}</td>
+      `)) || ''}
+    </tr>
+  `)
+  : fragmentChildren;
