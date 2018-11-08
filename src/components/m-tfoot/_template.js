5d72e73d9c4c3e7f35fe9bdb43c55e75656ccc7c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-08 10:55:06 +0100

    start adding tfoot

--- src/components/m-tfoot/_template.js
+++ src/components/m-tfoot/_template.js
@@ -1,5 +1,21 @@
 import html from 'nanohtml';
 
-export default ({ classes }) => html`
-  <article class=${classes}>Ready to start</article>
-`;
+import expandData from '../o-table/js/expand-data';
+
+export default ({ items }, fragmentChildren) => {
+  let rows;
+
+  if (items) {
+    ({ rows } = expandData(items));
+  }
+
+  return Array.isArray(rows) && rows.length ?
+    rows.map(({ cells, ...rowAttrs }) => html`
+      <tr is="axa-tr" head ${rowAttrs}>
+        ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
+          <td is="axa-td" ${attrs}>${text}</td>
+        `)) || ''}
+      </tr>
+    `)
+    : fragmentChildren;
+};
--- src/components/m-tfoot/index.js
+++ src/components/m-tfoot/index.js
@@ -1,75 +1,38 @@
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-import BaseComponentGlobal from '../../js/abstract/base-component-global';
+import { withBase, withAllHocs } from '../../js/abstract/hocs';
 import defineOnce from '../../js/define-once';
 // import the styles used for this component
 import styles from './index.scss';
 // import the template used for this component
 import template from './_template';
+import { tableFootPropTypes } from '../../js/prop-types';
 
-class AXATfoot extends BaseComponentGlobal {
+const HTMLTableSectionElementBase = withAllHocs(withBase(HTMLTableSectionElement));
+
+class AXATFoot extends HTMLTableSectionElementBase {
   static tagName = 'axa-tfoot'
+  static buildinTagName = 'tfoot'
 
   // specify runtime type-checking here, if you use custom attributes
   // this will also derived your needed observed attributes automatically for you
   static propTypes = {
     classes: PropTypes.string,
+    items: tableFootPropTypes,
   }
 
-  // Only use this if you need to observe attributes other than your prop-types!
-  // Specify observed attributes so that attributeChangedCallback will work,
-  // this is essential for external re-rendering trigger.
-  // static get observedAttributes() {
-  //  return ['classes'];
-  // }
-
-  constructor() {
-    super({ styles, template });
-
-    // does this provide context (See docs for context) ?
-    // this.provideContext()
-
-    // or do you want to consume a specific context
-    // this.consumeContext('axa-context-provider');
+  init() {
+    super.init({ styles, template });
   }
 
-  /**
-   * REF: https://www.w3.org/TR/custom-elements/#custom-element-conformance
-   */
-  connectedCallback() {
-    super.connectedCallback();
+  willRenderCallback() {
+    const { classes } = this.props;
 
-    this.className = `${this.initialClassName} m-tfoot`;
-    // Your DOM interaction here, but keep it decoupled.
-    // If you don't have any, just remove this function
+    this.className = classnames('m-tfoot', classes);
   }
-
-  // You have some special logic? Or need to update the web-components DOM node itself?
-  // Then don't forget to make sure that incremental rendering works properly.
-  // attributeChangedCallback(name, oldValue, newValue) {
-  //   super.attributeChangedCallback(name, oldValue, newValue);
-  // }
-
-  // You may want to update stuff before rendering.
-  // willRenderCallback(initial) {
-  // }
-
-  // You may want to update staff after rendering
-  // didRenderCallback(initial) {
-  // }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-
-    // Don't forget to cleanup :)
-  }
-
-  // Do you consume context?
-  // contextCallback(contextNode) {
-  //   contextNode is now available.
-  // }
 }
 
-defineOnce(AXATfoot.tagName, AXATfoot);
+defineOnce(AXATFoot.tagName, AXATFoot, { extends: AXATFoot.buildinTagName });
 
-export default AXATfoot;
+export default AXATFoot;
