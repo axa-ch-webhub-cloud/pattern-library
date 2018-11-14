a12e003d39d5626c7f1fce307179098ea5247be8
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-14 15:24:56 +0100

    refactored base component global

--- src/js/abstract/base-component-global.js
+++ src/js/abstract/base-component-global.js
@@ -1,3 +1,12 @@
-import { withAllHocs, withBase, withBaseGlobal } from './hocs';
+import BaseComponent from './base-component';
 
-export default withAllHocs(withBaseGlobal(withBase(HTMLElement)));
+import withBaseGlobal from './hocs/with-base-global';
+
+/**
+ * Base class {BaseComponentGlobal}. This class extends the {BaseComponent} and
+ * applies threat the component as a global element. the use is not recommended but
+ * in some occasion it can make sense. Typical use case is if a component
+ * is used more than once and has lots of css.
+ * The style will be included only once in the DOM and is insert in the head of the main document.
+ */
+export default withBaseGlobal(BaseComponent);
--- src/js/abstract/hocs/with-base-global.js
+++ src/js/abstract/hocs/with-base-global.js
@@ -2,7 +2,7 @@ const memory = {};
 
 const withBaseGlobal = Base =>
   /**
-   * Base class {BaseComponentGlobal}. This class extends the {BaseComponent} and
+   * Base class {WithBaseGlobal}. This class extends the {BaseComponent} and
    * applies threat the component as a global element. the use is not recommended but
    * in some occasion it can make sense. Typical use case is if a component
    * is used more than once and has lots of css.
