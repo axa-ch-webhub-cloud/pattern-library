20e97a5d03995ceec349f39b3c7e0c4ca8b7d6f4
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-20 17:08:49 +0100

    fixed no constructor anymore

--- src/components/o-pagination/index.js
+++ src/components/o-pagination/index.js
@@ -29,8 +29,8 @@ class AXAPagination extends BaseComponentGlobal {
   //  return ['classes'];
   // }
 
-  constructor() {
-    super({ styles, template });
+  init() {
+    super.init({ styles, template });
 
     // does this provide context (See docs for context) ?
     // this.provideContext()
