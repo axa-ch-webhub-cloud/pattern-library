e81cff8a4ac0b2f4868a0d2d997e40739c56c22b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-20 10:04:19 +0100

    should be foot boolean

--- src/components/m-tfoot/_template.js
+++ src/components/m-tfoot/_template.js
@@ -11,7 +11,7 @@ export default ({ items }, fragmentChildren) => {
 
   return Array.isArray(rows) && rows.length ?
     rows.map(({ cells, ...rowAttrs }) => html`
-      <tr is="axa-tr" head ${rowAttrs}>
+      <tr is="axa-tr" foot ${rowAttrs}>
         ${(Array.isArray(cells) && cells.map(({ text, ...attrs }) => html`
           <td is="axa-td" ${attrs}>${text}</td>
         `)) || ''}
