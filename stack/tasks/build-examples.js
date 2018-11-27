61bb2e80fca8ed0b09f1f095642412a57118fcf0
Marc Illien <marc.illien@siroop.ch>
2018-11-27 17:49:57 +0100

    Improve build-examples
    - Clean it up
    - Faster -> Improved and removed  and combine - loops

--- stack/tasks/build-examples.js
+++ stack/tasks/build-examples.js
@@ -1,16 +1,17 @@
 const dir = require('node-dir');
 const fs = require('fs');
 const { adaptSlashes } = require('../utils/adaptslashes.js');
+const { capitalize } = require('../utils/capitalize.js');
 const constants = require('../constants');
 const nsh = require('node-syntaxhighlighter');
 const sass = require('node-sass');
 const autoprefixer = require('autoprefixer');
 const { lstatSync, readdirSync } = require('fs')
-const { join } = require('path')
+const path = require('path')
 
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = source =>
-  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+  readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
 
 // const jsLang = nsh.getLanguage('js');
 const htmlLang = nsh.getLanguage('html');
@@ -29,8 +30,9 @@ const SINGLE = 'single-component';
 
 const filePath = `${CWD}/${ENV === constants.ENV.PROD ? 'dist' : '.tmp'}`;
 
-const reGetExamples = /\/components\/[^/]+\/_example\.html$/;
-const reGetPreviews = /\/components\/[^/]+\/_preview\.html$/;
+// const reGetExamples = /\/components\/[^/]+\/_example\.html$/;
+// const reGetPreviews = /\/components\/[^/]+\/_preview\.html$/;
+// const reGetJavascript = /\/components\/[^/]+\/index\.js$/;
 const reGetDemosHtml = /\/src\/[^/]+\/demo\.react\.html$/;
 
 // @TODO: import order seems to be crucial for chrome, whats going on here??
@@ -65,11 +67,9 @@ const pageHtml = {
 
 let scripts = [];
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-
-const createAmoPage = (indexHtml, scripts, styles, typeAmo, filePath) => {
+const createAmoPage = (indexHtml, scriptTags, styles, typeAmo, filePath) => {
   const resultAtoms = indexHtml
-    .replace(/<!-- {CUT AND INJECT IMPORTS HERE} -->/g, scripts.join('\n'))
+    .replace(/<!-- {CUT AND INJECT IMPORTS HERE} -->/g, scriptTags.join('\n'))
     .replace(new RegExp(`<!-- {CUT AND INJECT ACTIVE ${typeAmo.toUpperCase()}} -->`, 'g'), ', "isActive": true')
 
     .replace(new RegExp('<!-- {CUT AND INJECT ATOMS BUTTONS} -->', 'g'), buttonsHtml[ATOMS].join('\n'))
@@ -92,18 +92,22 @@ const createAmoPage = (indexHtml, scripts, styles, typeAmo, filePath) => {
 };
 
 // Basically to test/develop a component on a single page encapsulated from others,to prevent side effects
-const createSingleComponentPage = (template, componentName) => {
-  const out = template;
-  fs.writeFileSync(`${filePath}/${componentName}.html`, out);
+const createSingleComponentPage = (filePath, template, component, scriptTags) => {
+  let out = template;
+  out = out
+    .replace(new RegExp('<!-- {CUT AND INJECT SINGLECOMPONENT HTML HERE} -->', 'g'), component.pageHtml)
+    .replace(/<!-- {CUT AND INJECT IMPORTS HERE} -->/g, scriptTags.join('\n'))
+  fs.writeFileSync(`${filePath}`, out);
 };
 
-const createAmoComponent = (amoType, previewName, atomicName, preview, resultCssString, example, scriptTag) => {
+const createAmoComponent = (amoType, previewName, atomicName, preview, resultCssString, example, scriptTag, showStyles = true, showButtons = true) => {
   let component = {
     type: amoType,
     buttonsHtml: '',
     mobileButtonsHtml: '',
     pageHtml: '',
-    scriptTag: ''
+    scriptTag: '',
+    scriptFile: 'index.js' 
   }
   component.scriptTag = scriptTag;
   component.buttonsHtml = `
@@ -129,40 +133,57 @@ const createAmoComponent = (amoType, previewName, atomicName, preview, resultCss
         <article class="js--section-source o-sg-section__section o-sg-section__section--source">
           ${preview}
         </article>
+        ${showButtons ? `
         <section class="o-sg-section__section o-sg-section__section--buttons">
           <axa-button ghost color="red" motion classes="js--toggle o-sg-section__button o-sg-section__button--selected" data-toggle="source">Close</axa-button>
           <axa-button ghost color="white" motion classes="js--toggle o-sg-section__button o-sg-section__button--selected" data-toggle="wc-html">Webcomponent HTML</axa-button>
           <axa-button ghost color="white" motion classes="js--toggle o-sg-section__button o-sg-section__button--selected" data-toggle="html">Traditional HTML</axa-button>
           <axa-button ghost color="white" motion classes="js--toggle o-sg-section__button o-sg-section__button--selected" data-toggle="css">CSS</axa-button>
         </section>
+        ` : ``}
+        ${showStyles ? `
         <pre class="js--section-wc-html o-sg-section__section o-sg-section__section--src">${nsh.highlight(preview, htmlLang)}</pre>
         <pre class="js--section-html o-sg-section__section o-sg-section__section--src">${nsh.highlight(example, htmlLang)}</pre>
         <pre class="js--section-css o-sg-section__section o-sg-section__section--src">${resultCssString ? nsh.highlight(resultCssString.toString(), cssLang) : ''}</pre>
+        ` : ``}
       </article>
     `;
 
     return component;
 };
 
-dir.files(`${CWD}/src/demos`, (err, allFiles) => {
-  allFiles = allFiles.map(adaptSlashes); // eslint-disable-line no-param-reassign
-  const demoHtmls = allFiles.filter(_file => _file.match(reGetDemosHtml));
-  demoHtmls.forEach((_file) => {
-    pageHtml[REACT].push(`${fs.readFileSync(_file, 'utf8')}<script src="./app/all-demos.js"></script>`);
-  });
-});
-
 // let foo = getDirectories(`${CWD}/src/components`);
 // console.log('foo dirs', foo);
+// dir.files(`${CWD}/src/demos`, (err, allFiles) => {
+//   allFiles = allFiles.map(adaptSlashes); // eslint-disable-line no-param-reassign
+//   const demoHtmls = allFiles.filter(_file => _file.match(reGetDemosHtml));
+//   demoHtmls.forEach((_file) => {
+//     pageHtml[REACT].push(`${fs.readFileSync(_file, 'utf8')}<script src="./app/all-demos.js"></script>`);
+//   });
+// });
 
-dir.files(`${CWD}/src/components`, (err, _allFiles) => {
+dir.files(`${CWD}/src/components`, (err, allFiles) => {
   if (err) throw err;
 
   let previewHtmls = [];
   let exampleHtmls = [];
-  const allFiles = _allFiles.map(adaptSlashes);
-  previewHtmls = allFiles.filter(_file => _file.match(reGetPreviews)).sort(sortUp);
-  exampleHtmls = allFiles.filter(_file => _file.match(reGetExamples)).sort(sortUp);
+  let filesJavascript = [];
+
+  allFiles.forEach(_file => {
+    if (_file.match(/\/components\/[^/]+\/_preview\.html$/)) {
+      previewHtmls.push(_file);
+    }
+    if (_file.match(/\/components\/[^/]+\/_example\.html$/)) {
+      exampleHtmls.push(_file);
+    }
+    if (_file.match(/\/components\/[^/]+\/index\.js$/)) {
+      filesJavascript.push(_file);
+    }
+  });
+  
+  previewHtmls.sort(sortUp);
+  exampleHtmls.sort(sortUp);
+  filesJavascript.sort(sortUp);
 
   if (exampleHtmls.length !== previewHtmls.length || !previewHtmls.length || !exampleHtmls.length) {
     throw new Error('Every component must contain a _preview and a _example html');
@@ -171,37 +192,45 @@ dir.files(`${CWD}/src/components`, (err, _allFiles) => {
   const indexHtml = fs.readFileSync('./src/index.html', 'utf8');
   const indexTitle = fs.readFileSync('./src/partials/title.html', 'utf8');
   const singleHtml = fs.readFileSync('./src/single-component.html', 'utf8');
+
   const stylesPath = highlightStyles.filter(style => style.name === 'midnight')[0].sourcePath;
   const styles = fs.readFileSync(stylesPath, 'utf8');
+  
+  const scriptTags = filesJavascript.map((item) => {
+    let distPath = path.relative( path.resolve(process.cwd(), 'src'), item);
+    return `<script src="/${distPath}"></script>`;
+  });
 
   // Generate Component Packages / Previews
   previewHtmls.forEach((previewHtmlPath, index) => {
     const exampleHtmlPath = exampleHtmls[index];
-    let component = generateComponent(previewHtmlPath, exampleHtmlPath);
+    let component = generateComponent(previewHtmlPath, exampleHtmlPath, false, false);
+    const componentPreviewIndexPath = previewHtmlPath.replace(adaptSlashes(`${CWD}/src/`), '')
+    .replace('_preview.html', 'index.html');
+    const componentIndexDestinationPath = `${filePath}/${componentPreviewIndexPath}`;
     registerComponent(component);
+    createSingleComponentPage(componentIndexDestinationPath, singleHtml, component, scriptTags);
   });
 
-  createAmoPage(indexHtml, scripts, styles, ORGANISMS, filePath);
-  createAmoPage(indexHtml, scripts, styles, MOLECULES, filePath);
-  createAmoPage(indexHtml, scripts, styles, ATOMS, filePath);
-  createAmoPage(indexHtml, scripts, styles, REACT, filePath);
+  //Generate Subpages of component types
+  createAmoPage(indexHtml, scriptTags, styles, ORGANISMS, filePath);
+  createAmoPage(indexHtml, scriptTags, styles, MOLECULES, filePath);
+  createAmoPage(indexHtml, scriptTags, styles, ATOMS, filePath);
+  createAmoPage(indexHtml, scriptTags, styles, REACT, filePath);
 
   const _indexHtml = indexHtml
     .replace(/<!-- {CUT AND INJECT INDEX HTML HERE} -->/g, indexTitle);
 
-  createAmoPage(_indexHtml, scripts, styles, INDEX, filePath);
-  createSingleComponentPage(singleHtml, 'single');
-
+  createAmoPage(_indexHtml, scriptTags, styles, INDEX, filePath);
 });
 
-const generateComponent = (componentPreviewPath, componentExamplePath) => {
-    const partial = componentPreviewPath.replace(adaptSlashes(`${CWD}/src/`), '')
-      .replace('_preview.html', 'index.js');
-    const scriptTag = `<script src="${partial}"></script>`;
-
+const generateComponent = (componentPreviewPath, componentExamplePath, showStyles, showButtons) => {
+    const componentDirectory = componentPreviewPath.replace(adaptSlashes(`${CWD}/src/`), '')
+      .replace('_preview.html', '');
+    const componentDirectoryPath = path.join(componentDirectory); // .tmp/comp
+    const scriptTag = `<script src="../../${componentDirectoryPath}index.js"></script>`;
     const name = componentPreviewPath.split('/').slice(-2).join('/');
     const previewName = name.replace('/_preview.html', '');
-
     const previewHtml = fs.readFileSync(componentPreviewPath, 'utf8');
     const exampleHtml = fs.readFileSync(componentExamplePath, 'utf8');
 
@@ -222,13 +251,13 @@ const generateComponent = (componentPreviewPath, componentExamplePath) => {
     let component = {}
     switch (previewName.substring(0, 2)) {
       case 'a-':
-        component = createAmoComponent(ATOMS, previewName, 'Atom', previewHtml, resultCssString, exampleHtml, scriptTag);
+        component = createAmoComponent(ATOMS, previewName, 'Atom', previewHtml, resultCssString, exampleHtml, scriptTag, showStyles, showButtons);
         break;
       case 'm-':
-        component = createAmoComponent(MOLECULES, previewName, 'Molecule', previewHtml, resultCssString, exampleHtml, scriptTag);
+        component = createAmoComponent(MOLECULES, previewName, 'Molecule', previewHtml, resultCssString, exampleHtml, scriptTag, showStyles, showButtons);
         break;
       case 'o-':
-        component = createAmoComponent(ORGANISMS, previewName, 'Organism', previewHtml, resultCssString, exampleHtml, scriptTag);
+        component = createAmoComponent(ORGANISMS, previewName, 'Organism', previewHtml, resultCssString, exampleHtml, scriptTag, showStyles, showButtons);
         break;
       default:
         throw new Error('Could not determine component type');
@@ -241,5 +270,4 @@ const registerComponent = (component) => {
   buttonsHtml[component.type].push(component.buttonsHtml);
   mobileButtonsHtml[component.type].push(component.mobileButtonsHtml);
   pageHtml[component.type].push(component.pageHtml);
-  scripts.push(component.scriptTag)
 };
