2a510168dfee230eb7c939967510af2dda344b63
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-12-03 17:51:20 +0100

    fix(built-in): added missing polyfills for IE11

--- src/app/es6-polyfills.js
+++ src/app/es6-polyfills.js
@@ -4,6 +4,11 @@ import 'core-js/es6/reflect';
 import 'core-js/es6/promise';
 // IE11 does not support Array.from
 import 'core-js/fn/array/from';
+// IE11 does not support Object.* which is needed for built-in element polyfill
+import 'core-js/fn/object/assign';
+import 'core-js/fn/object/create';
+import 'core-js/fn/object/define-properties';
+import 'core-js/fn/object/set-prototype-of';
 import 'innersvg-polyfill/innersvg';
 // Needed for url-prop-type check
 import 'url-polyfill';
