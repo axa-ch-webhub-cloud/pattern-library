3a3895d8c6d6a3bad99361bc46c0258330a1bc6c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-12-12 10:06:20 +0100

    added working dynamic table example

--- src/demos/todomvc/todos-table.jsx
+++ src/demos/todomvc/todos-table.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 import withReact from '../../js/with-react';
 import AXAContainer from '../../components/o-container';
 import AXATable from '../../components/o-table';
+import AXACaption from '../../components/a-caption';
+import AXATHead from '../../components/m-thead';
+import AXATBody from '../../components/m-tbody';
+import AXATr from '../../components/m-tr';
+import AXATh from '../../components/a-th';
+import AXATd from '../../components/a-td';
 
 const AXAContainerReact = withReact(AXAContainer);
 const AXATableReact = withReact(AXATable);
+const AXACaptionReact = withReact(AXACaption);
+const AXATHeadReact = withReact(AXATHead);
+const AXATBodyReact = withReact(AXATBody);
+const AXATrReact = withReact(AXATr);
+const AXAThReact = withReact(AXATh);
+const AXATdReact = withReact(AXATd);
 
-const headings = JSON.stringify(['Completed', 'Title']);
+const headings = ['Completed', 'Title'];
+const headingsJson = JSON.stringify(headings);
 const toRow = ({ id, title, completed }) => ({
   id,
   cells: [
@@ -29,7 +42,29 @@ const TodoTable = ({
   shownTodos,
 }) => Array.isArray(shownTodos) && shownTodos.length ? (
   <AXAContainerReact>
-    <AXATableReact headings={headings} items={toItems(shownTodos)} cap={cap} capSide={capSide} capAlign={capAlign} />
+    <AXATableReact headings={headingsJson} items={toItems(shownTodos)} cap={`${cap} (broken)`} capSide={capSide} capAlign={capAlign} />
+
+    <AXATableReact>
+      <AXACaptionReact side={capSide} align={capAlign}>{cap} (working)</AXACaptionReact>
+
+      <AXATHeadReact>
+        <AXATrReact>
+          {Array.isArray(headings) && headings.map((head, index) => (
+            <AXAThReact key={index}>{head}</AXAThReact>
+          ))}
+        </AXATrReact>
+      </AXATHeadReact>
+
+      <AXATBodyReact>
+        {Array.isArray(shownTodos) && shownTodos.map(toRow).map(({ id: rowId, cells }) => (
+          <AXATrReact key={rowId}>
+            {Array.isArray(cells) && cells.map(({ id: cellId, text }) => (
+              <AXATdReact key={cellId}>{text}</AXATdReact>
+            ))}
+          </AXATrReact>
+        ))}
+      </AXATBodyReact>
+    </AXATableReact>
   </AXAContainerReact>
 ) : null;
 
