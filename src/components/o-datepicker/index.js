4d2c5665af10becf17c2eb77acbcf04656839ee1
Marc Illien <marc.illien@siroop.ch>
2019-01-09 18:11:28 +0100

    Fix lint

--- src/components/o-datepicker/index.js
+++ src/components/o-datepicker/index.js
@@ -110,23 +110,20 @@ class AXADatepicker extends BaseComponentGlobal {
   // Respects current timezone of the date object we convert to "iso like format"
   toLocalISOString = (date) => {
     // ISO 8601
-    var d = date
-      , pad = function (n){return n<10 ? '0'+n : n}
-      , tz = d.getTimezoneOffset() //mins
-      , tzs = (tz>0?"-":"+") + pad(parseInt(tz/60))
-    
-    if (tz%60 != 0)
-      tzs += pad(tz%60)
-    
-    if (tz === 0) // Zulu time == UTC
-      tzs = 'Z'
-      
-     return d.getFullYear()+'-'
-          + pad(d.getMonth()+1)+'-'
-          + pad(d.getDate())+'T'
-          + pad(d.getHours())+':'
-          + pad(d.getMinutes())+':'
-          + pad(d.getSeconds()) + tzs
+    const d = date;
+    const pad = n => n < 10 ? `0${n}` : n;
+    const tz = d.getTimezoneOffset(); // mins
+    let tzs = (tz > 0 ? '-' : '+') + pad(parseInt(tz / 60, 10));
+
+    if (tz % 60 !== 0) { tzs += pad(tz % 60); }
+    if (tz === 0) { tzs = 'Z'; }
+
+    return `${d.getFullYear()}-${
+      pad(d.getMonth() + 1)}-${
+      pad(d.getDate())}T${
+      pad(d.getHours())}:${
+      pad(d.getMinutes())}:${
+      pad(d.getSeconds())}${tzs}`;
   }
 
   closeDatepicker() {
