da6f671df69a2371a115adcb9ce72abd6437538b
Marc Illien <marc.illien@siroop.ch>
2019-01-15 16:00:54 +0100

    Add default for items in dropdowns

--- src/components/m-dropdown/_template.js
+++ src/components/m-dropdown/_template.js
@@ -4,20 +4,22 @@ import classnames from 'classnames';
 
 const arrowIcon = '<axa-icon icon="angle-bracket-down" classes="m-dropdown__icon"></axa-icon>';
 
-const nativeSelect = ({ title, items, size }) => [
+const nativeSelect = ({ title, items = [], size }) => [
   html`
   <div class="${classnames('m-dropdown__select-wrap', { [`m-dropdown__select-wrap--${size}`]: size })}" tabindex="0">
     <select class="${classnames('m-dropdown__select', 'js-dropdown__native-select', {
       [`m-dropdown__select--${size}`]: size,
     })}">
     ${title && html`<option value="" disabled hidden selected class="m-dropdown__select-option--hidden" >${title}</option>`}
-    ${Array.isArray(items) && items.map(({ name, value, url, isSelected }) => html`<option value="${value}" data-url="${url}" ${isSelected ? 'selected' : ''}>${name}</option>`)}
+    ${Array.isArray(items) && items.map(({ name, value, url, isSelected }) =>
+       html`<option value="${value}"${url ? ` data-url="${url}"` : ''}${isSelected ? ' selected' : ''}>${name}</option>`)
+    }
     </select>
     <div class="${classnames('m-dropdown__select-icon', { [`m-dropdown__select-icon--${size}`]: size })}">${raw(arrowIcon)}</div>
   </div>`,
 ];
 
-const enhancedSelect = ({ title, items, size }) => [
+const enhancedSelect = ({ title, items = [], size }) => [
   html`
   <button type="button" class="${classnames('m-dropdown__toggle js-dropdown__toggle', { [`m-dropdown__toggle--${size}`]: size })}">
     ${title}${raw(arrowIcon)}
