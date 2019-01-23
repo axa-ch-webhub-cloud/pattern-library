5cac7f430cd4337fcab7a61f210925b921cc13de
Marc Illien <marc.illien@siroop.ch>
2019-01-23 12:59:04 +0100

    Use data attributes

--- src/components/o-card/_template.js
+++ src/components/o-card/_template.js
@@ -4,11 +4,11 @@ export default (props, childrenFragment) => html`
   <article class="o-card__wrap">
     
     <div class="o-card__side o-card__side--1" name="side-1">
-      ${childrenFragment.querySelector('[slot="side-1"]')}
+      ${childrenFragment.querySelector('[data-slot="side-1"]')}
     </div>
 
     <div class="o-card__side o-card__side--2" name="side-2">
-      ${childrenFragment.querySelector('[slot="side-2"]')}
+      ${childrenFragment.querySelector('[data-slot="side-2"]')}
     </div>
   </div>
 `;
