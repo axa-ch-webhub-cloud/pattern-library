328c07bad743f33aa79153bb76d6d7dca481a967
Luca Mele <luca.mele@axa-winterthur.ch>
2019-01-23 13:30:22 +0100

    fix closing tag

--- src/components/o-card/_template.js
+++ src/components/o-card/_template.js
@@ -2,7 +2,7 @@ import html from 'nanohtml';
 
 export default (props, childrenFragment) => html`
   <article class="o-card__wrap">
-    
+
     <div class="o-card__side o-card__side--1">
       ${childrenFragment.querySelector('[data-slot="side-1"]')}
     </div>
@@ -10,5 +10,5 @@ export default (props, childrenFragment) => html`
     <div class="o-card__side o-card__side--2">
       ${childrenFragment.querySelector('[data-slot="side-2"]')}
     </div>
-  </div>
+  </article>
 `;
