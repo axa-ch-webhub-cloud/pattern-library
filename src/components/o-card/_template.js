3d230e67e5690802f4d73a7f062e42950f052cb3
Marc Illien <marc.illien@siroop.ch>
2019-01-23 11:20:36 +0100

    Add o-card

---
+++ src/components/o-card/_template.js
@@ -0,0 +1,14 @@
+import html from 'nanohtml';
+
+export default (props, childrenFragment) => html`
+  <article class="o-card__wrap">
+    
+    <div class="o-card__side o-card__side--1" name="side-1">
+      ${childrenFragment.querySelector('[slot="side-1"]')}
+    </div>
+
+    <div class="o-card__side o-card__side--2" name="side-2">
+      ${childrenFragment.querySelector('[slot="side-2"]')}
+    </div>
+  </div>
+`;
