9616941aaffc8991d0fbddef2c8dc90fd72be588
Luca Mele <luca.mele@axa-winterthur.ch>
2019-02-19 14:14:33 +0100

    Add example button story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
 import { configure } from '@storybook/html';
 
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+  requireAll(require.context('..', true, /_demo/));
 }
 
 configure(loadStories, module);
--- stories/index.js
+++
@@ -1,5 +0,0 @@
-/* global document */
-import { storiesOf } from '@storybook/html';
-import '../src/components/molecules/button';
-
-storiesOf('Button', module).add('button', () => '<axa-button motion>Im a button</axa-button>');
