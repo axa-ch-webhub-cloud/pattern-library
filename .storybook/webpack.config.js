3df5b329a47f09cc8b016e9eb50b57a776d5b086
Luca Mele <luca.mele@axa-winterthur.ch>
2019-02-19 18:20:37 +0100

    Add IE11 compatibility

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const fs = require('fs');
+const babelRc = fs.readFileSync(path.resolve(`${__dirname}/.babelrc`));
 
 module.exports = {
+  devtool: 'none',
   module: {
     rules: [
       {
         test: /\.scss$/,
         loaders: ['to-string-loader', 'css-loader', 'sass-loader'],
         include: path.resolve(__dirname, '../'),
+      },
+      {
+        test: /\.js/,
+        // No exclude
+        exclude: /node_modules\/(?![lit\-element|lit\-html])/,
+        loader: 'babel-loader',
+        options: JSON.parse(babelRc),
       },
     ],
   },
