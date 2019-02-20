43c00cbc02f517ea41514773c4851019d52edaf7
codepleb <codepleb4@gmail.com>
2019-02-20 09:12:07 +0100

    Add close icon to modal, remove dead code

--- src/components/o-modal/_template.js
+++ src/components/o-modal/_template.js
@@ -7,29 +7,12 @@ export default () => {
   };
 
   const closeModalByClickingOutside = event => {
-    var modal = document.getElementById("modal-wrapper");
-    if (event.target.id == "modal-wrapper") closeModal();
+    if (event.target.id === "modal-wrapper") closeModal();
   };
 
   const closeModal = () => {
     var modal = document.getElementById("modal-wrapper");
     modal.style.display = "none";
-  };
-
-  const close = () => {
-    var span = document.getElementsByClassName("close")[0];
-    var modal = document.getElementById("modal-wrapper");
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    };
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
   };
 
   return html`
@@ -41,7 +24,7 @@ export default () => {
       onclick="${closeModalByClickingOutside}"
     >
       <div class="modal-content">
-        <span class="close">&times;</span>
+        <axa-icon icon="cross-gap" style="float:right;"></axa-icon>
         <p>Some text in the Modal..</p>
       </div>
     </div>
