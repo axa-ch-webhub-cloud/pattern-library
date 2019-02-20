57e2659e2ebd89ca46e73c4ef4039729d16d6240
codepleb <codepleb4@gmail.com>
2019-02-20 08:32:17 +0100

    Add open/close functionality to modal

--- src/components/o-modal/_template.js
+++ src/components/o-modal/_template.js
@@ -1,15 +1,24 @@
 import html from "nanohtml";
 
 export default () => {
-  const call = () => {
-    console.log("hi");
-    var modal = document.getElementById("myModal");
+  const openModal = () => {
+    var modal = document.getElementById("modal-wrapper");
     modal.style.display = "block";
+  };
+
+  const closeModalByClickingOutside = event => {
+    var modal = document.getElementById("modal-wrapper");
+    if (event.target.id == "modal-wrapper") closeModal();
+  };
+
+  const closeModal = () => {
+    var modal = document.getElementById("modal-wrapper");
+    modal.style.display = "none";
   };
 
   const close = () => {
     var span = document.getElementsByClassName("close")[0];
-    var modal = document.getElementById("myModal");
+    var modal = document.getElementById("modal-wrapper");
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
@@ -24,11 +33,13 @@ export default () => {
   };
 
   return html`
-    <button id="myBtn" onclick="${call}">Open Modal</button>
+    <button id="myBtn" onclick="${openModal}">Open Modal</button>
 
-    <p>hallo</p>
-
-    <div id="myModal" class="modal">
+    <div
+      id="modal-wrapper"
+      class="modal"
+      onclick="${closeModalByClickingOutside}"
+    >
       <div class="modal-content">
         <span class="close">&times;</span>
         <p>Some text in the Modal..</p>
