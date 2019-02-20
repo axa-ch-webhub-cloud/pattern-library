550cce5d4ec2b3b98df22f5219ea465aeaf2b9dc
Marc Illien <marc.illien@gmail.com>
2019-02-20 13:48:57 +0100

    Revert double quotes
    Misconfiguruation by IDE config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,19 @@
-import { configure } from "@storybook/html";
-import "@webcomponents/webcomponentsjs";
-import "core-js/es6/reflect";
-import "core-js/es6/promise";
-import "core-js/fn/array/from";
-import "core-js/fn/object/assign";
-import "core-js/fn/object/create";
-import "core-js/fn/object/define-properties";
-import "core-js/fn/object/set-prototype-of";
+import { configure } from '@storybook/html';
+import '@webcomponents/webcomponentsjs';
+import 'core-js/es6/reflect';
+import 'core-js/es6/promise';
+import 'core-js/fn/array/from';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/object/create';
+import 'core-js/fn/object/define-properties';
+import 'core-js/fn/object/set-prototype-of';
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
 }
 
 function loadStories() {
-  requireAll(require.context("..", true, /_demo/));
+  requireAll(require.context('..', true, /_demo/));
 }
 
 configure(loadStories, module);
