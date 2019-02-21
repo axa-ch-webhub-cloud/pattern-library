1797a2ca61440b39e24060260f14d93d93e5357c
Marc Illien <marc.illien@gmail.com>
2019-02-21 17:12:31 +0100

    Add AXA logo and AXA styles to the theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/html';
+import { addDecorator, configure } from '@storybook/html';
+import { withOptions } from '@storybook/addon-options';
+import { themes } from '@storybook/components';
+
 import '@webcomponents/webcomponentsjs';
 import 'core-js/es6/reflect';
 import 'core-js/es6/promise';
@@ -7,6 +10,29 @@ import 'core-js/fn/object/assign';
 import 'core-js/fn/object/create';
 import 'core-js/fn/object/define-properties';
 import 'core-js/fn/object/set-prototype-of';
+
+// Option defaults.
+addDecorator(
+  withOptions({
+    name: 'AXA Living Styleguide',
+    theme: {
+      ...themes.normal,
+      mainTextFace: '"Source Sans Pro", Arial, sans-serif;',
+      menuLink: {
+        color: 'black',
+      },
+      brand: {
+        height: '55px',
+        paddingLeft: '65px',
+        background: 'url("https://axa.ch/etc/clientlibs/axa/frontend-lib/images/logo-AXA.svg") no-repeat',
+      },
+      brandLink: {
+        border: 'none',
+        textAlign: 'left',
+      },
+    },
+  })
+);
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
