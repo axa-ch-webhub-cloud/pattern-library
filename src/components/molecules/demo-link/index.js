09d2e1ba94bf0a179343b4a05c4eb7475b039a2a
Marc Illien <marc.illien@gmail.com>
2019-02-22 15:26:43 +0100

    Add new link destination in axa link

--- src/components/molecules/demo-link/index.js
+++ src/components/molecules/demo-link/index.js
@@ -3,7 +3,7 @@ import { LitElement, html } from 'lit-element';
 class AXADemoLink extends LitElement {
   render() {
     return html`
-      <a href="https://axa.ch">AXA Demo Link</a>
+      <a href="https://axa-winterthur.ch">AXA Demo Link</a>
     `;
   }
 }
