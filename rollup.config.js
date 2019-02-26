38c80a749d6cdea2d5eeb4fb9ac61640d89f176c
Luca Mele <luca.mele@axa-winterthur.ch>
2019-02-26 15:04:43 +0100

    Add all src/index.js file to be transpiled into es module inside lib/index.js, sibling of the src folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 // This build removes all font-facees from the css, inlines scss in the final bundle and can deal with json imports
-
 const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const replace = require('rollup-plugin-replace'); // use to setup project enviroment variables
@@ -7,45 +6,51 @@ const sass = require('rollup-plugin-sass');
 const autoprefixer = require('autoprefixer');
 const stripFontFace = require('postcss-strip-font-face'); // strip all font faces in the bundled css
 const postcss = require('postcss');
+const glob = require('glob');
 
 const fs = require('fs');
 
 const babelOptions = JSON.parse(fs.readFileSync('./.storybook/.babelrc')); // get the babelrc file
 
-export default {
-  input: 'src/components/molecules/button/src/index.js',
+// options is optional
+const input = glob.sync('src/**/src/index.js');
+
+const plugins = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  }),
+  sass({
+    insert: true,
+    include: ['**/*.scss'],
+    options: {
+      includePaths: [
+        'node_modules',
+      ],
+    },
+    processor: css => postcss([autoprefixer, stripFontFace])
+      .process(css)
+      .then(result => result.css),
+  }),
+  babel({
+    ...babelOptions,
+    babelrc: false,
+    exclude: [
+      'node_modules/**',
+    ],
+    runtimeHelpers: true,
+  }),
+  resolve({
+    jsnext: true,
+    module: true,
+    only: [/^\.{0,2}\/|\.scss$/i], // threat all node_modules as external apart od .scss files
+  }),
+];
+
+export default input.map(entry => ({
+  input: entry,
   output: {
-    file: 'src/components/molecules/button/lib/index.js',
+    file: entry.replace('/src/index.js', '/lib/index.js'),
     format: 'es',
   },
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    sass({
-      insert: true,
-      include: ['**/*.scss'],
-      options: {
-        includePaths: [
-          'node_modules',
-        ],
-      },
-      processor: css => postcss([autoprefixer, stripFontFace])
-        .process(css)
-        .then(result => result.css),
-    }),
-    babel({
-      ...babelOptions,
-      babelrc: false,
-      exclude: [
-        'node_modules/**',
-      ],
-      runtimeHelpers: true,
-    }),
-    resolve({
-      jsnext: true,
-      module: true,
-      only: [/^\.{0,2}\/|\.scss$/i], // threat all node_modules as external apart od .scss files
-    }),
-  ],
-};
+  plugins,
+}));
