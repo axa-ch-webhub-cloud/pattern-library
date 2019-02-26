ba00499c8032a51ef890446612bdfbdad3d10e74
Luca Mele <luca.mele@axa-winterthur.ch>
2019-02-26 15:36:35 +0100

    Add eslintignore and fix broken path

--- src/components/molecules/button/_story.react.js
+++ src/components/molecules/button/_story.react.js
@@ -1,5 +1,5 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
-import './index';
+import './src/index';
 
 storiesOf('Button', module).add('Button React', () => '<axa-button motion>Im a button with react</axa-button>');
