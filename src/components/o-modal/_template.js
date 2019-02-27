68e22c82453001f3dc39f2bda1999af8dbf1d7cc
codepleb <codepleb4@gmail.com>
2019-02-27 17:55:39 +0100

    Change div to article

--- src/components/o-modal/_template.js
+++ src/components/o-modal/_template.js
@@ -1,10 +1,10 @@
 import html from 'nanohtml';
 
 export default ({ open }, childrenFragment) => html`
-  <div class="js-modal m-modal ${open ? '' : 'm-modal--hidden'}" tabindex="0">
+  <article class="js-modal m-modal ${open ? '' : 'm-modal--hidden'}" tabindex="0">
     <div class="m-modal__content">
       <button class="js-modal-close-button m-modal__close-button"><axa-icon icon="cross-gap"></axa-icon></button>
       <div>${childrenFragment}</div>
     </div>
-  </div>
+  </article>
 `;
