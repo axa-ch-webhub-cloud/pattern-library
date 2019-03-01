10ec1677ec9e521a6f1176db94e78a9ea74052f5
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-03-01 17:18:27 +0100

    expanded logic of the interaction of value-attribute and max-attribute

--- src/components/o-progress-bar/_template.js
+++ src/components/o-progress-bar/_template.js
@@ -1,19 +1,42 @@
 import html from 'nanohtml';
-import isEmptyFragment from '../../js/is-empty-fragment';
+// import isEmptyFragment from '../../js/is-empty-fragment';
 
 const percentageSymbol = '%';
+const closingIcon = 'cross-gap';
+
 export default function ({
   classes,
   value = '0',
-  max = '100',
+  max,
   showIcon,
   showPercentage,
-}, childrenFragment) {
-  const noChildren = isEmptyFragment(childrenFragment);
-  console.log(noChildren);
-  return html`<div class=${classes}>
-    ${console.log(value, max, showIcon, showPercentage)}
-    <progress value="${value}" max="${max}"><span>looool</span>${percentageSymbol}</progress>
-    <axa-icon icon="cross-gap"></axa-icon>
-  </div>`;
+}) {
+  if (value < 0 || max < 0) {
+    console.log('negative');// TODO Errorhandling
+    // TODO set value to 0
+  } else if (value > max) {
+    console.log('value > max');// TODO Errorhandling
+  }
+
+  // console.log(childrenFragment.childNodes[0].nodeValue > -1);
+
+  // the percentage shouldn't be round up cause then the progressbar could show 100% even if the task is not finished
+  let percentage = 100;
+  if ((max === undefined || max === '') && value <= 1) {
+    percentage = value * 100;
+  } else if (value === 0) {
+    percentage = 0;
+  } else if (max >= value) {
+    percentage = (value / max) * 100;
+  }
+  percentage = Math.floor(percentage);
+
+  return html`<article class="${classes}">
+    <div class="o-progress-bar__container">
+      <p class="o-progress-bar__percentage"><span>${percentage}</span>${percentageSymbol}</p>
+      ${console.log(value, max, showIcon, showPercentage)}
+      <progress value="${value}" max="${max}"><span>${percentage}</span>${percentageSymbol}</progress>
+      <axa-icon class="o-progress-bar__closing-icon" icon="${closingIcon}"></axa-icon>
+    </div>
+  </article>`;
 }
