3dd215f81ee4ea9ba4f20e6122b03be3df07012d
Raphael Lueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-01 18:07:34 +0100

    Introduce cypress with sample test

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,37 @@
+describe('My First Test', function() {
+  it('Visits the Kitchen Sink', function() {
+    cy.visit('http://localhost:9001');
+
+    cy.contains('Demo').click();
+    cy.contains('Button').click();
+    cy.contains('Button React').click();
+
+    cy.get('ul')
+      .find('[href = "?selectedKind=Demo&selectedStory=Button%20React&full=0&addons=1&stories=1&panelRight=0"]')
+      .click();
+
+    // Sadly, this workaround is needed for iframes, which storybooks uses
+    cy.get('#storybook-preview-iframe').then(function($iframe) {
+      const $jbody = $iframe.contents().find('body');
+      const $body = $jbody[0];
+
+      // Go for hard attributes
+      cy.wrap($body)
+        .find('axa-button')
+        .contains("I'm disabled")
+        .and('have.attr', 'disabled');
+
+      // Go for purpose, here, visibility and not clickable
+      cy.wrap($body)
+        .find('axa-button')
+        .contains("I'm disabled")
+        .and('be.visible');
+      // This does not work, probably because of shadow dom
+      // .and('be.disabled');
+
+      cy.wrap($body)
+        .find('axa-button')
+        .contains("I'm clickable");
+    });
+  });
+});
