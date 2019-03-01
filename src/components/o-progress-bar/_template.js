fd69b81893de31ff87c799db08eb0d9b71851084
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-03-01 13:54:26 +0100

    basic implementation of progress-bar, all attributes can be received

--- src/components/o-progress-bar/_template.js
+++ src/components/o-progress-bar/_template.js
@@ -1,5 +1,19 @@
 import html from 'nanohtml';
+import isEmptyFragment from '../../js/is-empty-fragment';
 
-export default ({ classes }) => html`
-  <div class=${classes}>Ready to start</div>
-`;
+const percentageSymbol = '%';
+export default function ({
+  classes,
+  value = '0',
+  max = '100',
+  showIcon,
+  showPercentage,
+}, childrenFragment) {
+  const noChildren = isEmptyFragment(childrenFragment);
+  console.log(noChildren);
+  return html`<div class=${classes}>
+    ${console.log(value, max, showIcon, showPercentage)}
+    <progress value="${value}" max="${max}"><span>looool</span>${percentageSymbol}</progress>
+    <axa-icon icon="cross-gap"></axa-icon>
+  </div>`;
+}
