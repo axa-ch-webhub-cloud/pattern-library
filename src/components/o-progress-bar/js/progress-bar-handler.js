5f6be9fbf554cc9f75f9de8fae1c0fdeef4a2216
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-03-07 13:50:47 +0100

    Date.now() instead of timestamp, default priority

--- src/components/o-progress-bar/js/progress-bar-handler.js
+++ src/components/o-progress-bar/js/progress-bar-handler.js
@@ -3,16 +3,14 @@ import { Error, Warning, Information } from './error-handler';
 const LOWEST_PRIORITY = 2;
 
 export default class ProgressBarHandler {
-  constructor(wcNode, priority) {
+  constructor(wcNode, priority = LOWEST_PRIORITY) {
     this.wcNode = wcNode;
     this.priority = priority;
     this.logs = [];
-
-    this.init();
   }
 
   init() {
-    this.logs.push(new Warning(new Date(), 'das ist die message'));
+    this.logs.push(new Warning(Date.now(), 'das ist die message'));
     this.logs.map(log => log.print());
   }
 }
