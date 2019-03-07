c0e5f3771717c87e10567c24d2bab230f98eb8ad
Marco Kruse <marco-kruse@freenet.de>
2019-03-07 14:10:28 +0100

    Migrate Storybook v4.1 to v5

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
-import { addDecorator, configure } from '@storybook/html';
-import { withOptions } from '@storybook/addon-options';
-import { themes } from '@storybook/components';
+import { addParameters, configure } from '@storybook/html';
+import { create } from '@storybook/theming';
 
 import '@webcomponents/webcomponentsjs';
 import 'core-js/es6/reflect';
@@ -11,29 +10,44 @@ import 'core-js/fn/object/create';
 import 'core-js/fn/object/define-properties';
 import 'core-js/fn/object/set-prototype-of';
 
+import logo from '../src/static/svg/logo-axa.svg';
+
+// old config
 // Option defaults.
-addDecorator(
-  withOptions({
-    name: 'AXA Living Styleguide',
-    url: 'https://github.com/axa-ch/patterns-library/tree/develop-v2',
-    theme: {
-      ...themes.normal,
-      mainTextFace: '"Source Sans Pro", Arial, sans-serif;',
-      menuLink: {
-        color: 'black',
-      },
-      brand: {
-        height: '55px',
-        paddingLeft: '65px',
-        background: `url("/svg/logo-axa.svg") no-repeat`,
-      },
-      brandLink: {
-        border: 'none',
-        textAlign: 'left',
-      },
-    },
-  })
-);
+// addDecorator(
+//   withOptions({
+//     name: 'AXA Living Styleguide',
+//     url: 'https://github.com/axa-ch/patterns-library/tree/develop-v2',
+//     theme: {
+//       ...themes.normal,
+//       mainTextFace: '"Source Sans Pro", Arial, sans-serif;',
+//       menuLink: {
+//         color: 'black',
+//       },
+//       brand: {
+//         height: '55px',
+//         paddingLeft: '65px',
+//         background: `url("/svg/logo-axa.svg") no-repeat`,
+//       },
+//       brandLink: {
+//         border: 'none',
+//         textAlign: 'left',
+//       },
+//     },
+//   })
+// );
+
+// theme - logo some sort of broken
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+      brandTitle: 'AXA Living Styleguide',
+      brandUrl: 'https://github.com/axa-ch/patterns-library/tree/develop-v2',
+      brandImage: logo,
+    }),
+  },
+});
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
