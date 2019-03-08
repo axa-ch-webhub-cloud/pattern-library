7decf56f1e06842a6d01d0e71033525e1132f026
Raphael Lueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-08 08:12:00 +0100

    remove recorded video by default

--- cypress/start-cypress-run.sh
+++ cypress/start-cypress-run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# npm run storybook &
 npx start-storybook -p 9999 -c .storybook -s ../src/static --ci --quiet > /dev/null 2>&1 &
 
 npx wait-on http://localhost:9999
 
-npm run cy
-SUCCESS=$?
+npx cypress run --config video=false
+test_status=$?
 
 # Kill storybook (cleanup) - By port
 kill -9 $(lsof -t -i:9999 -sTCP:LISTEN)
 
-exit $SUCCESS
+exit $test_status
