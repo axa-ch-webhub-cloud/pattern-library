5e91d509598a5d4bb9012ee18ab9c8a63a85a897
Raphael Lueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-13 17:53:48 +0100

    make the whole footer attributifyable

--- src/components/molecules/footer-small/_stories.native.js
+++ src/components/molecules/footer-small/_stories.native.js
@@ -3,16 +3,19 @@ import { storiesOf } from '@storybook/html';
 import './index';
 
 const languages = [
-  { code: 'DE', link: 'https://google.ch' },
-  { code: 'FR', link: 'https://google.ch' },
-  { code: 'IT', link: 'https://google.ch' },
-  { code: 'EN', link: 'https://google.ch' },
+  { text: 'DE', link: 'https://google.ch' },
+  { text: 'FR', link: 'https://google.ch' },
+  { text: 'IT', link: 'https://google.ch' },
+  { text: 'EN', link: 'https://google.ch' },
 ];
 
+const disclaimer = [{ text: 'Terms of use', link: 'https://google.ch' }, { text: 'Data protection', link: 'https://google.ch' }];
+
 const languagesAsString = JSON.stringify(languages);
+const disclaimerAsString = JSON.stringify(disclaimer);
 
 storiesOf('Footer Small', module).add(
   'Footer Small',
   () =>
-    `<axa-footer-small languageLinks=${languagesAsString} disclaimerLinks="[]" copyrightText="© 2019 AXA Insurance Ltd."></axa-footer-small>`
+    `<axa-footer-small languageLinks='${languagesAsString}' disclaimerLinks='${disclaimerAsString}' copyrightText="© 2019 AXA Insurance Ltd."></axa-footer-small>`
 );
