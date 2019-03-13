37d9dd810c185f48acdb1c2163e36421be05d90e
Marc Illien <marc.illien@gmail.com>
2019-03-13 17:42:34 +0100

    Add datepicker example with two on the same page

--- src/components/20-molecules/datepicker/index.story.js
+++ src/components/20-molecules/datepicker/index.story.js
@@ -1,4 +1,12 @@
 import { storiesOf } from '@storybook/html';
 import './index';
 
-storiesOf('Molecules/Datepicker', module).add('Datepicker', () => '<axa-datepicker year="2019" month="1" day="1"></axa-datepicker>');
+storiesOf('Molecules/Datepicker', module)
+  .add('Datepicker', () => '<axa-datepicker year="2019" month="1" day="1"></axa-datepicker>')
+  .add(
+    'Datepicker Input',
+    () =>
+      `<axa-datepicker inputField year="2019" month="1" day="1"></axa-datepicker>
+      <axa-datepicker inputField year="2019" month="1" day="1"></axa-datepicker>
+      `
+  );
