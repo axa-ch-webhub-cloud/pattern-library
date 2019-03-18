ea46e6ecfca9a591d7ea9bfa0e629077fc306ced
Marc Illien <marc.illien@gmail.com>
2019-03-18 20:38:52 +0100

    Add better stories for datepicker

--- src/components/20-molecules/datepicker/index.story.js
+++ src/components/20-molecules/datepicker/index.story.js
@@ -2,10 +2,30 @@ import { storiesOf } from '@storybook/html';
 import './index';
 
 storiesOf('Molecules/Datepicker', module)
-  .add('Datepicker', () => '<axa-datepicker year="2019" month="1" day="1"></axa-datepicker>')
+  .add('Datepicker', () => '<axa-datepicker year="2020" month="3" day="20"></axa-datepicker>')
   .add(
     'Datepicker Input',
     () => `
-      <axa-datepicker inputField year="2020" month="3" day="20"></axa-datepicker>
-      `
+    <axa-datepicker inputField year="2020" month="3" day="20"></axa-datepicker>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <axa-datepicker inputField year="2020" month="3" day="20"></axa-datepicker>
+    `
   );
