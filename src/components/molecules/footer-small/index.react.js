9ffc6b68f73200eadcf294e9485f667e6c586e4d
Raphael Lueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-18 07:53:59 +0100

    remove unused files

--- src/components/molecules/footer-small/index.react.js
+++
@@ -1,4 +0,0 @@
-import withReact from '../../../utils/with-react';
-import AXAFooterSmall from './index';
-
-export default createElement => ({}) => withReact(createElement)(AXAFooterSmall.tagName, {}, children);
