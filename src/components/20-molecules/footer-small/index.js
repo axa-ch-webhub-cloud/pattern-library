48bc706a536aabe96c345d2610875e30b6289be6
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-19 09:06:07 +0100

    Introduce axa footer small

---
+++ src/components/20-molecules/footer-small/index.js
@@ -0,0 +1,67 @@
+import { LitElement, html, css, unsafeCSS } from 'lit-element';
+import footerSmallCSS from './index.scss';
+
+class AXAFooterSmall extends LitElement {
+  static tagName = 'axa-footer-small';
+  static styles = css`
+    ${unsafeCSS(footerSmallCSS)}
+  `;
+
+  static get properties() {
+    return {
+      languageLinks: { type: Array },
+      disclaimerLinks: { type: Array },
+      copyrightText: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.languageLinks = [];
+    this.disclaimerLinks = [];
+    this.copyrightText = '';
+  }
+
+  formatText(text, arrIndex, arrLength) {
+    return `${text}${arrIndex !== arrLength - 1 ? ' | ' : ''}`;
+  }
+
+  render() {
+    const languageHtml = this.languageLinks.map(
+      (language, index) =>
+        html`
+          <a class="m-footer-small__link" href="${language.link}">${this.formatText(language.text, index, this.languageLinks.length)}</a>
+        `
+    );
+
+    const disclaimerHtml = this.disclaimerLinks.map(
+      (disclaimer, index) =>
+        html`
+          <a class="m-footer-small__link" href="${disclaimer.link}"
+            >${this.formatText(disclaimer.text, index, this.disclaimerLinks.length)}</a
+          >
+        `
+    );
+
+    return html`
+      <article class="m-footer-small">
+        <div class="m-footer-small__side-aligned">
+          ${languageHtml}
+        </div>
+
+        <div class="m-footer-small__side-aligned">
+          <div class="m-footer-small__disclaimer">
+            <span class="m-footer-small__disclaimer--group-right">
+              ${disclaimerHtml}
+            </span>
+            <span footer-small__disclaimer--group-right>${this.copyrightText}</span>
+          </div>
+        </div>
+      </article>
+    `;
+  }
+}
+
+customElements.define(AXAFooterSmall.tagName, AXAFooterSmall);
+
+export default AXAFooterSmall;
---
+++ src/components/20-molecules/footer-small/index.story.js
@@ -0,0 +1,24 @@
+/* global document */
+import { storiesOf } from '@storybook/html';
+import './index';
+
+const languages = [
+  { text: 'DE', link: 'https://axa.ch/de/privatkunden.html' },
+  { text: 'FR', link: 'https://axa.ch/fr/particuliers.html' },
+  { text: 'IT', link: 'https://axa.ch/it/clienti-privati.html' },
+  { text: 'EN', link: 'https://axa.ch/en/private-customers.html' },
+];
+
+const disclaimer = [
+  { text: 'Terms of use', link: 'https://axa.ch/en/information/terms-of-use.html' },
+  { text: 'Data protection', link: 'https://axa.ch/en/information/data-protection.html' },
+];
+
+const languagesAsString = JSON.stringify(languages);
+const disclaimerAsString = JSON.stringify(disclaimer);
+
+storiesOf('Footer Small', module).add(
+  'Footer Small',
+  () =>
+    `<axa-footer-small languageLinks='${languagesAsString}' disclaimerLinks='${disclaimerAsString}' copyrightText="© 2019 AXA Insurance Ltd."></axa-footer-small>`
+);
