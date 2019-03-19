90a9c9f93b1186216e8b2af612716f6c2139a08d
Marc Illien <marc.illien@gmail.com>
2019-03-19 14:41:19 +0100

    Fix conflicts between prettier and eslint the right way
     - Usage of prettier plugin for eslint disables formatting rules which are in conflict with eslint and the part of prettier (comma dangles, spaces after parens and and)

--- src/components/00-materials/icons/arrow-right.js
+++ src/components/00-materials/icons/arrow-right.js
@@ -1,4 +1,5 @@
 import { html } from 'lit-element';
+
 // only a mock for the moment
 const arrowRight = () => html`
   <svg xmlns="http://www.w3.org/2000/svg" class="m-button__icon" width="12" height="7" viewBox="0 0 12 7">
@@ -6,6 +7,6 @@ const arrowRight = () => html`
       <path d="M.5 3.5H11M9 7l2.798-3.5L9 0" />
     </g>
   </svg>
-  `;
+`;
 
 export default arrowRight;
