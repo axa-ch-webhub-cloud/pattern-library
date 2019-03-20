e1ad9f73a3ca3e680c4b943a5a893091594682dd
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-20 10:59:02 +0100

    Enable external links that open in a new tab

--- src/components/20-molecules/link/index.js
+++ src/components/20-molecules/link/index.js
@@ -13,20 +13,13 @@ class AXAFooterSmall extends LitElement {
 
   static get properties() {
     return {
-      text: { type: String },
-      // color: PropTypes.oneOf(['red', 'white', 'blue']),
+      href: { type: String },
       color: { type: String },
-      size: { type: String },
-      motion: { type: Boolean },
+      bold: { type: Boolean },
       arrowLeft: { type: Boolean },
       arrowRight: { type: Boolean },
-      href: { type: String },
-      listed: { type: Boolean },
-      icon: { type: String },
       deco: { type: Boolean },
-      iconsPathPrefix: { type: String },
-      // target: PropTypes.oneOf(['_blank', '_self']),
-      target: { type: String },
+      external: { type: Boolean },
     };
   }
 
@@ -34,47 +27,29 @@ class AXAFooterSmall extends LitElement {
     super();
     this.href = '';
     this.color = '';
-    this.size = '';
-    this.motion = false;
+    this.bold = false;
     this.arrowLeft = false;
     this.arrowRight = false;
-    this.listed = false;
-    this.icon = '';
     this.deco = false;
-    this.iconsPathPrefix = '';
-    this.target = '';
+    this.external = false;
   }
-
-  // render() {
-  //   return html`
-  //     <a href="${href}" class="${classes}" target="${target}">
-  //       ${icon &&
-  //         html`
-  //           <axa-icon path-prefix="${iconsPathPrefix}" icon="${icon}" icon-class="m-link__icon"></axa-icon>
-  //         `}
-  //       ${listed &&
-  //         html`
-  //           <axa-icon path-prefix="${iconsPathPrefix}" icon="arrow" icon-class="m-link__listed"></axa-icon>
-  //         `}
-  //       ${text}
-  //       ${arrow &&
-  //         html`
-  //           <axa-icon path-prefix="${iconsPathPrefix}" icon="arrow" icon-class="m-link__arrow"></axa-icon>
-  //         `}
-  //     </a>
-  //   `;
-  // }
 
   render() {
     const classes = {
       'm-link--red': this.color === 'red',
       'm-link--blue': this.color === 'blue',
       'm-link--white': this.color === 'white',
+      'm-link--bold': this.bold,
       'm-link--deco': this.deco,
     };
 
     return html`
-      <a class="m-link ${classMap(classes)}" href="${this.href}">
+      <a
+        class="m-link ${classMap(classes)}"
+        href="${this.href}"
+        target="${this.external ? '_blank' : '_top'}"
+        rel="${this.external ? 'noreferrer noopener' : ''}"
+      >
         ${this.arrowLeft === true ? svg`${arrowRight()}` : ''}
         <slot></slot>
         ${this.arrowRight === true ? svg`${arrowRight()}` : ''}
--- src/components/20-molecules/link/index.story.js
+++ src/components/20-molecules/link/index.story.js
@@ -12,4 +12,6 @@ storiesOf('Link', module)
   )
   .add('Decorated link', () => `<axa-link href="https://axa.ch/en/private-customers.html" deco>Red Link</axa-link>`)
   .add('Link static arrow left', () => `<axa-link href="https://axa.ch/en/private-customers.html" arrowLeft>Arrow Left Link</axa-link>`)
-  .add('Link static arrow right', () => `<axa-link href="https://axa.ch/en/private-customers.html" arrowRight>Arrow Right Link</axa-link>`);
+  .add('Link static arrow right', () => `<axa-link href="https://axa.ch/en/private-customers.html" arrowRight>Arrow Right Link</axa-link>`)
+  .add('Bold', () => `<axa-link href="https://axa.ch/en/private-customers.html" bold>Bold Link</axa-link>`)
+  .add('External Source', () => `<axa-link href="https://axa.ch/en/private-customers.html" external>External Link</axa-link>`);
