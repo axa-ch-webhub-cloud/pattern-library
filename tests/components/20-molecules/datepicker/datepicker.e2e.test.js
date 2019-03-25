336102d3dba6488d14467c29e1b0002745906b96
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-03-25 11:57:52 +0100

    add tests to assert datepicker edge cases

--- tests/components/20-molecules/datepicker/datepicker.e2e.test.js
+++ tests/components/20-molecules/datepicker/datepicker.e2e.test.js
@@ -6,7 +6,7 @@ fixture('Datepicker').page('http://localhost:6006/iframe.html?id=molecules-datep
 test('Should select february the 14th', async t => {
   const datePickerAccessor = new DatePickerAccessor(t);
   await datePickerAccessor.chooseFebruary();
-  // 14. February
+  // 14. february
   await datePickerAccessor.chooseDayByIndex(18);
 
   await datePickerAccessor.assertYear(2020);
@@ -14,4 +14,24 @@ test('Should select february the 14th', async t => {
   await datePickerAccessor.assertDay(14);
 });
 
-test('Should select the first of march from within the february view', async t => {});
+test('Should select the first of march from within the february view', async t => {
+  const datePickerAccessor = new DatePickerAccessor(t);
+  await datePickerAccessor.chooseFebruary();
+  // 1. march
+  await datePickerAccessor.chooseDayByIndex(34);
+
+  await datePickerAccessor.assertYear(2020);
+  await datePickerAccessor.assertMonth('März');
+  await datePickerAccessor.assertDay(1);
+});
+
+test('Should select the 31th of january from within the february view', async t => {
+  const datePickerAccessor = new DatePickerAccessor(t);
+  await datePickerAccessor.chooseFebruary();
+  // 31. january
+  await datePickerAccessor.chooseDayByIndex(4);
+
+  await datePickerAccessor.assertYear(2020);
+  await datePickerAccessor.assertMonth('Januar');
+  await datePickerAccessor.assertDay(31);
+});
