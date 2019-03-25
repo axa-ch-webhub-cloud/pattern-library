e1524311772e35d1d3ca1f3ae99ab42c57f4f525
Luca Mele <luca.mele@axa-winterthur.ch>
2019-03-25 13:10:28 +0100

    Move test scripts to scripts

---
+++ scripts/run-testcafe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+npx start-storybook -p 9999 -c .storybook -s ../src/static --ci --quiet > /dev/null 2>&1 &
+
+npx wait-on http://localhost:9999
+
+npx testcafe "chrome:headless" tests/
+test_status=$?
+
+# Kill storybook (cleanup) - By port
+kill -9 $(lsof -t -i:9999 -sTCP:LISTEN)
+
+exit $test_status
