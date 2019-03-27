41b0df011ecbca0d1669aa933b291dc397e8ec73
Markus Walther <markus.walther@axa-winterthur.ch>
2019-03-27 15:28:23 +0100

    storybook demo: add dynamic-children experiment (originally https://github.com/axa-ch/lit-element-and-dynamic-children)

--- src/demo/react/story.jsx
+++ src/demo/react/story.jsx
@@ -5,6 +5,7 @@ import { storiesOf } from '@storybook/html';
 import ReactDOM from 'react-dom';
 import App from './App';
 import DemoControlledInputsApp from '../demo-controlled-inputs/App';
+import DemoDynamicChildrenApp from '../demo-dynamic-children/App';
 
 storiesOf('Demo', module)
   .add('Button React', () => {
@@ -16,5 +17,11 @@ storiesOf('Demo', module)
   .add('Controlled Inputs React', () => {
     const div = document.createElement('div');
     ReactDOM.render(<DemoControlledInputsApp />, div);
+    return div;
+  })
+  .add('Dynamic Children React', () => {
+    const div = document.createElement('div');
+    div.id = 'root-dynamic-children';
+    ReactDOM.render(<DemoDynamicChildrenApp />, div);
     return div;
   });
