35b850226ae8d3c6756b47e655e62889c63b1023
Markus Walther <markus.walther@axa-winterthur.ch>
2019-03-27 11:28:20 +0100

    storybook demo: add controlled-inputs-under-react experiment (originally https://github.com/axa-ch/react-with-lit-element)

---
+++ src/demo/demo-controlled-inputs/customelements/define-once.js
@@ -0,0 +1,9 @@
+export default (name, customElementClass) => {
+  // custom element not already registered?
+  if (!window.customElements.get(name)) {
+    // no, register it now
+    window.customElements.define(name, customElementClass);
+  }
+  // return its name as a convenience
+  return name;
+};
--- src/demo/react/story.jsx
+++ src/demo/react/story.jsx
@@ -4,10 +4,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/html';
 import ReactDOM from 'react-dom';
 import App from './App';
+import DemoControlledInputsApp from '../demo-controlled-inputs/App';
 
-storiesOf('Demo', module).add('Button React', () => {
-  const div = document.createElement('div');
-  div.setAttribute('id', 'root-button');
-  ReactDOM.render(<App />, div);
-  return div;
-});
+storiesOf('Demo', module)
+  .add('Button React', () => {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'root-button');
+    ReactDOM.render(<App />, div);
+    return div;
+  })
+  .add('Controlled Inputs React', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<DemoControlledInputsApp />, div);
+    return div;
+  });
