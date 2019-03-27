af030482c38d895dad7d33e050daff918a1d4ef4
Luca Mele <luca.mele@axa-winterthur.ch>
2019-03-27 10:44:02 +0100

    Fillup height

--- src/components/o-modal/_template.js
+++ src/components/o-modal/_template.js
@@ -2,7 +2,7 @@ import html from 'nanohtml';
 
 export default ({ open }, childrenFragment) => html`
   <article class="js-modal m-modal ${open ? '' : 'm-modal--hidden'}" tabindex="0">
-    <div class="m-modal__content">
+    <div class="m-modal__content js-modal__content">
       <button class="js-modal-close-button m-modal__close-button"><axa-icon icon="cross-gap"></axa-icon></button>
       <div>${childrenFragment}</div>
     </div>
