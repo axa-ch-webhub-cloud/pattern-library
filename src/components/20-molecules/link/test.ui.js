8266e88f7730078281f45806539599e732bae4a8
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-04-04 13:41:02 +0200

    Add test for white link

--- src/components/20-molecules/link/test.ui.js
+++ src/components/20-molecules/link/test.ui.js
@@ -49,3 +49,27 @@ test('should display red link correctly', async t => {
   );
   await t.expect(linkText.textContent).eql('Red Link');
 });
+
+fixture('Datepicker').page(`${host}/iframe.html?id=atoms-link--white-link`);
+
+test('should display white link correctly', async t => {
+  const axaLink = Selector('axa-link');
+  await t.expect(axaLink.getAttribute('href')).eql('https://axa.ch/en/private-customers.html');
+
+  const linkElement = Selector(() => document.querySelector('axa-link').shadowRoot).find('a');
+  await t.expect(linkElement.getAttribute('href')).eql('https://axa.ch/en/private-customers.html');
+
+  const link = Selector(() => document.querySelector('axa-link').shadowRoot.querySelector('a'));
+  await t.expect(link.exists).ok();
+  await t.expect(link.getStyleProperty('color')).eql('rgb(255, 255, 255)');
+  await t.expect(link.getStyleProperty('text-decoration')).eql('none solid rgb(255, 255, 255)');
+
+  const linkText = Selector(
+    () =>
+      document
+        .querySelector('axa-link')
+        .shadowRoot.querySelector('slot')
+        .assignedNodes()[0]
+  );
+  await t.expect(linkText.textContent).eql('White Link');
+});
