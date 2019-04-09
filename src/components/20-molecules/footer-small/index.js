ac637cf11f5f16624d2822ffdea7623f125b6b2f
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-04-09 16:26:56 +0200

    Refactor footer component

--- src/components/20-molecules/footer-small/index.js
+++ src/components/20-molecules/footer-small/index.js
@@ -10,45 +10,40 @@ class AXAFooterSmall extends LitElement {
 
   static get properties() {
     return {
-      languageitems: { type: Array },
-      disclaimeritems: { type: Array },
-      copyrighttext: { type: String },
+      languageItems: { type: Array },
+      disclaimerItems: { type: Array },
+      copyrightText: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.languageitems = [];
-    this.disclaimeritems = [];
-    this.copyrighttext = '';
+    this.languageItems = [];
+    this.disclaimerItems = [];
+    this.copyrightText = '';
   }
+
+  renderFooterList = items => html`
+    <ul class="m-footer-small__list">
+      ${repeat(
+        items,
+        item => html`
+          <li class="m-footer-small__list-item">
+            <axa-link class="m-footer-small__link" href="${item.link}" color="white">${item.text}</axa-link>
+          </li>
+        `
+      )}
+    </ul>
+  `;
 
   render() {
     return html`
       <article class="m-footer-small">
-        <ul class="m-footer-small__list">
-          ${repeat(
-            this.languageitems,
-            languageItem => html`
-              <li class="m-footer-small__list-item">
-                <axa-link class="m-footer-small__link" href="${languageItem.link}" color="white">${languageItem.text}</axa-link>
-              </li>
-            `
-          )}
-        </ul>
+        ${this.renderFooterList(this.languageItems)}
 
         <div class="m-footer-small__disclaimer">
-          <ul class="m-footer-small__list">
-            ${repeat(
-              this.disclaimeritems,
-              languageItem => html`
-                <li class="m-footer-small__list-item">
-                  <axa-link class="m-footer-small__link" href="${languageItem.link}" color="white">${languageItem.text}</axa-link>
-                </li>
-              `
-            )}
-          </ul>
-          <div class="js-footer-small__copyright">${this.copyrighttext}</div>
+          ${this.renderFooterList(this.disclaimerItems)}
+          <div class="js-footer-small__copyright">${this.copyrightText}</div>
         </div>
       </article>
     `;
