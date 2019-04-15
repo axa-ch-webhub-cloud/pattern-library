b2426ed562a76a75da896d64ba371820b88d058a
Luca Mele <luca.mele@axa-winterthur.ch>
2019-04-15 16:50:27 +0200

    Add dev dependecies and intro text for a new element

---
+++ scripts/create-a-m-o/index.js
@@ -0,0 +1,24 @@
+const outdent = require('outdent');
+const chalk = require('chalk');
+
+process.stdin.setEncoding('utf8');
+
+console.log(chalk.cyan(outdent`
+    Hello Dear developer, thank you for contributing with us. 😊
+
+    I will help you to create a new web component 😎.
+
+    You can choose between ATOM📗, MOLECULE📘 or ORGANISM📙.
+
+    As a general guideline, an ATOM📗 is the smallest component.
+    It won't make sense to use it on its own, but is an essential building block.
+    An ATOM📗 should not have dependencies to other elements.
+
+    An ORGANISM📙 is the finished and ready to use component. It must have at least one element as dependency.
+    As a rule of thumb, everything that has 100% with is an organism.
+
+    A MOLECULE📘 can be a finished component and can be reused somewhere else but has never 100% with.
+    It must contain at least one ATOM📗.
+
+    Now, please tell me what do you wan to create:
+  `));
