b4a9e8cdf68cdbe4b7c166c178d7891f474b99d0
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-04-26 07:36:41 +0200

    Fix tests

--- src/components/20-molecules/footer-small/test.ui.js
+++ src/components/20-molecules/footer-small/test.ui.js
@@ -5,68 +5,37 @@ const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 fixture('Footer Small - Static links').page(`${host}/iframe.html?id=molecules-footer-small--footer-small-static-links`);
 
 test('should display four languages', async t => {
-  const german = Selector(
-    () =>
-      document
-        .querySelector(`axa-footer-small`)
-        .shadowRoot.querySelector('.m-footer-small__list')
-        .querySelectorAll('axa-link')[0]
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0]
-  );
+  const german = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot)
+    .find('a')
+    .withText('DE');
   await t.expect(german.exists).ok();
-  await t.expect(german.textContent).eql('DE');
 
-  const french = Selector(
-    () =>
-      document
-        .querySelector(`axa-footer-small`)
-        .shadowRoot.querySelector('.m-footer-small__list')
-        .querySelectorAll('axa-link')[1]
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0]
-  );
+  const french = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot)
+    .find('a')
+    .withText('FR');
   await t.expect(french.exists).ok();
-  await t.expect(french.textContent).eql('FR');
 
-  const italian = Selector(
-    () =>
-      document
-        .querySelector(`axa-footer-small`)
-        .shadowRoot.querySelector('.m-footer-small__list')
-        .querySelectorAll('axa-link')[2]
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0]
-  );
+  const italian = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot)
+    .find('a')
+    .withText('IT');
   await t.expect(italian.exists).ok();
-  await t.expect(italian.textContent).eql('IT');
 
-  const english = Selector(
-    () =>
-      document
-        .querySelector(`axa-footer-small`)
-        .shadowRoot.querySelector('.m-footer-small__list')
-        .querySelectorAll('axa-link')[3]
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0]
-  );
+  const english = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot)
+    .find('a')
+    .withText('EN');
   await t.expect(english.exists).ok();
-  await t.expect(english.textContent).eql('EN');
 });
 
 test('should display disclaimer section', async t => {
-  const termsOfUse = Selector(
-    () =>
-      document
-        .querySelector(`axa-footer-small`)
-        .shadowRoot.querySelector('.m-footer-small__disclaimer')
-        .querySelector('.m-footer-small__list-item')
-        .querySelectorAll('axa-link')[0]
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0]
-  );
+  const termsOfUse = Selector(() =>
+    document
+      .querySelector(`axa-footer-small`)
+      .shadowRoot.querySelector('.m-footer-small__disclaimer')
+      .querySelector('.m-footer-small__list-item')
+  )
+    .find('a')
+    .withText('Terms of use');
   await t.expect(termsOfUse.exists).ok();
-  await t.expect(termsOfUse.textContent).eql('Terms of use');
 });
 
 test('should have a copyright section', async t => {
@@ -78,23 +47,17 @@ test('should have a copyright section', async t => {
 });
 
 test('should have a link in the href attribute', async t => {
-  const german = Selector(() =>
-    document
-      .querySelector(`axa-footer-small`)
-      .shadowRoot.querySelector('.m-footer-small__list')
-      .querySelector('axa-link')
-  );
+  const german = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot.querySelector('.m-footer-small__list'))
+    .find('a')
+    .withText('DE');
   await t.expect(german.getAttribute('href')).eql('https://axa.ch/de/privatkunden.html');
 });
 
 fixture('Footer Small - Dynamic links').page(`${host}/iframe.html?id=molecules-footer-small--footer-small-dynamic-links`);
 
 test("should have 'javascript:void(0)' the href attribute", async t => {
-  const german = Selector(() =>
-    document
-      .querySelector(`axa-footer-small`)
-      .shadowRoot.querySelector('.m-footer-small__list')
-      .querySelector('axa-link')
-  );
+  const german = Selector(() => document.querySelector(`axa-footer-small`).shadowRoot.querySelector('.m-footer-small__list'))
+    .find('a')
+    .withText('DE');
   await t.expect(german.getAttribute('href')).eql(undefined);
 });
