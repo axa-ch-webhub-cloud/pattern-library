807a3be9fd5db0302d04bad1436f2bc5d6c78b80
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-02 09:28:35 +0200

    Add 'Item' declaration to TS types for footer

--- src/components/20-molecules/footer-small/index.react.d.ts
+++ src/components/20-molecules/footer-small/index.react.d.ts
@@ -1,15 +1,20 @@
 import React from 'react';
+import { link } from 'fs';
+
+interface Item {
+  text: string;
+  link: string;
+}
 
 // Not polished/finalised yet. Just an example.
 interface AXAFooterSmallProps {
-  languageItems: object[],
-  disclaimerItems: object[],
+  languageItems: Item[];
+  disclaimerItems: Item[];
   copyrightText: String;
   activeLanguage: String;
-  dynamic: boolean
+  dynamic: boolean;
 }
 
 declare function createAXAFooterSmall(createElement: typeof React.createElement): React.ComponentType<AXAFooterSmallProps>;
 
 export = createAXAFooterSmall;
-
