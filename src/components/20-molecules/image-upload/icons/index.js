ff806d63335cc0933e70ea690b44954176a6ceaf
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-02 13:28:33 +0200

    icons

---
+++ src/components/20-molecules/image-upload/icons/index.js
@@ -0,0 +1,4 @@
+export { default as CheckListSvg } from './check-list.svg';
+export { default as DocumentSvg } from './document.svg';
+export { default as ImagesSvg } from './images.svg';
+export { default as PlusRoundedSvg } from './plus-rounded.svg';
