41534960a465625f77c3c354cd5bdd4b8e652215
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-03 09:01:46 +0200

    add new attribute  "showImageOverview" f

--- src/components/20-molecules/image-upload/index.react.js
+++ src/components/20-molecules/image-upload/index.react.js
@@ -1,11 +1,12 @@
 import withReact from '../../../utils/with-react';
 import AXAImageUpload from './index';
 
-export default createElement => ({ title = 'Image Upload', fileButtonText = 'Datei hochlanden' }) =>
+export default createElement => ({ title = 'Image Upload', fileButtonText = 'Datei hochlanden', showImageOverview = false }) =>
   withReact(createElement)(
     AXAImageUpload.tagName,
     {
       title,
       fileButtonText,
+      showImageOverview,
     },
   );
