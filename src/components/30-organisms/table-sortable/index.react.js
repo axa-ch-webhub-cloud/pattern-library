7b6d5092961d4ecd0d10cdfcfd71e3073b9863a2
Luca Mele <lucamele1@hotmail.com>
2019-05-03 18:31:01 +0200

    Add mapping for model and successfull test

--- src/components/30-organisms/table-sortable/index.react.js
+++ src/components/30-organisms/table-sortable/index.react.js
@@ -1,5 +1,5 @@
 import withReact from '../../../utils/with-react';
 import AXATable from './';
 
-export default createElement => ({ children }) =>
-  withReact(createElement)(AXATable.tagName, null, children);
+export default createElement => ({ children, model, innerscroll }) =>
+  withReact(createElement)(AXATable.tagName, { model, innerscroll }, children);
--- src/components/30-organisms/table/index.react.js
+++ src/components/30-organisms/table/index.react.js
@@ -1,5 +1,5 @@
 import withReact from '../../../utils/with-react';
 import AXATable from './';
 
-export default createElement => ({ children }) =>
-  withReact(createElement)(AXATable.tagName, null, children);
+export default createElement => ({ children, innerscroll }) =>
+  withReact(createElement)(AXATable.tagName, { innerscroll }, children);
