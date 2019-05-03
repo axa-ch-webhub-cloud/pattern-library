9c7d1f3a8a81b939162aa18b83d401b048df78d8
Luca Mele <lucamele1@hotmail.com>
2019-05-03 13:49:35 +0200

    Add ui tests to axa-table

--- src/components/30-organisms/table/story.js
+++ src/components/30-organisms/table/story.js
@@ -7,7 +7,7 @@ import Readme from './README.md';
 storiesOf('Organisms/Table', module)
   .addDecorator(withMarkdown(Readme))
   .add(
-    'Table',
+    'Table - default',
     () => `
       <h3>AXA Table Default</h3>
       <axa-table>
@@ -38,37 +38,39 @@ storiesOf('Organisms/Table', module)
             </tbody>
           </table>
       </axa-table>
-      <br/>
-      <br/>
-
-      <h3>AXA Table innerscroll:</h3>
-      <axa-table innerscroll="500">
-          <table class="any-for-test">
-            <thead>
-              <tr>
-                <th>Titel 1</th>
-                <th>Titel 2</th>
-                <th>Titel 3</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Body 1</td>
-                <td class="o-table--light">Longer Body 2</td>
-                <td>Super Long Body 3 </td>
-              </tr>
-              <tr>
-                <td>Body A1 AA</td>
-                <td class="o-table--light">Longer Body A2</td>
-                <td>Super Long Body A3 </td>
-              </tr>
-              <tr>
-                <td>Body B1</td>
-                <td class="o-table--light">Longer Body B2</td>
-                <td>Super Long Body B3 </td>
-              </tr>
-            </tbody>
-          </table>
-      </axa-table>
+      `
+  )
+  .add(
+    'Table - innerscroll',
+    () => `
+    <h3>AXA Table innerscroll:</h3>
+    <axa-table innerscroll="500">
+        <table class="any-for-test">
+          <thead>
+            <tr>
+              <th>Titel 1</th>
+              <th>Titel 2</th>
+              <th>Titel 3</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Body 1</td>
+              <td class="o-table--light">Longer Body 2</td>
+              <td>Super Long Body 3 </td>
+            </tr>
+            <tr>
+              <td>Body A1 AA</td>
+              <td class="o-table--light">Longer Body A2</td>
+              <td>Super Long Body A3 </td>
+            </tr>
+            <tr>
+              <td>Body B1</td>
+              <td class="o-table--light">Longer Body B2</td>
+              <td>Super Long Body B3 </td>
+            </tr>
+          </tbody>
+        </table>
+    </axa-table>
       `
   );
---
+++ src/components/30-organisms/table/ui.test.js
@@ -0,0 +1,42 @@
+import { Selector } from 'testcafe';
+
+fixture('Table - basic functionality').page(
+  'http://localhost:9999/iframe.html?id=organisms-table--table-default'
+);
+
+const TAG = 'axa-table';
+const CLASS = '.o-table';
+
+test('should render table', async t => {
+  const $el = await Selector(TAG);
+  await t.expect($el.exists).ok();
+  const $elEl = await $el.find(CLASS);
+  await t.expect($elEl.exists).ok();
+});
+
+test('should render default table on mobile', async t => {
+  await t.resizeWindow(300, 1000);
+  const $el = await Selector('axa-table th');
+  await t.expect(await $el.getStyleProperty('display')).eql('none');
+});
+
+fixture('Table - innerscroll functionality').page(
+  'http://localhost:9999/iframe.html?id=organisms-table--table-innerscroll'
+);
+
+test('should render innerscroll table on mobile', async t => {
+  await t.resizeWindow(300, 1000);
+  const $el = await Selector('axa-table');
+  const $elChild = await Selector('.o-table');
+  const $elTh = await Selector('axa-table th');
+  await t.expect(await $elTh.getStyleProperty('display')).eql('table-cell');
+  await t.expect(await $el.getStyleProperty('overflow-x')).eql('auto');
+  const innerscroll = parseInt(await $el.getAttribute('innerscroll'), 10);
+  await t
+    .expect(parseInt(await $elChild.getStyleProperty('width'), 10))
+    .within(innerscroll - 1, innerscroll + 1);
+
+  await t
+    .expect(parseInt(await $el.getStyleProperty('width'), 10))
+    .within(250, 305);
+});
