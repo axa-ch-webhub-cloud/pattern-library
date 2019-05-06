8d6ac04e42627c652357fc81308178c8bce830be
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-06 11:42:15 +0200

    set figcaption with name from file, add classes for queryselector, refactor

--- src/components/20-molecules/image-upload/utils/imageFigure.js
+++ src/components/20-molecules/image-upload/utils/imageFigure.js
@@ -1,45 +1,37 @@
-export default (node, file, key) => {
-  // TODO
-  // if (this.wcNode[key].getAttribute('data-image-set')) {
-  //   return;
-  // }
+export default (node, file) => {
+
   const urlCreator = window.URL || window.webkitURL;
   const imageUrl = urlCreator.createObjectURL(file);
 
   const figure = document.createElement('figure');
-  figure.className = 'm-image-upload__img-figure';
+  figure.className = 'js-image-upload__img-figure m-image-upload__img-wrapper';
   node.appendChild(figure);
 
   const hoverArea = document.createElement('div');
-  hoverArea.className = 'm-image-upload__icon-hover-area';
+  hoverArea.className = 'm-image-upload__icon-hover-area js-image-upload__icon-hover-area';
   figure.appendChild(hoverArea);
 
   const img = document.createElement('img');
   img.className = 'm-image-upload__img-element';
   img.src = imageUrl;
-  img.alt = ''; // TODO
+  img.alt = file.name;
   hoverArea.appendChild(img);
 
   const iconLayer = document.createElement('div');
   iconLayer.className = 'm-image-upload__icon-layer';
   hoverArea.appendChild(iconLayer);
 
-  const binIcon = document.createElement('axa-icon');
-  // TODO class doesn't get set
-  binIcon.classList.add('m-image-upload__icon-baseline-delete-forever');
-  binIcon.setAttribute('icon', 'baseline-delete-forever');
-  iconLayer.appendChild(binIcon);
+  // TODO not axa-icon
+  // const binIcon = document.createElement('axa-icon');
+  // binIcon.classList.add('m-image-upload__icon-baseline-delete-forever');
+  // binIcon.setAttribute('icon', 'baseline-delete-forever');
+  // iconLayer.appendChild(binIcon);
 
   const figCaption = document.createElement('figcaption');
   figCaption.className = 'm-image-upload__img-caption';
-  const textNode = document.createTextNode('der titel');
+  const textNode = document.createTextNode(file.name);
   figCaption.appendChild(textNode);
   figure.appendChild(figCaption);
 
-  // this.wcNode.setAttribute('data-image-set', 'true');
-  // TODO
-  // on(hoverArea, 'click', () => console.log('image entfernen'), {
-  //   passive: false,
-  // });
   return figure;
 }
