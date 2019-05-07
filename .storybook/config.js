e35d58ae34ff76fdd760253e623dd81d42b9baab
Luca Mele <lucamele1@hotmail.com>
2019-05-07 16:50:23 +0200

    Remove polyfills

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,7 @@
 import { addParameters, configure } from '@storybook/html';
 import { create } from '@storybook/theming';
 
-import '@webcomponents/webcomponentsjs';
-import 'core-js/es6/reflect';
-import 'core-js/es6/promise';
-import 'core-js/fn/array/from';
-import 'core-js/fn/object/assign';
-import 'core-js/fn/object/create';
-import 'core-js/fn/object/define-properties';
-import 'core-js/fn/object/set-prototype-of';
+import '@axa-ch/patterns-library-polyfill';
 
 import logo from '../src/static/svg/logo-axa.svg';
 
