cc75367ca6d6d9b9623166bbfb1842629e280b2f
Luca Mele <lucamele1@hotmail.com>
2019-05-07 13:14:39 +0200

    Remove storybook overall polyfills as we relay on component based

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,6 @@
 import { addParameters, configure } from '@storybook/html';
 import { create } from '@storybook/theming';
 
-import '../src/utils/polyfills';
-
 import logo from '../src/static/svg/logo-axa.svg';
 
 // theme - logo some sort of broken
