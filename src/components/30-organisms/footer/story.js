05b9dc7278f4be3499dd93dbaa64b4b9bd4d9532
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-14 08:23:39 +0200

    Define props, init handlers

--- src/components/30-organisms/footer/story.js
+++ src/components/30-organisms/footer/story.js
@@ -7,5 +7,53 @@ import Readme from './README.md';
 storiesOf('Organisms/Footer', module)
   .addDecorator(withMarkdown(Readme))
   .add('Footer - default', () => {
-    return `<axa-footer></axa-footer>`;
+    // const columns = [{title: 'AXA & YOU'},{},{}];
+    const content = JSON.stringify([
+      {
+        title: 'axa & you',
+        items: [
+          { text: 'Contact', link: 'https://axa.ch/en/private-customers.html' },
+          {
+            text: 'Report a claim',
+            link: 'https://axa.ch/en/private-customers.html',
+          },
+          {
+            text: 'Job vacancies',
+            link: 'https://axa.ch/en/private-customers.html',
+          },
+          { text: 'MyAXA', link: 'https://axa.ch/en/private-customers.html' },
+          {
+            text: 'Customer reviews',
+            link: 'https://axa.ch/en/private-customers.html',
+          },
+          {
+            text: 'Garage Portal',
+            link: 'https://axa.ch/en/private-customers.html',
+          },
+        ],
+      },
+      {
+        title: 'axa worldwide',
+        items: [
+          {
+            text: 'AXA worldwide',
+            link: 'https://axa.ch/en/private-customers.html',
+          },
+        ],
+      },
+    ]);
+
+    const icons = JSON.stringify({
+      title: 'stay in touch',
+      icons: [
+        'facebook',
+        'instagram',
+        'twitter',
+        'xing',
+        'youtube',
+        'linkedin',
+      ],
+    });
+
+    return `<axa-footer content=${content}" icons="${icons}"></axa-footer>`;
   });
