582356b26cb5da975d103e075198525d877f899f
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-14 08:23:39 +0200

    Define props, init handlers

--- src/components/30-organisms/footer/index.js
+++ src/components/30-organisms/footer/index.js
@@ -1,5 +1,6 @@
 import '@webcomponents/webcomponentsjs';
 import { LitElement, html, css, unsafeCSS } from 'lit-element';
+import '@axa-ch/icon';
 
 /* eslint-disable import/no-extraneous-dependencies */
 import defineOnce from '../../../utils/define-once';
@@ -18,24 +19,66 @@ class AXAFooter extends LitElement {
 
   static get properties() {
     return {
+      content: { type: Array },
+      icons: { type: Object },
       dynamic: { type: Boolean },
     };
   }
 
   constructor() {
     super();
+    this.content = [];
+    this.icons = {};
     this.dynamic = false;
   }
 
   firstUpdated() {
-    // Add DOM changes here
-    // This will be rendered when the component is connected to the DOM
+    // var acc = document.getElementsByClassName('accordion');
+    // var i;
+    // for (i = 0; i < acc.length; i++) {
+    //   acc[i].addEventListener('click', function() {
+    //     this.classList.toggle('active');
+    //     var panel = this.nextElementSibling;
+    //     if (panel.style.maxHeight) {
+    //       panel.style.maxHeight = null;
+    //     } else {
+    //       panel.style.maxHeight = panel.scrollHeight + 'px';
+    //     }
+    //   });
+    // }
+  }
+
+  handleClick(e, index) {
+    e.stopPropagation();
+    console.log('newhandle', e.target);
   }
 
   render() {
     return html`
       <article class="o-footer">
-        <slot></slot>
+        ${repeat(
+          this.icons,
+          icon =>
+            html`
+              <axa-icon icon="download"></axa-icon>
+            `
+        )}
+
+        <button class="accordion active" @click="${this.handleClick}">
+          Section 1
+        </button>
+        <ul class="panel">
+          <li>hello</li>
+          <li>bonjour</li>
+          <li>hallo</li>
+        </ul>
+
+        <button class="accordion">Section 2</button>
+        <ul class="panel">
+          <li>hello</li>
+          <li>bonjour</li>
+          <li>hallo</li>
+        </ul>
       </article>
     `;
   }
