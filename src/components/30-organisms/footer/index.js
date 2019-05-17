1ee45629d91c968272a54b17b0ffd62f7bc56831
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-17 18:10:10 +0200

    Refactor flexbox architecture

--- src/components/30-organisms/footer/index.js
+++ src/components/30-organisms/footer/index.js
@@ -62,78 +62,94 @@ class AXAFooter extends LitElement {
   render() {
     return html`
       <article class="o-footer">
-        ${repeat(
-          this.content,
-          (contentItem, index) =>
-            html`
-              <div class="o-footer__accordion">
-                <button
-                  class="o-footer__accordion-content"
-                  @click="${ev => this.handleClick(ev, index)}"
-                >
-                  <span>
-                    ${contentItem.title}
-                  </span>
-                  <span
-                    class="o-footer__accordion-content-caret o-footer__accordion-content-caret${contentItem.active
-                      ? '--open'
-                      : ''}"
-                    >${svg([CaretSvg || ''])}</span
-                  >
-                </button>
-                <ul
-                  class="o-footer__accordion-content-panel o-footer__accordion-content-panel${contentItem.active
-                    ? '--open'
-                    : ''}"
-                >
-                  ${repeat(
-                    contentItem.items,
-                    i =>
-                      html`
-                        <li>
-                          <a href=${i.link}>${i.text}</a>
-                        </li>
-                      `
-                  )}
-                </ul>
-              </div>
-            `
-        )}
-
-        <div class="o-footer__collection">
+        <div class="mobile">
           ${repeat(
             this.content,
             (contentItem, index) =>
               html`
-                <div class="o-footer__collection-container">
-                  <div>${contentItem.title}</div>
-                  <div class="o-footer__collection-container-list">
+                <div class="o-footer__accordion">
+                  <button
+                    class="o-footer__accordion-content"
+                    @click="${ev => this.handleClick(ev, index)}"
+                  >
+                    <span>
+                      ${contentItem.title}
+                    </span>
+                    <span
+                      class="o-footer__accordion-content-caret o-footer__accordion-content-caret${contentItem.active
+                        ? '--open'
+                        : ''}"
+                      >${svg([CaretSvg || ''])}</span
+                    >
+                  </button>
+                  <ul
+                    class="o-footer__accordion-content-panel o-footer__accordion-content-panel${contentItem.active
+                      ? '--open'
+                      : ''}"
+                  >
                     ${repeat(
                       contentItem.items,
-                      i => html`
-                        <div class="o-footer__collection-container-list-item">
-                          <a href=${i.link}>${i.text}</a>
-                        </div>
-                      `
+                      i =>
+                        html`
+                          <li>
+                            <a href=${i.link}>${i.text}</a>
+                          </li>
+                        `
                     )}
-                  </div>
+                  </ul>
                 </div>
               `
           )}
+          <div>
+            <ul class="o-footer__social-media">
+              ${repeat(this.iconArea.icons, icon => {
+                return html`
+                  <li>
+                    <a href="${icon.link}" target="_blank"
+                      >${svg([this.iconMap.get(icon.title) || ''])}</a
+                    >
+                  </li>
+                `;
+              })}
+            </ul>
+          </div>
         </div>
 
-        <div>
-          <ul class="o-footer__social-media">
-            ${repeat(this.iconArea.icons, icon => {
-              return html`
-                <li>
-                  <a href="${icon.link}" target="_blank"
-                    >${svg([this.iconMap.get(icon.title) || ''])}</a
-                  >
-                </li>
-              `;
-            })}
-          </ul>
+        <div class="tablet">
+          <div class="o-footer__collection">
+            ${repeat(
+              this.content,
+              (contentItem, index) =>
+                html`
+                  <div class="o-footer__collection-container">
+                    <div>${contentItem.title}</div>
+                    <div class="o-footer__collection-container-list">
+                      ${repeat(
+                        contentItem.items,
+                        i => html`
+                          <div class="o-footer__collection-container-list-item">
+                            <a href=${i.link}>${i.text}</a>
+                          </div>
+                        `
+                      )}
+                    </div>
+                  </div>
+                `
+            )}
+          </div>
+          <div>
+            <ul class="o-footer__social-media">
+              ${repeat(this.iconArea.icons, icon => {
+                return html`
+                  <li>
+                    <a href="${icon.link}" target="_blank"
+                      >${svg([this.iconMap.get(icon.title) || ''])}</a
+                    >
+                  </li>
+                `;
+              })}
+            </ul>
+          </div>
         </div>
       </article>
     `;
