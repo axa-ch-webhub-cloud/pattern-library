31c8f6f7cf87b7a2ff51d362ef38ce55a8ffc1ed
Marco Kruse <marco-kruse@freenet.de>
2019-05-17 15:56:15 +0200

    Work on input-text

--- src/components/10-atoms/input-text/index.js
+++ src/components/10-atoms/input-text/index.js
@@ -1,11 +1,11 @@
 import '@webcomponents/webcomponentsjs';
-import { LitElement, html, css, unsafeCSS } from 'lit-element';
-
+import { html, css, unsafeCSS } from 'lit-element';
+import NoShadowDOM from '../../../utils/no-shadow';
 /* eslint-disable import/no-extraneous-dependencies */
 import defineOnce from '../../../utils/define-once';
 import styles from './index.scss';
 
-class AXAInputText extends LitElement {
+class AXAInputText extends NoShadowDOM {
   static get tagName() {
     return 'axa-input-text';
   }
@@ -17,22 +17,48 @@ class AXAInputText extends LitElement {
   }
 
   static get properties() {
-    // Define properties and types
     return {
+      // Could be Field class
       id: { type: String },
-      type: { type: String },
       name: { type: String },
-      onClick: { type: Function },
+      label: { type: String },
       placeholder: { type: String },
       value: { type: String },
       valid: { type: Boolean },
-      disabled: { type: Boolean }
+      // Error Message
+      error: { type: String },
+      required: { type: Boolean },
+      // Validation
+
+      validate: { type: String },
+      disabled: { type: Boolean },
+      // Info Message proposal maybe obsolete
+      info: { type: String },
+
+      // text, email
+      type: { type: String },
+
+      // proposal
+      debug: { type: Boolean },
     };
   }
 
   constructor() {
     super();
-    this.onClick = () => {};
+    this.id = this.id || `input-text-${Math.random().toString(36).slice(2)}`;
+    this.name = '';
+    this.type = 'text';
+    this.label = '';
+    this.placeholder = '';
+
+    // internal properties
+    this.state = {
+      isControlled: false,
+      timer: null,
+      checked: false,
+      native: false,
+    };
+
   }
 
   firstUpdated() {
@@ -41,8 +67,12 @@ class AXAInputText extends LitElement {
   }
 
   render() {
+    const { id, label = '' } = this;
+
     return html`
     <div>
+    
+      ${label && html`<label for=${id}>${label}</label>`}
       <input
         class="a-input-text"
         autocomplete="off"
