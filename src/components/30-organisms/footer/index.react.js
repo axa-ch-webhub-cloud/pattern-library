0d803ed5c667f6f5fb32b72a120bbc4535d77033
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-20 18:02:16 +0200

    Make available for react

--- src/components/30-organisms/footer/index.react.js
+++ src/components/30-organisms/footer/index.react.js
@@ -1,7 +1,13 @@
 import withReact from '../../../utils/with-react';
 import AXAFooter from './index';
 
-export default createElement => ({ dynamic = false }) =>
+export default createElement => ({
+  content = [],
+  social = {},
+  dynamic = false,
+}) =>
   withReact(createElement)(AXAFooter.tagName, {
+    content,
+    social,
     dynamic,
   });
