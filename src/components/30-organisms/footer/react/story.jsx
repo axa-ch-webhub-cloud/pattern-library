7605edbc2179510f093f26883bfe61bcd7705f2e
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-21 09:30:06 +0200

    Create react example of footer with working dynamic links

--- src/components/30-organisms/footer/react/story.jsx
+++ src/components/30-organisms/footer/react/story.jsx
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom';
 import { withMarkdown } from '../../../../../.storybook/addons/markdown';
 import Readme from '../README.md';
 
-storiesOf('Molecules/Footer Small/React', module)
+storiesOf('Organisms/Footer/React', module)
   .addDecorator(withMarkdown(Readme))
-  .add('Footer', () => {
+  .add('Footer React', () => {
     const div = document.createElement('div');
     div.id = 'footer';
     ReactDOM.render(<DemoFooter />, div);
