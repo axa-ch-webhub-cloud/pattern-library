585761513e191120c9f5fd2bf7b41c8c848ed482
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-21 10:18:21 +0200

    Adapt footer test to point to right location

--- src/components/30-organisms/footer/ui.test.js
+++ src/components/30-organisms/footer/ui.test.js
@@ -2,7 +2,9 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Footer - basic functionality').page(`${host}/iframe.html?id=organisms-footer--footer-default`);
+fixture('Footer - basic functionality').page(
+  `${host}/iframe.html?id=organisms-footer--footer`
+);
 
 const TAG = 'axa-footer';
 const CLASS = '.o-footer';
@@ -10,7 +12,9 @@ const CLASS = '.o-footer';
 test('should render footer', async t => {
   const $axaElem = await Selector(TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(() => document.querySelector('axa-footer').shadowRoot);
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-footer').shadowRoot
+  );
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
 });
