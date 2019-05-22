23056c2f3a5cbc16f75ff82f04c8dc44f15720a6
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-05-22 15:37:54 +0200

    Add tests for footer

--- src/components/30-organisms/footer/ui.test.js
+++ src/components/30-organisms/footer/ui.test.js
@@ -1,10 +1,12 @@
 import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
-// await t.resizeWindow(300, 1000);
-fixture('Footer - basic functionality').page(
-  `${host}/iframe.html?id=organisms-footer--footer`
-);
+
+fixture('Footer - basic functionality')
+  .page(`${host}/iframe.html?id=organisms-footer--footer`)
+  .beforeEach(async t => {
+    await t.maximizeWindow();
+  });
 
 const TAG = 'axa-footer';
 const CLASS = '.o-footer';
@@ -19,12 +21,60 @@ test('should render footer', async t => {
   await t.expect($axaElemShadowEl.exists).ok();
 });
 
-test('should render footer titles', async t => {
-  const $axaContainer = Selector(() =>
+test('should correctly render footer titles', async t => {
+  const $footerTitle = Selector(() =>
     document
       .querySelector('axa-footer')
-      .shadowRoot.querySelector('.o-footer__title')
+      .shadowRoot.querySelector('.o-footer__tablet')
+      .querySelector('.o-footer__title')
   );
 
-  await t.expect($axaContainer.textContent).contains('axa & you');
+  await t.expect($footerTitle.textContent).contains('axa & you');
+  await t.expect().ok();
+});
+
+test('should correctly render footer link content', async t => {
+  const $footer = Selector(() =>
+    document
+      .querySelector('axa-footer')
+      .shadowRoot.querySelector('.o-footer__tablet')
+  );
+
+  const contactLink = $footer.find('a').withText('Contact');
+  await t.expect(contactLink.exists).ok();
+});
+
+test('should render accordion only in mobile mode', async t => {
+  await t.resizeWindow(576, 400);
+  const $accordion = Selector(() =>
+    document
+      .querySelector('axa-footer')
+      .shadowRoot.querySelector('.o-footer__accordion-content')
+  );
+
+  await t.expect($accordion.visible).notOk();
+  await t.resizeWindow(575, 400);
+  await t.expect($accordion.visible).ok();
+});
+
+test('should correctly render social media title in desktop view', async t => {
+  const $socialMediaTitle = Selector(() =>
+    document
+      .querySelector('axa-footer')
+      .shadowRoot.querySelector('.o-footer__tablet')
+      .querySelector('.o-footer__social-media-title')
+  );
+
+  await t.expect($socialMediaTitle.textContent).contains('stay in touch');
+
+  await t.resizeWindow(575, 400);
+  await t.expect($socialMediaTitle.visible).notOk();
+  await t.resizeWindow(767, 400);
+  await t.expect($socialMediaTitle.visible).notOk();
+  await t.resizeWindow(991, 400);
+  await t.expect($socialMediaTitle.visible).notOk();
+  await t.resizeWindow(992, 400);
+  await t.expect($socialMediaTitle.visible).ok();
+  await t.resizeWindow(1200, 400);
+  await t.expect($socialMediaTitle.visible).ok();
 });
