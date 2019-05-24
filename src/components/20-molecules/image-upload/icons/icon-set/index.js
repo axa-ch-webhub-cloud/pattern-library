885eac4b3ad77eccc4d6237a0679261e809b60ad
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-24 10:56:26 +0200

    set structure for image-upload specific icons

---
+++ src/components/20-molecules/image-upload/icons/icon-set/index.js
@@ -0,0 +1,5 @@
+export { default as ImageUploadGroupSvg } from './image-upload-group.svg';
+export { default as PlusIconSvg } from './plus.svg';
+export { default as DeleteSvg } from './delete.svg';
+export { default as UploadCloudSvg } from './upload-cloud.svg';
+export { default as TickSvg } from './tick.svg';
