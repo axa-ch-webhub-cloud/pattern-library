3d268f45a0100f590d7c5302afd181d2b668736f
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-24 11:12:10 +0200

    add component specific icons

--- src/components/20-molecules/image-upload/icons/icon-set/index.js
+++ src/components/20-molecules/image-upload/icons/icon-set/index.js
@@ -1,5 +1,5 @@
 export { default as ImageUploadGroupSvg } from './image-upload-group.svg';
-export { default as PlusIconSvg } from './plus.svg';
+export { default as PlusRoundedSvg } from './plus-rounded.svg';
 export { default as DeleteSvg } from './delete.svg';
 export { default as UploadCloudSvg } from './upload-cloud.svg';
 export { default as TickSvg } from './tick.svg';
