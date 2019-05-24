412a965ab1ae4921b5dd33824bb3cb92b746a3b2
Marc Illien <marc.illien@gmail.com>
2019-05-24 16:24:13 +0200

    Move styles into style folder

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,6 @@
 module.exports = [
-  'components/00-materials/00-colors.scss',
-  'components/00-materials/10-helpers.scss',
-  'components/00-materials/20-typography-and-text.scss',
-  'components/00-materials/30-layout.scss',
+  'components/00-materials/styles/00-colors.scss',
+  'components/00-materials/styles/10-helpers.scss',
+  'components/00-materials/styles/20-typography-and-text.scss',
+  'components/00-materials/styles/30-layout.scss',
 ];
