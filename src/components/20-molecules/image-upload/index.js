a3376c81898a7ce71fae8b322e38fae65cfbe38c
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-24 15:22:55 +0200

    render image upload overview

--- src/components/20-molecules/image-upload/index.js
+++ src/components/20-molecules/image-upload/index.js
@@ -1,4 +1,5 @@
 import { LitElement, html, svg, css, unsafeCSS } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
 import {
   DeleteSvg,
   ImageUploadGroupSvg,
@@ -9,6 +10,9 @@ import {
 /* eslint-disable import/no-extraneous-dependencies */
 import defineOnce from '../../../utils/define-once';
 import styles from './index.scss';
+
+const OR = 'or';
+const INFO = 'Drag and drop to upload your file';
 
 const DeleteIcon = svg([DeleteSvg]);
 const ImageUploadGroupIcon = svg([ImageUploadGroupSvg]);
@@ -36,14 +40,50 @@ class AXAImageUpload extends LitElement {
 
   constructor() {
     super();
-    this.inputFileText = 'Datei hochladen';
+    this.inputFileText = 'Upload file';
     this.onClick = () => {};
+
+    this.showImageOverview = false;
   }
 
   render() {
     return html`
       <article class="m-image-upload">
-        <slot></slot>
+        <h1><slot></slot></h1>
+        <section
+          @dragover="${this.handleImageUploadDropZoneDragover}"
+          @dragleave="${this.handleImageUploadDropZoneDragleave}"
+          @drop="${this.handleImageUploadDropZoneDrop}"
+          @click="${this.handleImageUploadDropZoneClick}"
+          class="${classMap(classes)}"
+        >
+          ${this.showImageOverview
+            ? html`
+                <p>TODO</p>
+              `
+            : html`
+                <div class="m-image-upload__icons">
+                  ${ImageUploadGroupIcon}
+                </div>
+                <p class="m-image-upload__information">${INFO}</p>
+                <p class="m-image-upload__or">${OR}</p>
+                <!-- TODO: replace with AXAInputFile when released------------->
+                <label
+                  for="file-upload"
+                  class="m-button__image-upload-label"
+                ></label>
+                <input
+                  @change="${this.handleImageUploadButtonChange}"
+                  type="file"
+                  accept="image/jpg, image/jpeg, application/pdf, image/png"
+                  capture="camera"
+                  multiple="multiple"
+                  class="m-button__image-upload-input"
+                  id="file-upload"
+                />
+                <!-- END ------------------------------------------------------>
+              `}
+        </section>
       </article>
     `;
   }
