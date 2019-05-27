b49bcc640df5111feb3c6da76ef78c22e4aab238
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-27 15:53:21 +0200

    test disabled, accept, multiple, capture and react

--- src/components/10-atoms/input-file/ui.test.js
+++ src/components/10-atoms/input-file/ui.test.js
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
@@ -6,12 +6,84 @@ fixture('Input file - basic functionality').page(
   `${host}/iframe.html?id=atoms-input-file--input-file-default`
 );
 
-const TAG = 'axa-input-file';
-const CLASS = '.a-input-file';
+const INPUT_FILE_TAG = 'axa-input-file';
+const ICON_TAG = 'axa-icon';
+const INPUT_FILE_CLASS = '.a-input-file';
+const INPUT_FILE_INPUT_CLASS = '.a-input-file__input';
 
 test('should render input-file', async t => {
-  const $axaElem = await Selector(TAG);
+  const $axaElem = await Selector(INPUT_FILE_TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemEl = await $axaElem.find(CLASS);
+  const $axaElemEl = await $axaElem.find(INPUT_FILE_CLASS);
   await t.expect($axaElemEl.exists).ok();
+});
+
+fixture('Input File - icon').page(
+  `${host}/iframe.html?id=atoms-input-file--input-file-icon`
+);
+
+test('should render icon', async t => {
+  const $axaElem = await Selector(INPUT_FILE_TAG);
+  await t.expect($axaElem.exists).ok();
+  const $axaIcon = await $axaElem.find(ICON_TAG);
+  await t.expect($axaIcon.exists).ok();
+});
+
+fixture('Input File - set properties').page(
+  `${host}/iframe.html?id=atoms-input-file--input-file-default`
+);
+
+test('should set input file element disabled', async t => {
+  const setDisabled = ClientFunction(() => {
+    document.querySelector('axa-input-file').disabled = true;
+  });
+  await setDisabled();
+  const $axaElm = await Selector('axa-input-file');
+  await t.expect(await $axaElm.hasAttribute('disabled')).ok();
+});
+
+test('should set input file accept', async t => {
+  const setAccept = ClientFunction(() => {
+    document.querySelector('axa-input-file > input').accept = 'application/pdf';
+  });
+  await setAccept();
+  const $axaElm = await Selector('axa-input-file');
+  const $axaElmInput = await $axaElm.find(INPUT_FILE_INPUT_CLASS);
+  await t
+    .expect((await $axaElmInput.getAttribute('accept')) === 'application/pdf')
+    .ok();
+});
+
+test('should set input file multiple', async t => {
+  const setMultiple = ClientFunction(() => {
+    document.querySelector('axa-input-file > input').multiple = true;
+  });
+  await setMultiple();
+  const $axaElm = await Selector('axa-input-file');
+  const $axaElmInput = await $axaElm.find(INPUT_FILE_INPUT_CLASS);
+  await t.expect(await $axaElmInput.hasAttribute('multiple')).ok();
+});
+
+test('should set input file capture', async t => {
+  const setCapture = ClientFunction(() => {
+    document.querySelector('axa-input-file > input').capture = true;
+  });
+  await setCapture();
+  const $axaElm = await Selector('axa-input-file');
+  const $axaElmInput = await $axaElm.find(INPUT_FILE_INPUT_CLASS);
+  await t.expect(await $axaElmInput.hasAttribute('capture')).ok();
+});
+
+fixture('Input File - react smoke test').page(
+  `${host}/iframe.html?id=atoms-input-file-react--inputfile-default`
+);
+
+test('should render react input file', async t => {
+  const $axaButton = await Selector('axa-input-file');
+  await t.expect($axaButton.exists).ok();
+  const $axaButtonShadow = await Selector(() =>
+    document.querySelector('axa-input-file')
+  );
+  const $axaButtonShadowEl = await $axaButtonShadow.find(INPUT_FILE_CLASS);
+  await t.expect($axaButtonShadowEl.exists).ok();
 });
