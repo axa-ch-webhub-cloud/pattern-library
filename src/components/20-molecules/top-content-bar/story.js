7728d2ed4e0834049d419c68bdaa9a67f1337d99
Luca Mele <lucamele1@hotmail.com>
2019-05-27 12:25:56 +0200

    Add more meaningfulls texts to top content bar

--- src/components/20-molecules/top-content-bar/story.js
+++ src/components/20-molecules/top-content-bar/story.js
@@ -11,47 +11,45 @@ storiesOf('Molecules/Top content bar', module)
   .add(
     'Top content bar - Default with button',
     () =>
-      `<axa-top-content-bar ctatext="Click me">
-          Some children Some children Some children Some children
-          Some children Some children Some children
+      `<axa-top-content-bar ctatext="See Blog article">
+          Undefined flighting object detected in your region.
+          People are paniking. Stay calm
        </axa-top-content-bar>`
   )
   .add(
     'Top content bar - Default no button',
     () =>
       `<axa-top-content-bar>
-          Some children Some children Some children Some children
-          Some children Some children Some children
+        Undefined flighting object detected in your region.
+        People are paniking. Stay calm
        </axa-top-content-bar>`
   )
   .add(
     'Top content bar - Red with button',
     () =>
-      `<axa-top-content-bar variant="warning" ctatext="Click me">
-          Some children Some children Some children Some children
-          Some children Some children Some children
+      `<axa-top-content-bar variant="warning" ctatext="Claim a damage">
+          Did the hailstorm damage your car?
        </axa-top-content-bar>`
   )
   .add(
     'Top content bar - Red no button',
     () =>
       `<axa-top-content-bar variant="warning">
-          Some children Some children Some children Some children
-          Some children Some children Some children
+          Attention, tunderstorm is approaching near your house!
        </axa-top-content-bar>`
   )
   .add(
     'Top content bar - Default in text link',
     () =>
       `<axa-top-content-bar>
-          Some children <axa-link>and a link</axa-link> in the text
+          Undefined flighting object detected in your region. <axa-link>Link to the blog article</axa-link>
        </axa-top-content-bar>`
   )
   .add(
     'Top content bar - Default Button-link',
     () =>
-      `<axa-top-content-bar ctatext="Click Me" href="http://www.axa.ch">
-          Some children Some children Some children Some children
-          Some children Some children Some children
+      `<axa-top-content-bar ctatext="See Blog article" href="http://www.axa.ch">
+          Undefined flighting object detected in your region.
+          People are paniking. Stay calm
        </axa-top-content-bar>`
   );
