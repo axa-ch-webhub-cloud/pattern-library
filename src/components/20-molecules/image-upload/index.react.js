538c53e466209956a2af3c8cfc33f783156ed2db
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-29 15:18:09 +0200

    add showImageOverview as a attribute

--- src/components/20-molecules/image-upload/index.react.js
+++ src/components/20-molecules/image-upload/index.react.js
@@ -1,11 +1,16 @@
 import withReact from '../../../utils/with-react';
 import AXAImageUpload from './index';
 
-export default createElement => ({ inputFileText = 'Upload file', children }) =>
+export default createElement => ({
+  inputFileText = 'Upload file',
+  showImageOverview = false,
+  children,
+}) =>
   withReact(createElement)(
     AXAImageUpload.tagName,
     {
       inputFileText,
+      showImageOverview,
     },
     children
   );
