072e05a5479be49a1c9297cf6c3cd37953b965ba
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-05-29 16:52:03 +0200

    Changes PR - delete unnecessary comment and empty line - remove object deconstruction - set text-align to center, that when text in button uses two lines the text is still aligned in the middle

--- src/components/10-atoms/input-file/demo.js
+++ src/components/10-atoms/input-file/demo.js
@@ -1,13 +1,11 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
-// import { html, render } from 'lit-html';
 import { withMarkdown } from '../../../../.storybook/addons/markdown';
 import Readme from './README.md';
 import './index';
 
 storiesOf('Atoms/Input File/Demos', module)
   .addDecorator(withMarkdown(Readme))
-
   .add('Feature - InputFile clickable', () => {
     const inputFile = document.createElement('axa-input-file');
     inputFile.setAttribute('multiple', 'multiple');
@@ -15,10 +13,7 @@ storiesOf('Atoms/Input File/Demos', module)
     inputFile.innerHTML = `You have chosen ${counter} files`;
 
     inputFile.addEventListener('input', e => {
-      const {
-        target: { files },
-      } = e;
-      counter = files.length;
+      counter = e.target.files.length;
       inputFile.querySelector(
         '.a-input-file'
       ).innerHTML = `You have chosen ${counter} files`;
