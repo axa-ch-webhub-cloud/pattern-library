c0b1f603b66132ab4204633f2dbccadbc32eb098
Marc Illien <marc.illien@gmail.com>
2019-05-29 23:37:24 +0200

    Add test for testing the variant

--- src/components/10-atoms/title-section/ui.test.js
+++ src/components/10-atoms/title-section/ui.test.js
@@ -1,8 +1,10 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Title section - basic functionality').page(`${host}/iframe.html?id=atoms-title-section--title-section-default`);
+fixture('Title section - basic functionality').page(
+  `${host}/iframe.html?id=atoms-title-section--title-section`
+);
 
 const TAG = 'axa-title-section';
 const CLASS = '.a-title-section';
@@ -10,7 +12,24 @@ const CLASS = '.a-title-section';
 test('should render title-section', async t => {
   const $axaElem = await Selector(TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(() => document.querySelector('axa-title-section').shadowRoot);
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-title-section').shadowRoot
+  );
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+fixture('Title section - basic functionality').page(
+  `${host}/iframe.html?id=atoms-title-section--title-section-white`
+);
+
+test('should render a title with white color', async t => {
+  const getTextColor = ClientFunction(() => {
+    const titleSection = document.querySelector('axa-title-section').shadowRoot;
+    return window
+      .getComputedStyle(titleSection.querySelector('.a-title-section--white'))
+      .getPropertyValue('color');
+  });
+  const measuredColor = await getTextColor();
+  await t.expect(measuredColor).eql('rgb(255, 255, 255)');
 });
