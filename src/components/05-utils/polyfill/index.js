ccd43079c0c6f919b06f60fe383d6762ad7db682
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-06-07 13:45:57 +0200

    Replace string includes polyfill with the fn reference

--- src/components/05-utils/polyfill/index.js
+++ src/components/05-utils/polyfill/index.js
@@ -1,7 +1,7 @@
 import 'core-js/es6/reflect';
 import 'core-js/es6/promise';
 import 'core-js/fn/array/includes';
-import 'core-js/modules/es6.string.includes';
+import 'core-js/fn/string/includes';
 import 'core-js/fn/array/from';
 import 'core-js/fn/object/assign';
 import './index.webcomponents';
