32f0c7caf2c8701896e3f25fbce2878f805ea1e8
Marc Illien <marc.illien@gmail.com>
2019-06-11 19:57:06 +0200

    Add tests for responsive fonts

--- src/components/10-atoms/title-primary/story.js
+++ src/components/10-atoms/title-primary/story.js
@@ -7,31 +7,13 @@ import Readme from './README.md';
 storiesOf('Atoms/Title primary', module)
   .addDecorator(withMarkdown(Readme))
   .add(
-    'Title primary - Size 1',
-    () => '<axa-title-primary>Title primary 1</axa-title-primary>'
-  )
-  .add(
-    'Title primary - Size 2',
-    () =>
-      '<axa-title-primary variant="size-2">Title primary 2</axa-title-primary>'
-  )
-  .add(
-    'Title primary - Size 3',
-    () =>
-      '<axa-title-primary variant="size-3">Title primary 3</axa-title-primary>'
-  )
-  .add(
-    'Title primary - Size 4',
-    () =>
-      '<axa-title-primary variant="size-4">Title primary 4</axa-title-primary>'
-  )
-  .add(
-    'Title primary - Size 5',
-    () =>
-      '<axa-title-primary variant="size-5">Title primary 5</axa-title-primary>'
-  )
-  .add(
-    'Title primary - Size 6',
-    () =>
-      '<axa-title-primary variant="size-6">Title primary 6</axa-title-primary>'
+    'Title primary',
+    () => `
+      <axa-title-primary>Title primary 1</axa-title-primary>
+      <axa-title-primary variant="size-2">Title primary 2</axa-title-primary>
+      <axa-title-primary variant="size-3">Title primary 3</axa-title-primary>
+      <axa-title-primary variant="size-4">Title primary 4</axa-title-primary>
+      <axa-title-primary variant="size-5">Title primary 5</axa-title-primary>
+      <axa-title-primary variant="size-6">Title primary 6</axa-title-primary>
+    `
   );
