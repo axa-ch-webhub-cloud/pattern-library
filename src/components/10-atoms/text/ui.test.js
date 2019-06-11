42f8fd41bfb77e57f267c7b58666990a2f5db6df
Marc Illien <marc.illien@gmail.com>
2019-06-11 21:24:51 +0200

    Improve tests for axa test

--- src/components/10-atoms/text/ui.test.js
+++ src/components/10-atoms/text/ui.test.js
@@ -2,9 +2,11 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Text - basic functionality').page(
-  `${host}/iframe.html?id=atoms-text--text-default`
-);
+fixture('Text - basic functionality')
+  .page(`${host}/iframe.html?id=atoms-text--text-default`)
+  .beforeEach(async t => {
+    await t.resizeWindow(380, 680);
+  });
 
 const TAG = 'axa-text';
 const CLASS = '.a-text';
@@ -17,4 +19,110 @@ test('should render text', async t => {
   );
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+test('should have correct font definitions for text size 1', async t => {
+  const $axaElemShadow = await Selector(() =>
+    document.querySelector('axa-text').shadowRoot.querySelector('.a-text')
+  );
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('18px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('27px');
+
+  await t.resizeWindow(800, 600);
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('20px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('30px');
+});
+
+fixture('Text - Size 2')
+  .page(`${host}/iframe.html?id=atoms-text--text-size-2`)
+  .beforeEach(async t => {
+    await t.resizeWindow(380, 680);
+  });
+
+test('should have correct font definitions for text size 2', async t => {
+  const $axaElemShadow = await Selector(() =>
+    document
+      .querySelector('axa-text[variant="size-2"]')
+      .shadowRoot.querySelector('.a-text')
+  );
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('16px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('24px');
+
+  await t.resizeWindow(800, 600);
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('18px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('27px');
+});
+
+fixture('Text - Size 3')
+  .page(`${host}/iframe.html?id=atoms-text--text-size-3`)
+  .beforeEach(async t => {
+    await t.resizeWindow(380, 680);
+  });
+
+test('should have correct font definitions for text size 3', async t => {
+  const $axaElemShadow = await Selector(() =>
+    document
+      .querySelector('axa-text[variant="size-3"]')
+      .shadowRoot.querySelector('.a-text')
+  );
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('14px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('17px');
+
+  await t.resizeWindow(800, 600);
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-size'))
+    .eql('16px');
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('line-height'))
+    .eql('24px');
+});
+
+fixture('Text - Bold')
+  .page(`${host}/iframe.html?id=atoms-text--text-bold`)
+  .beforeEach(async t => {
+    await t.resizeWindow(380, 680);
+  });
+
+test('should have correct font weight for text bold', async t => {
+  const $axaElemShadow = await Selector(() =>
+    document
+      .querySelector('axa-text[variant="bold"]')
+      .shadowRoot.querySelector('.a-text')
+  );
+
+  await t
+    .expect(await $axaElemShadow.getStyleProperty('font-weight'))
+    .eql('700');
 });
