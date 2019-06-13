3f7dea8b12aac9c14e447504ddce98a88f17f2e2
Marc Illien <marc.illien@gmail.com>
2019-06-13 11:31:56 +0200

    Add react version for atom text

--- src/components/10-atoms/text/index.react.js
+++ src/components/10-atoms/text/index.react.js
@@ -1,7 +1,7 @@
 import withReact from '../../../utils/with-react';
 import AXAText from './index';
 
-export default createElement => ({ variant, children }) =>
+export default createElement => ({ variant = '', children }) =>
   withReact(createElement)(
     AXAText.tagName,
     {
