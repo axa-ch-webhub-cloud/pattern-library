4768e4fabc9f7db25716bb309668cc5d62fa0e44
Luca Mele <lucamele1@hotmail.com>
2019-06-20 17:20:23 +0200

    Add tests to cookie disclaimer

--- src/components/20-molecules/cookie-disclaimer/ui.test.js
+++ src/components/20-molecules/cookie-disclaimer/ui.test.js
@@ -1,8 +1,10 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Cookie disclaimer - basic functionality').page(`${host}/iframe.html?id=molecules-cookie-disclaimer--cookie-disclaimer-default`);
+fixture('Cookie disclaimer - basic functionality').page(
+  `${host}/iframe.html?id=molecules-cookie-disclaimer--cookie-disclaimer-default`
+);
 
 const TAG = 'axa-cookie-disclaimer';
 const CLASS = '.m-cookie-disclaimer';
@@ -10,7 +12,42 @@ const CLASS = '.m-cookie-disclaimer';
 test('should render cookie-disclaimer', async t => {
   const $axaElem = await Selector(TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(() => document.querySelector('axa-cookie-disclaimer').shadowRoot);
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-cookie-disclaimer').shadowRoot
+  );
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+test('should store in localStorage the date of when accepted', async t => {
+  const $axaElem = await Selector(TAG);
+  await t.expect($axaElem.exists).ok();
+  const $axaBtnShadow = await Selector(() =>
+    document
+      .querySelector('axa-cookie-disclaimer')
+      .shadowRoot.querySelector('axa-button')
+  );
+  await t.click($axaBtnShadow);
+  const timeStamp = await ClientFunction(() => {
+    const time = window.localStorage.getItem(
+      'axa-ch-cookie-disclaimer-accepted'
+    );
+    return time;
+  });
+  const time = new Date().getTime();
+  await t
+    .expect(parseInt(await timeStamp(), 10))
+    .within(time - 100, time + 100);
+});
+
+test('should not render once accepted', async t => {
+  const $axaElem = await Selector(TAG);
+  await t.expect($axaElem.exists).ok();
+  const $axaBtnShadow = await Selector(() =>
+    document
+      .querySelector('axa-cookie-disclaimer')
+      .shadowRoot.querySelector('axa-button')
+  );
+  await t.click($axaBtnShadow);
+  await t.expect($axaElem.exists).notOk();
 });
