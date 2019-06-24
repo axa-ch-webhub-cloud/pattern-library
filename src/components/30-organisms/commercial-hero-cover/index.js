c5742e459e5ead589c4242dbff02abee94a5caf2
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-06-24 16:44:55 +0200

    Add template and correct props

--- src/components/30-organisms/commercial-hero-cover/index.js
+++ src/components/30-organisms/commercial-hero-cover/index.js
@@ -19,12 +19,12 @@ class AXACommercialHeroCover extends LitElement {
     // Define properties and types
     return {
       // onClick: { type: Function },
-      src: { type: string },
-      alt: { type: string },
-      gradient: { type: string }, // PropTypes.oneOf(['white', 'black']),
-      contentAlign: { type: string }, //PropTypes.oneOf(['left', 'right']),
-      pictureClasses: { type: string },
-      heroObjectPosition: { type: string },
+      src: { type: String },
+      alt: { type: String },
+      gradient: { type: String }, // PropTypes.oneOf(['white', 'black']),
+      contentAlign: { type: String }, //PropTypes.oneOf(['left', 'right']),
+      pictureClasses: { type: String },
+      heroObjectPosition: { type: String },
     };
   }
 
@@ -46,11 +46,47 @@ class AXACommercialHeroCover extends LitElement {
 
   render() {
     return html`
-      <article class="o-commercial-hero-cover">
-        <slot></slot>
-      </article>
+      <header
+        class="o-commercial-hero-cover o-commercial-hero-cover__header o-commercial-hero-cover__header__${this
+          .gradient === 'white'
+          ? 'gradient-white'
+          : 'gradient-black'}${this.contentAlign === 'right'
+          ? '-inverted'
+          : ''}"
+      >
+        <picture class="m-picture o-commercial-hero-cover__picture">
+          <img
+            class="m-picture__image o-commercial-hero-cover__picture__image  ${this
+              .pictureClasses}"
+            src="${this.src}"
+            alt="${this.alt}"
+            data-object-fit="cover"
+            data-object-position="${this.heroObjectPosition}"
+          />
+        </picture>
+        <axa-container
+          class="o-commercial-hero-cover__info"
+          classes="o-commercial-hero-cover__full-height"
+        >
+          <axa-row classes="o-commercial-hero-cover__full-height"> </axa-row>
+        </axa-container>
+      </header>
     `;
   }
+  // <axa-col
+  //   size="sm-12 lg-6"
+  //   order="${this.contentAlign === 'left' ? '0' : '1'}"
+  //   classes="o-commercial-hero-cover__flex"
+  // >
+  //   ${childrenFragment.querySelector(
+  //     '.js-commercial-hero-cover__first'
+  //   ) || childrenFragment}
+  // </axa-col>
+  // <axa-col size="sm-12 lg-6">
+  //   ${childrenFragment.querySelector(
+  //     '.js-commercial-hero-cover__second'
+  //   ) || ''}
+  // </axa-col>
 
   disconnectedCallback() {
     super.disconnectedCallback();
--- src/components/30-organisms/commercial-hero-cover/story.js
+++ src/components/30-organisms/commercial-hero-cover/story.js
@@ -6,4 +6,25 @@ import Readme from './README.md';
 
 storiesOf('Organisms/Commercial hero cover', module)
   .addDecorator(withMarkdown(Readme))
-  .add('Commercial hero cover - default', () => '<axa-commercial-hero-cover>Some children</axa-commercial-hero-cover>')
+  .add(
+    'Commercial hero cover - default',
+    () => `
+    <axa-commercial-hero-cover>
+      <section class="o-commercial-hero-cover__section js-commercial-hero-cover__first">
+        <h2 class="o-commercial-hero-cover__subtitle">Car Insurance</h2>
+        <h1 class="o-commercial-hero-cover__title">Drive with peace of mind</h1>
+        <p class="o-commercial-hero-cover__text">Whether you need to insure your first car or renew your existing car
+          insurance, AXA can provide a range of car insurance policies to suit your requirements and offer great product
+          benefits at a price you can afford
+        </p>
+        <axa-button tag="a" href="http://www.google.com" class="o-commercial-hero-cover__button" color="blue" motion size="lg">Get a
+          quote
+        </axa-button>
+      </section>
+
+      <section class="o-commercial-hero-cover__section js-commercial-hero-cover__second">
+        <p class="o-commercial-hero-cover__text">This area can be used to place custom teasers (if agreed on by design team).</p>
+      </section>
+    </axa-commercial-hero-cover>
+  `
+  );
