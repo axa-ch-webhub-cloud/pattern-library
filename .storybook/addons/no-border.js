765e8af9c94363c7586c3ca94326ae14db0a5876
Marc Illien <marc.illien@gmail.com>
2019-06-24 16:49:19 +0200

    Remove stale old files

--- .storybook/addons/no-border.js
+++
@@ -1,3 +0,0 @@
-// This resets the default body margin in the story preview panel.
-// Sometimes we need this to demonstrate 0-border-stories, i.e le footer
-export default story => `<style>.sb-show-main{margin: 0}</style>${story()}`
