f37657b999012aa9d785720f98c4af363aa5d7d1
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-06-26 11:39:28 +0200

    remove projectspecific icons except upload-group

--- src/components/20-molecules/image-upload/icons/icon-set/index.js
+++ src/components/20-molecules/image-upload/icons/icon-set/index.js
@@ -1,5 +1,1 @@
 export { default as ImageUploadGroupSvg } from './image-upload-group.svg';
-export { default as PlusRoundedSvg } from './plus-rounded.svg';
-export { default as DeleteSvg } from './delete.svg';
-export { default as UploadCloudSvg } from './upload-cloud.svg';
-export { default as TickSvg } from './tick.svg';
