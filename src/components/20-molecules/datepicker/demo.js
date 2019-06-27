85b6a7d93c68c5ef495021ce2039f0c6beb54f2a
Luca Mele <lucamele1@hotmail.com>
2019-06-27 15:36:16 +0200

    Add new markdown logic

--- src/components/20-molecules/datepicker/demo.js
+++ src/components/20-molecules/datepicker/demo.js
@@ -1,12 +1,15 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
-import { withMarkdown } from '../../../../.storybook/addons/markdown';
 import Readme from './README.md';
 import './index';
 
 storiesOf('Molecules/Datepicker/Demos', module)
-  .addDecorator(withMarkdown(Readme))
+  .addParameters({
+    readme: {
+      sidebar: Readme,
+    },
+  })
   .add('Feature - Datepicker in a form', () => {
     const handleSubmit = event => {
       event.preventDefault();
