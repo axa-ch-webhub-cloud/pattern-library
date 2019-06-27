8523ff7d78e65414ef2114216409ab018be1a976
Luca Mele <lucamele1@hotmail.com>
2019-06-27 15:31:15 +0200

    Add new markdown

--- src/components/20-molecules/datepicker/react/demo.jsx
+++ src/components/20-molecules/datepicker/react/demo.jsx
@@ -3,11 +3,14 @@ import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DemoControlledDatepickerReact from './DemoDatepickerReact';
-import { withMarkdown } from '../../../../../.storybook/addons/markdown';
 import Readme from '../README.md';
 
 storiesOf('Molecules/Datepicker/React/Demos', module)
-  .addDecorator(withMarkdown(Readme))
+  .addParameters({
+    readme: {
+      sidebar: Readme,
+    },
+  })
   .add('Controlled-component React-ified Datepicker with inputfield', () => {
     const div = document.createElement('div');
     ReactDOM.render(<DemoControlledDatepickerReact />, div);
