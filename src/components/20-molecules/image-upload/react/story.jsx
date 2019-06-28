119a1bc25af7217bb737c462682ae5c72ef7e634
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-06-28 13:28:56 +0200

    changes in storybook

--- src/components/20-molecules/image-upload/react/story.jsx
+++ src/components/20-molecules/image-upload/react/story.jsx
@@ -3,12 +3,14 @@ import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AXAImageUploadReact from './AXAImageUploadReact';
-import { withMarkdown } from '../../../../../.storybook/addons/markdown';
 import Readme from '../README.md';
 
 storiesOf('Molecules/Image Upload/React', module)
-  .addDecorator(withMarkdown(Readme))
-
+  .addParameters({
+    readme: {
+      sidebar: Readme,
+    },
+  })
   /* Default */
   .add('Image Upload - default', () => {
     const div = document.createElement('div');
