1bbb2f3bdacc92740df94e0d54c249c8adc48e1c
Luca Mele <lucamele1@hotmail.com>
2019-06-28 13:32:47 +0200

    Revert unneeded changes

--- src/utils/define-once.js
+++ src/utils/define-once.js
@@ -1,4 +1,4 @@
-export default function defineOnce(name, customElementClass) {
+export default (name, customElementClass) => {
   // custom element not already registered?
   if (!window.customElements.get(name)) {
     // no, register it now
@@ -6,4 +6,4 @@ export default function defineOnce(name, customElementClass) {
   }
   // return its name as a convenience
   return name;
-}
+};
