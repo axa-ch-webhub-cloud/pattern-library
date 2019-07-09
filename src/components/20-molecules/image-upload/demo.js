1da5a04953ff69a067a1c585bea53d8a1b6cdb24
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-07-09 11:32:38 +0200

    update read.me, demo of image-upload in form, new story

--- src/components/20-molecules/image-upload/demo.js
+++ src/components/20-molecules/image-upload/demo.js
@@ -11,10 +11,53 @@ storiesOf('Molecules/Image Upload/Demos', module)
     },
   })
   .add('Feature - image upload in a form', () => {
+    const form = document.createElement('form');
+
+    const submitButton = document.createElement('button');
+    submitButton.innerHTML = 'Submit';
+    submitButton.style = 'margin-top: 40px;';
+    submitButton.setAttribute('type', 'submit');
+
     const wrapper = document.createElement('div');
-    wrapper.setAttribute('style', 'width:440px');
+    wrapper.setAttribute('style', 'width:453px');
+
     const imgUpload = document.createElement('axa-image-upload');
-    imgUpload.innerHTML = 'Image Upload';
+    imgUpload.innerHTML = 'Folgende Dateien werden übertragen';
+
+    form.appendChild(wrapper);
     wrapper.appendChild(imgUpload);
-    return wrapper;
+    form.appendChild(submitButton);
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const listWrapperElement = document.querySelector(
+        '.js-image-upload__list-wrapper'
+      );
+
+      if (listWrapperElement) {
+        listWrapperElement.parentNode.removeChild(listWrapperElement);
+      }
+
+      if (imgUpload.finalFiles.length > 0) {
+        const listWrapper = document.createElement('div');
+        listWrapper.classList.add('js-image-upload__list-wrapper');
+
+        const header = document.createElement('h3');
+        const textNode = document.createTextNode(
+          'Folgende Dateien wurden ausgewählt'
+        );
+
+        const ol = document.createElement('ol');
+        imgUpload.finalFiles.forEach(file => {
+          const li = document.createElement('li');
+          li.innerHTML = file.name;
+          ol.appendChild(li);
+        });
+        header.appendChild(textNode);
+        form.appendChild(listWrapper);
+        listWrapper.appendChild(header);
+        listWrapper.appendChild(ol);
+      }
+    });
+    return form;
   });
