c5606c6a7a72db8446aadb89a6143874a9b65c4f
Marek Laco <marek.laco@axa-winterthur.ch>
2019-07-09 16:08:25 +0200

    chore(datepicker): update typescript type declarations for datepicker

--- src/components/20-molecules/datepicker/index.react.d.ts
+++ src/components/20-molecules/datepicker/index.react.d.ts
@@ -1,16 +1,30 @@
 import React from 'react';
 
+type AXADatepickerChangeEvent = {
+  target: {
+    value: string;
+  }
+}
+
 interface AXADatepickerProps {
   locale?: string;
+  /**
+   * Year is mandatory until this issue gets resolved:
+   * https://github.com/axa-ch/patterns-library/issues/1149
+   */
   year: number;
   month?: number;
   day?: number;
-  onAXADateChange?: (utcDate: string) => any;
+  onChange?: (e: AXADatepickerChangeEvent) => any;
   inputfield?: boolean
   allowedyears?: string;
   labelbuttoncancel?: string;
   labelbuttonok?: string;
-  inputplaceholder?: string;
+  placeholder?: string;
+  monthtitle?: string;
+  yeartitle?: string;
+  invaliddatetext?: string;
+  value?: string;
 }
 
 declare function createAXADatepicker(
