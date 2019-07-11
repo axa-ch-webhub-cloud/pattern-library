1e9f84ee1d0e0e24cce33575b5bf6743214d8ea7
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-11 16:20:38 +0200

    switched testdata columns to be consistent to ui tests; fixed ui tests

--- src/components/30-organisms/table-sortable/story.js
+++ src/components/30-organisms/table-sortable/story.js
@@ -4,45 +4,45 @@ import Readme from './README.md';
 
 const model = {
   thead: [
-    { html: 'Name', sort: 'ASC' },
     { html: 'Age', sort: 'ASC' },
+    { html: 'Name', sort: 'ASC' },
     { html: 'City', sort: 'DESC' },
     { html: 'Department' },
   ],
   tbody: [
     [
-      { html: '<span>Peter</span>' },
       { html: '<span>55</span>' },
+      { html: '<span>Peter</span>' },
       { html: '<span>Winterthur</span>' },
       { html: 'A' },
     ],
     [
-      { html: '<span>Chris</span>' },
       { html: '<span>22</span>' },
+      { html: '<span>Chris</span>' },
       { html: '<span>Zürich</span>' },
       { html: 'B' },
     ],
     [
-      { html: '<span>Hubert</span>' },
       { html: '<span>46</span>' },
+      { html: '<span>Hubert</span>' },
       { html: '<span>Kreuzlingen</span>' },
       { html: 'C' },
     ],
     [
-      { html: '<span>Petra</span>' },
       { html: '<span>51</span>' },
+      { html: '<span>Petra</span>' },
       { html: '<span>Kreuzlingen</span>' },
       { html: 'D' },
     ],
     [
-      { html: '<span>Maria</span>' },
       { html: '<span>18</span>' },
+      { html: '<span>Maria</span>' },
       { html: '<span>Frauenfeld</span>' },
       { html: 'E' },
     ],
     [
-      { html: '<span>David</span>' },
       { html: '<span>29</span>' },
+      { html: '<span>David</span>' },
       { html: '<span>Frauenfeld</span>' },
       { html: 'F' },
     ],
