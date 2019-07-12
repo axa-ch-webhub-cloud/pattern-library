51504f230952f71322e45226175f53f6f458acc2
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-12 12:37:27 +0200

    add migration notes to scaffolding

--- scripts/create-a-m-o/functions.js
+++ scripts/create-a-m-o/functions.js
@@ -145,6 +145,10 @@ const createFiles = (store, a, m, o, done) => () => {
     ### onClick
 
     The function-valued attribute \`onClick\` can be used as a callback prop for React and other frameworks.
+    
+    ### Migration Notes (v1.0 -> v2.0)
+    
+    You don't have to pay attention to anything for upgrading to version 2.
 
     `,
     'utf8',
