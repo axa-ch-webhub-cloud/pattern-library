879aa87384b9c5844918fcafe071035c777ed30b
Markus Walther <markus.walther@axa-winterthur.ch>
2019-07-17 18:23:22 +0200

    Update axa-radio to design specs; adjust example to shorter labels; adjust fieldset horizontal mode

--- src/components/10-atoms/radio/story.js
+++ src/components/10-atoms/radio/story.js
@@ -1,22 +1,23 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
 import './index';
-import { withMarkdown } from '../../../../.storybook/addons/markdown';
+
 import Readme from './README.md';
 
 storiesOf('Atoms/Radio', module)
-  .addDecorator(withMarkdown(Readme))
+  .addParameters({
+    readme: {
+      sidebar: Readme,
+    },
+  })
   .add(
     'Radio - default',
     () => `
   <axa-fieldset horizontal>
-  <axa-radio name="contract" label="Ja, ich möchte online einen
-  Versicherungsvertrag abschliessen.
-  Die Allgemeinen Vertragsbedingungen (AVB),
-  die Beraterinformation, sowie die Hinweise zum Datenschutz habe ich zur
-  Kenntnis genommen und bin damit einverstanden." error="Bitte akzeptieren Sie die allgemeinen Versicherungsbedingungen." checked></axa-radio>
-  <axa-radio name="contract" label="Ich brauche noch mehr Informationen und möchte, dass ein Berater mit mir Kontakt aufnimmt."></axa-radio>
-  <axa-radio name="contract" label="Nein, ich möchte momentan keinen Versicherungsvertrag abschliessen."></axa-radio>
-  <axa-radio name="contract" label="Nein, ich bin bereits anderweitig versichert." disabled></<axa-radio>
+  <axa-radio name="contract" label="Ja,
+  Versicherungsvertrag abschliessen." error="Bitte akzeptieren Sie die allgemeinen Versicherungsbedingungen." checked></axa-radio>
+  <axa-radio name="contract" label="Ich brauche noch mehr Informationen"></axa-radio>
+  <axa-radio name="contract" label="Nein, ich möchte keinen Versicherungsvertrag"></axa-radio>
+  <axa-radio name="contract" label="Nein, ich bin bereits versichert" disabled></<axa-radio>
   </axa-fieldset>`
   );
