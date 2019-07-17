537d24df3b1e08884a14533d96789a87ef86324f
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-17 09:10:11 +0200

    deactivate watcher for jest to solve azure problem

--- scripts/test.unit.js
+++ scripts/test.unit.js
@@ -12,10 +12,12 @@ const test = require('jest');
 
 const argv = process.argv.slice(2);
 
+/*
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
+*/
 
 console.log('test.unit.js: Start Tests with these arguments: ', argv);
 
