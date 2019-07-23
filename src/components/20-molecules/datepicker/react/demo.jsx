6d74d5a03d9a7f9427720dd986c1c6607895d65c
Markus Walther <markus.walther@axa-winterthur.ch>
2019-07-23 15:56:49 +0200

    Add uncontrolled, onDateChange-driven React demo

--- src/components/20-molecules/datepicker/react/demo.jsx
+++ src/components/20-molecules/datepicker/react/demo.jsx
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DemoControlledDatepickerReact from './DemoDatepickerReact';
+import DemoDatepickerOnDateChange from './DemoDatepickerOnDateChange';
 import Readme from '../README.md';
 
 storiesOf('Molecules/Datepicker/React/Demos', module)
@@ -15,4 +16,12 @@ storiesOf('Molecules/Datepicker/React/Demos', module)
     const div = document.createElement('div');
     ReactDOM.render(<DemoControlledDatepickerReact />, div);
     return div;
-  });
+  })
+  .add(
+    'Uncontrolled, onDateChange-driven React-ified Datepicker with inputfield',
+    () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<DemoDatepickerOnDateChange />, div);
+      return div;
+    }
+  );
