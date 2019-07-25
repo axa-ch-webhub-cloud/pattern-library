b742054781b497df930cd394690ccda256a52dbd
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-25 13:50:52 +0200

    added new util

---
+++ src/utils/requests.js
@@ -0,0 +1,9 @@
+export function xhrCall(url){
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
+}
