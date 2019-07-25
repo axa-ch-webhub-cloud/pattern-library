be053ea6a2ad72ae860eed6b4cb1f24c399bbd34
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-25 16:37:38 +0200

    minor refactoring

--- src/components/20-molecules/policy-features/story.js
+++ src/components/20-molecules/policy-features/story.js
@@ -1,12 +1,6 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
-import {
-  boolean,
-  select,
-  radios,
-  text,
-  withKnobs,
-} from '@storybook/addon-knobs';
+import { radios, select, text, withKnobs } from '@storybook/addon-knobs';
 import './policy-features-item/index';
 import './index';
 import Readme from './README.md';
@@ -15,7 +9,7 @@ const story = storiesOf('Molecules/Policy features', module);
 story.addDecorator(withKnobs);
 story.addParameters({
   readme: {
-    sidebar: Readme
+    sidebar: Readme,
   },
 });
 
@@ -30,21 +24,54 @@ const STYLE_WHITELIST = [
 const ITEM_ICONS = ['email', 'download'];
 
 story.add('Policy features', () => {
-  const axaStyles = select('axaStyle', STYLE_WHITELIST, STYLE_WHITELIST[0]);
-  const title = text('title', 'A 5 star car insurance with affordable premium services');
+  const axaStyles = select(
+    'axaStyle',
+    STYLE_WHITELIST,
+    STYLE_WHITELIST[0],
+    'axa-policy-features'
+  );
+  const title = text(
+    'title',
+    'A 5 star car insurance with affordable premium services',
+    'axa-policy-features'
+  );
 
   // props of axa-policy-features-item
-  const itemTitle = text('axa-policy-features-item: title', 'Get Discount');
-  const itemIcon = select('axa-policy-features-item: icon', ITEM_ICONS, ITEM_ICONS[0]);
-  const itemDescription = text('axa-policy-features-item: description', 'A 5 star car insurance with affordable premium services');
+  const itemTitleRadio = radios(
+    'Show title?',
+    { yes: 'y', no: 'n' },
+    'y',
+    'axa-policy-features-item'
+  );
+  const itemTitle = text(
+    'title (of item)',
+    'Get Discount',
+    'axa-policy-features-item'
+  );
+  const itemIcon = select(
+    'icon',
+    ITEM_ICONS,
+    ITEM_ICONS[0],
+    'axa-policy-features-item'
+  );
+  const itemIconUrl = text(
+    'icon - load svg icon from this url instead:',
+    '',
+    'axa-policy-features-item'
+  );
+  const itemDescription = text(
+    'description',
+    'A 5 star car insurance with affordable premium services',
+    'axa-policy-features-item'
+  );
 
   return `
   <axa-policy-features 
   title="${title}"
   axaStyle="${axaStyles}"
   >
-    <axa-policy-features-item title="${itemTitle}"
-                         icon="${itemIcon}"
+    <axa-policy-features-item title="${itemTitleRadio === 'y' ? itemTitle : ''}"
+                         icon="${itemIconUrl || itemIcon}"
                          description="${itemDescription}">
     </axa-policy-features-item>
     <axa-policy-features-item title="24/7 assistance"
@@ -52,7 +79,7 @@ story.add('Policy features', () => {
                          description="We reward safe drivers : 75% no claims discount + an extra 10% off if you get a quote online. This is a long text.">
     </axa-policy-features-item>
     <axa-policy-features-item title="Discount partners"
-                         icon="http://localhost:6006/static/media/logo-axa.ce44e802.svg"
+                         icon="email"
                          description="This SVG is loaded externally.">
     </axa-policy-features-item>
     <axa-policy-features-item title="Online & Apps"
