17708a6d055aedacd5ca214351ab7aae837240d8
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-07-26 10:36:31 +0200

    add knobs

--- src/components/20-molecules/image-upload/story.js
+++ src/components/20-molecules/image-upload/story.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/html';
-import { boolean, text, select, withKnobs } from '@storybook/addon-knobs';
+import { text, number, select, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import './index';
 import Readme from './README.md';
@@ -12,7 +12,7 @@ storyImageUpload.addParameters({
   },
 });
 
-// TODO:: Move icon variants into icons and export it from there
+// TODO: Move icon variants into icons and export it from there
 const icons = {
   none: '',
   'arrow-right': 'arrow-right',
@@ -29,15 +29,58 @@ const icons = {
 };
 
 storyImageUpload.add('Image upload - default', () => {
-  // const open = boolean('open', false);
-  // const name = text('name', '');
+  const rangeSliderOptions = {
+    range: true,
+    min: 1,
+    max: 25,
+    step: 1,
+  };
   const inputFileText = text('inputFileText', 'Upload file');
-
-  const iconOptions = select('Icon', icons);
+  const maxSizeOfSingleFileMB = number(
+    'maxSizeOfSingleFileMB',
+    15,
+    rangeSliderOptions
+  );
+  const maxSizeOfAllFilesMB = number(
+    'maxSizeOfAllFilesMB',
+    20,
+    rangeSliderOptions
+  );
+  const maxNumberOfFiles = number('maxNumberOfFiles', 10, rangeSliderOptions);
+  const deleteStatusText = text('deleteStatusText', 'Delete');
+  const addStatusText = text('addStatusText', 'Add more');
+  const fileTooBigStatusText = text(
+    'fileTooBigStatusText',
+    `File size exceeds ${maxSizeOfSingleFileMB}MB`
+  );
+  const filesTooBigStatusText = text(
+    'filesTooBigStatusText',
+    `File sizes exceeds ${maxSizeOfAllFilesMB}MB`
+  );
+  const tooManyFilesStatusText = text(
+    'tooManyFilesStatusText',
+    `You exceeded the maximum number of files: ${maxNumberOfFiles}`
+  );
+  const orText = text('orText', 'or');
+  const infoText = text('infoText', 'Drag and drop to upload your file');
+  const iconOptions = select('Icon', icons, 'cloud-upload');
   const wrapper = document.createElement('div');
+
   const template = html`
     <div style="width:453px;">
-      <axa-image-upload inputFileText="${inputFileText}" icon="${iconOptions}"
+      <axa-image-upload
+        inputFileText="${inputFileText}"
+        maxSizeOfSingleFileMB="${maxSizeOfSingleFileMB}"
+        maxSizeOfAllFilesMB="${maxSizeOfAllFilesMB}"
+        maxNumberOfFiles="${maxNumberOfFiles}"
+        deleteStatusText="${deleteStatusText}"
+        addStatusText="${addStatusText}"
+        fileTooBigStatusText="${fileTooBigStatusText}"
+        filesTooBigStatusText="${filesTooBigStatusText}"
+        tooManyFilesStatusText="${tooManyFilesStatusText}"
+        orText="${orText}"
+        infoText="${infoText}"
+        icon="${iconOptions}"
         >Folgende Dateien werden übertragen:</axa-image-upload
       >
     </div>
@@ -46,16 +89,3 @@ storyImageUpload.add('Image upload - default', () => {
   render(template, wrapper);
   return wrapper;
 });
-// <axa-popup-button ?open="${open}" name="${name}"></axa-popup-button>
-
-// .add(
-//   'Image upload - default',
-//   () =>
-//     `<div style="width:453px;"><axa-image-upload>Folgende Dateien werden übertragen:</axa-image-upload></div>`
-// )
-// .add(
-//   'Image upload - custom',
-//   () =>
-//     `<div style="width:600px;"><axa-image-upload inputFileText="Datei hochladen" icon="cloud-upload" errorStatusText="Fehler aufgetreten" deleteStatusText="Löschen langgggggg"
-//     addStatusText="Ein sehr langer Text">Image-Upload:</axa-image-upload></div>`
-// );
