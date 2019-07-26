bf7556ab3bf86b7b618219cda909e946dc49fdde
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-07-26 11:13:17 +0200

    add ui test

--- src/components/20-molecules/policy-features/ui.test.js
+++ src/components/20-molecules/policy-features/ui.test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Policy features - basic functionality').page(`${host}/iframe.html?id=molecules-policy-features--policy-features-default`);
+fixture('Policy features - basic functionality').page(`${host}/iframe.html?id=molecules-policy-features--policy-features`);
 
 const TAG = 'axa-policy-features';
 const CLASS = '.m-policy-features';
@@ -13,4 +13,15 @@ test('should render policy-features', async t => {
   const $axaElemShadow = await Selector(() => document.querySelector('axa-policy-features').shadowRoot);
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+test('should render title', async t => {
+  const titleElement = Selector(
+    () => document.querySelector('axa-policy-features').shadowRoot
+  ).find('h1');
+  await t
+    .expect(titleElement.textContent)
+    .eql('A 5 star car insurance with affordable premium services');
+
+
 });
