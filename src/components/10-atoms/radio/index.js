2a3486a39b811fa7e4470f0163aac49ddb6100b1
Markus Walther <markus.walther@axa-winterthur.ch>
2019-07-29 18:20:49 +0200

    Add button behaviour to axa-radio; fix radio behaviour and checked reflection for non-controlled mode

--- src/components/10-atoms/radio/index.js
+++ src/components/10-atoms/radio/index.js
@@ -1,12 +1,16 @@
-import { html } from 'lit-element';
+import { html /* , svg */, svg } from 'lit-element';
+// import { CarSvg } from '@axa-ch/materials/images';
 import NoShadowDOM from '../../../utils/no-shadow';
+import defaultName from '../../../utils/create-ref-id';
 
 /* eslint-disable import/no-extraneous-dependencies */
 import defineOnce from '../../../utils/define-once';
 import styles from './index.scss';
 
 // module globals
+const radioButtonGroup = {};
 const selectedRadioButton = {};
+const maxWidth = {};
 
 // CE
 class AXARadio extends NoShadowDOM {
@@ -28,6 +32,9 @@ class AXARadio extends NoShadowDOM {
         reflect: true,
       },
       disabled: { type: Boolean, reflect: true },
+      button: { type: Boolean, reflect: true },
+      nogap: { type: Boolean, reflect: true },
+      icon: { type: String },
       error: {
         type: String,
         reflect: true,
@@ -76,8 +83,15 @@ class AXARadio extends NoShadowDOM {
       onChange,
     } = this;
     onChange(event);
+    const isChecked = event.target.checked;
+
     if (!isControlled) {
-      this.state.native = event.target.checked;
+      if (isChecked && selectedRadioButton[name]) {
+        selectedRadioButton[name].checked = false;
+      }
+
+      this.checked = true;
+
       return;
     }
     // set UI from model state
@@ -105,35 +119,38 @@ class AXARadio extends NoShadowDOM {
     this.value = '';
     this.name = '';
     this.label = '';
-    this.disabled = false;
+    this.icon = '';
     this.error = '';
+    this.disabled = false;
+    this.button = false;
+    this.nogap = false;
     this.isReact = false;
     this.onFocus = () => {};
     this.onBlur = () => {};
     this.onChange = () => {};
   }
 
+  getDefaultName() {
+    this.name = this.name || defaultName();
+    return this.name;
+  }
+
   render() {
     const {
       id,
-      name,
+      name = this.getDefaultName(),
+      button,
+      icon,
       checked,
       value,
       disabled,
       label,
       isReact,
-      state: { isControlled, checked: stateChecked },
+      state: { isControlled },
     } = this;
 
     // controlledness is a React-only notion
-    const _isControlled = isControlled && isReact;
-    this.state.isControlled = _isControlled;
-
-    // we are the selected element of a group of controlled inputs?
-    if (_isControlled && stateChecked && name) {
-      // yes, cache ourselves under group name
-      selectedRadioButton[name] = this;
-    }
+    this.state.isControlled = isControlled && isReact;
 
     return html`
       <label class="a-radio__wrapper">
@@ -149,15 +166,28 @@ class AXARadio extends NoShadowDOM {
           @focus="${this.onFocus}"
           @blur="${this.onBlur}"
         />
-        <span class="a-radio__icon"></span>
+        ${button
+          ? html``
+          : html`
+              <span class="a-radio__icon"></span>
+            `}
+        ${icon ? svg(icon) : svg``}
         <div class="a-radio__content">${label}</div>
       </label>
     `;
   }
 
+  firstUpdated() {
+    this.input = this.querySelector('input');
+    const { name } = this;
+    radioButtonGroup[name] = radioButtonGroup[name] || new Set();
+    radioButtonGroup[name].add(this.querySelector('.a-radio__content'));
+  }
+
   updated() {
     const {
       name,
+      button,
       input,
       state: { isControlled, checked },
     } = this;
@@ -167,13 +197,31 @@ class AXARadio extends NoShadowDOM {
       // to maintain controlledness in the face of native UI-state changes
       input.checked = checked;
     }
+
+    if (this.checked) selectedRadioButton[name] = this;
+
+    if (button) {
+      setTimeout(() => {
+        const { width: labelTextWidth } = this.querySelector(
+          '.a-radio__content'
+        ).getBoundingClientRect();
+        maxWidth[name] = Math.max(labelTextWidth | 0, maxWidth[name] | 0);
+        // equalize width for all <axa-radio button> with same name
+        radioButtonGroup[name].forEach(radioButton => {
+          radioButton.style.minWidth = `${maxWidth[name]}px`;
+        });
+      }, /* give DOM some time to paint before measuring width */ 10);
+    }
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
     const { name } = this;
-    if (name) {
-      selectedRadioButton[name] = null; // help GC
+    selectedRadioButton[name] = null; // help GC
+    maxWidth[name] = null; // help GC
+    radioButtonGroup[name].delete(this.querySelector('.a-radio__content'));
+    if (radioButtonGroup[name].size === 0) {
+      radioButtonGroup[name] = null; // help GC
     }
   }
 }
