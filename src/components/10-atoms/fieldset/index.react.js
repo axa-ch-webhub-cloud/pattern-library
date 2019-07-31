c145e0d1e211cc0c2ac57533d9eb742f66680ac8
Markus Walther <markus.walther@axa-winterthur.ch>
2019-07-31 14:07:37 +0200

    Extend axa-fieldset with 'error' property

--- src/components/10-atoms/fieldset/index.react.js
+++ src/components/10-atoms/fieldset/index.react.js
@@ -1,11 +1,12 @@
 import withReact from '../../../utils/with-react';
 import AXAFieldset from './index';
 
-export default createElement => ({ horizontal, children }) =>
+export default createElement => ({ horizontal, error, children }) =>
   withReact(createElement)(
     AXAFieldset.tagName,
     {
       horizontal,
+      error,
     },
     children
   );
