23a236814b6c3fd406efc6f7df1a26a4fdc6ee86
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-02 14:44:17 +0200

    add knob story

--- src/components/30-organisms/testimonials/story.js
+++ src/components/30-organisms/testimonials/story.js
@@ -1,12 +1,36 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
+import {
+  boolean,
+  select,
+  radios,
+  text,
+  withKnobs,
+} from '@storybook/addon-knobs';
 import './index';
 import Readme from './README.md';
 
-storiesOf('Organisms/Testimonials', module)
-  .addParameters({
-    readme: {
-      sidebar: Readme,
-    },
-  })
-  .add('Testimonials - default', () => '<axa-testimonials>Some children</axa-testimonials>')
+const story = storiesOf('Organisms/Testimonials', module);
+story.addDecorator(withKnobs);
+story.addParameters({
+  readme: {
+    sidebar: Readme,
+  },
+});
+
+story.add('Testimonials', () => {
+
+  const buttonText = text('Text', 'Click me');
+  const motionOff = boolean('motionOff', false);
+  const disabled = boolean('disabled', false);
+  const large = boolean('large', false); // should probably be a variant.
+  const type = radios('Types', { submit: 'submit', reset: 'reset' });
+
+  return `
+  <axa-testimonials
+  title="Customer Reviews"
+  subtitle="AXA works hard to provide the best service possible to its customers.">
+    <div style="display: flex; align-self: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
+    <div style="text-align: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
+</axa-testimonials>`;
+});
