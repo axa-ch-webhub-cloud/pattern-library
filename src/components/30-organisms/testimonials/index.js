822c5061e50ced44fc20a9e1d9dd31678945062c
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-02 14:46:18 +0200

    add basic functionality of carousel

--- src/components/30-organisms/testimonials/index.js
+++ src/components/30-organisms/testimonials/index.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css, unsafeCSS } from 'lit-element';
 
 /* eslint-disable import/no-extraneous-dependencies */
+import 'macro-carousel';
 import '@axa-ch/container';
 import defineOnce from '../../../utils/define-once';
 import styles from './index.scss';
@@ -31,30 +32,59 @@ class AXATestimonials extends LitElement {
 
   constructor() {
     super();
+    this.onClickLeft = () => {
+      if (this.sliderElement && this.sliderElement.previous) {
+        this.sliderElement.previous();
+      }
+    };
+    this.onClickRight = () => {
+      if (this.sliderElement && this.sliderElement.next) {
+        this.sliderElement.next();
+      }
+    };
+    this.sliderElement = null;
   }
 
   firstUpdated() {
     // Add DOM changes here
     // This will be rendered when the component is connected to the DOM
+    this.sliderElement = this.shadowRoot.querySelector('macro-carousel');
   }
 
   render() {
-    const { title, subtitle} = this;
+    const { title, subtitle } = this;
 
     return html`
       <article class="o-testimonials">
         <axa-container>
-        
-            ${title && html`<h1 class="o-testimonials__title">${title}</h1>`}
-        ${subtitle && html`<p class="o-testimonials__subtitle">${subtitle}</p>`}
-        <div class="o-testimonials__navigator js-o-testimonials__navigator">
-          <button class="o-testimonials__arrow-left js-o-testimonials__control-left" type="button"></button>
-          <div class="o-testimonials__content">
-            <slot></slot>
-          </div>
-          <button class="o-testimonials__arrow-right js-o-testimonials__control-right" type="button"></button>
-        </div>
+          ${title &&
+            html`
+              <h1 class="o-testimonials__title">${title}</h1>
+            `}
+          ${subtitle &&
+            html`
+              <p class="o-testimonials__subtitle">${subtitle}</p>
+            `}
 
+          <div class="o-testimonials__navigator">
+            <div class="o-testimonials__navigator__flexbox-container">
+              <button
+                class="o-testimonials__navigator__left"
+                type="button"
+                @click="${this.onClickLeft}"
+              ></button>
+            </div>
+            <macro-carousel loop="true">
+              <slot></slot>
+            </macro-carousel>
+            <div class="o-testimonials__navigator__flexbox-container">
+              <button
+                class="o-testimonials__navigator__right"
+                type="button"
+                @click="${this.onClickRight}"
+              ></button>
+            </div>
+          </div>
         </axa-container>
       </article>
     `;
