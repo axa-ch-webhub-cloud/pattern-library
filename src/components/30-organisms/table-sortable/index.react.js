86db90a363940c5fe5a2b31f421ecf2fc10a499f
Marco Kruse <marco-kruse@freenet.de>
2019-08-04 14:45:14 +0200

    Enable className for table sortable

--- src/components/30-organisms/table-sortable/index.react.js
+++ src/components/30-organisms/table-sortable/index.react.js
@@ -1,9 +1,15 @@
 import withReact from '../../../utils/with-react';
 import AXATable from './index';
 
-export default createElement => ({ children, model, innerscroll, maxheight }) =>
+export default createElement => ({
+  children,
+  model,
+  innerscroll,
+  maxheight,
+  className,
+}) =>
   withReact(createElement)(
     AXATable.tagName,
-    { model, innerscroll, maxheight },
+    { model, innerscroll, maxheight, className },
     children
   );
