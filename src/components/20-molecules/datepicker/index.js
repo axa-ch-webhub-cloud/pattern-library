8c3fe0b6f12767e08bfabaef017dde3b753578c3
Markus Walther <markus.walther@axa-winterthur.ch>
2019-08-05 14:47:43 +0200

    Remove embedded from axa-datepicker

--- src/components/20-molecules/datepicker/index.js
+++ src/components/20-molecules/datepicker/index.js
@@ -122,7 +122,6 @@ class AXADatepicker extends NoShadowDOM {
       invalid: { type: Boolean, reflect: true },
       invaliddatetext: { type: String },
       error: { type: String, reflect: true },
-      embedded: { type: Boolean, reflect: true },
       height: { type: String, reflect: true },
       width: { type: String, reflect: true },
     };
@@ -246,13 +245,15 @@ class AXADatepicker extends NoShadowDOM {
 
     this.setMonthAndYearItems(month, year);
 
-    const { width = 'auto', height = 'auto' } = this;
+    const { width = 'auto', height = 'auto', error, invalid } = this;
 
     const formattedStyle = parameter =>
       `${parameter}${/^\d+$/.test(parameter) ? 'px' : ''}`;
 
     const formattedWidth = `width:${formattedStyle(width)}`;
     const formattedHeight = `height:${formattedStyle(height)}`;
+
+    const needToShowError = error || invalid;
 
     return html`
       <article
@@ -357,11 +358,9 @@ class AXADatepicker extends NoShadowDOM {
               </div>
             `
           : ''}
-        ${!this.embedded
+        ${needToShowError
           ? html`
-              <span class="m-datepicker__error"
-                >${this.error || this.invalid ? this.invaliddatetext : ''}</span
-              >
+              <span class="m-datepicker__error">${this.invaliddatetext}</span>
             `
           : html``}
       </article>
