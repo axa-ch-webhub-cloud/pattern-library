f3eb6f5a2aa7d55febc9337ef9a214a00edc6d67
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-06 18:10:22 +0200

    Introduce variant to hero banner

--- src/components/30-organisms/commercial-hero-banner/story.js
+++ src/components/30-organisms/commercial-hero-banner/story.js
@@ -25,7 +25,7 @@ storyAXACommercialHeroBanner.add('Commercial hero banner', () => {
 
   const wrapper = document.createElement('div');
   const template = html`
-    <axa-commercial-hero-banner>
+    <axa-commercial-hero-banner variant="light">
       <h2 slot="category">Insurance</h2>
       <h1 slot="title">Title</h1>
       <p slot="content">This is the content</p>
@@ -35,7 +35,6 @@ storyAXACommercialHeroBanner.add('Commercial hero banner', () => {
       <p slot="content">This is the content</p>
       <small slot="disclaimer">Terms and Conditions apply...</small>
       <button slot="button">YES!</button>
-      <button slot="button">Tell me more...</button>
       <button slot="button">Tell me more...</button>
     </axa-commercial-hero-banner>
   `;
