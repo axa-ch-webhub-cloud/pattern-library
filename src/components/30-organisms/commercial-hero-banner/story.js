cceac30c4d32f5fd1539b0fc7c994a63b0e3144f
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-06 13:32:17 +0200

    Make hero banner borderless in storybook

--- src/components/30-organisms/commercial-hero-banner/story.js
+++ src/components/30-organisms/commercial-hero-banner/story.js
@@ -3,10 +3,15 @@ import { storiesOf } from '@storybook/html';
 // if your need more boolean, select, radios
 import { text, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
+import withNoBorder from '../../../../.storybook/addons/no-border';
 import './index';
 import Readme from './README.md';
 
-const storyAXACommercialHeroBanner = storiesOf('Organisms/Commercial hero banner', module);
+const storyAXACommercialHeroBanner = storiesOf(
+  'Organisms/Commercial hero banner',
+  module
+);
+storyAXACommercialHeroBanner.addDecorator(withNoBorder);
 storyAXACommercialHeroBanner.addDecorator(withKnobs);
 storyAXACommercialHeroBanner.addParameters({
   readme: {
@@ -16,12 +21,12 @@ storyAXACommercialHeroBanner.addParameters({
 
 storyAXACommercialHeroBanner.add('Commercial hero banner', () => {
   const children = text('Text', 'Some Children');
-  
+
   const wrapper = document.createElement('div');
   const template = html`
-    <axa-commercial-hero-banner>${children}<axa-commercial-hero-banner>
+    <axa-commercial-hero-banner>${children}</axa-commercial-hero-banner>
   `;
-  
+
   render(template, wrapper);
   return wrapper;
 });
