881cd584eeecdb4ffc24788409da633bfc1aee3c
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-06 13:40:08 +0200

    Remove dead code

--- src/components/30-organisms/commercial-hero-banner/index.js
+++ src/components/30-organisms/commercial-hero-banner/index.js
@@ -71,49 +71,6 @@ class AXACommercialHeroBanner extends LitElement {
         </axa-container>
       </header>
     `;
-
-    // <header
-    //   class="o-commercial-hero-cover o-commercial-hero-cover__header o-commercial-hero-cover__header__${this
-    //   .gradient === 'white'
-    //   ? 'gradient-white'
-    //   : 'gradient-black'}${this.contentAlign === 'right'
-    //   ? '-inverted'
-    //   : ''}"
-    // >
-    //   <picture class="m-picture o-commercial-hero-cover__picture">
-    //     <img
-    //       class="m-picture__image o-commercial-hero-cover__picture__image  ${this
-    //       .pictureClasses}"
-    //       src="${this.src}"
-    //       alt="${this.alt}"
-    //       data-object-fit="cover"
-    //       data-object-position="${this.heroObjectPosition}"
-    //     />
-    //   </picture>
-    //   <axa-container
-    //     class="o-commercial-hero-cover__info"
-    //     classes="o-commercial-hero-cover__full-height"
-    //   >
-    //     <axa-row classes="o-commercial-hero-cover__full-height">
-    //       <axa-col
-    //         size="sm-12 lg-6"
-    //         order="${contentAlign === 'left' ? '0' : '1'}"
-    //         classes="o-commercial-hero-cover__flex"
-    //       >
-    //         $
-    //         {childrenFragment.querySelector(
-    //           '.js-commercial-hero-cover__first'
-    //         ) || childrenFragment}
-    //       </axa-col>
-    //       <axa-col size="sm-12 lg-6">
-    //         $
-    //         {childrenFragment.querySelector(
-    //           '.js-commercial-hero-cover__second'
-    //         ) || ''}
-    //       </axa-col>
-    //     </axa-row>
-    //   </axa-container>
-    // </header>;
   }
 
   disconnectedCallback() {
