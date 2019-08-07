070450768dc8766c2be45ffb278fc989d40564fb
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-07 16:13:55 +0200

    add third slide to story

--- src/components/30-organisms/testimonials/story.js
+++ src/components/30-organisms/testimonials/story.js
@@ -28,10 +28,11 @@ story.add('Testimonials', () => {
 
   return `
   <axa-testimonials
-  showAllInline
+  autorotatedisabled
   title="Customer Reviews"
   subtitle="AXA works hard to provide the best service possible to its customers.">
     <div style="display: flex; align-self: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
     <div style="text-align: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
+    <div style="text-align: center;"><span>This is a small text.</span></div>
 </axa-testimonials>`;
 });
