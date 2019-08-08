fc6ee3771df9301355a9eb064a821d880d9d3c05
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-08 16:06:09 +0200

    Make banner desktop work on IE and other browsers without polyfill

--- src/components/30-organisms/commercial-hero-banner/index.js
+++ src/components/30-organisms/commercial-hero-banner/index.js
@@ -66,18 +66,18 @@ class AXACommercialHeroBanner extends InlineStyles {
       'o-commercial-hero-banner__content_dark': this.variant === 'dark',
     };
 
+    //   <img
+    //   src="https://d5cplpsrt2s33.cloudfront.net/m/24c1b33e4e8ceda1/WIDE_1440_560_X2-hero_kv_neu_kv_breit_web.jpg"
+    //   alt="example"
+    //   data-object-fit="cover"
+    //   data-object-position="top left"
+    // />
+
     return html`
       <header class="o-commercial-hero-banner ${classMap(variantClass)}">
-        <picture
-          ><img
-            src="https://d5cplpsrt2s33.cloudfront.net/m/24c1b33e4e8ceda1/WIDE_1440_560_X2-hero_kv_neu_kv_breit_web.jpg"
-            alt="example"
-            data-object-fit="cover"
-            data-object-position="top left"
-        /></picture>
-
         <div class="o-commercial-hero-banner__container">
           <axa-container>
+            <div class="photo-holder"></div>
             <div class="o-commercial-hero-banner__content">
               <div class="o-commercial-hero-banner__content-item">
                 <div class="o-commercial-hero-banner__content-item-box">
