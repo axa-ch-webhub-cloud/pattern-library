8122f4455f6b89d606f67c88e42044aecb67c05f
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-08 13:32:20 +0200

    adding styles for different breakpoints; clean up story

--- src/components/30-organisms/testimonials/story.js
+++ src/components/30-organisms/testimonials/story.js
@@ -7,6 +7,7 @@ import {
   text,
   withKnobs,
 } from '@storybook/addon-knobs';
+import { html, render } from 'lit-html';
 import './index';
 import Readme from './README.md';
 
@@ -19,20 +20,44 @@ story.addParameters({
 });
 
 story.add('Testimonials', () => {
-
   const buttonText = text('Text', 'Click me');
   const motionOff = boolean('motionOff', false);
   const disabled = boolean('disabled', false);
   const large = boolean('large', false); // should probably be a variant.
   const type = radios('Types', { submit: 'submit', reset: 'reset' });
 
-  return `
-  <axa-testimonials
-  autorotatedisabled
-  title="Customer Reviews"
-  subtitle="AXA works hard to provide the best service possible to its customers.">
-    <div style="display: flex; align-self: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
-    <div style="text-align: center;"><span>Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I got through to the correct person but I was constantly being transferred from person to person, in the end I called into my local axa once in the city and got sorted in 10 minutes after 3 weeks of phone calls.</span></div>
-    <div style="text-align: center;"><span>This is a small text.</span></div>
-</axa-testimonials>`;
+  const wrapper = document.createElement('div');
+  const template = html`
+    <axa-testimonials
+      autorotatedisabled
+      title="Customer Reviews"
+      subtitle="AXA works hard to provide the best service possible to its customers."
+    >
+      <span
+        >Very helpful once I got through to the correct person but I was
+        constantly being transferred from person to person, in the end I called
+        into my local axa once in the city and got sorted in 10 minutes after 3
+        weeks of phone calls.</span
+      >
+      <span
+        >Very helpful once I got through to the correct person but I was
+        constantly being transferred from person to person, in the end I called
+        into my local axa once in the city and got sorted in 10 minutes after 3
+        weeks of phone calls. Very helpful once I got through to the correct
+        person but I was constantly being transferred from person to person, in
+        the end I called into my local axa once in the city and got sorted in 10
+        minutes after 3 weeks of phone calls. Very helpful once I got through to
+        the correct person but I was constantly being transferred from person to
+        person, in the end I called into my local axa once in the city and got
+        sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I
+        got through to the correct person but I was constantly being transferred
+        from person to person, in the end I called into my local axa once in the
+        city and got sorted in 10 minutes after 3 weeks of phone calls.</span
+      >
+      <span>This is a small text.</span>
+    </axa-testimonials>
+  `;
+
+  render(template, wrapper);
+  return wrapper;
 });
