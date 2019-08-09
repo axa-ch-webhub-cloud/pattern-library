ceabd8d1585e51d140f6cd42c1c247e771e76b54
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-09 15:19:39 +0200

    Introduce position knob

--- src/components/30-organisms/commercial-hero-banner/story.js
+++ src/components/30-organisms/commercial-hero-banner/story.js
@@ -8,12 +8,6 @@ import withNoBorder from '../../../../.storybook/addons/no-border';
 import './index';
 import Readme from './README.md';
 
-const label = 'Variant';
-const options = {
-  light: 'light',
-  dark: 'dark',
-};
-
 const storyAXACommercialHeroBanner = storiesOf(
   'Organisms/Commercial hero banner',
   module
@@ -27,14 +21,33 @@ storyAXACommercialHeroBanner.addParameters({
 });
 
 storyAXACommercialHeroBanner.add('Commercial hero banner', () => {
-  const variant = radios(label, options, 'light');
+  const variant = radios(
+    'Variant',
+    {
+      light: 'light',
+      dark: 'dark',
+    },
+    'light'
+  );
+
+  const imagePosition = radios(
+    'Position',
+    {
+      'center center': 'center center',
+      'top left': 'top left',
+      'top right': 'top right',
+      'bottom right': 'bottom right',
+      'bottom left': 'bottom left',
+    },
+    'center center'
+  );
 
   const wrapper = document.createElement('div');
   const template = html`
     <axa-commercial-hero-banner
       variant="${variant}"
       src="https://d5cplpsrt2s33.cloudfront.net/m/24c1b33e4e8ceda1/WIDE_1440_560_X2-hero_kv_neu_kv_breit_web.jpg"
-      imageposition="center center"
+      imageposition="${imagePosition}"
     >
       <h2 slot="category">Insurance</h2>
       <h1 slot="title">Title</h1>
