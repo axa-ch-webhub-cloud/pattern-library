3fda88bcc9f276eac99a7440bed5fbaa9ffc1adf
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-09 14:30:53 +0200

    Introduce knobs to hero banner

--- src/components/30-organisms/commercial-hero-banner/story.js
+++ src/components/30-organisms/commercial-hero-banner/story.js
@@ -2,11 +2,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/html';
 // if your need more boolean, select, radios
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { radios, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import withNoBorder from '../../../../.storybook/addons/no-border';
 import './index';
 import Readme from './README.md';
+
+const label = 'Variant';
+const options = {
+  light: 'light',
+  dark: 'dark',
+};
 
 const storyAXACommercialHeroBanner = storiesOf(
   'Organisms/Commercial hero banner',
@@ -21,13 +27,12 @@ storyAXACommercialHeroBanner.addParameters({
 });
 
 storyAXACommercialHeroBanner.add('Commercial hero banner', () => {
-  const children = text('Text', 'Some Children');
-  children.toString(); // TODO A placeholder for future knob implementation
+  const variant = radios(label, options, 'light');
 
   const wrapper = document.createElement('div');
   const template = html`
     <axa-commercial-hero-banner
-      variant="dark"
+      variant="${variant}"
       src="https://d5cplpsrt2s33.cloudfront.net/m/24c1b33e4e8ceda1/WIDE_1440_560_X2-hero_kv_neu_kv_breit_web.jpg"
       imageposition="center center"
     >
