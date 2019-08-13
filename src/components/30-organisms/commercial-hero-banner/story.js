3003f3277e09e545b398686140a661636b699a7b
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-08-13 16:39:26 +0200

    Remove image position from props

--- src/components/30-organisms/commercial-hero-banner/story.js
+++ src/components/30-organisms/commercial-hero-banner/story.js
@@ -35,46 +35,11 @@ storyAXACommercialHeroBanner.add('Commercial Hero Banner', () => {
     'https://d5cplpsrt2s33.cloudfront.net/m/24c1b33e4e8ceda1/WIDE_1440_560_X2-hero_kv_neu_kv_breit_web.jpg'
   );
 
-  const imagePosition = radios(
-    'Position',
-    {
-      'center center': 'center center',
-      'top left': 'top left',
-      'top right': 'top right',
-      'bottom right': 'bottom right',
-      'bottom left': 'bottom left',
-    },
-    'center center'
-  );
-
   const wrapper = document.createElement('div');
   const template = html`
     <axa-commercial-hero-banner
       variant="${variant}"
       imagesource="${src}"
-      imageposition="${imagePosition}"
-    >
-      <h2 slot="category">This example shows specific picture classes</h2>
-      <h1 slot="title">Drive with peace of mind</h1>
-      <p slot="content">
-        Whether you need to insure your first car or renew your existing car
-        insurance, AXA can provide a range of car insurance policies to suit
-        your requirements and offer great product benefits at a price you can
-        afford
-      </p>
-      <small slot="disclaimer">Terms and Conditions apply.</small>
-      <axa-button-link
-        href="https://axa.ch"
-        slot="button"
-        variant="red"
-        size="large"
-        >GET A QUOTE</axa-button-link
-      > </axa-commercial-hero-banner
-    ><br />
-    <axa-commercial-hero-banner
-      variant="${variant}"
-      imagesource="${src}"
-      imageposition="${imagePosition}"
     >
       <h2 slot="category">This example shows specific picture classes</h2>
       <h1 slot="title">Drive with peace of mind</h1>
@@ -92,29 +57,7 @@ storyAXACommercialHeroBanner.add('Commercial Hero Banner', () => {
         size="large"
         >GET A QUOTE</axa-button-link
       >
-    </axa-commercial-hero-banner>
-    <axa-commercial-hero-banner
-      variant="${variant}"
-      imagesource="${src}"
-      imageposition="${imagePosition}"
-    >
-      <h2 slot="category">This example shows specific picture classes</h2>
-      <h1 slot="title">Drive with peace of mind</h1>
-      <p slot="content">
-        Whether you need to insure your first car or renew your existing car
-        insurance, AXA can provide a range of car insurance policies to suit
-        your requirements and offer great product benefits at a price you can
-        afford
-      </p>
-      <small slot="disclaimer">Terms and Conditions apply.</small>
-      <axa-button-link
-        href="https://axa.ch"
-        slot="button"
-        variant="red"
-        size="large"
-        >GET A QUOTE</axa-button-link
-      >
-    </axa-commercial-hero-banner>
+    </axa-commercial-hero-banner
   `;
 
   render(template, wrapper);
