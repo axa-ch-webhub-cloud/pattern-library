e1be6b5bd6ffbce9804138e7cdd03dfd02d4c132
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-08-13 17:05:36 +0200

    tests: add files and look if screen is switched and add invalid file

--- src/components/20-molecules/image-upload/ui.test.js
+++ src/components/20-molecules/image-upload/ui.test.js
@@ -2,15 +2,61 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Image upload - basic functionality').page(`${host}/iframe.html?id=molecules-image-upload--image-upload-default`);
+fixture('Image upload - basic functionality').page(
+  `${host}/iframe.html?id=molecules-image-upload--image-upload-default`
+);
 
-const TAG = 'axa-image-upload';
-const CLASS = '.m-image-upload';
+const IMAGE_UPLOAD_TAG = 'axa-image-upload';
+const IMAGE_UPLOAD_CLASS = '.m-image-upload';
+
+const files = [
+  './ui-test-files/wolf.jpg',
+  './ui-test-files/screenshot.png',
+  './ui-test-files/test.pdf',
+];
 
 test('should render image-upload', async t => {
-  const $axaElem = await Selector(TAG);
+  const $axaElem = await Selector(IMAGE_UPLOAD_TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(() => document.querySelector('axa-image-upload').shadowRoot);
-  const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-image-upload').shadowRoot
+  );
+  const $axaElemShadowEl = await $axaElemShadow.find(IMAGE_UPLOAD_CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+test('should upload file and switch view', async t => {
+  const $inputFileInputElem = await Selector(() =>
+    document
+      .querySelector('axa-image-upload')
+      .shadowRoot.querySelector('axa-input-file .a-input-file__input')
+  );
+  await t.expect($inputFileInputElem.exists).ok();
+
+  await t.setFilesToUpload($inputFileInputElem, files);
+
+  const $fileOverviewElem = await Selector(() =>
+    document
+      .querySelector('axa-image-upload')
+      .shadowRoot.querySelector('.m-image-upload__dropzone-file-overview')
+  );
+  await t.expect($fileOverviewElem.exists).ok();
+});
+
+test("shouldn't upload file because its an invalid type", async t => {
+  const $inputFileInputElem = await Selector(() =>
+    document
+      .querySelector('axa-image-upload')
+      .shadowRoot.querySelector('axa-input-file .a-input-file__input')
+  );
+  await t.expect($inputFileInputElem.exists).ok();
+
+  await t.setFilesToUpload($inputFileInputElem, './ui-test-files/test.rtf');
+
+  const $fileOverviewElem = await Selector(() =>
+    document
+      .querySelector('axa-image-upload')
+      .shadowRoot.querySelector('.m-image-upload__dropzone-file-overview')
+  );
+  await t.expect($fileOverviewElem.exists).notOk();
 });
