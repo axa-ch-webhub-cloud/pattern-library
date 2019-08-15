ec55cd20334431c358ab2ec9ee22d2eec5868d21
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-15 16:23:37 +0200

    added ui tests (TODO refactor)

--- src/components/30-organisms/testimonials/ui.test.js
+++ src/components/30-organisms/testimonials/ui.test.js
@@ -2,15 +2,45 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL || 'http://localhost:9999';
 
-fixture('Testimonials - basic functionality').page(`${host}/iframe.html?id=organisms-testimonials--testimonials-default`);
+fixture('Testimonials - basic functionality').page(
+  `${host}/iframe.html?id=organisms-testimonials--testimonials-default`
+);
 
 const TAG = 'axa-testimonials';
 const CLASS = '.o-testimonials';
+const $axaElemShadow = Selector(
+  () => document.querySelector(TAG).shadowRoot,
+  { dependencies: { TAG } }
+);
 
 test('should render testimonials', async t => {
   const $axaElem = await Selector(TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(() => document.querySelector('axa-testimonials').shadowRoot);
+
   const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
   await t.expect($axaElemShadowEl.exists).ok();
+});
+
+test('should set title text', async t => {
+  const $titleElement = await $axaElemShadow.find('.o-testimonials__title');
+  await t.expect($titleElement.textContent).eql('Customer Reviews');
+});
+
+test('should set subtitle text', async t => {
+  const $subTitleElement = await $axaElemShadow.find('.o-testimonials__subtitle');
+  await t.expect($subTitleElement.textContent).eql('AXA works hard to provide the best service possible to its customers.');
+});
+
+test('should find axa-carousel tag if attribute showallinline is not set', async t => {
+  const $carouselElement = await $axaElemShadow.find('axa-carousel');
+  await t.expect($carouselElement.exists).ok();
+});
+
+fixture('Testimonials - attribute showallinline is set').page(
+  `${host}/iframe.html?id=organisms-testimonials--testimonials-default`
+);
+
+test('should find inline class if attribute showallinline is set', async t => {
+  const $inlineElement = await $axaElemShadow.find('.o-testimonials__content__inline');
+  await t.expect($inlineElement.exists).ok();
 });
