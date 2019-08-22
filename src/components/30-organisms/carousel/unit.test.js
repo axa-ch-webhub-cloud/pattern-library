31c3232fedc4f35cf6ce9568137005c348f8bbb8
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-22 16:25:40 +0200

    add unit test skeleton

---
+++ src/components/30-organisms/carousel/unit.test.js
@@ -0,0 +1,33 @@
+import AXACarousel from './index';
+
+describe('AXACarousel', () => {
+  describe('public methods', () => {
+    test('', () => {
+      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    });
+  });
+
+  describe('allgemeine private methods', () => {
+    test('', () => {
+      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    });
+  });
+
+  describe('auto rotate', () => {
+    test('', () => {
+      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    });
+  });
+
+  describe('swipe', () => {
+    test('', () => {
+      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    });
+  });
+
+  describe('key navigation', () => {
+    test('', () => {
+      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    });
+  });
+});
