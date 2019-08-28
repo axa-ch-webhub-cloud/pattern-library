bb59490b842c29757fb3cb771a818354fe08c7c6
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-08-28 16:10:03 +0200

    Zwischenstand of unit tests

--- src/components/30-organisms/carousel/unit.test.js
+++ src/components/30-organisms/carousel/unit.test.js
@@ -1,85 +1,271 @@
-import AXACarousel from './index';
+let AXACarousel; // we have to require this module to use resetModules()
+const ANIMATION_LEFT_CLASS = 'animation-left'; // should be equal to index.js
+const ANIMATION_RIGHT_CLASS = 'animation-right';
 
 describe('AXACarousel', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line global-require
+    AXACarousel = require('./index').default;
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+
   describe('public methods', () => {
     test('handleNextButtonClick() should call methods', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      const mockedNextSlide = jest.fn(() => {});
+      const mockedStopAutoRotate = jest.fn(() => {});
+      AXACarousel.prototype._nextSlide = mockedNextSlide;
+      AXACarousel.prototype._stopAutoRotate = mockedStopAutoRotate;
+
+      AXACarousel.prototype.handleNextButtonClick();
+
+      expect(mockedNextSlide.mock.calls.length).toBe(1);
+      expect(mockedStopAutoRotate.mock.calls.length).toBe(1);
     });
 
     test('handlePreviousButtonClick() should call methods', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      const mockedPreviousSlide = jest.fn(() => {});
+      const mockedStopAutoRotate = jest.fn(() => {});
+      AXACarousel.prototype._previousSlide = mockedPreviousSlide;
+      AXACarousel.prototype._stopAutoRotate = mockedStopAutoRotate;
+
+      AXACarousel.prototype.handlePreviousButtonClick();
+
+      expect(mockedPreviousSlide.mock.calls.length).toBe(1);
+      expect(mockedStopAutoRotate.mock.calls.length).toBe(1);
     });
   });
 
   describe('allgemeine private methods', () => {
-    test('_getSlides() should return array', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    /*test('_getSlides() should call querySelector with correct selector', () => {
+      const mockedQuerySelector = jest.fn(() => {});
+      //document = {shadowRoot: {querySelector: mockedQuerySelector}}; //TODO how to mock document? evtol per spy?
+      // AXACarousel.prototype.shadowRoot.querySelector = mockedQuerySelector;
+
+      //global.document.shadowRoot = {querySelector: mockedQuerySelector}; //TODO: erstmal auskommentieren....
+      AXACarousel.prototype.shadowRoot = {querySelector: mockedQuerySelector}; // error only getter
+      AXACarousel.prototype._getSlides();
+
+      // check first arg of first call
+      expect(mockedQuerySelector.mock.calls[0][0]).toBe('.o-carousel__slot');
+    });*/
+
+    test('_setSlideVisibleWithAnimation() should set _animationWrapperClass', () => {
+      const givenAnimationClass = 'myclass';
+      AXACarousel.prototype.slides = [{ style: {} }];
+
+      AXACarousel.prototype._setSlideVisibleWithAnimation(
+        0,
+        givenAnimationClass
+      );
+
+      jest.runOnlyPendingTimers();
+
+      expect(AXACarousel.prototype._animationWrapperClass).toBe(
+        givenAnimationClass
+      );
+    });
+    test('_setSlideVisibleWithAnimation() should set block style of given slide', () => {
+      const activeSlideNumber = 0;
+      AXACarousel.prototype.slides = [{ style: {} }];
+
+      AXACarousel.prototype._setSlideVisibleWithAnimation(
+        activeSlideNumber,
+        ''
+      );
+
+      jest.runOnlyPendingTimers();
+
+      expect(
+        AXACarousel.prototype.slides[activeSlideNumber].style.display
+      ).toBe('block');
     });
 
-    test('_setSlideVisibleWithAnimation() should set given class to wrapper', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    test('_nextSlide() should call method with nextSlideIndex = 1', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 0;
+      const expectedNextSlideIndex = visibleSlideIndex + 1;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: 2 };
+
+      AXACarousel.prototype._nextSlide();
+
+      // The first arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][0]).toBe(
+        expectedNextSlideIndex
+      );
     });
-    test('_setSlideVisibleWithAnimation() should set display:block to given slide', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+    test('_nextSlide() should call method with nextSlideIndex = 0', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 2;
+      const expectedNextSlideIndex = 0;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: 2 };
+
+      AXACarousel.prototype._nextSlide();
+
+      // The first arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][0]).toBe(
+        expectedNextSlideIndex
+      );
+    });
+    test('_nextSlide() should call method with correct second argument', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 0;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: 0 };
+
+      AXACarousel.prototype._nextSlide();
+
+      // The second arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][1]).toBe(
+        ANIMATION_RIGHT_CLASS
+      );
     });
 
-    test('_nextSlide() should call method with correct parameters', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
-    });
+    test('_previousSlide() should call method with nextSlideIndex = thePreviousIndexInArray', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 2;
+      const expectedPreviousSlideIndex = visibleSlideIndex - 1;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: 2 };
 
-    test('_previousSlide() should call method with correct parameters', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype._previousSlide();
+
+      // The first arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][0]).toBe(
+        expectedPreviousSlideIndex
+      );
+    });
+    test('_previousSlide() should call method with nextSlideIndex = theLastIndexInArray', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 0;
+      const expectedPreviousSlideIndex = 2;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: expectedPreviousSlideIndex + 1 };
+
+      AXACarousel.prototype._previousSlide();
+
+      // The first arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][0]).toBe(
+        expectedPreviousSlideIndex
+      );
+    });
+    test('_previousSlide() should call method with correct second argument', () => {
+      const mockedSetSlideVisibleWithAnimation = jest.fn(() => {});
+      const visibleSlideIndex = 0;
+      AXACarousel.prototype._setSlideVisibleWithAnimation = mockedSetSlideVisibleWithAnimation;
+      AXACarousel.prototype.visibleSlide = visibleSlideIndex;
+      AXACarousel.prototype.slides = { length: 0 };
+
+      AXACarousel.prototype._previousSlide();
+
+      // The second arg of the first call to the function
+      expect(mockedSetSlideVisibleWithAnimation.mock.calls[0][1]).toBe(
+        ANIMATION_LEFT_CLASS
+      );
     });
 
     test('_calculateContainerMinHeight() should set biggest height', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
-    });
+      AXACarousel.prototype.slides = [
+        { style: { display: '' }, clientHeight: 1 },
+        { style: { display: '' }, clientHeight: 2 },
+      ];
+      AXACarousel.prototype._carouselMinHeight = 0;
 
-    test('_onResize() should set a variable to 0 and call 2 methods', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype._calculateContainerMinHeight();
+
+      expect(AXACarousel.prototype._carouselMinHeight).toBe(2);
+    });
+    test('_calculateContainerMinHeight() should not change _carouselMinHeight', () => {
+      AXACarousel.prototype.slides = [
+        { style: { display: '' }, clientHeight: 1 },
+        { style: { display: '' }, clientHeight: 2 },
+      ];
+      AXACarousel.prototype._carouselMinHeight = 999;
+
+      AXACarousel.prototype._calculateContainerMinHeight();
+
+      expect(AXACarousel.prototype._carouselMinHeight).toBe(999);
     });
   });
 
   describe('auto rotate', () => {
     test('_startAutoRotate() should init a interval', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype._startAutoRotate();
+      expect(setInterval).toHaveBeenCalled();
+      expect(AXACarousel.prototype.autoRotateTimerID).toBeGreaterThan(-1);
+    });
+    test('_startAutoRotate() should call _nextSlide()', () => {
+      const mockedNextSlide = jest.fn(() => {});
+      AXACarousel.prototype._nextSlide = mockedNextSlide;
+
+      AXACarousel.prototype._startAutoRotate();
+      jest.runOnlyPendingTimers();
+
+      expect(mockedNextSlide).toHaveBeenCalled();
     });
     test('_startAutoRotate() should not init a interval if autorotatedisabled is set', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype.autorotatedisabled = true;
+      AXACarousel.prototype._startAutoRotate();
+      expect(setInterval).not.toHaveBeenCalled();
     });
 
     test('_stopAutoRotate() should call clearInterval with correct property', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype._stopAutoRotate();
+      expect(clearInterval).toHaveBeenCalledWith(
+        AXACarousel.prototype.autoRotateTimerID
+      );
     });
   });
 
   describe('swipe', () => {
-    test('_onSwipeLeft() should call method', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
-    });
-
-    test('_onSwipeRight() should call method', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
-    });
-
     test('_initSwipe() should instantiate Swipe an call a method', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      AXACarousel.prototype.swiper = null;
+
+      AXACarousel.prototype._initSwipe(); // TODO: mock new Swipe?
+
+      expect(AXACarousel.prototype.swiper).not.toBe(null);
     });
 
     test('_terminateSwipe() should call a method', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
-    });
-    test('_terminateSwipe() should not call a method if swiper not set', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      const mockedStop = jest.fn(() => {}); //TODO: refactor. shorter!
+      AXACarousel.prototype.swiper = {
+        stop: mockedStop,
+      };
+
+      AXACarousel.prototype._terminateSwipe();
+
+      expect(mockedStop).toHaveBeenCalled();
     });
   });
 
   describe('key navigation', () => {
     test('_initKeyNavigation() should add an EventListener', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      const mockedAddEventListener = jest.fn();
+      const mockedDocument = { addEventListener: mockedAddEventListener };
+      AXACarousel.prototype.keysenabled = true;
+
+      AXACarousel.prototype._initKeyNavigation(mockedDocument);
+
+      expect(mockedAddEventListener).toHaveBeenCalled();
     });
     test('_initKeyNavigation() should not add an EventListener if keysenabled=false', () => {
-      expect(AXACarousel.prototype.validateModel()).toBe(true);
+      const mockedAddEventListener = jest.fn();
+      const mockedDocument = { addEventListener: mockedAddEventListener };
+      AXACarousel.prototype.keysenabled = false;
+
+      AXACarousel.prototype._initKeyNavigation(mockedDocument);
+
+      expect(mockedAddEventListener).not.toHaveBeenCalled();
     });
 
     test('_terminateKeyNavigation() should remove EventListener', () => {
