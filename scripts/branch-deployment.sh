27a5f0e67dfc10d4ae8de4b3dcc57a19d2ba565b
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-09-05 08:28:06 +0200

    current state

--- scripts/branch-deployment.sh
+++ scripts/branch-deployment.sh
@@ -2,3 +2,9 @@
 mkdir -p ./dist/$BRANCH_NAME
 mv -v ./storybook-static/* ./dist/$BRANCH_NAME/
 npm run deploy-storybook -- --ci --host-token-env-variable=$GITHUB_TOKEN --remote=https://github.com/raphaellueckl/feature-branch-deployments.git --existing-output-dir=./dist
+
+git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1
+cd feature-branch-deployments
+ALL_BRANCHES=$(git --no-pager branch -a)
+ALL_FOLDERS_DEPTH_1=$(find . -maxdepth 1 -type d -exec ls -ld "{}" \;)
+
