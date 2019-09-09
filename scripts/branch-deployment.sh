6948043a9ce7df852ba84047962afd027f0359bb
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-09-09 09:12:38 +0200

    Make more progress towards multibranch deployment

--- scripts/branch-deployment.sh
+++ scripts/branch-deployment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir -p ./dist/$BRANCH_NAME
 mv -v ./storybook-static/* ./dist/$BRANCH_NAME/
-npm run deploy-storybook -- --ci --host-token-env-variable=$GITHUB_TOKEN --remote=https://github.com/raphaellueckl/feature-branch-deployments.git --existing-output-dir=./dist
+# npm run deploy-storybook -- --ci  --host-token-env-variable=$GITHUB_TOKEN --remote=https://github.com/raphaellueckl/feature-branch-deployments.git --existing-output-dir=./dist
+cd ./dist
+git add *
+git comm # continue here... deployment script did not work
+mv -v ./dist/$BRANCH_NAME/* ./storybook-static/
 
 git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1
 cd feature-branch-deployments
 ALL_BRANCHES=$(git --no-pager branch -a)
 ALL_FOLDERS_DEPTH_1=$(find . -type d -maxdepth 2 | cut -d '/' -f2-)
-
