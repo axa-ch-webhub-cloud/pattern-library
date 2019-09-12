44c67dce08c673a7a0fb89905bab36661184386e
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-09-12 09:11:13 +0200

    Add works including commit and push

--- scripts/branch-deployment.sh
+++ scripts/branch-deployment.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+mkdir ./dist
+git clone https://github.com/raphaellueckl/feature-branch-deployments.git ./dist
 mkdir -p ./dist/$BRANCH_NAME
 mv -v ./storybook-static/* ./dist/$BRANCH_NAME/
-# npm run deploy-storybook -- --ci  --host-token-env-variable=$GITHUB_TOKEN --remote=https://github.com/raphaellueckl/feature-branch-deployments.git --existing-output-dir=./dist
 cd ./dist
 git add *
-git comm # continue here... deployment script did not work
-mv -v ./dist/$BRANCH_NAME/* ./storybook-static/
+git commit -m "Deploy Branch: ${BRANCH_NAME}"
+git push
 
-git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1
-cd feature-branch-deployments
-ALL_BRANCHES=$(git --no-pager branch -a)
-ALL_FOLDERS_DEPTH_1=$(find . -type d -maxdepth 2 | cut -d '/' -f2-)
+# git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1
+# cd feature-branch-deployments
+ALL_BRANCHES=$(git --no-pager branch -a $1)
+ALL_FOLDERS_DEPTH_1=$(find . -type d -maxdepth 2 -path ./.git -prune -o -print | cut -d '/' -f2-)
+
+(IFS='
+'
+for x in $ALL_BRANCHES; do echo $x; done)
+
+# Cleanup
+cd ..
+mv -v ./dist/$BRANCH_NAME/* ./storybook-static/
