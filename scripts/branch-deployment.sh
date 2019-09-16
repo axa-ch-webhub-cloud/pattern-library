86e992cfe00269224c762c936c537303ba2ccc68
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-09-16 10:11:55 +0200

    Improve script

--- scripts/branch-deployment.sh
+++ scripts/branch-deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ./dist
-git clone https://github.com/raphaellueckl/feature-branch-deployments.git ./dist
+git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1 ./dist
 mkdir -p ./dist/$BRANCH_NAME
 mv -v ./storybook-static/* ./dist/$BRANCH_NAME/
 
@@ -8,18 +8,16 @@ ALL_BRANCHES=$(git --no-pager branch -a -r) # List only remote branches
 
 cd ./dist
 
-# git clone https://github.com/raphaellueckl/feature-branch-deployments.git --depth 1
-# cd feature-branch-deployments
-
 # Find all folders for 2 levels down, ignore .git folder and cut './' at the beginning of each folder
 ALL_FOLDERS_DEPTH_1=$(find . -type d -maxdepth 2 -path ./.git -prune -o -print | cut -d '/' -f2-)
 
 ###################################################################################################
 # There is one issue with the loop below: It checks the branch names on the pattern-library-repo. #
-# But if someby creates a branch called 'feature/a', it is likely that it never (or way too late) #
-# will get deleted, because we check 'in all our branches, is this branch name contained'. If no, #
-# we clean it up because we no longer need it. As long as we have another branch open that starts #
-# with 'feature/a', the oudated branch will not be removed.                                       #
+# But if somebody creates a branch called 'feature/a', it is likely that it will never (or way    #
+# too late) gets deleted, because we check 'in all pattern library branches, is this branch name  #
+# contained'. If no, we clean it up because we no longer need it (because the branch probably got #
+# merged and deleted). As long as we have another branch open that starts with 'feature/a', the   #
+# oudated branch will not be removed.                                                             #
 #                                                                                                 #
 # tl;dr:                                                                                          #
 # 'feature/a' will not be deleted, as long as something like 'feature/axa-button-fix-bug' exists, #
@@ -29,7 +27,7 @@ ALL_FOLDERS_DEPTH_1=$(find . -type d -maxdepth 2 -path ./.git -prune -o -print |
 (IFS='
 '
 for x in $ALL_FOLDERS_DEPTH_1; do
-    # If folder is no longer linked to a remove branch, remove it.
+    # If folder is no longer linked to a remote branch, remove it.
     if [[ $ALL_BRANCHES != *"$x"* ]]; then
       rm -rf $x
     fi
@@ -39,7 +37,6 @@ git add .
 git commit -m "Deploy Branch: ${BRANCH_NAME}"
 git push
 
-
-# Cleanup
+# Move everything back to not mess with the other tasks.
 cd ..
 mv -v ./dist/$BRANCH_NAME/* ./storybook-static/
