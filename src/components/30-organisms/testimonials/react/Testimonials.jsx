619025b177126ce6e6f634d920689d8826cb0ade
Daniel Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-09-19 14:44:13 +0200

    Add React Demo to see React Bugs

---
+++ src/components/30-organisms/testimonials/react/Testimonials.jsx
@@ -0,0 +1,6 @@
+import { createElement } from 'react';
+import createAXATestimonialsReact from '../index.react';
+
+const AXATestimonialsReact = createAXATestimonialsReact(createElement);
+
+export default AXATestimonialsReact;
---
+++ src/components/30-organisms/testimonials/react/story.jsx
@@ -0,0 +1,74 @@
+import AXATestimonialsReact from './Testimonials';
+
+import { storiesOf } from '@storybook/html';
+import { boolean, text, withKnobs, number } from '@storybook/addon-knobs';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Readme from '../README.md';
+import withNoBorder from '../../../../../.storybook/addons/no-border';
+
+const story = storiesOf('Organisms/Testimonials/React', module);
+story.addDecorator(withKnobs);
+story.addDecorator(withNoBorder);
+story.addParameters({
+  readme: {
+    sidebar: Readme,
+  },
+});
+
+story.add('Testimonials', () => {
+  const title = text('title', 'Customer Reviews');
+  const subtitle = text(
+    'Text',
+    'AXA works hard to provide the best service possible to its customers.'
+  );
+  const autorotatedisabled = boolean('autorotatedisabled', false);
+  const autorotatetime = number('autorotatetime', 5000);
+  const keysenabled = boolean('keysenabled', true);
+  const showallinline = boolean('showallinline', false);
+
+  const wrapper = document.createElement('div');
+  ReactDOM.render(
+    <AXATestimonialsReact
+      autorotatetime={autorotatetime}
+      autorotatedisabled={autorotatedisabled}
+      keysenabled={keysenabled}
+      showallinline={showallinline}
+      title={title}
+      subtitle={subtitle}
+    >
+      <div>
+        <span>
+          Very helpful once I got through to the correct person but I was
+          constantly being transferred from person to person, in the end I
+          called into my local axa once in the city and got sorted in 10 minutes
+          after 3 weeks of phone calls.
+        </span>
+        <span className="author">Andrew Jackson, Advocate</span>
+      </div>
+      <div>
+        <span>
+          Very helpful once I got through to the correct person but I was
+          constantly being transferred from person to person, in the end I
+          called into my local axa once in the city and got sorted in 10 minutes
+          after 3 weeks of phone calls. Very helpful once I got through to the
+          correct person but I was constantly being transferred from person to
+          person, in the end I called into my local axa once in the city and got
+          sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I
+          got through to the correct person but I was constantly being
+          transferred from person to person, in the end I called into my local
+          axa once in the city and got sorted in 10 minutes after 3 weeks of
+          phone calls. Very helpful once I got through to the correct person but
+          I was constantly being transferred from person to person, in the end I
+          called into my local axa once in the city and got sorted in 10 minutes
+          after 3 weeks of phone calls.
+        </span>
+        <span className="author">Andrew Müller, Advocate</span>
+      </div>
+      <span>This is a small text without an author.</span>
+    </AXATestimonialsReact>,
+    wrapper
+  );
+
+  return wrapper;
+});
