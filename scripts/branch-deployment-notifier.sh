33ace987ea059007dcc75101ca8fcbfced6e8def
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-09-20 16:05:05 +0200

    Shrink script

--- scripts/branch-deployment-notifier.sh
+++ scripts/branch-deployment-notifier.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Do not forget to have the $SLACK_TOKEN as environment variable
-slackhost=axa-ch
-channel=plib-deployments
+# Do not forget to have the $SLACK_TOKEN set as environment variable
 
 text="$BRANCH_NAME was deployed here: https://axa-ch.github.io/plib-feature/$BRANCH_NAME"
-escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
 
-curl -X POST -H "Content-type: application/json;charset=UTF-8" -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"channel\":\"#plib-deployments\", \"text\": \"$escapedText\", \"username\": \"Donald Duck\"}" https://slack.com/api/chat.postMessage
+curl -X POST -H "Content-type: application/json;charset=UTF-8" -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"channel\":\"#plib-deployments\", \"text\": \"$text\", \"username\": \"Donald Duck\"}" https://slack.com/api/chat.postMessage
