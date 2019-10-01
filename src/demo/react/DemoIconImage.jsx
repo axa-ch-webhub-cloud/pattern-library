6294c30d1f2b5debb78e56ba040d9b2bf8202177
Noah  Liechti <noah.liechti@axa-winterthur.ch>
2019-10-01 09:54:24 +0200

    Revert "create react app"
    This reverts commit cf76435e93e1f5851c75464e2fdafdc4948d9ebb.

--- src/demo/react/DemoIconImage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DemoIconImage = () => {
-  return <div>hoi</div>;
-};
-
-export default DemoIconImage;
--- src/demo/react/story.jsx
+++ src/demo/react/story.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/html';
 import ReactDOM from 'react-dom';
 import DemoTable from './DemoTable';
-import DemoIconImage from './DemoIconImage';
 import DemoControlledInputsApp from '../demo-controlled-inputs/App';
 import DemoDynamicChildrenApp from '../demo-dynamic-children/App';
 
 storiesOf('Demos', module)
-  .add('Icons and Images overview', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<DemoIconImage />, div);
-    return div;
-  })
   .add('Controlled Inputs React', () => {
     const div = document.createElement('div');
     ReactDOM.render(<DemoControlledInputsApp />, div);
