457506941328b7b2175443d631ff9200582f79ec
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-10-03 08:19:47 +0200

    Change carousel text

--- src/components/10-atoms/carousel/story.js
+++ src/components/10-atoms/carousel/story.js
@@ -29,27 +29,15 @@ storyAXACarousel.add('Carousel', () => {
     ?autorotatedisabled="${autorotatedisabled}"
     ?keysenabled="${keysenabled}"
     >
-      <span
-        >Very helpful once I got through to the correct person but I was
+      <span>
+        Very helpful once I got through to the correct person but I was
         constantly being transferred from person to person, in the end I called
         into my local axa once in the city and got sorted in 10 minutes after 3
-        weeks of phone calls.</span
-      >
-      <span
-        >Very helpful once I got through to the correct person but I was
-        constantly being transferred from person to person, in the end I called
-        into my local axa once in the city and got sorted in 10 minutes after 3
-        weeks of phone calls. Very helpful once I got through to the correct
-        person but I was constantly being transferred from person to person, in
-        the end I called into my local axa once in the city and got sorted in 10
-        minutes after 3 weeks of phone calls. Very helpful once I got through to
-        the correct person but I was constantly being transferred from person to
-        person, in the end I called into my local axa once in the city and got
-        sorted in 10 minutes after 3 weeks of phone calls. Very helpful once I
-        got through to the correct person but I was constantly being transferred
-        from person to person, in the end I called into my local axa once in the
-        city and got sorted in 10 minutes after 3 weeks of phone calls.</span
-      >
+        weeks of phone calls.
+      </span>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor orci, bibendum vitae elementum sit amet, elementum sit amet velit. Sed condimentum mi metus, at facilisis sapien tristique eget. Pellentesque facilisis mauris eu dapibus vehicula. Sed ligula lectus, varius quis mauris in, aliquet ullamcorper nibh. Donec ultricies sapien at purus efficitur, in finibus urna facilisis. Proin accumsan faucibus ipsum, ut efficitur nisl venenatis a. Morbi nec sollicitudin justo. Nunc nec dolor libero. Cras eget accumsan ipsum. Morbi cursus in neque non bibendum. Nunc fermentum condimentum dui vel porttitor. Nullam enim felis, accumsan a massa vitae, condimentum tempus dolor. Nullam massa arcu, ultricies a magna vel, accumsan rutrum diam. Nunc a fermentum dolor. In hac habitasse platea dictumst. Fusce vitae pulvinar elit. Etiam gravida, elit nec varius tempor, eros erat tincidunt orci, et rutrum turpis ipsum eget elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque congue eu purus nec dictum. Nunc vitae sapien dui. Cras a consequat justo. Duis sed turpis non dolor feugiat porta. Vivamus dignissim purus sed felis bibendum, sit amet ullamcorper ante ultrices. Phasellus purus ante, feugiat iaculis pulvinar non, tempus et mauris. Morbi id orci vulputate lorem tincidunt congue. Mauris quis sapien posuere, tempus diam et, eleifend ipsum.
+      </span>
       <span>This is a small text.</span>
    </div> 
   `;
