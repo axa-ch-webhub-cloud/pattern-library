01e0698ebe88c3ccf17e0824619a2267426feefe
Noah Liechti <noah.liechti@gmail.com>
2019-10-08 11:07:10 +0200

    add knobs for react

--- src/components/10-atoms/link/react/story.jsx
+++ src/components/10-atoms/link/react/story.jsx
@@ -1,60 +1,99 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
+import { select, boolean, text, withKnobs } from '@storybook/addon-knobs';
 import React from 'react';
 import AXALinkReact from './AXALinkReact';
 import Readme from '../README.md';
 
-import customIcon from '../../../../static/svg/logo-axa.svg';
 import wrap from '../../../../demo/react/utils/wrap-render-react';
 
+const variantOptions = {
+  none: '',
+  icon: 'icon',
+  red: 'red',
+  white: 'white',
+  'icon-red': 'icon-red',
+  'icon-white': 'icon-white',
+  arrowright: 'arrowright',
+  arrowleft: 'arrowleft',
+  'arrowright-animated': 'arrowright-animated',
+  'arrowleft-animated': 'arrowleft-animated',
+  'arrowright-red': 'arrowright-red',
+  'arrowleft-red': 'arrowleft-red',
+  'arrowright-white': 'arrowright-white',
+  'arrowleft-white': 'arrowleft-white',
+  'arrowright-animated-red': 'arrowright-animated-red',
+  'arrowleft-animated-red': 'arrowleft-animated-red',
+  'arrowright-animated-white': 'arrowright-animated-white',
+  'arrowleft-animated-white': 'arrowleft-animated-white',
+  'hyperlink-white': 'hyperlink-white',
+  'hyperlink-white-underline': 'hyperlink-white-underline',
+  'hyperlink-red': 'hyperlink-red',
+  'hyperlink-red-underline': 'hyperlink-red-underline',
+  secondary: 'secondary',
+};
+
+const iconOptions = {
+  none: '',
+  'arrow-right': 'arrow-right',
+  collapse: 'collapse',
+  document: 'document',
+  download: 'download',
+  email: 'email',
+  expand: 'expand',
+  mobile: 'mobile',
+  phone: 'phone',
+  search: 'search',
+  upload: 'upload',
+  'cloud-upload': 'cloud-upload',
+  'axa-logo': 'axa-logo',
+  'axa-logo-open': 'axa-logo-open',
+};
+
 storiesOf('Atoms/Link/React', module)
+  .addDecorator(withKnobs)
   .addParameters({
     readme: {
       sidebar: Readme,
     },
   })
-  /* Default */
-  .add('Link - default', () => {
-    return wrap(<AXALinkReact>Default</AXALinkReact>);
-  })
+  .add(
+    'Hyperlink', () => {
+      const link = text(
+        'link',
+        'https://axa.ch/en/private-customers.html'
+      );
+      const linkText = text(
+        'Link text',
+        'This is a simple link'
+      );
+      const external = boolean('external', false)
+      const variant = select('variant', variantOptions)
+      const icon = select('icon', iconOptions);
+      const backgrounds = select(
+        'Background color',
+        ['red', 'blue', 'white', 'black'],
+        'white'
+      );
+      const css = `
+        body {
+            background-color: ${backgrounds};
+        }
+      `
 
-  /* Variants */
-  .add('Link - variant: red', () => {
-    return wrap(<AXALinkReact variant="red">Red</AXALinkReact>);
-  })
-
-  .add('Link - variant: secondary', () => {
-    return wrap(<AXALinkReact variant="secondary">Secondary</AXALinkReact>);
-  })
-
-  /* Icon */
-  .add('Link - icon', () => {
-    return wrap(
-      <AXALinkReact variant="icon" icon="arrow-right">
-        Icon
-      </AXALinkReact>
-    );
-  })
-
-  .add('Link - icon onClick', () => {
-    return wrap(
-      <AXALinkReact
-        onClick={() => {
-          // eslint-disable-next-line no-alert, no-undef
-          alert('on link click');
-        }}
-        variant="icon"
-        icon="arrow-right"
-      >
-        Icon
-      </AXALinkReact>
-    );
-  })
-
-  .add('Link - custom icon', () => {
-    return wrap(
-      <AXALinkReact variant="icon" icon={customIcon}>
-        Icon
-      </AXALinkReact>
-    );
-  });
+      return wrap(
+        <div>
+          <style>{css}</style>
+          <AXALinkReact
+            href={link}
+            external={external}
+            variant={variant}
+            icon={icon}
+            onClick={() => {
+              // eslint-disable-next-line no-alert, no-undef
+              alert('on link click');
+            }}
+          >{linkText}
+          </AXALinkReact>
+        </div>);
+    });
