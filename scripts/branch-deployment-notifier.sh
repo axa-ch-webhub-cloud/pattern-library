cdfdaec80578d850a87f2df0e83d0f8304e1fe9d
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-10-11 15:02:43 +0200

    Improve deployment notifier script by adding slash at the end of link

--- scripts/branch-deployment-notifier.sh
+++ scripts/branch-deployment-notifier.sh
@@ -11,6 +11,6 @@
 # source branch that we would be interested in.
 [[ $PR_SOURCE_BRANCH_NAME != *"$"* ]] && BRANCH_NAME="$PR_SOURCE_BRANCH_NAME" || BRANCH_NAME="$SOURCE_BRANCH_NAME"
 
-text="$BRANCH_NAME was deployed here: https://axa-ch.github.io/plib-feature/$BRANCH_NAME"
+text="$BRANCH_NAME was deployed here: https://axa-ch.github.io/plib-feature/$BRANCH_NAME/"
 
 curl -X POST -H "Content-type: application/json;charset=UTF-8" -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"channel\":\"#plib-deployments\", \"text\": \"$text\", \"username\": \"Donald Duck\"}" https://slack.com/api/chat.postMessage
