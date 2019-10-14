a6855b07eb52230ab8e2435dc2bab060fe5bb83e
Daniel B <daniel.berkelmann@axa-winterthur.ch>
2019-10-14 15:32:03 +0200

    Added UI tests

--- src/components/10-atoms/icon/ui.test.js
+++ src/components/10-atoms/icon/ui.test.js
@@ -2,19 +2,39 @@ import { Selector } from 'testcafe';
 
 const host = process.env.TEST_HOST_STORYBOOK_URL;
 
-fixture('Icon - show all icons').page(
-  `${host}/iframe.html?id=atoms-icon--icon-show-all-icons`
+const TAG = 'axa-icon';
+
+const axaIconItem = Selector(
+  () => {
+    return document.querySelector(TAG);
+  },
+  { dependencies: { TAG } }
 );
+const svgImageItem = Selector(
+  () => {
+    return document.querySelector(TAG).shadowRoot;
+  },
+  { dependencies: { TAG } }
+).find('svg');
 
-const ICON_TAG = 'axa-icon';
-const SVG_TAG = 'svg';
+fixture('Icon - set as named property').page(
+  `${host}/iframe.html?id=atoms-icon--icon`
+);
+test('should render svg icon', async t => {
+  await t.expect(svgImageItem.exists).ok();
+});
 
-test('should render icon', async t => {
-  const $axaIcon = await Selector(ICON_TAG);
-  await t.expect($axaIcon.exists).ok();
-  const $axaIconShadow = await Selector(
-    () => document.querySelector('axa-icon').shadowRoot
-  );
-  const $axaIconShadowEl = await $axaIconShadow.find(SVG_TAG);
-  await t.expect($axaIconShadowEl.exists).ok();
+fixture('Icon - no icon is set').page(
+  `${host}/iframe.html?id=atoms-icon--icon&knob-Select%20icon:=download&knob-Do%20not%20set%20icon%20property=true&knob-Load%20icon%20from%20this%20URL:=/svg/logo-axa.svg`
+);
+test('should not render svg icon', async t => {
+  await t.expect(axaIconItem.exists).ok();
+  await t.expect(svgImageItem.exists).notOk();
+});
+
+fixture('Icon - component properties').page(
+  `${host}/iframe.html?id=atoms-icon--icon&knob-Select%20icon:=download&knob-Load%20icon%20from%20URL=true&knob-Load%20icon%20from%20this%20URL:=/svg/logo-axa.svg`
+);
+test('should set size to auto', async t => {
+  await t.expect(axaIconItem.getAttribute('size')).eql('auto');
 });
