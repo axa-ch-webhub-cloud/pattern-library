c4e182bff2c4957922622f2d4755530d24c13bcf
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-10-29 18:21:30 +0100

    Add avatar to slack bot notifier

--- scripts/branch-deployment-notifier.sh
+++ scripts/branch-deployment-notifier.sh
@@ -13,4 +13,4 @@
 
 text="$BRANCH_NAME was deployed here: https://axa-ch.github.io/plib-feature/$BRANCH_NAME/"
 
-curl -X POST -H "Content-type: application/json;charset=UTF-8" -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"channel\":\"#plib-deployments\", \"text\": \"$text\", \"username\": \"Donald Duck\"}" https://slack.com/api/chat.postMessage
+curl -X POST -H "Content-type: application/json;charset=UTF-8" -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"channel\":\"#plib-deployments\", \"text\": \"$text\", \"username\": \"Donald Duck\", \"icon_url\": \"https://avatars2.githubusercontent.com/u/55502240?s=400&v=4\"}" https://slack.com/api/chat.postMessage
