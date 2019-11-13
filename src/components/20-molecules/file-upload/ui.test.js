72a66c88c95abf0994a39a1bdd3e246fd2260522
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2019-11-13 13:17:23 +0100

    Revert: noahs changes

--- src/components/20-molecules/file-upload/ui.test.js
+++ src/components/20-molecules/file-upload/ui.test.js
@@ -177,7 +177,7 @@ test('should exceed maximum number of files', async t => {
 
   await t.expect($inputFileInputElem.exists).ok();
 
-  await t.setFilesToUpload($inputFileInputElem, [validFiles[0], validFiles[1]]);
+  await t.setFilesToUpload($inputFileInputElem, validFiles);
 
   const $figureElems = await Selector(
     () => document.querySelector('axa-file-upload').shadowRoot
