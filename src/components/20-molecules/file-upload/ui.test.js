ac60cf9ccf2f8ea183a16ee24d64add2d16c5461
Noah Liechti <noah.liechti@gmail.com>
2019-11-13 09:34:42 +0100

    add files separately

--- src/components/20-molecules/file-upload/ui.test.js
+++ src/components/20-molecules/file-upload/ui.test.js
@@ -177,7 +177,8 @@ test('should exceed maximum number of files', async t => {
 
   await t.expect($inputFileInputElem.exists).ok();
 
-  await t.setFilesToUpload($inputFileInputElem, validFiles);
+  await t.setFilesToUpload($inputFileInputElem, validFiles[0]);
+  await t.setFilesToUpload($inputFileInputElem, validFiles[1]);
 
   const $figureElems = await Selector(
     () => document.querySelector('axa-file-upload').shadowRoot
