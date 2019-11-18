0bb0097233d9c06cffa7a37f154636c916e47da3
Luca Mele <lucamele1@hotmail.com>
2019-11-18 14:18:02 +0100

    Make logic of undefined more robust

--- src/utils/with-react.js
+++ src/utils/with-react.js
@@ -1,12 +1,16 @@
 import val from '@skatejs/val';
 
+// if a prop is not set, its type is null. safer take null and undefined as a falsy value
+// but accept empty string or 0 as truly value
+const isUndefined = value => value === undefined || value === null;
+
 const applyDefaults = self => {
   const {
     constructor: { properties },
   } = self;
   Object.keys(properties).forEach(property => {
     const { defaultValue } = properties[property];
-    if (defaultValue !== undefined) {
+    if (!isUndefined(defaultValue)) {
       self[property] = defaultValue;
     }
   });
@@ -18,7 +22,7 @@ const distributeProperties = ({ children, ...props }, componentClass) => {
   Object.keys(props).forEach(name => {
     // apply defaults to appropriately marked props
     const value = componentClass.properties[name].defaultValue || undefined;
-    if (value !== undefined && props[name] === undefined) {
+    if (!isUndefined(value) && !isUndefined(props[name])) {
       props[name] = value;
     }
     // FIXME: might be better to detect eventhood via Function constructor?!
