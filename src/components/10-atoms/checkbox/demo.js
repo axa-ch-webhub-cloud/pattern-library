12e515826ab933ebbdff57d229631f80748822d1
Noah Liechti <noah.liechti@gmail.com>
2019-11-26 13:47:41 +0100

    2 checkboxes per demo

--- src/components/10-atoms/checkbox/demo.js
+++ src/components/10-atoms/checkbox/demo.js
@@ -3,7 +3,6 @@ import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
 import './index';
 import Readme from './README.md';
-import createRefId from '../../../utils/create-ref-id';
 
 storiesOf('Atoms/Checkbox/Demos', module)
   .addParameters({
@@ -15,7 +14,17 @@ storiesOf('Atoms/Checkbox/Demos', module)
     const template = html`
       <div>
         <label style="display:flex;"
-          >Label as a parent<axa-checkbox
+          >Label as a parent 1<axa-checkbox
+            class="hover"
+            name="checkbox"
+            ?checked="false"
+            ?required="true"
+            onchange='console.log("checkbox", this.name, " changed to: ", this.checked)'
+            style="margin-left: 10px;"
+          ></axa-checkbox>
+        </label>
+        <label style="display:flex;"
+          >Label as a parent 2<axa-checkbox
             class="hover"
             name="checkbox"
             ?checked="false"
@@ -33,18 +42,33 @@ storiesOf('Atoms/Checkbox/Demos', module)
   })
   .add('Feature - Use your own label as a sibling', () => {
     const template = html`
-      <div style="display:flex;">
-        <label for="checkbox-${createRefId}">Label as a sibling</label>
-        <axa-checkbox
-          refId="checkbox-${createRefId}"
-          class="hover"
-          name="checkbox-${createRefId}"
-          ?checked="false"
-          ?required="true"
-          onchange='console.log("checkbox", this.name, " changed to: ", this.checked)'
-          style="margin-left: 10px;"
-        >
-        </axa-checkbox>
+      <div>
+        <span style="display:flex;">
+          <label for="checkbox-sibling-1">Label as a sibling 1</label>
+          <axa-checkbox
+            refId="checkbox-sibling-1"
+            class="hover"
+            name="checkbox-sibling-1"
+            ?checked="false"
+            ?required="true"
+            onchange='console.log("checkbox", this.name, " changed to: ", this.checked)'
+            style="margin-left: 10px;"
+          >
+          </axa-checkbox>
+        </span>
+        <span style="display:flex;">
+          <label for="checkbox-sibling-2">Label as a sibling 2</label>
+          <axa-checkbox
+            refId="checkbox-sibling-2"
+            class="hover"
+            name="checkbox-sibling-2"
+            ?checked="false"
+            ?required="true"
+            onchange='console.log("checkbox", this.name, " changed to: ", this.checked)'
+            style="margin-left: 10px;"
+          >
+          </axa-checkbox>
+        </span>
       </div>
     `;
 
