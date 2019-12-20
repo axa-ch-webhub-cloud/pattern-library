f05313acb6808464f6f0d2680f437a50c4c219d5
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-12-20 16:04:31 +0100

    Add first test for headings

--- src/components/10-atoms/heading/ui.test.js
+++ src/components/10-atoms/heading/ui.test.js
@@ -9,12 +9,28 @@ fixture('Heading - basic functionality').page(
 const TAG = 'axa-heading';
 const CLASS = '.a-heading';
 
-test('should render heading', async t => {
+async function _getHeadingElement(t) {
   const $axaElem = await Selector(TAG);
   await t.expect($axaElem.exists).ok();
-  const $axaElemShadow = await Selector(
-    () => document.querySelector('axa-heading').shadowRoot
+  const $axaElemShadow = await Selector(() =>
+    document.querySelector('axa-heading').shadowRoot.querySelector('.a-heading')
   );
-  const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
+  // const $axaElemShadowEl = await $axaElemShadow.find(CLASS);
+  const $axaElemShadowEl = $axaElemShadow;
   await t.expect($axaElemShadowEl.exists).ok();
+  return $axaElemShadowEl;
+}
+
+test('should render h1 primary correctly', async t => {
+  const $axaElemShadowEl = await _getHeadingElement(t);
+
+  await t
+    .expect($axaElemShadowEl.getStyleProperty('font-family'))
+    .eql('"Source Sans Pro", Arial, sans-serif');
+  // font-family: "Source Sans Pro", Arial, sans-serif;
+  //   font-weight: 400;
+  //   font-style: normal;
+  //   font-size: 24px;
+  //   line-height: 29px;
+  //   letter-spacing: -0.01em;
 });
