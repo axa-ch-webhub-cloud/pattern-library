e05f68673c44e47c2caf5c8da97cc1dd7bba3cd4
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-01-07 16:43:34 +0100

    change addon id

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,7 +3,7 @@ import '@storybook/addon-knobs/register';
 import { STORIES_CONFIGURED, STORY_MISSING } from '@storybook/core-events'
 import addonAPI from '@storybook/addons'
 
-addonAPI.register('my-organisation/first-page', storybookAPI => {
+addonAPI.register('axa-ch/defaultpage', storybookAPI => {
   storybookAPI.on(STORIES_CONFIGURED, (kind, story) => {
     if (storybookAPI.getUrlState().path === '/story/*') {
       storybookAPI.selectStory('Welcome', 'to Patterns Library')
