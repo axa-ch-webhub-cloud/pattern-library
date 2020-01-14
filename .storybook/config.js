e313e8f259341bc082c873de7b73398719b83704
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-01-14 09:45:50 +0100

    Fix tests by importing components in a storted way

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,34 @@ import { create } from '@storybook/theming';
 import { addReadme } from 'storybook-readme/html';
 import logo from '../src/static/svg/logo-axa.svg';
 
+import "../src/components/10-atoms/icon/index";
+import "../src/components/10-atoms/text/index";
+import "../src/components/30-organisms/container/index";
+import "../src/components/10-atoms/button/index";
+import "../src/components/10-atoms/button-link/index";
+import "../src/components/10-atoms/carousel/index";
+import "../src/components/10-atoms/checkbox/index";
+import "../src/components/10-atoms/fieldset/index";
+import "../src/components/10-atoms/heading/index";
+import "../src/components/10-atoms/radio/index";
+import "../src/components/10-atoms/input-file/index";
+import "../src/components/10-atoms/input-text/index";
+import "../src/components/10-atoms/link/index";
+import "../src/components/10-atoms/textarea/index";
+import "../src/components/20-molecules/cookie-disclaimer/index";
+import "../src/components/20-molecules/dropdown/index";
+import "../src/components/20-molecules/datepicker/index";
+import "../src/components/20-molecules/footer-small/index";
+import "../src/components/20-molecules/policy-features/index";
+import "../src/components/20-molecules/file-upload/index";
+import "../src/components/20-molecules/popup/index";
+import "../src/components/20-molecules/top-content-bar/index";
+import "../src/components/30-organisms/commercial-hero-banner/index";
+import "../src/components/30-organisms/footer/index";
+import "../src/components/30-organisms/table/index";
+import "../src/components/30-organisms/table-sortable/index";
+import "../src/components/30-organisms/testimonials/index";
+
 addParameters({
   readme: {
     codeTheme: 'github',
@@ -25,6 +53,7 @@ const landingpage = require.context('../src/landingpage', true, /(story\.(js|jsx
 // (https://v8.dev/blog/regexp-lookbehind-assertions), which is supported for node 9 and greater
 const components = require.context('../src/components', true, /(?<!node_modules.*)(story|demo)\.(js|jsx)$/);
 const demos = require.context('../src/demo', true, /(story\.(js|jsx)|demo.(js|jsx))$/);
+
 
 configure(() => {
   landingpage.keys().forEach(landingpage);
