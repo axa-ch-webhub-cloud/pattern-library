a2ffc5f59e9f2a0e65b76da7e36efb8ddda4cd1e
Donald Duck <raphael.lueckl@axa.ch>
2020-01-16 08:28:31 +0100

    First approach at improving deployment script. IF does not yet work.

--- scripts/branch-deployment.sh
+++ scripts/branch-deployment.sh
@@ -5,14 +5,24 @@
 #
 # External environment variables: $GITHUB_TOKEN, $PR_SOURCE_BRANCH_NAME, $SOURCE_BRANCH_NAME
 
+# Check that the needed evnironment variables are present
+if [[ -z "$SOURCE_BRANCH_NAME" && -z "$PR_SOURCE_BRANCH_NAME" || $PR_SOURCE_BRANCH_NAME != *"$"*]] then
+  echo "Env variable 'SOURCE_BRANCH_NAME' was not provided."
+  exit 1
+elif [[ -z "$string" ]]; then
+  echo "String is not empty"
+fi
+
 # If the PR_SOURCE_BRANCH_NAME contains a $ char, the variable could not be resolved, which means,
 # that the build does not originate form a pull request. In that case, we go after the branch name.
 # This is an issue with azure, that the source_branch contains whatever reference, but the actual
 # source branch that we would be interested in.
-[[ $PR_SOURCE_BRANCH_NAME != *"$"* ]] && BRANCH_NAME="$PR_SOURCE_BRANCH_NAME" || BRANCH_NAME="$SOURCE_BRANCH_NAME"
+[[ -n "$PR_SOURCE_BRANCH_NAME" && $PR_SOURCE_BRANCH_NAME != *"$"* ]] && BRANCH_NAME="$PR_SOURCE_BRANCH_NAME" || BRANCH_NAME="$SOURCE_BRANCH_NAME"
 echo "PR-Source-Branch-Name:              $PR_SOURCE_BRANCH_NAME"
 echo "Source-Branch-Name:                 $SOURCE_BRANCH_NAME"
 echo "Branch name that will be deployed:  $BRANCH_NAME"
+
+rm -rf ./dist
 
 mkdir ./dist
 git clone https://$GITHUB_TOKEN@github.com/axa-ch/plib-feature.git --depth 1 ./dist
@@ -20,6 +30,9 @@ git clone https://$GITHUB_TOKEN@github.com/axa-ch/plib-feature.git --depth 1 ./d
 rm -rf ./dist/$BRANCH_NAME/
 mkdir -p ./dist/$BRANCH_NAME
 mv -v ./storybook-static/* ./dist/$BRANCH_NAME/
+
+# Done! From here, everything would be ready to be committed and pushed. But
+# outdated branches need to be cleaned up too, so...
 
 # List only remote branches
 ALL_BRANCHES=$(git --no-pager branch -a -r)
@@ -53,6 +66,10 @@ for x in $ALL_FOLDERS_DEPTH_1; do
     fi
 done)
 
+# Outdated branches are removed at this point, and the current branch is up to
+# date. Ready to publish!
+echo "about to commit...."
+exit 1
 git config --global user.email "raphael.lueckl@axa.ch"
 git config --global user.name "Donald Duck"
 git add .
