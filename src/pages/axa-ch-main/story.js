2c161725ab1d0ca41c4e72bcddb3bb47c17bf19a
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-01-30 16:43:51 +0100

    Checkin knobs that highlight our webcomponents

--- src/pages/axa-ch-main/story.js
+++ src/pages/axa-ch-main/story.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
 
 import Readme from '../../../README.md';
 import '../../components/10-atoms/text';
@@ -12,14 +13,47 @@ import navbar from './navbar';
 import footerSmall from './footer-small';
 
 const story = storiesOf('Pages|AXA', module);
+story.addDecorator(withKnobs);
 story.addParameters({
   readme: {
     sidebar: Readme,
   },
 });
 
+function getFirstChildOfAllAxaCustomElements() {
+  return Array.prototype.slice
+    .call(document.querySelectorAll('*'))
+    .filter(function(el) {
+      return el.tagName.toUpperCase().startsWith('AXA-');
+    })
+    .map(el => {
+      if (el.shadowRoot) {
+        return el.shadowRoot.querySelector('*');
+      }
+      return el.querySelector('*');
+    });
+}
+
 story.add('Main Page', () => {
+  const markWebcomponents = boolean('Highlight Webcomponents', false);
   const wrapper = document.createElement('div');
+
+  // Execution is guaranteed to happen after the render.
+  setTimeout(() => {
+    // const a = document.querySelectorAll('*');
+
+    const allChildrenOfWebcomponents = getFirstChildOfAllAxaCustomElements();
+    if (markWebcomponents) {
+      allChildrenOfWebcomponents.forEach(el => {
+        el.style.border = '5px red solid';
+      });
+    } else {
+      allChildrenOfWebcomponents.forEach(el => {
+        el.style.border = 'none';
+      });
+    }
+    console.log(allChildrenOfWebcomponents);
+  });
 
   const resetBrowserDefaultStyles = html`
     <style>
@@ -48,6 +82,10 @@ story.add('Main Page', () => {
     <style>
       .pages-axa-main-page {
         font-family: Source Sans Pro, Arial, sans-serif;
+      }
+
+      .pages-axa-main-page--marked-webcomponents [] {
+        background: black;
       }
     </style>
   `;
