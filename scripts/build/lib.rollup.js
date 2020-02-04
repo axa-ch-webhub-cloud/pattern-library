933680f58d267d07cf76aedf9529509d91a98b01
Luca Mele <lucamele1@hotmail.com>
2020-02-04 11:20:57 +0100

    divide et impera

---
+++ scripts/build/lib.rollup.js
@@ -0,0 +1,65 @@
+const copy = require('rollup-plugin-copy');
+const resolve = require('@rollup/plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+
+const { commonPlugins } = require('./common.rollup.js');
+
+const lib = {
+  input: 'index.js',
+  external: [
+    'lit-element',
+    'lit-html/directives/class-map',
+    'lit-html/directives/repeat',
+    '@skatejs/val',
+  ],
+  output: {
+    file: './lib/index.js',
+    format: 'es',
+  },
+  plugins: [
+    ...commonPlugins,
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              esmodules: true,
+            },
+          },
+        ],
+      ],
+      plugins: ['@babel/plugin-proposal-class-properties'],
+      babelrc: false,
+      exclude: ['node_modules/**'],
+      runtimeHelpers: false,
+    }),
+    resolve({
+      mainFields: ['module', 'main'],
+      resolveOnly: [/^\.{0,2}\/|\.scss$/i], // threat all node_modules as external apart od .scss files
+    }),
+    copy({
+      targets: [
+        'index.d.ts',
+      ],
+      outputFolder: './lib'
+    })
+  ],
+};
+
+const libReact = {
+  ...lib,
+  input: 'index.react.js',
+  output: {
+    file: './lib/index.react.js',
+    format: 'es',
+  },
+  plugins: [...lib.plugins, copy({
+    targets: [
+      'index.react.d.ts',
+    ],
+    outputFolder: './lib'
+  })]
+};
+
+module.exports = { lib, libReact };
