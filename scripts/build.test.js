53672fd5f107b4c23da42bfdd354f01a13a0d666
Luca Mele <lucamele1@hotmail.com>
2020-02-05 10:49:52 +0100

    Add test for folder structure

--- scripts/build.test.js
+++ scripts/build.test.js
@@ -20,6 +20,11 @@ const ALLOWED_DEPENDENCIES = [
   'lit-html'
 ];
 
+console.log(chalk.cyan(outdent`
+  
+  👉 Testing project constraints...
+`));
+
 const { length : found } = Object.keys(dependencies).filter(
   key => ALLOWED_DEPENDENCIES.indexOf(key) === -1
 );
@@ -37,7 +42,7 @@ if (found) {
 }
 
 // get all components and test if there is any package json with main field or not allowed categories
-const files = glob.sync(`**${sep}src${sep}components${sep}*(${ALLOWED_CATEGORIES.join('|')})${sep}*${sep}index.js`);
+const files = glob.sync(`src${sep}components${sep}*(${ALLOWED_CATEGORIES.join('|')})${sep}*${sep}index.js`);
 
 files.forEach((file) => {
   const pkgPath = file.replace('index.js', 'package.json');
@@ -55,7 +60,7 @@ files.forEach((file) => {
 
           The package.json ${pkgPath} has a "main":"${pkg.main}"
           key/value pair which is not allowed to exist in the source code.
-          
+
           Maybe your last build or publish broke unexpectedly.
           How to fix this: Remove all main fields in the package.jsons of all components and try again.
           For your information: Main field will be added during build or publish and after it finishes is going to be removed.
@@ -65,5 +70,28 @@ files.forEach((file) => {
     }
   }
 });
+
+// Test if there are not allowed categories
+const allFiles = glob.sync(`src${sep}components${sep}*${sep}**${sep}*.js`);
+
+allFiles.forEach((file) => {
+  // file is like this: src/components/20-molecules/datepicker/index.js
+  const { 2: category } = file.split(sep);
+  if (ALLOWED_CATEGORIES.indexOf(category) === -1) {
+    throw new Error(
+      chalk.red(outdent`
+
+        The category ${category} is not allowed!
+
+        Allowed categories are: ${ALLOWED_CATEGORIES.join(', ')}
+        `)
+    );
+  }
+});
+
+console.log(chalk.green(outdent`
+
+  👉 All project constraints are met
+`));
 
 process.exit(0);
