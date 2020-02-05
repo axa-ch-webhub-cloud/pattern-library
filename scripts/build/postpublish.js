b7da966b44889956251c345e4b07afd4e09d05a5
Luca Mele <lucamele1@hotmail.com>
2020-02-05 09:09:21 +0100

    implement PR Feedbacks

--- scripts/build/postpublish.js
+++ scripts/build/postpublish.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
-// TODO Please document purpose/rationale for this file
+// Resets the package json to the original state, which was modified during
+// prepublish phase
 
 const fs = require('fs');
 
-const pkgPath = `${process.cwd()}/package.json`;
-const pkgPathTmp = `${process.cwd()}/.tmp.package.json`;
-// TODO DRYify process.cwd()
+const { cwd } = process;
 
+const pkgPath = `${cwd()}/package.json`;
+const pkgPathTmp = `${cwd()}/.tmp.package.json`;
+
+// No need to safeguard here cause if there is an error, the process gets aborted
+// and node closes all open and temporary files, ergo node takes care of the cleanup
 fs.copyFileSync(pkgPathTmp, pkgPath);
-// TODO file ops can always fail, since you're not the exclusive owner of the file system - guard against failure here and elsewhere
 
 fs.unlinkSync(pkgPathTmp);
--- scripts/build/prepublish.js
+++ scripts/build/prepublish.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
-// TODO Please document purpose/rationale for this file
+// This file modifies the package.json of a component and makes a backup of the original
+// package.json for later reset (postpublish).
 
 const fs = require('fs');
 
-const pkgPath = `${process.cwd()}/package.json`;
-const pkgPathTmp = `${process.cwd()}/.tmp.package.json`;
-// TODO DRYify process.cwd()
+const { cwd } = process;
 
+const pkgPath = `${cwd()}/package.json`;
+const pkgPathTmp = `${cwd()}/.tmp.package.json`;
+
+// eslint-disable-next-line import/no-dynamic-require
 const pkg = require(pkgPath);
 
+// No need to safeguard here cause if there is an error, the process gets aborted
+// and node closes all open and temporary files, ergo node takes care of the cleanup
 fs.copyFileSync(pkgPath, pkgPathTmp);
 
 pkg.main = "lib/index.js";
