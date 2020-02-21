28290beed5a195512e4d4f464200045a3027176e
Noah Liechti <noah.liechti@gmail.com>
2020-02-21 10:12:35 +0100

    knobs

--- src/components/30-organisms/table-sortable/story.js
+++ src/components/30-organisms/table-sortable/story.js
@@ -9,7 +9,9 @@ const model = {
   thead: [
     { html: 'Age', sort: 'ASC' },
     { html: 'Name', sort: 'ASC' },
-    { html: 'City', sort: 'DESC' },
+    { html: 'City', sort: 'ASC' },
+    { html: 'Date of entry', sort: 'DESC' },
+    { html: 'Date of exit', sort: 'DESC' },
     { html: 'Department' },
   ],
   tbody: [
@@ -17,36 +19,48 @@ const model = {
       { html: '<span>55</span>' },
       { html: '<span>Peter</span>' },
       { html: '<span>Winterthur</span>' },
+      { html: '<span>22.04.2019</span>' },
+      { html: '<span>10.01.2020</span>' },
       { html: 'A' },
     ],
     [
       { html: '<span>22</span>' },
       { html: '<span>Chris</span>' },
       { html: '<span>Zürich</span>' },
+      { html: '<span>04.04.2019</span>' },
+      { html: '<span>04.06.2019</span>' },
       { html: 'B' },
     ],
     [
       { html: '<span>46</span>' },
       { html: '<span>Hubert</span>' },
       { html: '<span>Kreuzlingen</span>' },
+      { html: '<span>01.01.2020</span>' },
+      { html: '<span>05.02.2020</span>' },
       { html: 'C' },
     ],
     [
       { html: '<span>51</span>' },
       { html: '<span>Petra</span>' },
       { html: '<span>Kreuzlingen</span>' },
+      { html: '<span>08.10.2018</span>' },
+      { html: '<span>11.12.2019</span>' },
       { html: 'D' },
     ],
     [
       { html: '<span>18</span>' },
       { html: '<span>Maria</span>' },
       { html: '<span>Frauenfeld</span>' },
+      { html: '<span>29.04.2018</span>' },
+      { html: '<span>01.01.2020</span>' },
       { html: 'E' },
     ],
     [
       { html: '<span>29</span>' },
       { html: '<span>David</span>' },
       { html: '<span>Frauenfeld</span>' },
+      { html: '<span>02.05.2013</span>' },
+      { html: '<span>02.05.2015</span>' },
       { html: 'F' },
     ],
   ],
@@ -65,11 +79,13 @@ storiesOf('Components|Organisms/Table Sortable', module)
 
     const innerscroll = text('innerscroll', '');
     const maxheight = text('maxheight', '');
+    const dateSortColumnIndex = text('dateSortColumnIndex', '3,4');
 
     const template = html`
       <axa-table-sortable
         innerscroll="${innerscroll}"
         maxheight="${maxheight}"
+        dateSortColumnIndex="3,4"="${dateSortColumnIndex}"
         model="${JSON.stringify(model)}"
       ></axa-table-sortable>
     `;
