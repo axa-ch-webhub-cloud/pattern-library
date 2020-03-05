d1977375979b43ef9345001c3439b48ccbf24b51
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-03-05 16:10:20 +0100

    Extract examples

--- src/components/20-molecules/datepicker/pup.test.js
+++ src/components/20-molecules/datepicker/pup.test.js
@@ -59,54 +59,19 @@ describe('Datepicker', () => {
         )
         .click();
     });
-    // await new Promise(resolve => {
-    //   setTimeout(async () => {
-    //     await page.click(
-    //       'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-    //     );
-    //     await page.click(
-    //       'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-    //     );
-    //     await page.click(
-    //       'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-    //     );
-    //     await page.click(
-    //       'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-    //     );
-    //     resolve();
-    //   }, 5000);
-    // });
 
-    // await new Promise(resolve => {
-    //   setTimeout(async () => {
-    //     await page.evaluate(() => {
-    //       // debugger;
-    //       const a = [
-    //         ...document.querySelector(
-    //           'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-    //         ).classList,
-    //       ];
-    //       console.log('hans', a);
-    //       return a;
-    //     });
-    //     resolve();
-    //   }, 5000);
-    // });
-    console.log('i run after bee');
+    // This timeout is necessary and prone to fail.
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
 
     const isSelectedDay = await page.evaluate(() => {
       return document.querySelector(
         'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
       ).classList;
     });
-
-    console.log(isSelectedDay);
-    console.log(
-      'is this the value?',
-      Object.values(isSelectedDay).includes(
-        'm-datepicker__calendar-selected-day'
-      )
-    );
 
     expect(
       Object.values(isSelectedDay).includes(
@@ -121,7 +86,6 @@ describe('Datepicker', () => {
         ).classList
       ).includes('m-datepicker__calendar-selected-day')
     );
-    console.log('desel', isNowDeselectedDay);
 
     expect(isNowDeselectedDay).toBe(false);
 
@@ -150,121 +114,13 @@ describe('Datepicker', () => {
     );
     expect(isNewDaySelected).toBe(true);
 
-    // const yearValue = await page.evaluate(
-    //   () =>
-    //     document.querySelector(
-    //       '.js-datepicker__dropdown-year .js-dropdown__title'
-    //     ).textContent
-    // );
-    // expect(yearValue).toBe('2020');
-  });
-
-  test('find some tag inside shadow root of hero banner', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-organisms-commercial-hero-banner--commercial-hero-banner',
-      { waitUntil: 'networkidle0' }
+    const yearValue = await page.evaluate(
+      () =>
+        document.querySelector(
+          '.js-datepicker__dropdown-year .js-dropdown__title'
+        ).textContent
     );
-    const banner = await page.evaluate(() => {
-      return document
-        .querySelector('axa-commercial-hero-banner')
-        .shadowRoot.querySelector('header');
-    });
-    expect(banner).toBeTruthy();
-  });
-
-  test('find textnode inside shadow root of hero banner', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-molecules-file-upload--file-upload',
-      { waitUntil: 'networkidle0' }
-    );
-    const banner = await page.evaluate(() => {
-      return document
-        .querySelector('axa-file-upload')
-        .shadowRoot.querySelector('.a-input-file--motion').textContent;
-    });
-    expect(banner).toContain('Upload file');
-  });
-
-  test('find textnode inside shadow root of hero banner', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-molecules-file-upload--file-upload',
-      { waitUntil: 'networkidle0' }
-    );
-    const banner = await page.evaluate(() => {
-      return document
-        .querySelector('axa-file-upload')
-        .shadowRoot.querySelector('.a-input-file--motion').textContent;
-    });
-    expect(banner).toContain('Upload file');
-  });
-
-  test('manipulate textnode and icon inside shadow root of hero banner', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-molecules-cookie-disclaimer--cookie-disclaimer',
-      { waitUntil: 'networkidle0' }
-    );
-
-    // Query node text, this acts like a dom snapshot and is not updated
-    const buttonText = await page.evaluate(() => {
-      return document
-        .querySelector('axa-cookie-disclaimer')
-        .shadowRoot.querySelector('axa-button').textContent;
-    });
-    expect(buttonText).toContain('Accept');
-
-    // Update variant with property / setAttribute
-    const newlyColoredAcceptButton = await page.evaluate(() => {
-      const acceptButton = document
-        .querySelector('axa-cookie-disclaimer')
-        .shadowRoot.querySelector('axa-button');
-
-      acceptButton.variant = 'red';
-      // acceptButton.setAttribute('variant', 'red'); Would also work!
-
-      return document
-        .querySelector('axa-cookie-disclaimer')
-        .shadowRoot.querySelector('axa-button').variant;
-    });
-    expect(newlyColoredAcceptButton).toContain('red');
-
-    const updatedButtonText = await page.evaluate(() => {
-      const acceptButton = document
-        .querySelector('axa-cookie-disclaimer')
-        .shadowRoot.querySelector('axa-button');
-
-      acceptButton.innerText = 'Akzeptieren';
-
-      return acceptButton.textContent;
-    });
-    expect(updatedButtonText).toContain('Akzeptieren');
-  });
-
-  test('click directly on the axa-link custom element', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-atoms-link-react-demos--feature-link-with-variable-icons',
-      { waitUntil: 'networkidle0' }
-    );
-
-    const axaLink = await page.evaluate(() => {
-      return document.querySelector('axa-link');
-    });
-
-    await page.click('axa-link');
-  });
-
-  test('accept cookie disclaimer', async () => {
-    await page.goto(
-      'https://localhost:6006/iframe.html?id=components-molecules-cookie-disclaimer--cookie-disclaimer',
-      { waitUntil: 'networkidle0' }
-    );
-
-    await page.evaluate(() => {
-      const acceptCookiesButton = document
-        .querySelector('axa-cookie-disclaimer')
-        .shadowRoot.querySelector('axa-button');
-
-      acceptCookiesButton.click();
-    });
+    expect(yearValue).toBe('2020');
   });
 
   afterAll(async () => {
