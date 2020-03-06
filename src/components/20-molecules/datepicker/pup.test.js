095e668fe464d72b3aa77126f19c7c27cc70e88f
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-03-06 07:48:04 +0100

    Refactor tests

--- src/components/20-molecules/datepicker/pup.test.js
+++ src/components/20-molecules/datepicker/pup.test.js
@@ -1,8 +1,3 @@
-/**
- * @name Alibaba product search
- * @desc Searches Alibaba.com for a product and checks if the results show up
- */
-
 /* eslint-disable */
 
 const puppeteer = require('puppeteer');
@@ -16,7 +11,6 @@ const config = {
   headless: false,
   executablePath:
     '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  // slowMo: 250,
   devtools: true,
 };
 
@@ -25,22 +19,20 @@ beforeAll(async () => {
   page = await browser.newPage();
 });
 
-const chooseMonth = async () => {
+const chooseMonthByIndex = async monthIndex => {
   await page.click('.js-datepicker__dropdown-month .js-dropdown__toggle');
-  // October
   await page.click(
-    '.js-datepicker__dropdown-month .js-dropdown__content .js-dropdown__button[data-index="10"]'
+    `.js-datepicker__dropdown-month .js-dropdown__content .js-dropdown__button[data-index="${monthIndex}"]`
   );
 };
 
-const assertMonth = async () => {
-  const monthValue = await page.evaluate(
+const getSelectedMonth = async () => {
+  return await page.evaluate(
     () =>
       document.querySelector(
         '.js-datepicker__dropdown-month .js-dropdown__title'
       ).textContent
   );
-  expect(monthValue).toBe('Oktober');
 };
 
 const clickCalendarDayOfCurrentMonth = async day => {
@@ -63,51 +55,56 @@ const isDaySelected = async day => {
   }, day);
 };
 
+const assertDayIsSelected = async day => {
+  return await page.waitFor(
+    `button[data-day="${day}"][class*="m-datepicker__calendar-current-month"][class*="m-datepicker__calendar-selected-day"]`
+  );
+};
+
+const assertDayIsDeselected = async day => {
+  return await page.waitFor(
+    `button[data-day="${day}"][class*="m-datepicker__calendar-current-month"]:not([class*="m-datepicker__calendar-selected-day"])`
+  );
+};
+
+const getSelectedYear = async () =>
+  await page.evaluate(
+    () =>
+      document.querySelector(
+        '.js-datepicker__dropdown-year .js-dropdown__title'
+      ).textContent
+  );
+
 describe('Datepicker', () => {
   test('should select February the 13th and then the 14th ', async () => {
     await page.goto(
       'https://patterns.axa.ch/iframe.html?id=components-molecules-datepicker--datepicker',
       { waitUntil: 'networkidle0' }
     );
-    await chooseMonth();
-    await assertMonth();
+    await chooseMonthByIndex(10);
+    const monthValue = await getSelectedMonth(10);
+    expect(monthValue).toBe('Oktober');
+
     await clickCalendarDayOfCurrentMonth(13);
 
-    // This timeout is necessary and prone to fail.
-    await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
-    });
+    await assertDayIsSelected(13);
 
-    const isSelectedDay = await isDaySelected(13);
+    const is13thDaySelected = await isDaySelected(13);
+    expect(is13thDaySelected).toBe(true);
 
-    expect(isSelectedDay).toBe(true);
-
-    const isNowDeselectedDay = await isDaySelected(22);
-
-    expect(isNowDeselectedDay).toBe(false);
+    const is22thNowDeselected = await isDaySelected(22);
+    expect(is22thNowDeselected).toBe(false);
 
     await clickCalendarDayOfCurrentMonth(14);
+    await assertDayIsDeselected(13);
 
-    const isOldDayStillSelected = await page.evaluate(() =>
-      Object.values(
-        document.querySelector(
-          'button[class*="m-datepicker__calendar-current-month"][data-day="13"]'
-        ).classList
-      ).includes('m-datepicker__calendar-selected-day')
-    );
+    const isOldDayStillSelected = await isDaySelected(13);
     expect(isOldDayStillSelected).toBe(false);
 
     const isNewDaySelected = await isDaySelected(14);
     expect(isNewDaySelected).toBe(true);
 
-    const yearValue = await page.evaluate(
-      () =>
-        document.querySelector(
-          '.js-datepicker__dropdown-year .js-dropdown__title'
-        ).textContent
-    );
+    const yearValue = getSelectedYear;
     expect(yearValue).toBe('2020');
   });
 
