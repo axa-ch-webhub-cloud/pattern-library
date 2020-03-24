f3de8eb5fe5c677a09f2f053743ead6e8b6177d7
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-03-24 17:29:40 +0100

    remove browser default margins

--- src/components/10-atoms/heading/story.js
+++ src/components/10-atoms/heading/story.js
@@ -19,17 +19,28 @@ storyAXAHeading.add('Heading', () => {
   const wrapper = document.createElement('div');
   const template = html`
     <axa-heading rank="1">H1 Primary Heading</axa-heading>
+    <br />
     <axa-heading rank="2">H2 Primary Heading</axa-heading>
+    <br />
     <axa-heading rank="3">H3 Primary Heading</axa-heading>
+    <br />
     <axa-heading rank="4">H4 Primary Heading</axa-heading>
+    <br />
     <axa-heading rank="5">H5 Primary Heading</axa-heading>
+    <br />
     <axa-heading rank="6">H6 Primary Heading</axa-heading>
+    <br />
 
     <axa-heading variant="secondary" rank="1">H1 Secondary Heading</axa-heading>
+    <br />
     <axa-heading variant="secondary" rank="2">H2 Secondary Heading</axa-heading>
+    <br />
     <axa-heading variant="secondary" rank="3">H3 Secondary Heading</axa-heading>
+    <br />
     <axa-heading variant="secondary" rank="4">H4 Secondary Heading</axa-heading>
+    <br />
     <axa-heading variant="secondary" rank="5">H5 Secondary Heading</axa-heading>
+    <br />
     <axa-heading variant="secondary" rank="6">H6 Secondary Heading</axa-heading>
   `;
 
