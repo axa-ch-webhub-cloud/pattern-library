c041b63a3d237f68bb82f9a3ef89d46c20b9bb77
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-03-27 18:50:00 +0100

    Add new variant to react story

--- src/components/10-atoms/checkbox/react/story.jsx
+++ src/components/10-atoms/checkbox/react/story.jsx
@@ -18,13 +18,23 @@ storiesOf('Components|Atoms/Checkbox/React', module)
   .add('Checkbox - uncontrolled', () => {
     const label = text('label', 'I agree to conditions of data protection.');
     const name = text('name', 'my-checkbox');
-    const variant = select('variant', ['square', 'checkmark']);
+    const variant = select('variant', [
+      'square',
+      'checkmark',
+      'inverted-checkmark',
+    ]);
     const checked = boolean('checked', true);
     const disabled = boolean('disabled', false);
     const errortext = boolean('error', false);
     const required = boolean('required', false);
+    const styled = boolean('styled', false);
 
     const div = document.createElement('div');
+
+    if (variant && variant.indexOf('inverted') > -1) {
+      div.style.backgroundColor = '#027180';
+      div.style.padding = '10px';
+    }
 
     ReactDOM.render(
       <AXACheckboxReact
@@ -34,6 +44,7 @@ storiesOf('Components|Atoms/Checkbox/React', module)
         disabled={disabled}
         variant={variant}
         required={required}
+        styled={styled}
         onChange={e => console.log('checkbox', name, ' changed to: ', checked)}
         error={errortext ? 'Please accept our terms and conditions.' : ''}
       />,
