c888f41097f211c2c5898f58c300c31d5821ca88
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-03-30 13:18:33 +0200

    Add changelog to story

--- src/components/10-atoms/heading/story.js
+++ src/components/10-atoms/heading/story.js
@@ -4,6 +4,7 @@ import { boolean, select, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import './index';
 import Readme from './README.md';
+import Changelog from './CHANGELOG.md';
 
 const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
@@ -13,6 +14,7 @@ storyAXAHeading.addParameters({
   readme: {
     sidebar: Readme,
   },
+  changelog: Changelog,
 });
 
 storyAXAHeading.add('Heading', () => {
