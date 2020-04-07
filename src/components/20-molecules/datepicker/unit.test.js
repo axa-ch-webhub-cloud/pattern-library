dcb58706b792626fb7625bf566a7136ab4cd9f02
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-04-07 08:57:06 +0200

    add unit tests for range function

--- src/components/20-molecules/datepicker/unit.test.js
+++ src/components/20-molecules/datepicker/unit.test.js
@@ -1,4 +1,4 @@
-import { getWeekdays } from './utils/date';
+import { getWeekdays, range } from './utils/date';
 
 describe('Datepicker unit tests', () => {
   it('should return correct month names with English abreviations', () => {
@@ -11,5 +11,31 @@ describe('Datepicker unit tests', () => {
       'Sa',
       'Su',
     ]);
+  });
+
+  describe('Range function', () => {
+    it('should return single year', () => {
+      expect(range(2020, 2020)).toEqual([2020]);
+    });
+
+    it('should return years from 1990 to 2000', () => {
+      expect(range(1990, 2000)).toEqual([
+        1990,
+        1991,
+        1992,
+        1993,
+        1994,
+        1995,
+        1996,
+        1997,
+        1998,
+        1999,
+        2000,
+      ]);
+    });
+
+    it('should return empty array if to-year is younger than from-year', () => {
+      expect(range(2001, 2000)).toEqual([]);
+    });
   });
 });
