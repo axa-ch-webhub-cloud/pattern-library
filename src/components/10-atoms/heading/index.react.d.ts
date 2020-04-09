c75fcda3ecc1f1c482952623698e9c5cf583541c
Marek Laco <marek.laco@axa-winterthur.ch>
2020-04-09 11:16:55 +0200

    chore(heading): update TS type to allow all HTMLHeadingElement's HTMLAttributes
    As all attributes are now passed down.

--- src/components/10-atoms/heading/index.react.d.ts
+++ src/components/10-atoms/heading/index.react.d.ts
@@ -3,7 +3,7 @@ import React from 'react';
 export type Variant = 'primary' | 'secondary';
 export type Rank = 1 | 2 | 3 | 4 | 5 | 6;
 
-export interface AXAHeadingProps {
+export interface AXAHeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   rank: Rank;
   variant?: Variant;
 }
