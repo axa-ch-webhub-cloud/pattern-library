0e656dbdca9d574e59ce064cf193fd1e1285fa65
Noah Liechti <noah.liechti@gmail.com>
2020-04-15 08:51:03 +0200

    make knobs select

--- src/components/10-atoms/fieldset/story.js
+++ src/components/10-atoms/fieldset/story.js
@@ -1,6 +1,6 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import './index';
 import Readme from './README.md';
@@ -16,29 +16,46 @@ storiesOf('Components|Atoms/Fieldset', module)
   })
   .add('Fieldset', () => {
     const error = text('error', '');
+    const options = {
+      None: null,
+      Stretch: 'stretch',
+      Horizontal: 'horizontal',
+    };
+    const horizontal = select('horizontal', options);
 
     const wrapper = document.createElement('div');
+
+    const radioElement = html`
+      <axa-checkbox
+        label="I agree to conditions of data protection."
+        error="Please accept our terms and conditions."
+      ></axa-checkbox>
+      <axa-checkbox label="I accept the conditions"></axa-checkbox>
+      <axa-checkbox
+        label="I want to sign up for the newsletter"
+        checked
+        disabled
+      ></axa-checkbox>
+    `;
+
     const template = html`
       <axa-text variant="size-3">
         Resize your browser to a tablet or mobile width to see the effect of
         'horizontal="stretch"'</axa-text
       >
       <br />
-
-      <axa-fieldset error="${error}">
-        <axa-checkbox
-          label="I agree to conditions of data protection."
-          error="Please accept our terms and conditions."
-        ></axa-checkbox>
-        <axa-checkbox label="I accept the conditions"></axa-checkbox>
-        <axa-checkbox
-          label="I want to sign up for the newsletter"
-          checked
-          disabled
-        ></axa-checkbox>
-      </axa-fieldset>
+      ${horizontal
+        ? html`
+            <axa-fieldset horizontal="${horizontal}" error="${error}"
+              >${radioElement}</axa-fieldset
+            >
+          `
+        : html`
+            <axa-fieldset error="${error}">
+              ${radioElement}
+            </axa-fieldset>
+          `}
     `;
-
     render(template, wrapper);
     return wrapper;
   });
