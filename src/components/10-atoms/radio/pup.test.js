1d44a6a066559d6796e9e0d5465a6ffc7ae99ecf
Markus Walther <markus.walther@axa-winterthur.ch>
2020-04-22 14:32:10 +0200

    Implement puppeteer test that deletes and queries DOM nodes (cf. issue #1737)

---
+++ src/components/10-atoms/radio/pup.test.js
@@ -0,0 +1,85 @@
+/* eslint-disable */
+
+// imports
+const puppeteer = require('puppeteer-core'); // let's not download a special Chromium if we use our local Chrome install anyways...
+
+// module globals
+let browser;
+let page;
+
+// set up
+jest.setTimeout(30 * 1000);
+
+const config = {
+  // headless: false,
+  defaultViewport: null, // <= set this to have viewport emulation off
+  executablePath:
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  // devtools: true,
+  // slowMo: 1000,
+};
+
+beforeAll(async () => {
+  browser = await puppeteer.launch(config);
+  page = await browser.newPage();
+});
+
+// generic helpers
+
+/* fun runs *in the browser*! It cannot see closure variables from node scopes!
+   Therefore provide such variables as args.
+   Also, fun needs to return a truthy value to stop polling and avoid timeout.
+   Therefore, define it such that it eventually returns true.
+   To *debug* fun, sprinkle with debugger statement(s) at the right places and
+   set headless: false, devtools: true in puppeteer config.
+   */
+const waitForFun = async (fun, ...args) =>
+  (await page.waitForFunction(fun, { polling: 'raf' }, ...args)).jsonValue();
+
+// tests
+describe('Radio', () => {
+  test('should clean up style nodes properly', async () => {
+    await page.goto(
+      'http://localhost:6006/iframe.html?id=components-atoms-radio--radio'
+    );
+
+    // helper fumctions
+    const styleExists = (componentName, expected = true) =>
+      waitForFun(
+        (selector, value) => !!document.querySelector(selector) === value,
+        `style[data-name="${componentName}"]`,
+        expected
+      );
+
+    const deleteNodes = componentName =>
+      waitForFun(selector => {
+        const nodes = [...document.querySelectorAll(selector)];
+        if (!nodes.length) {
+          return false;
+        }
+        nodes.forEach(node => node.remove());
+        return true;
+      }, componentName);
+
+    // tests proper:
+
+    // axa-text style node exists
+    expect(await styleExists('axa-text')).toBe(true);
+    // axa-radio  style node exists
+    expect(await styleExists('axa-radio')).toBe(true);
+    // delete axa-text
+    expect(await deleteNodes('axa-text')).toBe(true);
+    // axa-text style node no longer exists
+    expect(await styleExists('axa-text', false)).toBe(true);
+    // axa-radio style node still exists
+    expect(await styleExists('axa-radio')).toBe(true);
+    // delete all axa-radio instances
+    expect(await deleteNodes('axa-radio')).toBe(true);
+    // now axa-radio style node no longer exists
+    expect(await styleExists('axa-radio', false)).toBe(true);
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
