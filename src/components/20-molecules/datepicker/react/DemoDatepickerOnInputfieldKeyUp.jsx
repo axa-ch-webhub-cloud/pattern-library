a3ff09e2b1346a37fe9ccd57a9dae54316f5deda
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-06-29 08:36:21 +0200

    Add demo

---
+++ src/components/20-molecules/datepicker/react/DemoDatepickerOnInputfieldKeyUp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import AXADatepickerReact from './AXADatepickerReact';
+
+const DemoDatepickerOnInputfieldKeyUp = () => {
+  return (
+    <div>
+      <span id="datepicker-react-testoutput" />
+      <br />
+      <AXADatepickerReact
+        inputfield
+        id="datepicker-react-inputfield-on-date-change"
+        onInputfieldKeyUp={e => {
+          document.querySelector('#datepicker-react-testoutput').innerHTML =
+            e.key;
+        }}
+        allowedyears={[2019, 2020]}
+      />
+    </div>
+  );
+};
+
+export default DemoDatepickerOnInputfieldKeyUp;
