5eb9193c3f786a5c4bf5548f3ca6c1cdde4b7725
Noah Liechti <noah.liechti@gmail.com>
2020-07-02 08:03:51 +0200

    heading flat list

--- src/components/10-atoms/heading/story.js
+++ src/components/10-atoms/heading/story.js
@@ -8,7 +8,7 @@ import Changelog from './CHANGELOG.md';
 
 const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
-const storyAXAHeading = storiesOf('Components|Atoms/Heading', module);
+const storyAXAHeading = storiesOf('Components|Heading', module);
 storyAXAHeading.addDecorator(withKnobs);
 storyAXAHeading.addParameters({
   readme: {
