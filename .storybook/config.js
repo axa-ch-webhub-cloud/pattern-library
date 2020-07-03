851d8fce5fa5e2b81ca3101a111be707d309e9dd
Noah Liechti <noah.liechti@gmail.com>
2020-07-03 16:01:18 +0200

    config

--- .storybook/config.js
+++ .storybook/config.js
@@ -19,6 +19,8 @@ addParameters({
       brandUrl: 'https://github.com/axa-ch/patterns-library/tree/develop',
       brandImage: logo,
     }),
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 });
 
