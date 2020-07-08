48f7a0808665c19af39f6fe863bb79c6df7628cb
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-07-08 20:19:16 +0200

    improve polyfill and add headers

---
+++ .storybook/polyfills-ie11.js
@@ -0,0 +1,22 @@
+import 'core-js/es6/reflect';
+import 'core-js/es6/promise';
+import 'core-js/fn/array/includes';
+import 'core-js/fn/array/fill';
+import 'core-js/fn/string/includes';
+import 'core-js/fn/array/find-index';
+import 'core-js/fn/array/from';
+import 'core-js/fn/object/assign';
+import '@webcomponents/webcomponentsjs';
+import 'core-js/es6/object';
+import 'core-js/modules/es6.symbol';
+import 'core-js/es6/map';
+import 'core-js/es6/symbol';
+import 'core-js/es6/weak-map';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.array.find';
+import 'core-js/modules/es6.array.from';
+import 'core-js/modules/es6.array.find-index';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es7.object.entries';
+import 'core-js/modules/es6.string.starts-with';
+
