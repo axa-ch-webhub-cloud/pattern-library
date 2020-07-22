a892db140516e81ab2842380e1729955d90ad4a4
Raphael <raphael.suter@axa.ch>
2020-07-22 10:10:15 +0200

    Adjust clickable demo

--- src/components/10-atoms/input-file/demo.js
+++ src/components/10-atoms/input-file/demo.js
@@ -1,8 +1,8 @@
 /* global document */
 import { storiesOf } from '@storybook/html';
-import Readme from './README.md';
 import Changelog from './CHANGELOG.md';
 import './index';
+import Readme from './README.md';
 
 storiesOf('Components|Input File/Demos', module)
   .addParameters({
@@ -14,16 +14,20 @@ storiesOf('Components|Input File/Demos', module)
   .add('Clickable', () => {
     const inputFile = document.createElement('axa-input-file');
     inputFile.setAttribute('multiple', 'multiple');
+
     let counter = 0;
-    inputFile.innerHTML = `You have chosen ${counter} files`;
+    inputFile.text = `You have chosen ${counter} files`;
 
     inputFile.addEventListener('change', e => {
       counter = e.target.files.length;
+
       inputFile.querySelector('.a-input-file').innerHTML = `
         <span class="a-input-file__flex-wrapper">You have chosen ${counter} files</span>
       `;
+
       // eslint-disable-next-line no-console
       console.log('counter!!', counter);
     });
+
     return inputFile;
   });
