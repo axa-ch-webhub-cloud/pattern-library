f00c3a50041d197e1b899f75e07c9afae9234792
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-08-19 16:11:20 +0200

    add decorators in a new way

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import './additional-polyfills-ie11.js'; // TODO: remove this line and the file on upgrading to 6.X
 import '@axa-ch/patterns-library-polyfill';
 
-import { addParameters, addDecorator } from '@storybook/html';
+import { addParameters } from '@storybook/html';
 import { addReadme } from 'storybook-readme/html';
 import { withHTML } from './addons/codepreview/decorators/html';
 
