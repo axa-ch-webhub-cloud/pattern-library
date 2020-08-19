0a9b08431ee098d538268491628d58cb397b8822
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-08-19 15:48:39 +0200

    fix pathes

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,9 @@ module.exports = {
     stories: ['../src/other/landingpage/story.js', '../src/components/**/(story|demo).(js|jsx)', '../src/pages/axa-ch-main/story.js'],
     addons: [
       //'storybook-readme/register',
-      './.storybook/addons/changelog/register.js',
+      //'./addons/changelog/register',
       '@storybook/addon-knobs',
-      './.storybook/addons/codepreview/register.js',
+      //'./addons/codepreview/register',
       '@storybook/addon-a11y'
     ],
 };
