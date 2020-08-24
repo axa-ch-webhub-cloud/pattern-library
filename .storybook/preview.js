ce287a3ae14180ee9256ab76b766bbc7a907187b
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-08-24 15:15:37 +0200

    Remove readme addon because its written for sb5 and not working correctly with sb6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,4 @@
 import '@axa-ch/patterns-library-polyfill';
-
-import { addParameters } from '@storybook/html';
 import { withHTML } from '@whitespace/storybook-addon-html/html';
-
-addParameters({
-  readme: {
-    codeTheme: 'github',
-  },
-  options: {
-    theme: {}// this is just a workaround for addon-readme
-  }
-});
 
 export const decorators = [withHTML];
