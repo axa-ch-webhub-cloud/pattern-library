d453ecd58c24f96b89523ca8b7e4a1c72118e638
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-08-27 08:42:10 +0200

    deactivate code preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
     stories: ['../src/other/landingpage/story.js', '../src/components/**/@(story|demo).@(js|jsx)', '../src/pages/axa-ch-main/story.js'],
     addons: [
-      //'./addons/changelog/register',
+      //'./addons/changelog/register', // deactivated because of SB issue
       '@storybook/addon-knobs',
-      '@whitespace/storybook-addon-html/register',
+      //'@whitespace/storybook-addon-html/register', // deactivated because of IE11 issue
       '@storybook/addon-a11y'
     ],
 };
