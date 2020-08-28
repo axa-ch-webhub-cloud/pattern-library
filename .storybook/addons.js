cfa217e6151c25a4a8463c7c94b184c59f98a20f
Noah Liechti <noah.liechti@gmail.com>
2020-08-28 10:37:16 +0200

    merge conflict

---
+++ .storybook/addons.js
@@ -0,0 +1,18 @@
+import 'storybook-readme/register';
+import './addons/changelog/register.js';
+import '@storybook/addon-knobs/register';
+import './addons/codepreview/register.js';
+import '@storybook/addon-a11y/register';
+import { STORIES_CONFIGURED, STORY_MISSING } from '@storybook/core-events';
+import addonAPI from '@storybook/addons';
+
+addonAPI.register('axa-ch/defaultpage', storybookAPI => {
+  storybookAPI.on(STORIES_CONFIGURED, (kind, story) => {
+    if (storybookAPI.getUrlState().path === '/story/*') {
+      storybookAPI.selectStory('Overview|Welcome', 'to Pattern Library')
+    }
+  });
+  storybookAPI.on(STORY_MISSING, (kind, story) => {
+    storybookAPI.selectStory('Welcome', 'to Pattern Library')
+  })
+});
