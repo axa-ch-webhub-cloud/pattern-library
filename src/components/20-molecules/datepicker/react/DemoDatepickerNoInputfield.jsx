86b2f9744e67ef92740b98cdcce20a5e6f629e8e
Noah Liechti <noah.liechti@gmail.com>
2020-09-04 10:45:53 +0200

    replace console.log with axa-text

--- src/components/20-molecules/datepicker/react/DemoDatepickerNoInputfield.jsx
+++ src/components/20-molecules/datepicker/react/DemoDatepickerNoInputfield.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AXADatepickerReact from './AXADatepickerReact';
+import AXATextReact from './AXATextReact';
 
 const DemoDatepickerNoInputfield = () => {
   const [showDatepicker, setShowDatepicker] = useState(true);
@@ -10,15 +11,22 @@ const DemoDatepickerNoInputfield = () => {
       </button>
       <hr />
       {showDatepicker && (
-        <AXADatepickerReact
-          allowedyears={[2020, 2021, 2022]}
-          locale="de-CH"
-          year={2020}
-          month={1}
-          day={1}
-          // eslint-disable-next-line no-console
-          onDateChange={date => console.log(`date changed ${date}`)}
-        />
+        <>
+          <AXADatepickerReact
+            allowedyears={[2020, 2021, 2022]}
+            locale="de-CH"
+            year={2020}
+            month={1}
+            day={1}
+            // eslint-disable-next-line no-console
+            onDateChange={date =>
+              (document.getElementById(
+                'checkbox-output'
+              ).innerHTML = `Date changed to: ${date}`)
+            }
+          />
+          <AXATextReact id="checkbox-output">Date changed to:</AXATextReact>
+        </>
       )}
     </div>
   );
