035f3ce04976706ace933a769caf2ed5864e39df
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-09-10 15:33:12 +0200

    Added all the unit tests

--- src/components/10-atoms/input-text/unit.test.js
+++ src/components/10-atoms/input-text/unit.test.js
@@ -2,42 +2,66 @@ import AXAInputText from './index';
 
 describe('InputText', () => {
   describe('_formatCurrency', () => {
-    it('should create new NumberFormat object', () => {
+    beforeEach(() => {
       AXAInputText.prototype.currency = 'chf';
       AXAInputText.prototype.type = 'text';
       AXAInputText.prototype.currencyFormatter = null;
-      AXAInputText.prototype._formatCurrency('1');
+      AXAInputText.prototype.invalid = null;
+    });
 
+    it('should create new NumberFormat object', () => {
+      AXAInputText.prototype._formatCurrency('1');
       expect(AXAInputText.prototype.currencyFormatter).not.toBe(null);
     });
 
     it('should set invalid to false (if input contains a number)', () => {
-      expect().toEqual();
+      AXAInputText.prototype._formatCurrency('1');
+      expect(AXAInputText.prototype.invalid).toBe(false);
     });
     it('should set invalid to false (if input is empty)', () => {
-      expect().toEqual();
+      AXAInputText.prototype._formatCurrency('');
+      expect(AXAInputText.prototype.invalid).toBe(false);
     });
     it('should set invalid to true (if input contains just no-digits and is not empty)', () => {
-      expect().toEqual();
+      AXAInputText.prototype._formatCurrency('nodigits');
+      expect(AXAInputText.prototype.invalid).toBe(true);
     });
 
     it('should return formatted number', () => {
-      expect().toEqual();
+      const returnValue = AXAInputText.prototype._formatCurrency('1000');
+      expect(returnValue).toBe('CHF 1,000.00'); // be aware: there is no simple empty-space between CHF and 1! Its a HTML entity NBSP.
     });
 
     it('should return inputted value', () => {
-      expect().toEqual();
+      const inputValue = 'this is my input';
+      AXAInputText.prototype.currency = null;
+
+      expect(AXAInputText.prototype._formatCurrency(inputValue)).toBe(
+        inputValue
+      );
     });
 
-    // negative positive tests
+    // false positive tests
     it('should not create a NumberFormat object (wrong type)', () => {
-      expect().toEqual();
+      AXAInputText.prototype.type = 'password';
+      AXAInputText.prototype._formatCurrency('1');
+
+      expect(AXAInputText.prototype.currencyFormatter).toBe(null);
     });
-    it('should not create a NumberFormat object (already exists)', () => {
-      expect().toEqual();
+    it('should not create a new NumberFormat object (already exists)', () => {
+      const alreadyExistingNumberFormatObject = { format: () => {} };
+      AXAInputText.prototype.currencyFormatter = alreadyExistingNumberFormatObject;
+      AXAInputText.prototype._formatCurrency('1');
+
+      expect(AXAInputText.prototype.currencyFormatter).toBe(
+        alreadyExistingNumberFormatObject
+      );
     });
     it('should not create a NumberFormat object (no currency set)', () => {
-      expect().toEqual();
+      AXAInputText.prototype.currency = '';
+      AXAInputText.prototype._formatCurrency('1');
+
+      expect(AXAInputText.prototype.currencyFormatter).toBe(null);
     });
   });
 });
