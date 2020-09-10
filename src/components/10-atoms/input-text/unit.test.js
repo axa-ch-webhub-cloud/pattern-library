f2b68eb56ce19f10b0cec3b3211fa9de0b405d34
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2020-09-10 11:42:28 +0200

    Adding unit test skeleton

---
+++ src/components/10-atoms/input-text/unit.test.js
@@ -0,0 +1,27 @@
+import AXAInputText, { _formatCurrency } from './index';
+
+describe('InputText', () => {
+  describe('_formatCurrency', () => {
+    it('should create new NumberFormat object', () => {
+      expect().toEqual();
+    });
+
+    it('should set invalid to false (if input contains a number)', () => {
+      expect().toEqual();
+    });
+    it('should set invalid to false (if input is empty)', () => {
+      expect().toEqual();
+    });
+    it('should set invalid to true (if input contains just no-digits and is not empty)', () => {
+      expect().toEqual();
+    });
+
+    it('should return formatted number', () => {
+      expect().toEqual();
+    });
+
+    it('should return inputted value', () => {
+      expect().toEqual();
+    });
+  });
+});
