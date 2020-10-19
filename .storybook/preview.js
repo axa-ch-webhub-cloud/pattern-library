63c8f03612de666a83e9f4293f8115ad99eec4f8
Noah Liechti <noah.liechti@gmail.com>
2020-10-19 16:55:05 +0200

    apply comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,35 +4,35 @@ import { withHTML } from '@whitespace/storybook-addon-html/html';
 
 const customViewports = {
   xs: {
-    name: 'xs',
+    name: 'Extra Small (xs)',
     styles: {
       width: '576px',
       height: '100%',
     },
   },
   sm: {
-    name: 'sm',
+    name: 'Small (sm)',
     styles: {
       width: '768px',
       height: '100%',
     },
   },
   md: {
-    name: 'md',
+    name: 'Medium (md)',
     styles: {
       width: '992px',
       height: '100%',
     },
   },
   xl: {
-    name: 'xl',
+    name: 'Extra Large (xl)',
     styles: {
       width: '1200px',
       height: '100%',
     },
   },
   xxl: {
-    name: 'xxl',
+    name: 'Extra Extra Large (xxl)',
     styles: {
       width: '1440px',
       height: '100%',
