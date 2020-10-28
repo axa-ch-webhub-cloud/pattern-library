38bc8bd9d0625d7d414e2de22a384dc5c17f521a
Raphael <raphael.suter@axa.ch>
2020-10-28 07:59:00 +0100

    Improve story

--- src/components/20-molecules/stepper/story.js
+++ src/components/20-molecules/stepper/story.js
@@ -28,8 +28,8 @@ export const Stepper = () => {
   const template = html`
     <axa-stepper
       .steps=${steps}
-      stepactive=${stepActive || 0}
-      stepprogress=${stepProgress || 0}
+      .stepactive=${stepActive || 0}
+      .stepprogress=${stepProgress || 0}
     ></axa-stepper>
   `;
 
