3782189bf33b0272fcca96e213ab8637392cf6b8
Raphael <raphael.suter@axa.ch>
2020-11-23 08:44:30 +0100

    Remove highlighting to check ie

--- .storybook/addons/readme/register.js
+++ .storybook/addons/readme/register.js
@@ -1,25 +1,11 @@
 import { addons, types } from '@storybook/addons';
 import { useParameter } from '@storybook/api';
 import { AddonPanel } from '@storybook/components';
-import 'github-markdown-css/github-markdown.css';
-import hljs from 'highlight.js/lib/core';
-import bash from 'highlight.js/lib/languages/bash';
-import javascript from 'highlight.js/lib/languages/javascript';
-import html from 'highlight.js/lib/languages/xml';
-import 'highlight.js/styles/github-gist.css';
 import marked from 'marked';
 import React from 'react';
 
 const ADDON_ID = 'axa-ch/readme';
 const PANEL_ID = `${ADDON_ID}/panel`;
-
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('html', html);
-hljs.registerLanguage('bash', bash);
-
-marked.setOptions({
-  highlight: code => hljs.highlightAuto(code).value,
-});
 
 const MyPanel = () => {
   const value = useParameter('readme', null);
