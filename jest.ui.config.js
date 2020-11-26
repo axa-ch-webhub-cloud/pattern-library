a46c99e8894087f02cf80c491ea960c1c6e7a3b8
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-11-26 15:18:36 +0100

    shrink playwright jest test config

--- jest.ui.config.js
+++ jest.ui.config.js
@@ -1,10 +1,4 @@
-const path = require('path');
-
-const { gatherAllVersions } = require(path.resolve(__dirname, 'scripts', 'build', 'version_info.js'));
-const stringifiedVersionInfo = gatherAllVersions(path.resolve(__dirname));
-
 module.exports = {
-  globals: {'__VERSION_INFO__': JSON.parse(stringifiedVersionInfo)},
   testMatch: [
     '<rootDir>/src/**/pw.ui.test.{js,jsx,mjs}',
   ],
