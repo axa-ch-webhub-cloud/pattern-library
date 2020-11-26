722c081e0c2d704fa42607d95637f7b28ec5e17b
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-11-26 15:17:34 +0100

    shrink jest config

--- jest.ui.config.js
+++ jest.ui.config.js
@@ -5,33 +5,7 @@ const stringifiedVersionInfo = gatherAllVersions(path.resolve(__dirname));
 
 module.exports = {
   globals: {'__VERSION_INFO__': JSON.parse(stringifiedVersionInfo)},
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!**/dist/**',
-    '!**/lib/**',
-    '!**/demo/**',
-    '!**/00-materials/icons/**',
-    '!**/00-materials/images/**',
-    '!**/ui.test.js',
-    '!**/story.js',
-    '!**/index.react.js',
-  ],
-  coverageDirectory: '.coverage',
-  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.js'],
   testMatch: [
     '<rootDir>/src/**/pw.ui.test.{js,jsx,mjs}',
-  ],
-  transform: {
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
-    '^.+\\.(js|jsx)$': '<rootDir>config/jest/jestPreprocess.js',
-  },
-  transformIgnorePatterns: [
-    '/node_modules\\/(?![lit\\-element|lit\\-html])/',
-  ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
   ],
 };
