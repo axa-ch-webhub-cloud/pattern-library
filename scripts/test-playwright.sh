479c521feb6e86118f8c67112822bc92d267bb26
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-11-27 08:23:39 +0100

    make sure env variable reaches playwright run

--- scripts/test-playwright.sh
+++ scripts/test-playwright.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-npm run test-playwright --config="jest.ui.config.js"
+# npm run test-playwright --config="jest.ui.config.js"
+
+npx jest --config=jest.ui.config.js
 test_result=$?
 
 exit $test_result
