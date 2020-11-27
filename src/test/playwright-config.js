ce97be9112f3da96b98c40f7cd0d0fab0d1cc692
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-11-27 14:48:11 +0100

    checkin browser layer

---
+++ src/test/playwright-config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { chromium: browser } = require('playwright-chromium');
+
+const config = {};
+
+module.export = { browser, config };
