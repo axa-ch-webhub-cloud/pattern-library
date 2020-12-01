539e9b6bcd4723c9d575963365086c9899dea337
Raphael <raphael.suter@axa.ch>
2020-12-01 13:16:36 +0100

    Manipulate ui-store on load

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,6 +2,16 @@ import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 import logo from '../src/static/svg/logo-axa.svg';
 
+const UI_STORE = '@storybook/ui/store';
+let uiStore = { layout: { showPanel: false } };
+
+if (localStorage.getItem(UI_STORE)) {
+  uiStore = JSON.parse(localStorage.getItem(UI_STORE));
+  uiStore.layout.showPanel = false;
+}
+
+localStorage.setItem(UI_STORE, JSON.stringify(uiStore));
+
 addons.setConfig({
   theme: create({
     base: 'light',
