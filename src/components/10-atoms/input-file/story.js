54c9287da89cfff7fc37516ceb5735af6803f64a
Noah Liechti <noah.liechti@gmail.com>
2020-12-04 08:51:29 +0100

    merge conflict

--- src/components/10-atoms/input-file/story.js
+++ src/components/10-atoms/input-file/story.js
@@ -1,11 +1,4 @@
 /* global document */
-import {
-  boolean,
-  radios,
-  select,
-  text,
-  withKnobs,
-} from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import { iconList } from '../icon/icon-list';
 import changelog from './CHANGELOG.md';
@@ -14,41 +7,23 @@ import readme from './README.md';
 
 export default {
   title: 'Components/Input File',
-  decorators: [withKnobs],
-
   parameters: {
     readme,
     changelog,
   },
 };
 
-export const InputFile = () => {
-  const _text = text('text', 'Select a File');
-
-  const variant = radios(
-    'variant',
-    {
-      default: '',
-      secondary: 'secondary',
-      red: 'red',
-      inverted: 'inverted',
-    },
-    ''
-  );
-
-  const icon = select('icon', iconList, 'cloud-upload');
-  const large = boolean('large', false);
-  const motionOff = boolean('motionOff', false);
-  const disabled = boolean('disabled', false);
-
-  const accept = text(
-    'accept',
-    'image/jpg, image/jpeg, application/pdf, image/png'
-  );
-
-  const capture = boolean('capture', false);
-  const multiple = boolean('multiple', false);
-
+export const InputFile = ({
+  text,
+  variant,
+  icon,
+  large,
+  motionOff,
+  disabled,
+  accept,
+  capture,
+  multiple,
+}) => {
   const template = html`
     <div
       style="${variant.includes('inverted')
@@ -62,7 +37,7 @@ export const InputFile = () => {
         ?motionOff="${motionOff}"
         ?disabled="${disabled}"
         accept="${accept}"
-        text="${_text}"
+        text="${text}"
         ?capture="${capture}"
         ?multiple="${multiple}"
       ></axa-input-file>
@@ -73,4 +48,35 @@ export const InputFile = () => {
   render(template, wrapper);
 
   return wrapper;
+};
+InputFile.args = {
+  text: 'Select a File',
+  variant: '',
+  icon: 'cloud-upload',
+  large: false,
+  motionOff: false,
+  disabled: false,
+  accept: 'image/jpg, image/jpeg, application/pdf, image/png',
+  capture: false,
+  multiple: false,
+};
+
+InputFile.argTypes = {
+  variant: {
+    control: {
+      type: 'radio',
+      options: {
+        default: '',
+        secondary: 'secondary',
+        red: 'red',
+        inverted: 'inverted',
+      },
+    },
+  },
+  icon: {
+    control: {
+      type: 'select',
+      options: iconList,
+    },
+  },
 };
