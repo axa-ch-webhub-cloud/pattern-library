80d6239794443a77787a7066143cb9aed3a3ac94
Noah Liechti <noah.liechti@gmail.com>
2020-12-04 09:36:37 +0100

    merge conflict

--- src/components/10-atoms/button-link/demo.js
+++ src/components/10-atoms/button-link/demo.js
@@ -1,27 +1,29 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/Button Link/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/Button Link/Pure HTML',
+  parameters: {
     readme,
     changelog,
-  })
-  .add(
-    'Icon visible',
-    () => '<axa-button-link icon="arrow-right">Next step</axa-button-link>'
-  )
-  .add('Clickable', () => {
-    const btn = document.createElement('axa-button-link');
-    let counter = 0;
-    btn.innerHTML = `You clicked me: ${counter}`;
-    btn.addEventListener('click', e => {
-      e.preventDefault();
-      counter += 1;
-      btn.innerHTML = `You clicked me: ${counter}`;
-    });
+    controls: { disabled: true },
+  },
+};
 
-    return btn;
+export const IconVisible = () =>
+  '<axa-button-link icon="arrow-right">Next step</axa-button-link>';
+
+export const Clickable = () => {
+  const btn = document.createElement('axa-button-link');
+  let counter = 0;
+  btn.innerHTML = `You clicked me: ${counter}`;
+  btn.addEventListener('click', e => {
+    e.preventDefault();
+    counter += 1;
+    btn.innerHTML = `You clicked me: ${counter}`;
   });
+
+  return btn;
+};
