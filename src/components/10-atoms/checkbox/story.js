f7490b9cb54b60961cc051a610e9941d670717fc
Noah Liechti <noah.liechti@gmail.com>
2020-12-04 08:49:09 +0100

    merge conflict

--- src/components/10-atoms/checkbox/story.js
+++ src/components/10-atoms/checkbox/story.js
@@ -1,4 +1,3 @@
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import createRefId from '../../../utils/create-ref-id';
 import changelog from './CHANGELOG.md';
@@ -7,29 +6,23 @@ import readme from './README.md';
 
 export default {
   title: 'Components/Checkbox',
-  decorators: [withKnobs],
   parameters: {
     readme,
     changelog,
   },
 };
 
-export const Checkbox = () => {
-  const refId = text('refId', `checkbox-${createRefId()}`);
-  const label = text('label', 'I agree to conditions of data protection.');
-  const name = text('name', 'my-checkbox');
-  const variant = select('variant', [
-    'square',
-    'checkmark',
-    // 'inverted-square', // not officially supported yet
-    'checkmark-inverted',
-  ]);
-  const checked = boolean('checked', true);
-  const disabled = boolean('disabled', false);
-  const errortext = boolean('error', false);
-  const required = boolean('required', false);
-  const styled = boolean('styled', false);
-
+export const Checkbox = ({
+  checked,
+  disabled,
+  errortext,
+  required,
+  styled,
+  refId,
+  label,
+  name,
+  variant,
+}) => {
   const wrapper = document.createElement('div');
   const template = html`
     <div
@@ -60,6 +53,31 @@ Checkbox.story = {
   parameters: {
     knobs: {
       escapeHTML: false,
+    },
+  },
+};
+Checkbox.args = {
+  checked: true,
+  disabled: false,
+  errortext: false,
+  required: false,
+  styled: false,
+  refId: `checkbox-${createRefId()}`,
+  label: 'I agree to conditions of data protection.',
+  name: 'my-checkbox',
+  variant: 'square',
+};
+
+Checkbox.argTypes = {
+  variant: {
+    control: {
+      type: 'radio',
+      options: [
+        'square',
+        'checkmark',
+        // 'inverted-square', // not officially supported yet
+        'checkmark-inverted',
+      ],
     },
   },
 };
