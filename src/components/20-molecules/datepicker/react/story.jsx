781eb3395533748f49dc2d1e39d4dc35ab5bad5f
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 21:49:04 +0100

    merge conflict

--- src/components/20-molecules/datepicker/react/story.jsx
+++ src/components/20-molecules/datepicker/react/story.jsx
@@ -1,92 +1,106 @@
 /* global document */
-import {
-  boolean,
-  number,
-  object,
-  select,
-  text,
-  withKnobs,
-} from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import changelog from '../CHANGELOG.md';
 import readme from '../README.md';
 import AXADatepickerReact from './AXADatepickerReact';
 
-const localeOptions = {
-  'de-CH': 'de-CH',
-  'en-GB': 'en-GB',
-  'it-IT': 'it-IT',
-  'fr-CH': 'fr-CH',
-};
-
-storiesOf('Examples/Datepicker/React', module)
-  .addDecorator(withKnobs)
-  .addParameters({
+export default {
+  title: 'Examples/Datepicker/React',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Story', () => {
-    const inputfield = boolean('inputfield', false);
-    const locale = select('locale', localeOptions, 'de-CH');
-    const year = number('year', '');
-    const month = number('month', '');
-    const day = number('day', '');
-    const disabled = boolean('disabled', false);
-    const autofocus = boolean('autofocus', false);
-    const checkMark = boolean('checkMark', false);
-    const label = text('label', '');
-    const monthtitle = text('monthtitle', 'Choose Month');
-    const yeartitle = text('yeartitle', 'Choose Year');
-    const invaliddatetext = text('invaliddatetext', 'Invalid date');
-    const invalid = boolean('invalid', false);
-    const placeholder = text('placeholder', 'Please select a date');
-    const defaultValue = text('defaultValue', '');
-    const inlineWidth = text('width (not an attribute)', '');
-    const allowedYears = object('allowedyears', [
-      '1971-2000',
-      2012,
-      2014,
-      '2018-2022',
-    ]);
+    controls: { disabled: false },
+    options: { showPanel: true },
+  },
+};
 
-    const div = document.createElement('div');
-    const style = {
-      width: inlineWidth,
-    };
+const localeOptions = {
+  'de-CH': 'de-CH',
+  'it-CH': 'it-CH',
+  'fr-CH': 'fr-CH',
+  'en-CH': 'en-CH',
+  'en-GB': 'en-GB',
+  'it-IT': 'it-IT',
+  'invalid/ unsupported': 'ff-XX', // To show the default language fallback scenario
+};
 
-    ReactDOM.render(
-      <AXADatepickerReact
-        locale={locale}
-        autofocus={autofocus}
-        inputfield={inputfield}
-        checkMark={checkMark}
-        disabled={disabled}
-        allowedyears={allowedYears}
-        year={year}
-        month={month}
-        style={style}
-        day={day}
-        placeholder={placeholder}
-        defaultValue={defaultValue}
-        invaliddatetext={invaliddatetext}
-        invalid={invalid}
-        label={label}
-        monthtitle={monthtitle}
-        yeartitle={yeartitle}
-        id="datepicker-react"
-        data-test-id="datepicker-react"
-        data-selenium-id="0815"
-        data-display-name={AXADatepickerReact.displayName}
-        className="my-special-class"
-        onDateChange={newDate => {
-          document.querySelector(
-            'axa-datepicker[data-test-id="datepicker-react"]'
-          ).title = newDate;
-        }}
-      />,
-      div
-    );
-    return div;
-  });
+export const Story = ({
+  inputfield,
+  locale,
+  year,
+  month,
+  day,
+  allowedYears,
+  disabled,
+  autofocus,
+  checkMark,
+  defaultValue,
+  label,
+  monthtitle,
+  yeartitle,
+  invaliddatetext,
+  invalid,
+  placeholder,
+  width,
+}) => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <AXADatepickerReact
+      locale={locale}
+      autofocus={autofocus}
+      inputfield={inputfield}
+      checkMark={checkMark}
+      disabled={disabled}
+      allowedyears={allowedYears}
+      year={year}
+      month={month}
+      day={day}
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      invaliddatetext={invaliddatetext}
+      invalid={invalid}
+      label={label}
+      monthtitle={monthtitle}
+      yeartitle={yeartitle}
+      id="datepicker-react"
+      data-test-id="datepicker-react"
+      data-selenium-id="0815"
+      data-display-name={AXADatepickerReact.displayName}
+      className="my-special-class"
+      onDateChange={newDate => {
+        document.querySelector(
+          'axa-datepicker[data-test-id="datepicker-react"]'
+        ).title = newDate;
+      }}
+      width={width}
+    />,
+    div
+  );
+  return div;
+};
+
+Story.args = {
+  inputfield: false,
+  locale: 'de-CH',
+  year: 2020,
+  month: 4,
+  defaultValue: '',
+  day: 20,
+  allowedYears: ['1971-2000', 2012, 2014, '2018-2022'],
+  disabled: false,
+  autofocus: false,
+  checkMark: false,
+  label: '',
+  monthtitle: 'Choose month',
+  yeartitle: 'Choose year',
+  invaliddatetext: 'Invalid date',
+  invalid: false,
+  placeholder: 'Please select a date',
+  width: '',
+};
+
+Story.argTypes = {
+  locale: { control: { type: 'select', options: localeOptions } },
+};
