24cf170307df6f74171d066952df2185b18dfb80
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 21:21:06 +0100

    merge conflict

--- src/components/10-atoms/link/react/demo.jsx
+++ src/components/10-atoms/link/react/demo.jsx
@@ -1,18 +1,20 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import changelog from '../CHANGELOG.md';
 import readme from '../README.md';
 import DemoLink from './DemoLink';
 
-storiesOf('Examples/Link/React', module)
-  .addParameters({
+export default {
+  title: 'Examples/Link/React',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Variable icons', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<DemoLink />, div);
-    return div;
-  });
+  },
+};
+
+export const Clickable = () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DemoLink />, div);
+  return div;
+};
