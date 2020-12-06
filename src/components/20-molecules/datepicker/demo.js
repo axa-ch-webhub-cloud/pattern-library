3ae2b2ed1d0b935459ee878a4082eac07d483d64
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 21:53:10 +0100

    merge conflict

--- src/components/20-molecules/datepicker/demo.js
+++ src/components/20-molecules/datepicker/demo.js
@@ -1,124 +1,130 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/Datepicker/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/Datepicker/Pure HTML',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('In a form', () => {
-    const handleSubmit = event => {
-      event.preventDefault();
-      document.getElementById('form-data').open = true;
-      const formData = new window.FormData(event.target);
-      let n = 0;
-      for (const entries = formData.entries(); !entries.next().done; n++);
-      document.getElementById('form-data-date').textContent = `${formData.get(
-        'date'
-      )} (of ${n}  submittable elements)`;
-    };
+    controls: { disabled: true },
+  },
+};
 
-    const template = html`
-      <form id="datepicker-form" @submit="${handleSubmit}">
-        <fieldset>
-          <legend>Date</legend>
-          <axa-datepicker
-            data-test-id="datepicker-forms"
-            name="date"
-            inputfield
-            required
-            label="Insurance date"
-            placeholder="Bitte Datum wählen"
-            year="2020"
-            month="1"
-            day="2"
-          ></axa-datepicker>
-          <axa-button type="submit" id="datepicker-forms-submit">OK</axa-button>
-          <details
-            id="form-data"
-            style="display: block; margin-top: 2rem; width: 300px"
+export const InAForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    document.getElementById('form-data').open = true;
+    const formData = new window.FormData(event.target);
+    let n = 0;
+    for (const entries = formData.entries(); !entries.next().done; n++);
+    document.getElementById('form-data-date').textContent = `${formData.get(
+      'date'
+    )} (of ${n}  submittable elements)`;
+  };
+
+  const template = html`
+    <form id="datepicker-form" @submit="${handleSubmit}">
+      <fieldset width="589">
+        <legend>Date</legend>
+        <axa-datepicker
+          data-test-id="datepicker-forms"
+          name="date"
+          inputfield
+          required
+          label="Insurance date"
+          placeholder="Bitte Datum wählen"
+          year="2020"
+          month="1"
+          day="2"
+          width="300px"
+        ></axa-datepicker>
+        <axa-button type="submit" id="datepicker-forms-submit">OK</axa-button>
+        <details
+          id="form-data"
+          style="display: block; margin-top: 2rem; width: 300px"
+        >
+          <summary
+            style="background:rgb(0,0,143);color:#fff;padding:11px;font-family:sans-serif;outline:none"
+            >form content</summary
           >
-            <summary
-              style="background:rgb(0,0,143);color:#fff;padding:11px;font-family:sans-serif;outline:none"
-              >form content</summary
-            >
-            <div
-              id="datepicker-forms-content"
-              style="display:table; padding: 5px 0"
-            >
-              date = <span id="form-data-date"></span>
-            </div>
-          </details>
-          <label>
-            <input
-              type="checkbox"
-              @change="${({ target }) => {
-                document.querySelector(
-                  'axa-datepicker[data-test-id="datepicker-forms"]'
-                ).disabled = target.checked;
-              }}"
-            />
-            disable datepicker
-          </label>
-        </fieldset>
-      </form>
-    `;
+          <div
+            id="datepicker-forms-content"
+            style="display:table; padding: 5px 0"
+          >
+            date = <span id="form-data-date"></span>
+          </div>
+        </details>
+        <label>
+          <input
+            type="checkbox"
+            @change="${({ target }) => {
+              document.querySelector(
+                'axa-datepicker[data-test-id="datepicker-forms"]'
+              ).disabled = target.checked;
+            }}"
+          />
+          disable datepicker
+        </label>
+      </fieldset>
+    </form>
+  `;
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('With onchange handler', () => {
-    const template = html`
-      <axa-datepicker
-        inputfield
-        data-test-id="datepicker-onchange"
-        name="date"
-        required
-        label="Choose a date"
-        allowedyears='["1971-2020"]'
-        year="2020"
-        month="1"
-        day="29"
-        @change="${({ detail: { name, value } }) => {
-          document.querySelector(
-            '.event-log'
-          ).value += `{"name":"${name}","value":"${value}"}\n\n`;
-        }}"
-      ></axa-datepicker>
-      <textarea
-        class="event-log"
-        placeholder="event log..."
-        rows="31"
-        cols="30"
-      ></textarea>
-    `;
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('Set width with inline style', () => {
-    const template = html`
-      <axa-datepicker
-        style="width: 200px"
-        inputfield
-        data-test-id="datepicker-onchange"
-        name="date"
-        required
-        label="Choose a date"
-        allowedyears='["1971-2020"]'
-        year="2020"
-        month="1"
-        day="29"
-      ></axa-datepicker>
-    `;
+export const WithOnchangeHandler = () => {
+  const template = html`
+    <axa-datepicker
+      inputfield
+      data-test-id="datepicker-onchange"
+      name="date"
+      required
+      label="Choose a date"
+      allowedyears='["1971-2020"]'
+      year="2020"
+      month="1"
+      day="29"
+      @change="${({ detail: { name, value } }) => {
+        document.querySelector(
+          '.event-log'
+        ).value += `{"name":"${name}","value":"${value}"}\n\n`;
+      }}"
+    ></axa-datepicker>
+    <textarea
+      class="event-log"
+      placeholder="event log..."
+      rows="31"
+      cols="30"
+    ></textarea>
+  `;
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  });
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
+
+export const SetWidthWithInlineStyle = () => {
+  const template = html`
+    <axa-datepicker
+      style="width: 200px"
+      inputfield
+      data-test-id="datepicker-onchange"
+      name="date"
+      required
+      label="Choose a date"
+      allowedyears='["1971-2020"]'
+      year="2020"
+      month="1"
+      day="29"
+    ></axa-datepicker>
+  `;
+
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
