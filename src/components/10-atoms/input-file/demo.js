1c9cad885cb24457229ed06aec58eb9fe02544b8
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 13:00:40 +0100

    merge conflict

--- src/components/10-atoms/input-file/demo.js
+++ src/components/10-atoms/input-file/demo.js
@@ -1,28 +1,31 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/Input File/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/Input File/Pure HTML',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Clickable', () => {
-    const inputFile = document.createElement('axa-input-file');
-    inputFile.setAttribute('multiple', 'multiple');
+    controls: { disabled: true },
+  },
+};
 
-    let counter = 0;
-    inputFile.text = `You have chosen ${counter} files`;
+export const Clickable = () => {
+  const inputFile = document.createElement('axa-input-file');
+  inputFile.setAttribute('multiple', 'multiple');
 
-    inputFile.addEventListener('change', e => {
-      counter = e.target.files.length;
+  let counter = 0;
+  inputFile.text = `You have chosen ${counter} files`;
 
-      inputFile.querySelector('.a-input-file').innerHTML = `
-        <span class="a-input-file__flex-wrapper">You have chosen ${counter} files</span>
-      `;
-    });
+  inputFile.addEventListener('change', e => {
+    counter = e.target.files.length;
 
-    return inputFile;
+    inputFile.querySelector('.a-input-file').innerHTML = `
+      <span class="a-input-file__flex-wrapper">You have chosen ${counter} files</span>
+    `;
   });
+
+  return inputFile;
+};
