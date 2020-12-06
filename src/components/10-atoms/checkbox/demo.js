5209a2f351f028a80afc3edb89b814b76230851f
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 12:43:33 +0100

    merge conflict

--- src/components/10-atoms/checkbox/demo.js
+++ src/components/10-atoms/checkbox/demo.js
@@ -1,64 +1,68 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/Checkbox/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/Checkbox/Pure HTML',
+  parameters: {
     readme,
     changelog,
     controls: { disabled: true },
-  })
-  .add('Use your own label as a child of the component', () => {
-    const template = html`
-      <axa-checkbox
-        class="hover"
-        name="checkbox"
-        .checked="${false}"
-        required="true"
-        ><p>
-          <span class="non-link-label-text">I agree to</span>
-          <a href="https://www.google.ch" target="_blank"
-            >conditions of data protection.</a
-          >
-        </p>
-      </axa-checkbox>
-    `;
+  },
+};
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('Use a styled HTML label', () => {
-    const template = html`
-      <axa-checkbox
-        class="hover"
-        name="checkbox"
-        .checked="${false}"
-        required="true"
-        variant="checkmark"
-        label="<p><span class='non-link-label-text'>This is an HTML label with a </span><a href='https://www.google.ch' target='_blank'>link.</a></p>"
-        styled
-      >
-      </axa-checkbox>
-    `;
+export const UseYourOwnLabelAsAChildOfTheComponent = () => {
+  const template = html`
+    <axa-checkbox
+      class="hover"
+      name="checkbox"
+      .checked="${false}"
+      required="true"
+      ><p>
+        <span class="non-link-label-text">I agree to</span>
+        <a href="https://www.google.ch" target="_blank"
+          >conditions of data protection.</a
+        >
+      </p>
+    </axa-checkbox>
+  `;
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('Without a label', () => {
-    const template = html`
-      <axa-checkbox
-        name="checkbox"
-        error="This checkbox do not have a label, but its also clickable"
-      >
-      </axa-checkbox>
-    `;
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  });
+export const UseAStyledHtmlLabel = () => {
+  const template = html`
+    <axa-checkbox
+      class="hover"
+      name="checkbox"
+      .checked="${false}"
+      required="true"
+      variant="checkmark"
+      label="<p><span class='non-link-label-text'>This is an HTML label with a </span><a href='https://www.google.ch' target='_blank'>link.</a></p>"
+      styled
+    >
+    </axa-checkbox>
+  `;
+
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
+
+export const WithoutALabel = () => {
+  const template = html`
+    <axa-checkbox
+      name="checkbox"
+      error="This checkbox do not have a label, but its also clickable"
+    >
+    </axa-checkbox>
+  `;
+
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
