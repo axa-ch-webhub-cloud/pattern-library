a17f532108dec1965cc1523a5b7f70da69dc169d
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 13:13:20 +0100

    merge conflict

--- src/components/10-atoms/input-text/react/demo.jsx
+++ src/components/10-atoms/input-text/react/demo.jsx
@@ -1,25 +1,60 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import changelog from '../CHANGELOG.md';
 import readme from '../README.md';
 import DemoInputText from './DemoInputText';
 import DemoInputTextonKeyUp from './DemoInputTextonKeyUp';
+import AXAInputText from './AXAInputText';
 
-storiesOf('Examples/Input Text/React', module)
-  .addParameters({
+export default {
+  title: 'Examples/Input Text/React',
+  parameters: {
     readme,
     changelog,
-  })
-  /* Default */
-  .add('Controlled/uncontrolled', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<DemoInputText />, div);
-    return div;
-  })
-  .add('Using onKeyUp event', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<DemoInputTextonKeyUp />, div);
-    return div;
-  });
+  },
+};
+
+export const ControlledAndUncontrolled = () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DemoInputText />, div);
+  return div;
+};
+
+export const SimulateAutocomplete = () => {
+  const wrapper = document.createElement('div');
+  // there are two input fields because with only one is not possible to
+  // activate the Safari's autocomplete feature.
+  ReactDOM.render(
+    <form>
+      <AXAInputText
+        refid="fix-id-fake"
+        label="Type in your name, autocolplete feature should be activated"
+        name="Name"
+      />
+      <AXAInputText refid="fix-id-86452623" name="Email" />
+    </form>,
+    wrapper
+  );
+
+  // simulate autocomplete
+  setTimeout(() => {
+    const input = document.querySelector('#fix-id-86452623');
+    input.value = '123456789';
+    if ('createEvent' in document) {
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent('input', false, true);
+      input.dispatchEvent(evt);
+    } else {
+      input.fireEvent('input');
+    }
+  }, 1);
+
+  return wrapper;
+};
+
+export const UsingOnkeyupEvent = () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DemoInputTextonKeyUp />, div);
+  return div;
+};
