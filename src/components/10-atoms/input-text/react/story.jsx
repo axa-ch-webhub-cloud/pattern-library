c7b8387ef24aa811eafdfd8ee9b8c2aaf93096d4
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 21:17:05 +0100

    merge conflict

--- src/components/10-atoms/input-text/react/story.jsx
+++ src/components/10-atoms/input-text/react/story.jsx
@@ -1,11 +1,19 @@
 /* global document */
-import { boolean, radios, text, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/html';
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import changelog from '../CHANGELOG.md';
 import readme from '../README.md';
 import AXAInputText from './AXAInputText';
+
+export default {
+  title: 'Examples/Input Text/React',
+  parameters: {
+    readme,
+    changelog,
+    controls: { disabled: false },
+    options: { showPanel: true },
+  },
+};
 
 const typeOptions = {
   text: 'text',
@@ -13,32 +21,28 @@ const typeOptions = {
   password: 'password',
 };
 
-const storyInputText = storiesOf('Examples/Input Text/React', module);
-storyInputText.addDecorator(withKnobs);
-storyInputText.addParameters({
-  readme,
-  changelog,
-});
-
-storyInputText.add('Story', () => {
-  const label = text('label*', '');
-  const name = text('name*', '');
-  const refId = text('refId', '');
-  const placeholder = text('placeholder', '');
-  const error = text('error', '');
-  const info = text('info', '');
-  const currency = text('currency', '');
-  const defaultValue = text('defaultValue', '');
-  const value = text('value', '');
-  const checkMark = boolean('checkMark', false);
-  const disabled = boolean('disabled', false);
-  const required = boolean('required', false);
-  const invalid = boolean('invalid', false);
-  const types = radios('type', typeOptions, 'text');
-  const pattern = text('pattern', '');
-  const inputmode = text('inputmode', '');
-  const autofocus = boolean('autofocus', false);
-
+export const Story = ({
+  label,
+  name,
+  refId,
+  placeholder,
+  value,
+  currency,
+  error,
+  info,
+  checkMark,
+  disabled,
+  required,
+  invalid,
+  defaultValue,
+  types,
+  maxLength,
+  counter,
+  counterMax,
+  pattern,
+  inputmode,
+  autofocus,
+}) => {
   class InputText extends PureComponent {
     constructor() {
       super();
@@ -64,6 +68,9 @@ storyInputText.add('Story', () => {
           type={types}
           error={error}
           info={info}
+          maxLength={maxLength}
+          counter={counter}
+          counterMax={counterMax}
           pattern={pattern}
           inputmode={inputmode}
           autofocus={autofocus}
@@ -78,60 +85,29 @@ storyInputText.add('Story', () => {
   ReactDOM.render(<InputText />, wrapper);
 
   return wrapper;
-});
+};
 
-storyInputText.add('Story - Simulate autocomplete', () => {
-  const label = text('label*', '');
-  const placeholder = text('placeholder', '');
-  const error = text('error', '');
-  const info = text('info', '');
-  const checkMark = boolean('checkmark', false);
-  const disabled = boolean('disabled', false);
-  const required = boolean('required', false);
-  const invalid = boolean('invalid', false);
-  const counter = text('counter', 'Still ##counter## characters left');
-  const counterMax = text('counterMax', 'Over character limit!');
-  const autofocus = boolean('autofocus', false);
+Story.args = {
+  label: '',
+  name: '',
+  refId: '',
+  placeholder: '',
+  value: '',
+  error: '',
+  info: '',
+  checkMark: false,
+  disabled: false,
+  required: false,
+  invalid: false,
+  types: 'text',
+  maxLength: 50,
+  counter: 'Still ##counter## characters left',
+  counterMax: 'Over character limit!',
+  pattern: '',
+  inputmode: '',
+  autofocus: false,
+};
 
-  const wrapper = document.createElement('div');
-  // there are two input fields because with only one is not possible to
-  // activate the Safari's autocomplete feature.
-  ReactDOM.render(
-    <form>
-      <AXAInputText refid="fix-id-fake" name="Name" />
-      <AXAInputText
-        refid="fix-id-86452623"
-        name="Adresse"
-        label={label}
-        placeholder={placeholder}
-        checkMark={checkMark}
-        disabled={disabled}
-        required={required}
-        invalid={invalid}
-        type="text"
-        counter={counter}
-        counterMax={counterMax}
-        maxlength="5"
-        error={error}
-        info={info}
-        autofocus={autofocus}
-      />
-    </form>,
-    wrapper
-  );
-
-  // simulate autocomplete
-  setTimeout(() => {
-    const input = document.querySelector('#fix-id-86452623');
-    input.value = '123456789';
-    if ('createEvent' in document) {
-      const evt = document.createEvent('HTMLEvents');
-      evt.initEvent('input', false, true);
-      input.dispatchEvent(evt);
-    } else {
-      input.fireEvent('input');
-    }
-  }, 1);
-
-  return wrapper;
-});
+Story.argTypes = {
+  types: { name: 'type', control: { type: 'radio', options: typeOptions } },
+};
