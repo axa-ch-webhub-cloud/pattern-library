aa0ae82c678d86b4ef61a1af1f61ebe7f171457c
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 12:50:25 +0100

    merge conflict

--- src/components/10-atoms/input-file/react/story.jsx
+++ src/components/10-atoms/input-file/react/story.jsx
@@ -1,12 +1,4 @@
 /* global document */
-import {
-  boolean,
-  radios,
-  select,
-  text,
-  withKnobs,
-} from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { iconList } from '../../icon/icon-list';
@@ -15,73 +7,94 @@ import readme from '../README.md';
 import AXAInputFileReact from './AXAInputFileReact';
 import AXATextReact from './AXATextReact';
 
-storiesOf('Examples/Input File/React', module)
-  .addDecorator(withKnobs)
-  .addParameters({
+export default {
+  title: 'Examples/Input File/React',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Story', () => {
-    const _text = text('text', 'Select a File');
+  },
+};
 
-    const variant = radios(
-      'variant',
-      {
+export const Story = ({
+  text,
+  variant,
+  icon,
+  large,
+  motionOff,
+  disabled,
+  accept,
+  capture,
+  multiple,
+}) => {
+  const div = document.createElement('div');
+
+  if (variant.includes('inverted')) {
+    div.style.backgroundColor = '#00008f';
+    div.style.padding = '10px';
+  }
+
+  ReactDOM.render(
+    <>
+      <AXAInputFileReact
+        text={text}
+        variant={variant}
+        onChange={e => {
+          const allFileNames = [];
+          [...e.target.files].forEach(i => allFileNames.push(i.name));
+
+          document.getElementById(
+            'checkbox-output'
+          ).innerHTML = `Files selected: ${allFileNames.toString()}`;
+        }}
+        className="myCssClass"
+        icon={icon}
+        large={large}
+        motionOff={motionOff}
+        multiple={multiple}
+        accept={accept}
+        disabled={disabled}
+        capture={capture}
+      />
+      <br />
+      <AXATextReact id="checkbox-output">File selected:</AXATextReact>
+    </>,
+    div
+  );
+
+  return div;
+};
+
+Story.args = {
+  text: 'Select a File',
+  variant: '',
+  icon: 'cloud-upload',
+  large: false,
+  motionOff: false,
+  disabled: false,
+  accept: 'image/jpg, image/jpeg, application/pdf, image/png',
+  capture: false,
+  multiple: false,
+};
+
+Story.argTypes = {
+  text: {
+    name: 'set input-file content',
+  },
+  variant: {
+    control: {
+      type: 'radio',
+      options: {
         default: '',
         secondary: 'secondary',
         red: 'red',
         inverted: 'inverted',
       },
-      ''
-    );
-
-    const icon = select('icon', iconList, 'cloud-upload');
-    const large = boolean('large', false);
-    const motionOff = boolean('motionOff', false);
-    const disabled = boolean('disabled', false);
-
-    const accept = text(
-      'accept',
-      'image/jpg, image/jpeg, application/pdf, image/png'
-    );
-
-    const capture = boolean('capture', false);
-    const multiple = boolean('multiple', false);
-
-    const div = document.createElement('div');
-
-    if (variant.includes('inverted')) {
-      div.style.backgroundColor = '#00008f';
-      div.style.padding = '10px';
-    }
-
-    ReactDOM.render(
-      <>
-        <AXAInputFileReact
-          text={_text}
-          variant={variant}
-          onChange={e => {
-            const allFileNames = [];
-            [...e.target.files].forEach(i => allFileNames.push(i.name));
-
-            document.getElementById(
-              'checkbox-output'
-            ).innerHTML = `Files selected: ${allFileNames.toString()}`;
-          }}
-          className="myCssClass"
-          icon={icon}
-          large={large}
-          motionOff={motionOff}
-          multiple={multiple}
-          accept={accept}
-          disabled={disabled}
-          capture={capture}
-        />
-        <br />
-        <AXATextReact id="checkbox-output">Files selected:</AXATextReact>
-      </>,
-      div
-    );
-
-    return div;
-  });
+    },
+  },
+  icon: {
+    control: {
+      type: 'select',
+      options: iconList,
+    },
+  },
+};
