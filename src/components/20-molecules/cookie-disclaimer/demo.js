0fbfdeffa967a42c930aea5a6c07d7315211cd90
Noah Liechti <noah.liechti@gmail.com>
2020-12-06 21:46:32 +0100

    merge conflict

--- src/components/20-molecules/cookie-disclaimer/demo.js
+++ src/components/20-molecules/cookie-disclaimer/demo.js
@@ -1,52 +1,54 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import withNoBorder from '../../../../.storybook/addons/no-border';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/Cookie Disclaimer/Pure HTML', module)
-  .addDecorator(story => withNoBorder(story))
-  .addDecorator(withNoBorder)
-  .addParameters({
+export default {
+  title: 'Examples/Cookie Disclaimer/Pure HTML',
+  decorators: [withNoBorder],
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Dynamic (Story is invisible after click on ok button)', () => {
-    const cookieDisclaimer = document.createElement('axa-cookie-disclaimer');
-    cookieDisclaimer.setAttribute('buttonname', 'Akzeptieren');
-    cookieDisclaimer.setAttribute('title', 'This Website uses cookies');
-    cookieDisclaimer.innerHTML = `
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-        At vero eos et accusam et justo duo dolores et ea rebum.
-        <axa-link variant="arrowright-animated-white" href="https://axa.ch/de/informationen/datenschutz.html">Data protection</axa-link>
-      </p>
-    `;
+    controls: { disabled: true },
+  },
+};
 
-    const div = document.createElement('div');
-    const divWrapper = document.createElement('div');
-    div.classList.add('js-only-for-demo-local-storage');
-    divWrapper.appendChild(div);
-    divWrapper.appendChild(cookieDisclaimer);
+export const Dynamic = () => {
+  const cookieDisclaimer = document.createElement('axa-cookie-disclaimer');
+  cookieDisclaimer.setAttribute('buttonname', 'Akzeptieren');
+  cookieDisclaimer.setAttribute('title', 'This Website uses cookies');
+  cookieDisclaimer.innerHTML = `
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      At vero eos et accusam et justo duo dolores et ea rebum.
+      <axa-link variant="arrowright-animated-white" href="https://axa.ch/de/informationen/datenschutz.html">Data protection</axa-link>
+    </p>
+  `;
 
-    setTimeout(() => {
-      const time = window.localStorage.getItem(
-        'axa-ch-cookie-disclaimer-accepted'
-      );
+  const div = document.createElement('div');
+  const divWrapper = document.createElement('div');
+  div.classList.add('js-only-for-demo-local-storage');
+  divWrapper.appendChild(div);
+  divWrapper.appendChild(cookieDisclaimer);
 
-      document.querySelector('.js-only-for-demo-local-storage').innerHTML = time
-        ? `Accepted the disclaimer at: <b>
-          ${new Date(+time).toLocaleString()}
-          </b>. Please delete local storage entry or all cache data from the browser in order to see it again
-          ->
-          <axa-button onclick="localStorage.removeItem('axa-ch-cookie-disclaimer-accepted'); location.href=location.href;">
-            DELETE HERE
-          </axa-button>
-          `
-        : '';
-    }, 300);
+  setTimeout(() => {
+    const time = window.localStorage.getItem(
+      'axa-ch-cookie-disclaimer-accepted'
+    );
 
-    return divWrapper;
-  });
+    document.querySelector('.js-only-for-demo-local-storage').innerHTML = time
+      ? `Accepted the disclaimer at: <b>
+        ${new Date(+time).toLocaleString()}
+        </b>. Please delete local storage entry or all cache data from the browser in order to see it again
+        ->
+        <axa-button onclick="localStorage.removeItem('axa-ch-cookie-disclaimer-accepted'); location.href=location.href;">
+          DELETE HERE
+        </axa-button>
+        `
+      : '';
+  }, 300);
+
+  return divWrapper;
+};
