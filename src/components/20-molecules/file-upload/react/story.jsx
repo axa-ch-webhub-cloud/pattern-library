2a6be345bc0ad34389a4e55047055ec3eb9ae58b
Noah Liechti <noah.liechti@gmail.com>
2020-12-07 08:39:34 +0100

    merge conflict

--- src/components/20-molecules/file-upload/react/story.jsx
+++ src/components/20-molecules/file-upload/react/story.jsx
@@ -1,12 +1,4 @@
 /* global document */
-import {
-  number,
-  select,
-  text,
-  boolean,
-  withKnobs,
-} from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/html';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { iconList } from '../../../10-atoms/icon/icon-list';
@@ -14,106 +6,106 @@ import changelog from '../CHANGELOG.md';
 import readme from '../README.md';
 import AXAFileUploadReact from './AXAFileUploadReact';
 
-storiesOf('Examples/File Upload/React', module)
-  .addDecorator(withKnobs)
-  .addParameters({
+export default {
+  title: 'Examples/File Upload/React',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('Story', () => {
-    const wrapperWidth = text('Width', '455px');
-    const sizeSliderOptions = {
-      range: true,
+  },
+};
+
+export const Story = ({
+  wrapperWidth,
+  inputFileText,
+  maxSizeOfSingleFileKB,
+  maxSizeOfAllFilesKB,
+  maxNumberOfFiles,
+  deleteStatusText,
+  addStatusText,
+  fileTooBigStatusText,
+  filesTooBigStatusText,
+  tooManyFilesStatusText,
+  orText,
+  infoText,
+  wrongFileTypeStatusText,
+  icon,
+  headerText,
+}) => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <div style={{ width: wrapperWidth }}>
+      <AXAFileUploadReact
+        inputFileText={inputFileText}
+        maxSizeOfSingleFileKB={maxSizeOfSingleFileKB}
+        maxSizeOfAllFilesKB={maxSizeOfAllFilesKB}
+        maxNumberOfFiles={maxNumberOfFiles}
+        deleteStatusText={deleteStatusText}
+        addStatusText={addStatusText}
+        fileTooBigStatusText={fileTooBigStatusText}
+        filesTooBigStatusText={filesTooBigStatusText}
+        tooManyFilesStatusText={tooManyFilesStatusText}
+        orText={orText}
+        infoText={infoText}
+        wrongFileTypeStatusText={wrongFileTypeStatusText}
+        icon={icon}
+      >
+        {headerText}
+      </AXAFileUploadReact>
+    </div>,
+    div
+  );
+  return div;
+};
+
+Story.args = {
+  wrapperWidth: '455px',
+  inputFileText: 'Upload file',
+  maxSizeOfSingleFileKB: 100,
+  maxSizeOfAllFilesKB: 500,
+  maxNumberOfFiles: 20,
+  deleteStatusText: 'Delete',
+  addStatusText: 'Add more',
+  fileTooBigStatusText: 'File size exceeds maximum size',
+  filesTooBigStatusText: 'File sizes exceed maximum size',
+  tooManyFilesStatusText: 'You exceeded the maximum number of files',
+  orText: 'or',
+  infoText: 'Drag and drop to upload your file',
+  wrongFileTypeStatusText:
+    'Your file does not correspond with our allowed file-types',
+  icon: 'cloud-upload',
+  headerText: 'The following files are going to be uploaded',
+};
+
+Story.argTypes = {
+  icon: {
+    control: {
+      type: 'select',
+      options: iconList,
+    },
+  },
+  maxSizeOfSingleFileKB: {
+    control: {
+      type: 'range',
       min: 1,
-      max: 500,
+      max: 5000,
       step: 10,
-    };
-    const numberOfFilesSliderOptions = {
-      range: true,
+    },
+  },
+  maxSizeOfAllFilesKB: {
+    control: {
+      type: 'range',
+      min: 1,
+      max: 30000,
+      step: 10,
+    },
+  },
+  maxNumberOfFiles: {
+    control: {
+      type: 'range',
       min: 1,
       max: 20,
       step: 1,
-    };
-    const inputFileText = text('inputFileText', 'Upload file');
-    const maxSizeOfSingleFileKB = number(
-      'maxSizeOfSingleFileKB',
-      100,
-      sizeSliderOptions
-    );
-    const maxSizeOfAllFilesKB = number(
-      'maxSizeOfAllFilesKB',
-      500,
-      sizeSliderOptions
-    );
-    const maxNumberOfFiles = number(
-      'maxNumberOfFiles',
-      10,
-      numberOfFilesSliderOptions
-    );
-    const deleteStatusText = text('deleteStatusText', 'Delete');
-    const addStatusText = text('addStatusText', 'Add more');
-    const fileTooBigStatusText = text(
-      'fileTooBigStatusText',
-      `File size exceeds maximum size`
-    );
-    const filesTooBigStatusText = text(
-      'filesTooBigStatusText',
-      `File sizes exceed maximum size`
-    );
-    const tooManyFilesStatusText = text(
-      'tooManyFilesStatusText',
-      `You exceeded the maximum number of files`
-    );
-    const orText = text('orText', 'or');
-    const infoText = text('infoText', 'Drag and drop to upload your file');
-    const wrongFileTypeStatusText = text(
-      'wrongFileTypeText',
-      'Your file does not correspond with our allowed file-types'
-    );
-    const icon = select('icon', iconList, 'cloud-upload');
-    const headerText = text(
-      'headerText',
-      'The following files are being transferred:'
-    );
-    const preventFileCompression = boolean('preventFileCompression', false);
-
-    const div = document.createElement('div');
-    ReactDOM.render(
-      <div style={{ width: wrapperWidth }}>
-        <AXAFileUploadReact
-          inputFileText={inputFileText}
-          maxSizeOfSingleFileKB={maxSizeOfSingleFileKB}
-          maxSizeOfAllFilesKB={maxSizeOfAllFilesKB}
-          maxNumberOfFiles={maxNumberOfFiles}
-          deleteStatusText={deleteStatusText}
-          addStatusText={addStatusText}
-          preventFileCompression={preventFileCompression}
-          fileTooBigStatusText={fileTooBigStatusText}
-          filesTooBigStatusText={filesTooBigStatusText}
-          tooManyFilesStatusText={tooManyFilesStatusText}
-          orText={orText}
-          infoText={infoText}
-          wrongFileTypeStatusText={wrongFileTypeStatusText}
-          icon={icon}
-          onFileDrop={() => {
-            document.querySelector(
-              '#m-fileupload-story__events'
-            ).textContent = `File dropped on ${new Date()}`;
-          }}
-          onFileRemove={() => {
-            document.querySelector(
-              '#m-fileupload-story__events'
-            ).textContent = `File removed on ${new Date()}`;
-          }}
-        >
-          {headerText}
-        </AXAFileUploadReact>
-        <div>
-          <p>Events:</p>
-          <p id="m-fileupload-story__events">-</p>
-        </div>
-      </div>,
-      div
-    );
-    return div;
-  });
+    },
+  },
+  headerText: { name: 'set header text' },
+};
