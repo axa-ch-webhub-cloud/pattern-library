79300f302901926caa3708ccb150f91ab71e9ad2
Noah Liechti <noah.liechti@gmail.com>
2020-12-07 08:36:21 +0100

    merge conflict

--- src/components/20-molecules/file-upload/demo.js
+++ src/components/20-molecules/file-upload/demo.js
@@ -1,52 +1,55 @@
 /* global document */
-import { storiesOf } from '@storybook/html';
 import { html, render } from 'lit-html';
 import changelog from './CHANGELOG.md';
 import './index';
 import readme from './README.md';
 
-storiesOf('Examples/File Upload/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/File Upload/Pure HTML',
+  parameters: {
     readme,
     changelog,
-  })
-  .add('In a form', () => {
-    const handleSubmit = e => {
-      e.preventDefault();
+    controls: { disabled: true },
+  },
+};
 
-      const imgUpload = document.querySelector('.js-file-upload__file-upload');
+export const InAForm = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
 
-      if (imgUpload.files.length > 0) {
-        const listWrapper = html`
-          <div class="js-file-upload__list-wrapper">
-            <h3>The following files were selected:</h3>
-            <ol>
-              ${imgUpload.files.map(file => {
-                return html`
-                  <li>${file.name}</li>
-                `;
-              })}
-            </ol>
-          </div>
-        `;
+    const imgUpload = document.querySelector('.js-file-upload__file-upload');
 
-        render(listWrapper, document.getElementById('listWrapper'));
-      }
-    };
+    if (imgUpload.files.length > 0) {
+      const listWrapper = html`
+        <div class="js-file-upload__list-wrapper">
+          <h3>The following files were selected:</h3>
+          <ol>
+            ${imgUpload.files.map(file => {
+              return html`
+                <li>${file.name}</li>
+              `;
+            })}
+          </ol>
+        </div>
+      `;
 
-    const template = html`
-      <div style="width:455px;">
-        <axa-file-upload class="js-file-upload__file-upload"
-          >These files are going to be uploaded</axa-file-upload
-        >
-      </div>
-      <form @click="${handleSubmit}" style="margin-top:40px;">
-        <axa-button type="submit">Submit</axa-button>
-      </form>
-      <div id="listWrapper"></div>
-    `;
+      render(listWrapper, document.getElementById('listWrapper'));
+    }
+  };
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  });
+  const template = html`
+    <div style="width:455px;">
+      <axa-file-upload class="js-file-upload__file-upload"
+        >These files are going to be uploaded</axa-file-upload
+      >
+    </div>
+    <form @click="${handleSubmit}" style="margin-top:40px;">
+      <axa-button type="submit">Submit</axa-button>
+    </form>
+    <div id="listWrapper"></div>
+  `;
+
+  const wrapper = document.createElement('div');
+  render(template, wrapper);
+  return wrapper;
+};
