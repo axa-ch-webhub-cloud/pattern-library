7fd1d0910aeaed3a5ce3df714e919760e5822f26
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-12-08 14:32:25 +0100

    fix typo

--- src/components/10-atoms/toggle-switch/pw.ui.test.js
+++ src/components/10-atoms/toggle-switch/pw.ui.test.js
@@ -25,7 +25,7 @@ it('should fire correct change event in controlled mode', async () => {
   await page.goto(
     `${host}/iframe.html?id=examples-toggle-switch-react--controlled-with-dummy-onchange-listener&viewMode=story`
   );
-  await page.waitForSelector('.a-toggle-switcha');
+  await page.waitForSelector('.a-toggle-switch');
 
   const eventInfo = await page.textContent(
     '.axa-toggle-switch-demo__event-info'
