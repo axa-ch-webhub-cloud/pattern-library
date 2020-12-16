553453d98f00f51a741c4ad385982bd434d14921
Raphael <raphael.suter@axa.ch>
2020-12-16 14:39:09 +0100

    Revert "Remove loose"
    This reverts commit c4bf10cff458cd93ba635bf61798c6c73051e8fa.

--- .storybook/.babelrc.js
+++ .storybook/.babelrc.js
@@ -10,5 +10,5 @@ module.exports = {
     ],
     '@babel/preset-react',
   ],
-  plugins: [['@babel/plugin-proposal-class-properties']],
+  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
 };
