10d56ebf87e97d9cb39c27f555f2205f608f9e07
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2021-01-07 08:16:46 +0100

    Group pw tests

--- src/components/10-atoms/button/pw.ui.test.js
+++ src/components/10-atoms/button/pw.ui.test.js
@@ -1,28 +1,30 @@
 const host = process.env.TEST_HOST_STORYBOOK_URL;
 
-it('should render button', async () => {
-  await page.goto(
-    `${host}/iframe.html?id=examples-button-pure-html--clickable`
-  );
-  await page.waitForSelector('.a-button__flex-wrapper');
-  const axaButtonText = await page.textContent('axa-button');
+describe('Button', () => {
+  it('should render button', async () => {
+    await page.goto(
+      `${host}/iframe.html?id=examples-button-pure-html--clickable`
+    );
+    await page.waitForSelector('.a-button__flex-wrapper');
+    const axaButtonText = await page.textContent('axa-button');
 
-  expect(axaButtonText).toBe('You clicked me: 0, btw my event name is click');
-});
+    expect(axaButtonText).toBe('You clicked me: 0, btw my event name is click');
+  });
 
-it('should submit before and after text child updates', async () => {
-  const axaButtonSelector = '[data-test-id="button-submit-text-change"]';
-  await page.goto(`${host}/iframe.html?id=examples-button-react--in-a-form`);
-  await page.waitForSelector(axaButtonSelector);
+  it('should submit before and after text child updates', async () => {
+    const axaButtonSelector = '[data-test-id="button-submit-text-change"]';
+    await page.goto(`${host}/iframe.html?id=examples-button-react--in-a-form`);
+    await page.waitForSelector(axaButtonSelector);
 
-  const axaButton = await page.$(axaButtonSelector);
-  const getAxaButtonText = () => page.textContent(axaButtonSelector);
+    const axaButton = await page.$(axaButtonSelector);
+    const getAxaButtonText = () => page.textContent(axaButtonSelector);
 
-  expect(await getAxaButtonText()).toContain('0');
+    expect(await getAxaButtonText()).toContain('0');
 
-  await axaButton.click();
-  expect(await getAxaButtonText()).toContain('1');
+    await axaButton.click();
+    expect(await getAxaButtonText()).toContain('1');
 
-  await axaButton.click();
-  expect(await getAxaButtonText()).toContain('2');
+    await axaButton.click();
+    expect(await getAxaButtonText()).toContain('2');
+  });
 });
--- src/components/20-molecules/datepicker/pw.ui.test.js
+++ src/components/20-molecules/datepicker/pw.ui.test.js
@@ -1,36 +1,38 @@
 const host = process.env.TEST_HOST_STORYBOOK_URL;
 
-it('should submit datepicker correctly in form', async () => {
-  const datepickerForm = 'axa-datepicker[data-test-id="datepicker-forms"]';
-  await page.goto(
-    `${host}/iframe.html?id=examples-datepicker-pure-html--in-a-form`
-  );
-  await page.waitForSelector(datepickerForm);
+describe('Datepicker', () => {
+  it('should submit datepicker correctly in form', async () => {
+    const datepickerForm = 'axa-datepicker[data-test-id="datepicker-forms"]';
+    await page.goto(
+      `${host}/iframe.html?id=examples-datepicker-pure-html--in-a-form`
+    );
+    await page.waitForSelector(datepickerForm);
 
-  await page.type(
-    `axa-datepicker[data-test-id="datepicker-forms"] .js-datepicker__input`,
-    '29.2.2020'
-  );
-  await page.click('#datepicker-forms-submit');
+    await page.type(
+      `axa-datepicker[data-test-id="datepicker-forms"] .js-datepicker__input`,
+      '29.2.2020'
+    );
+    await page.click('#datepicker-forms-submit');
 
-  expect(await page.textContent('#form-data-date')).toBe(
-    '29.2.2020 (of 1 submittable elements)'
-  );
-});
+    expect(await page.textContent('#form-data-date')).toBe(
+      '29.2.2020 (of 1 submittable elements)'
+    );
+  });
 
-it('should submit before and after text child updates', async () => {
-  const axaButtonSelector = '[data-test-id="button-submit-text-change"]';
-  await page.goto(`${host}/iframe.html?id=examples-button-react--in-a-form`);
-  await page.waitForSelector(axaButtonSelector);
+  it('should submit before and after text child updates', async () => {
+    const axaButtonSelector = '[data-test-id="button-submit-text-change"]';
+    await page.goto(`${host}/iframe.html?id=examples-button-react--in-a-form`);
+    await page.waitForSelector(axaButtonSelector);
 
-  const axaButton = await page.$(axaButtonSelector);
-  const getAxaButtonText = () => page.textContent(axaButtonSelector);
+    const axaButton = await page.$(axaButtonSelector);
+    const getAxaButtonText = () => page.textContent(axaButtonSelector);
 
-  expect(await getAxaButtonText()).toContain('0');
+    expect(await getAxaButtonText()).toContain('0');
 
-  await axaButton.click();
-  expect(await getAxaButtonText()).toContain('1');
+    await axaButton.click();
+    expect(await getAxaButtonText()).toContain('1');
 
-  await axaButton.click();
-  expect(await getAxaButtonText()).toContain('2');
+    await axaButton.click();
+    expect(await getAxaButtonText()).toContain('2');
+  });
 });
