b052cb8d28563a2c18ec44947fbccf65f47edfa7
Milchmaa <marc.welz03@gmail.com>
2021-02-05 13:59:21 +0100

    git feedback changes

--- src/components/30-organisms/modal/index.js
+++ src/components/30-organisms/modal/index.js
@@ -2,17 +2,7 @@ import { LitElement, html, css, unsafeCSS, svg } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 import closeIcon from '@axa-ch/materials/icons/close.svg';
 
-/* eslint-disable import/no-extraneous-dependencies */
-
-//    If you need other axa-XXX components inside your new component,
-//    import them here like this:
-//
-//    import myDependentComponent1 from '@axa-ch/XXX;
-
-import {
-  defineVersioned,
-  /* versionedHtml, */
-} from '../../../utils/component-versioning';
+import { defineVersioned } from '../../../utils/component-versioning';
 import { applyDefaults } from '../../../utils/with-react';
 import styles from './index.scss';
 
@@ -28,7 +18,6 @@ class AXAModal extends LitElement {
   }
 
   static get properties() {
-    // Define properties and types
     return {
       open: { type: Boolean },
     };
@@ -36,20 +25,9 @@ class AXAModal extends LitElement {
 
   constructor() {
     super();
-    // this functions applies default values per type and verifies if
-    // the HTML attribute has been set before defining the custom element
     applyDefaults(this);
-    // if you depend on *other* axa-XXX components and imported them above,
-    // then you declare them as versioned here like this:
-    /* eslint-disable no-undef */
-    // defineVersioned([myDependentComponent1, myDependentComponent2, ...], __VERSION_INFO__, this);
-    /* eslint-enable no-undef */
   }
 
-  // if you use dependent components inside your html-tagged string templates below,
-  // first uncomment versionedHTML above.
-  // Then, wrap them using a new tag versionedHTML(this), like so:
-  // versionedHTML(this)`<axa-XXX foo="bar"></axa-XXX>`
   render() {
     const classes = {
       'o-modal--open': this.open,
@@ -60,7 +38,7 @@ class AXAModal extends LitElement {
           <div class="o-modal-window__close-button" @click="${this.closeModal}">
             ${svg([closeIcon])}
           </div>
-          <slot></slot>
+           <slot></slot>
           <div>
         </div>
       </article>
@@ -80,12 +58,6 @@ class AXAModal extends LitElement {
   }
 }
 
-/* eslint-disable no-undef */
 defineVersioned([AXAModal], __VERSION_INFO__);
 
 export default AXAModal;
-
-// Cancel-Button programmieren => Klasse wird geändert
-{
-  /* <a href="#" class="o-modal-windows__close" /> */
-}
--- src/components/30-organisms/modal/story.js
+++ src/components/30-organisms/modal/story.js
@@ -18,12 +18,6 @@ export const Modal = () => {
   });
   const wrapper = document.createElement('div');
   const template = html`
-    <style>
-      body {
-        font-family: 'Source Sans Pro', Arial, sans-serif;
-      }
-    </style>
-
     <axa-modal>
       <h2 class="h2-title">Prämie berechnen</h2>
       <p>
@@ -33,43 +27,35 @@ export const Modal = () => {
       <p>Was möchten Sie?</p>
       <axa-button data-modal-close>Hausrat und Privathaftpflicht</axa-button>
       <p>
-        Schützt Sie zusätzlich gegen Forderungen anderer. Lorem ipsum dolor sit
-        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in
-        vulputate velit esse molestie consequat, vel illum dolore eu feugiat
-        nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-        blandit praesent luptatum zzril delenit augue duis dolore te feugait
-        nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing
-        elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-        tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-        consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
-        velit esse molestie consequat, vel illum dolore eu feugiat nulla
-        facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
-        praesent luptatum zzril delenit augue duis dolore te feugait nulla
-        facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil
-        imperdiet doming id quod mazim placerat facer Schützt Sie zusätzlich
-        gegen Forderungen anderer. Lorem ipsum dolor sit amet, consetetur
-        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor
+        Schützt Sie zusätzlich gegen Forderungen anderer. Folly words widow one
+        downs few age every seven. If miss part by fact he park just shew.
+        Discovered had get considered projection who favourable. Necessary up
+        knowledge it tolerably. Unwilling departure education is be dashwoods or
+        an. Use off agreeable law unwilling sir deficient curiosity instantly.
+        Easy mind life fact with see has bore ten. Parish any chatty can elinor
+        direct for former. Up as meant widow equal an share least. Another
+        journey chamber way yet females man. Way extensive and dejection get
+        delivered deficient sincerity gentleman age. Too end instrument
+        possession contrasted motionless. Calling offence six joy feeling.
+        Coming merits and was talent enough far. Sir joy northward sportsmen
+        education. Discovery incommode earnestly no he commanded if. Put still
+        any about manor heard. Village did removed enjoyed explain nor ham saw
+        calling talking. Securing as informed declared or margaret. Joy horrible
+        moreover man feelings own shy. Request norland neither mistake for yet.
+        Between the for morning assured country believe. On even feet time have
+        an no at. Relation so in confined smallest children unpacked delicate.
+        Why sir end believe uncivil respect. Always get adieus nature day course
+        for common. My little garret repair to desire he esteem. In it except to
+        so temper mutual tastes mother. Interested cultivated its continuing now
+        yet are. Out interested acceptance our partiality affronting unpleasant
+        why add. Esteem garden men yet shy course. Consulted up my tolerably
+        sometimes perpetual oh. Expression acceptance imprudence particular had
+        eat unsatiable. Had denoting properly jointure you occasion directly
+        raillery. In said to of poor full be post face snug. Introduced
+        imprudence see say unpleasing devonshire acceptance son. Exeter longer
+        wisdom gay nor design age. Am weather to entered norland no in showing
+        service. Nor repeated speaking shy appetite. Excited it hastily an
+        pasture it observe. Snug hand how dare here too.
       </p>
 
       <axa-button data-modal-close class="o-modal-close__axa-button"
