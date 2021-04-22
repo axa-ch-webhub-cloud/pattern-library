8124111bc912a537ccf2689046c1f976b809fc7d
Berkelmann <daniel.berkelmann@axa-winterthur.ch>
2021-04-22 11:07:33 +0200

    show values for ide autocomplete

--- src/components/10-atoms/icon/index.d.ts
+++ src/components/10-atoms/icon/index.d.ts
@@ -1,3 +1,19 @@
-export type Icon = String;
+export type Icon =
+  | 'add'
+  | 'arrow-left'
+  | 'arrow-right'
+  | 'collapse'
+  | 'document'
+  | 'download'
+  | 'email'
+  | 'expand'
+  | 'mobile'
+  | 'phone'
+  | 'search'
+  | 'upload'
+  | 'cloud-upload'
+  | 'axa-logo'
+  | 'axa-logo-open'
+  | String;
 
 export type Size = 'small' | 'medium' | 'large' | 'original';
