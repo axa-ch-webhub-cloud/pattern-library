25601475715f513a2c3c6650a533b1af70445209
Milchmaa <marc.welz03@gmail.com>
2021-04-23 12:33:08 +0200

    removed the hover effect and added tests

--- src/components/10-atoms/toggle-switch/pw.ui.test.js
+++ src/components/10-atoms/toggle-switch/pw.ui.test.js
@@ -1,25 +1,44 @@
 const host = process.env.TEST_HOST_STORYBOOK_URL;
+const tag = 'axa-toggle-switch';
 
 describe('toggle-switch', () => {
   it('should fire correct change event in controlled mode', async () => {
-    await page.goto(
-      `${host}/iframe.html?id=examples-toggle-switch-react--controlled-with-dummy-onchange-listener&viewMode=story`
-    );
-    await page.waitForSelector('.a-toggle-switch');
+    await openToggleSwitch();
+    await page.waitForSelector(tag);
 
     const eventInfo = await page.textContent(
       '.axa-toggle-switch-demo__event-info'
     );
     expect(eventInfo).toBe('-');
-    await page.click('.a-toggle-switch');
+    await page.click(tag);
     const eventInfoAfterFirstClick = await page.textContent(
       '.axa-toggle-switch-demo__event-info'
     );
     expect(eventInfoAfterFirstClick).toBe('true');
-    await page.click('.a-toggle-switch');
+    await page.click(tag);
     const eventInfoAfterSecondClick = await page.textContent(
       '.axa-toggle-switch-demo__event-info'
     );
     expect(eventInfoAfterSecondClick).toBe('true');
   });
+
+  it('should write error message', async () => {
+    await openToggleSwitch();
+    await page.waitForSelector(tag);
+
+    await page.evaluate(evaluateTag => {
+      document
+        .querySelector(evaluateTag)
+        .setAttribute('error', 'Error Message');
+    }, tag);
+
+    const axaToggleSwitch = await page.$(tag);
+    expect(await axaToggleSwitch.getAttribute('error')).toBe('Error Message');
+  });
 });
+
+async function openToggleSwitch() {
+  await page.goto(
+    `${host}/iframe.html?id=examples-toggle-switch-react--controlled-with-dummy-onchange-listener&viewMode=story`
+  );
+}
