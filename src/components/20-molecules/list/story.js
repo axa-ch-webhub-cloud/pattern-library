a90950083713cb4b59986687ae8a461f9c7b7b50
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2021-05-06 15:36:23 +0200

    cleanup

--- src/components/20-molecules/list/story.js
+++ src/components/20-molecules/list/story.js
@@ -151,7 +151,13 @@ export const List = () => {
       variant="icon"
       icon='<svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.79508 10.8749L1.62508 6.70492L0.205078 8.11492L5.79508 13.7049L17.7951 1.70492L16.3851 0.294922L5.79508 10.8749Z" fill="green"/></svg>'
     >
-      <li>hello</li>
+      <li>
+        hello hello hello hello hello hello hello hello hello hello hello hello
+        hello hello hello hello hello hello hello hello hello hello hello hello
+        hello hello hello hello hello hello hello hello hello hello hello hello
+        hello hello hello hello hello hello hello hello hello hello hello hello
+        hello hello hello
+      </li>
       <li>world</li>
     </axa-list>
     <axa-list variant="unstyled">
