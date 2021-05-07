35d72517ff3123c8d63009907bb7435ebf71d375
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2021-05-07 12:20:12 +0200

    adapt icon position and mockdata

--- src/components/20-molecules/list/story.js
+++ src/components/20-molecules/list/story.js
@@ -1,5 +1,6 @@
 import { select, text, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
+import checkmark from '../../../../node_modules/@axa-ch/materials/icons/material-design/check.svg';
 import './index';
 
 export default {
@@ -14,17 +15,19 @@ export const List = () => {
     unstyled: 'unstyled',
     icon: 'icon',
   });
-  const icon = text(
-    'icon',
-    '<svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.79508 10.8749L1.62508 6.70492L0.205078 8.11492L5.79508 13.7049L17.7951 1.70492L16.3851 0.294922L5.79508 10.8749Z" fill="green"/></svg>'
-  );
+  const icon = text('icon', `${checkmark}`);
 
   const wrapper = document.createElement('div');
   const template = html`
     <axa-list variant="${variant}" icon="${icon}">
       <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
       <li>Aliquam tincidunt mauris eu risus.</li>
-      <li>Vestibulum auctor dapibus neque.</li>
+      <li>
+        Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris
+        sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis
+        dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a,
+        ultricies in, diam. Sed arcu. Cras consequat.
+      </li>
       <li>Nunc dignissim risus id metus.</li>
       <li>Cras ornare tristique elit.</li>
       <li>Vivamus vestibulum ntulla nec ante.</li>
@@ -33,7 +36,16 @@ export const List = () => {
       <li>Integer vitae libero ac risus egestas placerat.</li>
       <li>Vestibulum commodo felis quis tortor.</li>
       <li>Ut aliquam sollicitudin leo.</li>
-      <li>Cras iaculis ultricies nulla.</li>
+      <li>
+        Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut
+        turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a,
+        pede. Praesent dapibus, neque id cursus faucibus, tortor neque egestas
+        auguae, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui
+        mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.
+        Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer
+        ligula vulputate sem tristique cursus. Nam nulla quam, gravida non,
+        commodo a, sodales sit amet, nisi.
+      </li>
       <li>Donec quis dui at dolor tempor interdum.</li>
     </axa-list>
   `;
