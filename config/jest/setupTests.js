1393ec77188b4ed894acd4ee69a6f44d04b8ee8f
Noah Liechti <noah.liechti@gmail.com>
2021-08-12 13:41:46 +0200

    change order of pollyfills

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,8 +1,5 @@
 // polyfills etc
 import 'mutationobserver-shim';
-import '@ungap/global-this'
-import '@webcomponents/webcomponentsjs';
-import 'lit/polyfill-support';
 import 'core-js/es6/reflect';
 import 'core-js/es6/promise';
 import 'core-js/fn/array/from';
@@ -10,3 +7,6 @@ import 'core-js/fn/object/assign';
 import 'core-js/fn/object/create';
 import 'core-js/fn/object/define-properties';
 import 'core-js/fn/object/set-prototype-of';
+import '@ungap/global-this'
+import 'lit/polyfill-support';
+import '@webcomponents/webcomponentsjs';
