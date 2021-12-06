c3fcfcdc70965a1f39862256144eb7c41f7be10c
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2021-12-06 11:09:39 +0100

    improve styling

--- src/components/20-molecules/input-phone/story.js
+++ src/components/20-molecules/input-phone/story.js
@@ -1,4 +1,4 @@
-import { withKnobs } from '@storybook/addon-knobs';
+import { text, select, withKnobs } from '@storybook/addon-knobs';
 import { html, render } from 'lit-html';
 import './index';
 
@@ -8,11 +8,16 @@ export default {
 };
 
 export const InputPhone = () => {
+  const lang = select('lang', ['de', 'en', 'it', 'fr'], 'en');
+  const label = text('label', 'Phone Number');
+  const errorprefix = text('errorprefix', 'Invalid Phone Number');
+
   const wrapper = document.createElement('div');
   const template = html`
     <axa-input-phone
-      label="Phone Number"
-      errorprefix="Invalid Phone Number"
+      lang="${lang}"
+      label="${label}"
+      errorprefix="${errorprefix}"
     ></axa-input-phone>
   `;
 
