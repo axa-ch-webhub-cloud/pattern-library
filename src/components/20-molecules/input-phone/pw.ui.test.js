cecbc90d5c125a198dac280b8c3393d0576e49db
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2021-12-13 10:13:11 +0100

    DRYify tests and make them more readable

--- src/components/20-molecules/input-phone/pw.ui.test.js
+++ src/components/20-molecules/input-phone/pw.ui.test.js
@@ -2,6 +2,8 @@ const host = process.env.TEST_HOST_STORYBOOK_URL;
 const tag = 'axa-input-phone';
 const validPhoneNumber = '795002020';
 
+let hostElement;
+
 const selectGermany = async () => {
   return page.evaluate(evaluateTag => {
     document
@@ -15,43 +17,47 @@ const writePhoneNumber = async number => {
   return page.fill('.a-input-text__input', number);
 };
 
+const assertInvalidAndErrorMessageShown = async () => {
+  expect(await hostElement.getAttribute('invalid')).toBe('');
+  expect(await page.isVisible('.m-input-phone__error--show')).toBeTruthy();
+};
+
+const assertValidComponent = async () => {
+  expect(await hostElement.getAttribute('invalid')).toBe(null);
+  expect(await page.isVisible('.m-input-phone__error--show')).not.toBeTruthy();
+};
+
 describe('Input Phone', () => {
   it('should accept a valid phone number', async () => {
     await page.goto(
       `${host}/iframe.html?id=components--input-phone&knob-counter=Still&viewMode=story`
     );
-    const hostElement = await page.waitForSelector(tag);
+    hostElement = await page.waitForSelector(tag);
     await page.click('.js-dropdown__toggle');
     await page.waitForSelector('.m-dropdown__content');
     await selectGermany();
     await writePhoneNumber(validPhoneNumber);
-    await expect(await hostElement.getAttribute('invalid')).toBe(null);
+    assertValidComponent();
     await writePhoneNumber('79 500 20 20');
-    await expect(await hostElement.getAttribute('invalid')).toBe(null);
+    assertValidComponent();
   });
 
   it('should deny invalid inserted phone numbers', async () => {
     await page.goto(
       `${host}/iframe.html?id=components--input-phone&knob-counter=Still&viewMode=story`
     );
-    const hostElement = await page.waitForSelector(tag);
+    hostElement = await page.waitForSelector(tag);
 
     await writePhoneNumber('a');
-    expect(await hostElement.getAttribute('invalid')).toBe('');
-    expect(await page.isVisible('.m-input-phone__error--show')).toBeTruthy();
+    assertInvalidAndErrorMessageShown();
 
     await writePhoneNumber(validPhoneNumber);
-    expect(await hostElement.getAttribute('invalid')).toBe(null);
-    expect(
-      await page.isVisible('.m-input-phone__error--show')
-    ).not.toBeTruthy();
+    assertValidComponent();
 
     await writePhoneNumber(`0${validPhoneNumber}`);
-    expect(await hostElement.getAttribute('invalid')).toBe('');
-    expect(await page.isVisible('.m-input-phone__error--show')).toBeTruthy();
+    assertInvalidAndErrorMessageShown();
 
     await writePhoneNumber(`${validPhoneNumber}123`);
-    expect(await hostElement.getAttribute('invalid')).toBe('');
-    expect(await page.isVisible('.m-input-phone__error--show')).toBeTruthy();
+    assertInvalidAndErrorMessageShown();
   });
 });
