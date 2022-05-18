64df01d5f2d1ebee5b1145032cc39d1c9bb9d35c
Marco Kruse <marco.kruse.ext@iu.org>
2022-05-18 23:07:11 +0200

    Remove obsolete plugin that comes from preset env and use same targets than production build

--- .storybook/.babelrc.js
+++ .storybook/.babelrc.js
@@ -4,11 +4,10 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
-          browsers: ['last 2 versions', 'safari >= 8', 'ie >= 11'], // read comma as 'or', i.e. set *union*
+          esmodules: true,
         },
       },
     ],
     '@babel/preset-react',
   ],
-  plugins: [['@babel/plugin-proposal-class-properties']],
 };
