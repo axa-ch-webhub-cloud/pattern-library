d373289394a5e59679716f7b69e0545d3bf49f9a
Marco Kruse <marco.kruse.ext@iu.org>
2022-05-18 23:19:40 +0200

    Remove flag static controlled now in main.js

--- scripts/ui-test-runner.sh
+++ scripts/ui-test-runner.sh
@@ -6,7 +6,7 @@ export $(egrep -v '^#' .env | xargs) > /dev/null 2>&1
 
 echo "Running Storybook on address '$TEST_HOST_STORYBOOK_URL' and on port '$TEST_HOST_STORYBOOK_PORT'"
 
-npx start-storybook -p $TEST_HOST_STORYBOOK_PORT -c .storybook -s ./src/static --ci  &
+npx start-storybook -p $TEST_HOST_STORYBOOK_PORT -c .storybook --ci  &
 npx wait-on $TEST_HOST_STORYBOOK_PATH -t 120000 && echo "Storybook ready for testing"
 
 ### Start playwright tests
