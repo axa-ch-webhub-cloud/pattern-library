1c87b2bd54c5dce6674ace2d8f03e7d13a9ef045
Marco Kruse <marco.kruse@devahead.de>
2022-06-17 15:06:25 +0200

    Remove obsolete babel plugins

--- config/jest/jestPreprocess.js
+++ config/jest/jestPreprocess.js
@@ -1,9 +1,5 @@
 const babelOptions = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
-  plugins: [
-    'transform-custom-element-classes',
-    '@babel/plugin-proposal-class-properties',
-  ],
 };
 
 module.exports = require('babel-jest').createTransformer(babelOptions);
