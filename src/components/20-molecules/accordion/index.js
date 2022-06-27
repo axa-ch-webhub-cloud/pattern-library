61b632363a54b749aafd79beb6f7577c327317c6
Marco Kruse <marco.kruse@devahead.de>
2022-06-27 14:05:22 +0200

    Rename to accordion and remove axa text and add on resize calc of height

--- src/components/20-molecules/accordion/index.js
+++ src/components/20-molecules/accordion/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import expandLess from '@axa-ch/materials/icons/material-design/expand_less.svg';
@@ -8,12 +7,13 @@ import { classMap } from 'lit/directives/class-map.js';
 import { defineVersioned } from '../../../utils/component-versioning';
 import applyDefaults from '../../../utils/apply-defaults';
 import { sanitizeSVG } from '../../../utils/sanitize';
+import debounce from '../../../utils/debounce';
 
 import styles from './index.scss';
 
-class AXAAccordeon extends LitElement {
+class AXAAccordion extends LitElement {
   static get tagName() {
-    return 'axa-accordeon';
+    return 'axa-accordion';
   }
 
   static get styles() {
@@ -24,7 +24,7 @@ class AXAAccordeon extends LitElement {
 
   static get properties() {
     return {
-      small: { type: Boolean },
+      large: { type: Boolean },
       open: { type: Boolean },
       disabled: { type: Boolean },
       icon: { type: String },
@@ -35,6 +35,7 @@ class AXAAccordeon extends LitElement {
   constructor() {
     super();
     applyDefaults(this);
+    this.debouncedHandleViewportCheck = debounce(this.toggleAnimation, 500);
   }
 
   toggleAccordion() {
@@ -46,8 +47,10 @@ class AXAAccordeon extends LitElement {
 
   firstUpdated() {
     this.getFirstChild().shadowRoot.querySelector(
-      '.m-accordeon__container'
+      '.m-accordion__container'
     ).style.borderTop = 'solid 1px #ccc';
+
+    window.addEventListener('resize', this.debouncedHandleViewportCheck);
 
     // slot-content is not available on IE 11 at this time
     if (window.document.documentMode) {
@@ -60,48 +63,57 @@ class AXAAccordeon extends LitElement {
   }
 
   toggleAnimation() {
-    const accordeonContent = this.shadowRoot.querySelector(
-      '.m-accordeon__content'
+    const accordionContent = this.shadowRoot.querySelector(
+      '.m-accordion__content'
     );
 
-    if (this.open) {
-      accordeonContent.style.maxHeight = `${accordeonContent.scrollHeight}px`;
-    } else {
-      accordeonContent.style.maxHeight = 0;
+    if (!accordionContent) {
+      return;
     }
-  }
+
+    if (this.open) {
+      accordionContent.style.maxHeight = `${accordionContent.scrollHeight}px`;
+    } else {
+      accordionContent.style.maxHeight = 0;
+    }
+  };
 
   render() {
-    const { open, title, small, disabled, icon } = this;
+    const { open, title, large, disabled, icon } = this;
+    const titleContainerClasses = {
+      'm-accordion__title-container': true,
+      'm-accordion__title-container--disabled': disabled,
+    };
     const titleClasses = {
-      'm-accordeon__title-container--disabled': disabled,
+      'm-accordion__title': true,
+      'm-accordion__title--large': large,
     };
 
     const iconClass = icon
-      ? 'm-accordeon__title-container-title-icon'
-      : 'm-accordeon__title-container-title-icon--hidden';
+      ? 'm-accordion__title-container-title-icon'
+      : 'm-accordion__title-container-title-icon--hidden';
 
     const statusIcon = unsafeHTML(open ? expandLess : expandMore);
 
     const iconHTML = icon ? unsafeHTML(sanitizeSVG(icon)) : html``;
     return html`
-      <article class="m-accordeon">
-        <div class="m-accordeon__container">
+      <article class="m-accordion">
+        <div class="m-accordion__container">
           <div
-            class="m-accordeon__title-container ${classMap(titleClasses)}"
+            class="${classMap(titleContainerClasses)}"
             @click="${this.toggleAccordion}"
           >
-            <div class="m-accordeon__title-container-title">
+            <div class="m-accordion__title-container-title">
               <div class="${iconClass}">${iconHTML}</div>
-              <axa-text variant="${small ? 'size-3' : ''}"> ${title} </axa-text>
+              <span class="${classMap(titleClasses)}">${title}</span>
             </div>
-            <div class="m-accordeon__title-container--closed">
+            <div class="m-accordion__title-container--closed">
               ${statusIcon}
             </div>
           </div>
           <div
-            class="m-accordeon__content ${open
-              ? 'm-accordeon__content--open'
+            class="m-accordion__content ${open
+              ? 'm-accordion__content--open'
               : ''}"
           >
             <slot></slot>
@@ -115,11 +127,16 @@ class AXAAccordeon extends LitElement {
     return Array.prototype.slice
       .call(this.parentNode.querySelectorAll('*'))
       .find(el => {
-        return el.tagName.toLowerCase().includes('axa-accordeon');
+        return el.tagName.toLowerCase().includes('axa-accordion');
       });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('resize', this.debouncedHandleViewportCheck);
   }
 }
 
-defineVersioned([AXAAccordeon], __VERSION_INFO__);
+defineVersioned([AXAAccordion], __VERSION_INFO__);
 
-export default AXAAccordeon;
+export default AXAAccordion;
