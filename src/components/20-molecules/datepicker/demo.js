db5b75ef15df61acfb37391429a5094d2a0cfcad
Marco Kruse <marco.kruse@devahead.de>
2022-07-24 18:53:43 +0200

    Migrate demos to new sb api's

--- src/components/20-molecules/datepicker/demo.js
+++ src/components/20-molecules/datepicker/demo.js
@@ -1,126 +1,117 @@
-import { storiesOf } from '@storybook/web-components';
-import { html, render } from 'lit';
+import { html } from 'lit';
 import changelog from './CHANGELOG.md';
-import './index';
 import readme from './README.md';
+import './index';
 
-storiesOf('Examples/Datepicker/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/Datepicker/Pure HTML',
+  parameters: {
     readme,
     usage: { disable: true },
     changelog,
-  })
-  .add('In a form', () => {
-    const handleSubmit = event => {
-      event.preventDefault();
-      document.getElementById('form-data').open = true;
-      const formData = new window.FormData(event.target);
-      let n = 0;
-      for (const entries = formData.entries(); !entries.next().done; n++);
-      document.getElementById('form-data-date').textContent = `${formData.get(
-        'date'
-      )} (of ${n} submittable elements)`;
-    };
+    controls: { disable: true },
+  },
+};
 
-    const template = html`
-      <form id="datepicker-form" @submit="${handleSubmit}">
-        <fieldset>
-          <legend>Date</legend>
-          <axa-datepicker
-            data-test-id="datepicker-forms"
-            name="date"
-            inputfield
-            required
-            label="Insurance date"
-            placeholder="Bitte Datum wählen"
-            year="2020"
-            month="1"
-            day="2"
-            allowedyears="[2020]"
-          ></axa-datepicker>
-          <axa-button type="submit" id="datepicker-forms-submit">OK</axa-button>
-          <details
-            id="form-data"
-            style="display: block; margin-top: 2rem; width: 300px"
+export const InAForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    document.getElementById('form-data').open = true;
+    const formData = new window.FormData(event.target);
+    let n = 0;
+    for (const entries = formData.entries(); !entries.next().done; n++);
+    document.getElementById('form-data-date').textContent = `${formData.get(
+      'date'
+    )} (of ${n} submittable elements)`;
+  };
+
+  return html`
+    <form id="datepicker-form" @submit="${handleSubmit}">
+      <fieldset>
+        <legend>Date</legend>
+        <axa-datepicker
+          data-test-id="datepicker-forms"
+          name="date"
+          inputfield
+          required
+          label="Insurance date"
+          placeholder="Bitte Datum wählen"
+          year="2020"
+          month="1"
+          day="2"
+          allowedyears="[2020]"
+        ></axa-datepicker>
+        <axa-button type="submit" id="datepicker-forms-submit">OK</axa-button>
+        <details
+          id="form-data"
+          style="display: block; margin-top: 2rem; width: 300px"
+        >
+          <summary
+            style="background:rgb(0,0,143);color:#fff;padding:11px;font-family:sans-serif;outline:none"
           >
-            <summary
-              style="background:rgb(0,0,143);color:#fff;padding:11px;font-family:sans-serif;outline:none"
-            >
-              form content
-            </summary>
-            <div
-              id="datepicker-forms-content"
-              style="display:table; padding: 5px 0"
-            >
-              date = <span id="form-data-date"></span>
-            </div>
-          </details>
-          <label>
-            <input
-              type="checkbox"
-              @change="${({ target }) => {
-                document.querySelector(
-                  'axa-datepicker[data-test-id="datepicker-forms"]'
-                ).disabled = target.checked;
-              }}"
-            />
-            disable datepicker
-          </label>
-        </fieldset>
-      </form>
-    `;
+            form content
+          </summary>
+          <div
+            id="datepicker-forms-content"
+            style="display:table; padding: 5px 0"
+          >
+            date = <span id="form-data-date"></span>
+          </div>
+        </details>
+        <label>
+          <input
+            type="checkbox"
+            @change="${({ target }) => {
+              document.querySelector(
+                'axa-datepicker[data-test-id="datepicker-forms"]'
+              ).disabled = target.checked;
+            }}"
+          />
+          disable datepicker
+        </label>
+      </fieldset>
+    </form>
+  `;
+};
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('With onchange handler', () => {
-    const template = html`
-      <axa-datepicker
-        inputfield
-        data-test-id="datepicker-onchange"
-        name="date"
-        required
-        label="Choose a date"
-        allowedyears='["1971-2020"]'
-        year="2020"
-        month="1"
-        day="29"
-        @change="${({ detail: { name, value } }) => {
-          document.querySelector(
-            '.event-log'
-          ).value += `{"name":"${name}","value":"${value}"}\n\n`;
-        }}"
-      ></axa-datepicker>
-      <textarea
-        class="event-log"
-        placeholder="event log..."
-        rows="31"
-        cols="30"
-      ></textarea>
-    `;
+export const WithOnchangeHandler = () =>
+  html`
+    <axa-datepicker
+      inputfield
+      data-test-id="datepicker-onchange"
+      name="date"
+      required
+      label="Choose a date"
+      allowedyears='["1971-2020"]'
+      year="2020"
+      month="1"
+      day="29"
+      @change="${({ detail: { name, value } }) => {
+        document.querySelector(
+          '.event-log'
+        ).value += `{"name":"${name}","value":"${value}"}\n\n`;
+      }}"
+    ></axa-datepicker>
+    <textarea
+      class="event-log"
+      placeholder="event log..."
+      rows="31"
+      cols="30"
+    ></textarea>
+  `;
 
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('Set width with inline style', () => {
-    const template = html`
-      <axa-datepicker
-        style="width: 200px"
-        inputfield
-        data-test-id="datepicker-onchange"
-        name="date"
-        required
-        label="Choose a date"
-        allowedyears='["1971-2020"]'
-        year="2020"
-        month="1"
-        day="29"
-      ></axa-datepicker>
-    `;
-
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  });
+export const SetWidthWithInlineStyle = () =>
+  html`
+    <axa-datepicker
+      style="width: 200px"
+      inputfield
+      data-test-id="datepicker-onchange"
+      name="date"
+      required
+      label="Choose a date"
+      allowedyears='["1971-2020"]'
+      year="2020"
+      month="1"
+      day="29"
+    ></axa-datepicker>
+  `;
--- src/components/20-molecules/file-upload/demo.js
+++ src/components/20-molecules/file-upload/demo.js
@@ -1,125 +1,117 @@
-import { storiesOf } from '@storybook/web-components';
 import { html, render } from 'lit';
 import changelog from './CHANGELOG.md';
-import './index';
 import readme from './README.md';
+import './index';
 
-storiesOf('Examples/File Upload/Pure HTML', module)
-  .addParameters({
+export default {
+  title: 'Examples/File Upload/Pure HTML',
+  parameters: {
     readme,
     usage: { disable: true },
     changelog,
-  })
-  .add('Get files from onChange event', () => {
-    const changeHandler = e => {
-      const files = e.detail;
+    controls: { disable: true },
+  },
+};
 
-      if (files.length > 0) {
-        const listWrapper = html`
-          <div class="js-file-upload__list-wrapper">
-            <h3>The following files were selected:</h3>
-            <ol>
-              ${files.map(file => {
-                return html` <li>${file.name}</li> `;
-              })}
-            </ol>
-          </div>
-        `;
+export const GetFilesFromOnchange = () => {
+  const changeHandler = e => {
+    const files = e.detail;
 
-        render(listWrapper, document.getElementById('listWrapper'));
-      } else {
-        render('', document.getElementById('listWrapper'));
-      }
-    };
+    if (files.length > 0) {
+      const listWrapper = html`
+        <div class="js-file-upload__list-wrapper">
+          <h3>The following files were selected:</h3>
+          <ol>
+            ${files.map(file => {
+              return html` <li>${file.name}</li> `;
+            })}
+          </ol>
+        </div>
+      `;
 
-    const template = html`
-      <div style="width:455px;">
-        <axa-file-upload
-          @change="${changeHandler}"
-          maxSizeOfSingleFileKB="500"
-          class="js-file-upload__file-upload"
-          >These files are going to be uploaded</axa-file-upload
-        >
-      </div>
-      <div id="listWrapper"></div>
-    `;
-
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('In a form', () => {
-    const handleSubmit = e => {
-      e.preventDefault();
-
-      const imgUpload = document.querySelector('.js-file-upload__file-upload');
-
-      if (imgUpload.files.length > 0) {
-        const listWrapper = html`
-          <div class="js-file-upload__list-wrapper">
-            <h3>The following files were selected:</h3>
-            <ol>
-              ${imgUpload.files.map(file => {
-                return html` <li>${file.name}</li> `;
-              })}
-            </ol>
-          </div>
-        `;
-
-        render(listWrapper, document.getElementById('listWrapper'));
-      }
-    };
-
-    const template = html`
-      <div style="width:455px;">
-        <axa-file-upload
-          maxSizeOfSingleFileKB="500"
-          class="js-file-upload__file-upload"
-          >These files are going to be uploaded</axa-file-upload
-        >
-      </div>
-      <form @click="${handleSubmit}" style="margin-top:40px;">
-        <axa-button type="submit">Submit</axa-button>
-      </form>
-      <div id="listWrapper"></div>
-    `;
-
-    const wrapper = document.createElement('div');
-    render(template, wrapper);
-    return wrapper;
-  })
-  .add('invalid', () => {
-    const wrapper = document.createElement('div');
-
-    function handleSubmit() {
-      const textSelector = document.querySelector('axa-text');
-      if (document.querySelector('axa-file-upload').hasAttribute('invalid')) {
-        textSelector.innerHTML =
-          'could not submit because file-upload is invalid';
-        textSelector.style.color = '#c91432';
-      } else {
-        textSelector.innerHTML = 'submitted successfully';
-        textSelector.style.color = '#000000';
-      }
+      render(listWrapper, document.getElementById('listWrapper'));
+    } else {
+      render('', document.getElementById('listWrapper'));
     }
+  };
 
-    const template = html`
-      <div style="width: 455px">
-        <axa-file-upload
-          allowedFileTypes="image/jpg, image/jpeg, application/pdf, image/png"
-        >
-          The following files are being transferred:
-        </axa-file-upload>
-      </div>
+  return html`
+    <div style="width:455px;">
+      <axa-file-upload
+        @change="${changeHandler}"
+        maxSizeOfSingleFileKB="500"
+        class="js-file-upload__file-upload"
+        >These files are going to be uploaded</axa-file-upload
+      >
+    </div>
+    <div id="listWrapper"></div>
+  `;
+};
 
-      <axa-button style="margin: 10px 0" @click="${handleSubmit}">
-        Submit
-      </axa-button>
+export const InAForm = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
 
-      <br />
-      <axa-text variant="size-2"></axa-text>
-    `;
+    const imgUpload = document.querySelector('.js-file-upload__file-upload');
 
-    render(template, wrapper);
-    return wrapper;
-  });
+    if (imgUpload.files.length > 0) {
+      const listWrapper = html`
+        <div class="js-file-upload__list-wrapper">
+          <h3>The following files were selected:</h3>
+          <ol>
+            ${imgUpload.files.map(file => {
+              return html` <li>${file.name}</li> `;
+            })}
+          </ol>
+        </div>
+      `;
+
+      render(listWrapper, document.getElementById('listWrapper'));
+    }
+  };
+
+  return html`
+    <div style="width:455px;">
+      <axa-file-upload
+        maxSizeOfSingleFileKB="500"
+        class="js-file-upload__file-upload"
+        >These files are going to be uploaded
+      </axa-file-upload>
+    </div>
+    <form @click="${handleSubmit}" style="margin-top:40px;">
+      <axa-button type="submit">Submit</axa-button>
+    </form>
+    <div id="listWrapper"></div>
+  `;
+};
+
+export const Invalid = () => {
+  const handleSubmit = () => {
+    const textSelector = document.querySelector('axa-text');
+    if (document.querySelector('axa-file-upload').hasAttribute('invalid')) {
+      textSelector.innerHTML =
+        'could not submit because file-upload is invalid';
+      textSelector.style.color = '#c91432';
+    } else {
+      textSelector.innerHTML = 'submitted successfully';
+      textSelector.style.color = '#000000';
+    }
+  };
+
+  return html`
+    <div style="width: 455px">
+      <axa-file-upload
+        allowedFileTypes="image/jpg, image/jpeg, application/pdf, image/png"
+      >
+        The following files are being transferred:
+      </axa-file-upload>
+    </div>
+
+    <axa-button style="margin: 10px 0" @click="${handleSubmit}">
+      Submit
+    </axa-button>
+
+    <br />
+    <axa-text variant="size-2"></axa-text>
+  `;
+};
