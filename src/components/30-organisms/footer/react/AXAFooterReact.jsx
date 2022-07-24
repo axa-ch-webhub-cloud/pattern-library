b530ae71ed220b199e0559679ce647774e0ed459
Marco Kruse <marco.kruse@devahead.de>
2022-07-24 18:52:27 +0200

    Create react cps in seperate files

---
+++ src/components/30-organisms/footer/react/AXAFooterReact.jsx
@@ -0,0 +1,6 @@
+import { createElement } from 'react';
+import createAXAFooterReact from '../index.react';
+
+const AXAFooterReact = createAXAFooterReact(createElement);
+
+export default AXAFooterReact;
