9067825fdf670ac332b8c9eb893b52f631cc63dc
Marco Kruse <marco.kruse@devahead.de>
2022-08-02 12:40:18 +0200

    Remove obsolete globals

--- config/globals.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  'components/00-materials/styles/variables.scss',
-  'components/00-materials/styles/00-colors.scss',
-  'components/00-materials/styles/20-animations.scss',
-  'components/00-materials/styles/typography.scss',
-];
