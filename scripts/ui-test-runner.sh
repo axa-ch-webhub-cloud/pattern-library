dd0fa312acff3f9496c1027164927bd653f5587e
Marco Kruse <marco.kruse@devahead.de>
2022-08-08 17:19:56 +0200

    Test with seperate script

--- scripts/ui-test-runner.sh
+++ scripts/ui-test-runner.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+echo "Running Storybook on address '$TEST_HOST_STORYBOOK_URL' and on port '9999'"
+
+npx start-storybook -p 9999 -c .storybook --ci  &
+npx wait-on tcp:9999 -t 120000 && echo "Storybook ready for testing"
+
+test_status=$?
+
+if [ $test_status = 1 ]; then
+    # Kill storybook (cleanup) - By port
+    kill -9 $(lsof -t -i:9999 -sTCP:LISTEN)
+    exit 1;
+fi
+
+### Start testcafe tests
 NUM_CPUS=$(node -e 'console.log(require("os").cpus().length)')
+if [ -z "$1" ]
+then
+    npx testcafe -c ${NUM_CPUS} "chrome:headless" -q ./**/ui.test.js
+else
+    npx testcafe -F "${1}" -c ${NUM_CPUS} "chrome:headless" -q ./**/ui.test.js
+fi
+### End testcafe tests
 
-START_SB_STATIC="http-server storybook-static -s -p 9999"
-RUN_PLAYWRIGHT="cross-env SB_URL=http://localhost:9999 playwright test"
-RUN_TESTCAFE="cross-env TEST_HOST_STORYBOOK_URL=http://localhost:9999 testcafe -c ${NUM_CPUS} "chrome:headless" -q ./**/ui.test.js"
+test_status=$?
 
-npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue" "${START_SB_STATIC}" "wait-on tcp:9999 && ${RUN_PLAYWRIGHT} && ${RUN_TESTCAFE}"
+# Kill storybook (cleanup) - By port
+kill -9 $(lsof -t -i:9999 -sTCP:LISTEN)
+
+exit $test_status
