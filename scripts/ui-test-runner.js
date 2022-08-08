89d3ca7d019812a81a0a731419d327d6cc9e987d
Marco Kruse <marco.kruse@devahead.de>
2022-08-08 18:42:00 +0200

    Test ci only testcafe

--- scripts/ui-test-runner.js
+++ scripts/ui-test-runner.js
@@ -1,10 +1,10 @@
 const concurrently = require('concurrently');
-// const os = require('os');
+const os = require('os');
 const path = require('path');
 
 // testcafe cannot manage more than 6 instances.
-// const numCpus = os.cpus().length > 6 ? 6 : os.cpus().length;
-// const RUN_TESTCAFE = `cross-env TEST_HOST_STORYBOOK_URL=http://localhost:9999 testcafe -c ${numCpus} "chrome:headless" -q ./**/ui.test.js`;
+const numCpus = os.cpus().length > 6 ? 6 : os.cpus().length;
+const RUN_TESTCAFE = `cross-env TEST_HOST_STORYBOOK_URL=http://localhost:9999 testcafe -c ${numCpus} "chrome:headless" -q ./**/ui.test.js`;
 const RUN_PLAYWRIGHT = 'cross-env SB_URL=http://localhost:9999 playwright test';
 
 const sbStaticPath = path.resolve(__dirname, '../storybook-static');
@@ -17,7 +17,7 @@ const { result } = concurrently(
       prefixColor: 'magenta',
     },
     {
-      command: `wait-on tcp:9999 && ${RUN_PLAYWRIGHT}`,
+      command: `wait-on tcp:9999 && ${RUN_PLAYWRIGHT} && ${RUN_TESTCAFE}`,
       name: 'TEST',
       prefixColor: 'blue',
     },
