2fdc16f53d8e4ebae4cff152ac03db0b9f51b887
Marco Kruse <marco.kruse@devahead.de>
2022-08-10 19:30:39 +0200

    Rewrite accordion

--- src/components/20-molecules/accordion/index.js
+++ src/components/20-molecules/accordion/index.js
@@ -5,10 +5,50 @@ import expandMore from '@axa-ch/materials/icons/material-design/expand_more.svg'
 
 import { classMap } from 'lit/directives/class-map.js';
 import { defineVersioned } from '../../../utils/component-versioning';
-import applyDefaults from '../../../utils/apply-defaults';
 import { sanitizeSVG } from '../../../utils/sanitize';
+import applyDefaults from '../../../utils/apply-defaults';
 import fireCustomEvent from '../../../utils/custom-event';
+import createRefId from '../../../utils/create-ref-id';
 import styles from './index.scss';
+
+const collapseContent = el => {
+  // get the height of the element's inner content, regardless of its actual size
+  const contentHeight = el.scrollHeight;
+  // temporarily disable all css transitions
+  const elementTransition = el.style.transition;
+  el.style.transition = '';
+
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(() => {
+    el.style.height = `${contentHeight}px`;
+    el.style.transition = elementTransition;
+
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(() => {
+      el.style.height = '0px';
+    });
+  });
+};
+
+const expandContent = el => {
+  // get the height of the element's inner content, regardless of its actual size
+  const contentHeight = el.scrollHeight;
+  // have the element transition to the height of its inner content
+  el.style.height = `${contentHeight}px`;
+
+  const expandAnimation = () => {
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    el.style.height = null;
+  };
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  el.addEventListener('transitionend', expandAnimation);
+  // remove this event listener so it only gets triggered once
+  el.removeEventListener('transitionend', expandAnimation);
+};
 
 class AXAAccordion extends LitElement {
   static get tagName() {
@@ -23,7 +63,9 @@ class AXAAccordion extends LitElement {
 
   static get properties() {
     return {
+      refId: { type: String, defaultValue: `accordion-${createRefId()}` },
       small: { type: Boolean },
+      level: { type: String },
       open: { type: Boolean },
       disabled: { type: Boolean },
       icon: { type: String },
@@ -35,99 +77,82 @@ class AXAAccordion extends LitElement {
   constructor() {
     super();
     applyDefaults(this);
-    this.toggleAnimation = this.toggleAnimation.bind(this);
+    this.toggleAccordion = this.toggleAccordion.bind(this);
   }
 
   toggleAccordion() {
-    if (!this.disabled) {
-      this.open = !this.open;
-      this.toggleAnimation();
-      this.onStateChange(this.open);
-      fireCustomEvent('axa-state-change', this.open, this);
-    }
-  }
-
-  firstUpdated() {
-    this.getFirstChild().shadowRoot.querySelector(
-      '.m-accordion__container'
-    ).style.borderTop = 'solid 1px #ccc';
-
-    // When changing the window size, we have to calculate and set the max-height of m-accordion__content again.
-    window.addEventListener('resize', this.toggleAnimation);
-
-    this.toggleAnimation();
-  }
-
-  toggleAnimation() {
     const accordionContent = this.shadowRoot.querySelector(
       '.m-accordion__content'
     );
 
-    if (!accordionContent) {
+    if (!accordionContent || this.disabled) {
       return;
     }
 
-    accordionContent.style.maxHeight = this.open
-      ? `${accordionContent.scrollHeight}px`
-      : 0;
+    if (this.open) {
+      collapseContent(accordionContent);
+    } else {
+      expandContent(accordionContent);
+    }
+
+    this.open = !this.open;
+    this.onStateChange(this.open);
+    fireCustomEvent('axa-state-change', this.open, this);
   }
 
   render() {
     const { open, title, small, disabled, icon } = this;
     const titleContainerClasses = {
-      'm-accordion__title-container': true,
-      'm-accordion__title-container--disabled': disabled,
+      'm-accordion__header': true,
+      'm-accordion__header--disabled': disabled,
     };
     const titleClasses = {
       'm-accordion__title': true,
       'm-accordion__title--small': small,
     };
 
-    const iconClass = icon
-      ? 'm-accordion__title-container-title-icon'
-      : 'm-accordion__title-container-title-icon--hidden';
-
-    const statusIcon = unsafeHTML(open ? expandLess : expandMore);
-
-    const iconHTML = icon ? unsafeHTML(sanitizeSVG(icon)) : html``;
     return html`
-      <article class="m-accordion">
-        <div class="m-accordion__container">
-          <div
-            class="${classMap(titleContainerClasses)}"
-            @click="${this.toggleAccordion}"
+      <div class="m-accordion">
+        <div
+          class="${classMap(titleContainerClasses)}"
+          aria-role="${this.level ? 'heading' : ''}"
+          aria-level="${this.level ? this.level : ''}"
+          @click="${this.toggleAccordion}"
+        >
+          <button
+            id="${this.refId}"
+            class="m-accordion__button"
+            aria-expanded="${this.open}"
+            aria-controls="${this.refId}-content"
+            ?disabled="${this.disabled}"
           >
-            <div class="m-accordion__title-container-title">
-              <div class="${iconClass}">${iconHTML}</div>
-              <span class="${classMap(titleClasses)}">${title}</span>
+            <div class="m-accordion__button-flex-wrapper">
+              <div class="m-accordion__button-title-wrapper">
+                ${icon
+                  ? html`<div class="m-accordion__icon">
+                      ${unsafeHTML(sanitizeSVG(icon))}
+                    </div>`
+                  : html``}
+                <span class="${classMap(titleClasses)}">${title}</span>
+              </div>
+              <div class="m-accordion__icon-expand">
+                ${unsafeHTML(open ? expandLess : expandMore)}
+              </div>
             </div>
-            <div class="m-accordion__title-container--closed">
-              ${statusIcon}
-            </div>
-          </div>
-          <div
-            class="m-accordion__content ${open
-              ? 'm-accordion__content--open'
-              : ''}"
-          >
-            <slot></slot>
-          </div>
+          </button>
         </div>
-      </article>
+        <div
+          id="${this.refId}-content"
+          role="region"
+          aria-labelledby="${this.refId}"
+          class="m-accordion__content ${open
+            ? 'm-accordion__content--open'
+            : ''}"
+        >
+          <slot></slot>
+        </div>
+      </div>
     `;
-  }
-
-  getFirstChild() {
-    return Array.prototype.slice
-      .call(this.parentNode.querySelectorAll('*'))
-      .find(el => {
-        return el.tagName.toLowerCase().includes('axa-accordion');
-      });
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    window.removeEventListener('resize', this.toggleAnimation);
   }
 }
 
