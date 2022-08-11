6e633144cec8ea9264ad353d4b129e48f8b86b9c
Marco Kruse <marco.kruse@devahead.de>
2022-08-11 14:43:55 +0200

    Refactor button and button link functionality

--- src/components/10-atoms/button/index.js
+++ src/components/10-atoms/button/index.js
@@ -1,4 +1,4 @@
-import { html, css, unsafeCSS } from 'lit';
+import { html, css, unsafeCSS, LitElement } from 'lit';
 import { classMap } from 'lit/directives/class-map.js';
 
 import AXAIcon from '@axa-ch/icon';
@@ -9,12 +9,12 @@ import {
   versionedHtml,
 } from '../../../utils/component-versioning';
 import buttonCSS from './index.scss';
-import childStyles from './child.scss';
-import InlineStyles from '../../../utils/inline-styles';
 
 const ARROW_RIGHT = 'arrow-right';
 
-class AXAButton extends InlineStyles {
+const hasShadowDom = el => !!el.shadowRoot && !!el.attachShadow;
+
+class AXAButton extends LitElement {
   static get tagName() {
     return 'axa-button';
   }
@@ -23,12 +23,6 @@ class AXAButton extends InlineStyles {
     return css`
       ${unsafeCSS(buttonCSS)}
     `;
-  }
-
-  // Parent class InlineStyles needs a static method to retrive styles
-  // name of such method is passed when calling: this.inlineStyles('resetHeadingCss');
-  static get blockMouseEventsCss() {
-    return childStyles;
   }
 
   static get properties() {
@@ -41,6 +35,8 @@ class AXAButton extends InlineStyles {
       size: { type: String },
       motionOff: { type: Boolean },
       disabled: { type: Boolean, reflect: true },
+      href: { type: String },
+      external: { type: Boolean },
       onClick: { type: Function, attribute: false },
     };
   }
@@ -52,10 +48,6 @@ class AXAButton extends InlineStyles {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  get isTypeSubmitOrReset() {
-    return this.type === 'submit' || this.type === 'reset';
-  }
-
   get showIcon() {
     return this.icon && this.icon !== ARROW_RIGHT;
   }
@@ -64,90 +56,25 @@ class AXAButton extends InlineStyles {
     return this.icon === ARROW_RIGHT;
   }
 
-  watch(mode) {
-    switch (mode) {
-      case 'stop':
-        if (this._observer) {
-          this._observer.disconnect();
-        }
-        break;
-      case 'start':
-        if (!this._observer) {
-          this._observer = new MutationObserver(() => this.attachFakeButton());
-        }
-        this._observer.observe(this, {
-          childList: true,
-        });
-        break;
-      default:
-        break;
-    }
-  }
+  handleClick(ev) {
+    if (this.type !== 'button' && hasShadowDom(this)) {
+      // this button wants to specifically submit a form
+      // climb up the dom to see if we're in a <form>
+      // and if so, then use JS to submit it
+      const form = this.closest('form');
+      if (form) {
+        ev.preventDefault();
 
-  attachFakeButton() {
-    const fakeButton = document.createElement('button');
-    fakeButton.type = this.type;
-    fakeButton.style.display = 'none';
-    this.watch('stop');
-    this.appendChild(fakeButton);
-    this.watch('start');
-    this.fakeButton = fakeButton;
-  }
-
-  firstUpdated() {
-    this.inlineStyles('blockMouseEventsCss');
-
-    const { style } = this;
-
-    // shadow dom submit btn workaround
-    // only use fakeButton when shadowDom is natively supported
-    if (this.isTypeSubmitOrReset) {
-      this.attachFakeButton();
+        const fakeButton = document.createElement('button');
+        fakeButton.type = this.type;
+        fakeButton.style.display = 'none';
+        form.appendChild(fakeButton);
+        fakeButton.click();
+        fakeButton.remove();
+      }
     }
 
-    style.appearance = 'none';
-    style.mozAppearance = 'none';
-    style.webkitAppearance = 'none';
-    style.msAppearance = 'none';
-    style.oAppearance = 'none';
-
-    if (typeof this.onclick === 'function') {
-      // cache original event so that we can fire it when internal button is pressed
-      // We are going to override original event so that someone can manually trigger
-      // onclick via function call
-      this.originalOnclick = this.onclick;
-    }
-
-    // If someone fires a click on the button and its type is submit then trigger fake button
-    // press
-    this.onclick = e => {
-      // call handle click and pass flag to be sure that handle click does not call
-      // us back.
-      this.handleClick(e, true);
-    };
-  }
-
-  handleClick(e, eventIsManuallyFunctionTriggered = false) {
-    // block propagation if event is not synthetic. We need only that
-    // the event coming from fake button is fired so that default
-    // form behaviour works (submit, reset, etc). The reason why it works with fake button is
-    // that fake button is NOT inside a ShadowDOM. The event instead
-    // bubbles out of ShadowDOM, hence the stop propagation trick
-
-    if (e.isTrusted && this.isTypeSubmitOrReset) {
-      e.stopPropagation();
-      this.fakeButton.click();
-    }
-
-    // If we are under react, onClick will be camel Case onClick. If so, use it
-    // otherwise if a consumer defined a onclick="fn", call that instead
-    const onclick = this.onClick || this.originalOnclick;
-
-    // if click event is fired manually via javascript, the this.onclick = e => { function
-    // will be called and therefore make sure to not trigger it again.
-    if (!eventIsManuallyFunctionTriggered && typeof onclick === 'function') {
-      onclick(e);
-    }
+    this.onClick(ev);
   }
 
   render() {
@@ -158,6 +85,8 @@ class AXAButton extends InlineStyles {
       variant = '',
       icon = '',
       size = '',
+      href,
+      external,
     } = this;
 
     const classes = {
@@ -176,6 +105,32 @@ class AXAButton extends InlineStyles {
       'a-button--inverted-blue-teal': variant === 'inverted-blue-teal',
     };
 
+    const renderContent = () => html`<span class="a-button__flex-wrapper">
+      ${this.showIcon
+        ? versionedHtml(
+            this
+          )`<axa-icon class="a-button__icon" icon="${icon}"></axa-icon> `
+        : ''}
+      <slot></slot>
+      ${this.showArrow
+        ? versionedHtml(
+            this
+          )`<axa-icon class="a-button__arrow" icon="arrow-right"></axa-icon>`
+        : html``}
+    </span>`;
+
+    if (this.href) {
+      return html`<a
+        class="${classMap(classes)}"
+        href="${href}"
+        target="${external ? '_blank' : '_top'}"
+        rel="${external ? 'noreferrer noopener' : ''}"
+        aria-disabled="${disabled}"
+        @click="${this.onClick}"
+        >${renderContent()}</a
+      >`;
+    }
+
     return html`
       <button
         type="${type}"
@@ -183,31 +138,12 @@ class AXAButton extends InlineStyles {
         ?disabled="${disabled}"
         @click="${this.handleClick}"
       >
-        <span class="a-button__flex-wrapper">
-          ${this.showIcon
-            ? versionedHtml(this)`
-                <axa-icon class="a-button__icon" icon="${icon}"></axa-icon>
-              `
-            : ''}
-          <slot></slot>
-          ${this.showArrow
-            ? versionedHtml(this)`
-                <axa-icon class="a-button__arrow js-button__arrow" icon="arrow-right"></axa-icon>
-              `
-            : ''}
-        </span>
+        ${renderContent()}
       </button>
     `;
   }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    // remove installed observer, if any
-    this.watch('stop');
-  }
 }
 
-/* eslint-disable no-undef */
 defineVersioned([AXAButton], __VERSION_INFO__);
 
 export default AXAButton;
