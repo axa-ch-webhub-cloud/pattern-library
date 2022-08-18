a5d51904afe65c5eba4bc670003e4de0bc593a85
Marco Kruse <marco.kruse@devahead.de>
2022-08-18 14:49:17 +0200

    Add comment rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const { lib, libReact } = require('./scripts/build/lib.rollup.js');
 
+// Sometimes there is no react version to build. I.e icons
 const rollupConfig = [lib];
 
 if (fs.existsSync('./index.react.js')) {
