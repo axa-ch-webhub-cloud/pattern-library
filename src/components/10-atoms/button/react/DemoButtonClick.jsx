5d15f960744d09587c3bd0945e7c5bf3e0a3ef75
Marco Kruse <marco.kruse@devahead.de>
2022-08-23 14:59:54 +0200

    Remove obsolete testcafe button test

--- src/components/10-atoms/button/react/DemoButtonClick.jsx
+++ src/components/10-atoms/button/react/DemoButtonClick.jsx
@@ -2,18 +2,16 @@ import React, { useState } from 'react';
 import AXAButton from './AXAButton';
 
 const DemoButtonClick = () => {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const handleAXAButtonClick = () => {
-    setCount(count + 1);
+  const handleClick = () => {
+    setCounter(counter + 1);
   };
 
   return (
     <div>
       <p>With the react wrapper - callback props are possible!</p>
-      <AXAButton onClick={handleAXAButtonClick}>
-        You clicked me: {count} times!
-      </AXAButton>
+      <AXAButton onClick={handleClick}>counter: {counter}</AXAButton>
     </div>
   );
 };
