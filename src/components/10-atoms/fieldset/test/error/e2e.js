c5ba4951f3ebbea92988de50e2e525196df1557d
Marco Kruse <marco.kruse@devahead.de>
2022-09-07 16:26:05 +0200

    Replace testcafe tests with pw tests

---
+++ src/components/10-atoms/fieldset/test/error/e2e.js
@@ -0,0 +1,14 @@
+import { expect, test } from '@playwright/test';
+import { fixtureURL } from '../../../../../utils/e2e-helpers';
+
+test.describe('fieldset: error', () => {
+  test('should render an error message', async ({ page }) => {
+    await page.goto(
+      fixtureURL('components-fieldset--fieldset', {
+        error: 'Invalid selection',
+      })
+    );
+
+    await expect(page.locator('axa-fieldset', ':after')).toBeVisible();
+  });
+});
