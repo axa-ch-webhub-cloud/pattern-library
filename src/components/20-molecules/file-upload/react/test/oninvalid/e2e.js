9ec08c9c50578522fced440070ef26ce82ca4f92
Marco Kruse <marco.kruse@devahead.de>
2022-11-13 13:58:03 +0100

    Add test for onInvalid

---
+++ src/components/20-molecules/file-upload/react/test/oninvalid/e2e.js
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+import { fixtureURL } from '../../../../../../utils/e2e-helpers';
+
+test.describe('file-upload: onInvalid', () => {
+  test.only(`should add file trigger onInvalid and show error message`, async ({
+    page,
+  }) => {
+    await page.goto(fixtureURL('examples-file-upload-react--file-upload'));
+
+    await page
+      .locator('text=Upload file')
+      .setInputFiles(
+        './src/components/20-molecules/file-upload/ui-test-files/testbig.pdf'
+      );
+
+    await expect(page.locator('text=onInvalid triggered')).toBeVisible();
+    await expect(
+      page.locator('text="File sizes exceed maximum size"')
+    ).toBeVisible();
+  });
+});
