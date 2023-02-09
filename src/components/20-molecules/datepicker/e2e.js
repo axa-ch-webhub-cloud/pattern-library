8030d62059dcd38709adc3e9d7a4194d6c6d7756
thomas-krueger-axa <thomas.krueger@axa.ch>
2023-02-09 16:05:02 +0100

    Migrate some flaky datepicker tests to playwright

--- src/components/20-molecules/datepicker/e2e.js
+++ src/components/20-molecules/datepicker/e2e.js
@@ -36,5 +36,55 @@ test.describe('datepicker', () => {
         '29.02.2020 (of 1 submittable elements)'
       );
     });
+
+    test('should just add years of a given range', async ({ page }) => {
+      await page.goto(fixtureURL('components-datepicker--datepicker'));
+
+      const datepicker = await page.locator('axa-datepicker');
+      await datepicker.evaluate(node => {
+        node.allowedyears = ['1999-2000'];
+      });
+
+      const datepickerYearDropdown = await page.locator(
+        'axa-datepicker .js-datepicker__dropdown-year'
+      );
+      const itemsString = await datepickerYearDropdown.getAttribute('items');
+
+      const itemsArray = JSON.parse(itemsString);
+
+      expect(itemsArray.length).toBe(2);
+      expect(itemsString).toContain('"value":"1999"');
+      expect(itemsString).toContain('"value":"2000"');
+      expect(itemsString).not.toContain('"value":"2020"');
+    });
+
+    test('should fire the right events', async ({ page }) => {
+      await page.goto(
+        fixtureURL('examples-datepicker-pure-html--with-onchange-handler')
+      );
+
+      const datepickerForm = await page.locator(
+        `axa-datepicker[data-test-id="datepicker-onchange"]`
+      );
+      expect(datepickerForm).toBeTruthy();
+
+      await page.locator('.js-datepicker__input-button').click();
+
+      await page
+        .locator('.m-datepicker__calendar-current-month[data-day="16"]')
+        .click();
+      expect(await page.locator('.event-log').inputValue()).toContain(
+        `{"name":"date","value":"16.2.2020"}\n\n`
+      );
+
+      await page
+        .locator(
+          'axa-datepicker[data-test-id="datepicker-onchange"] .js-datepicker__input'
+        )
+        .fill('29.2.1976');
+      expect(await page.locator('.event-log').inputValue()).toContain(
+        `\n\n{"name":"date","value":"29.2.1976"}\n\n`
+      );
+    });
   });
 });
